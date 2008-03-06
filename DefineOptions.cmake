2f73648239f8990a25775238e4aa9ffb78fb7da0
Andreas Schneider <mail@cynapses.org>
2008-03-06 17:41:27 +0100

    Add DEVELOPER options.
    With this option you can work with the files in the build directory.

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,2 +1,3 @@
 option(WITH_LOG4C "Build csync without log4c" ON)
 option(UNIT_TESTING "Build with unit tests" OFF)
+option(DEVELOPER "Build developer version" OFF)
--- config.h.cmake
+++ config.h.cmake
@@ -4,6 +4,7 @@
 #cmakedefine DATADIR "${SHARE_INSTALL_PREFIX}"
 #cmakedefine LIBDIR "${LIB_INSTALL_DIR}"
 #cmakedefine PLUGINDIR "${PLUGIN_INSTALL_DIR}"
-#cmakedefine SYSCONFDIR "${SYSCONF_INSTALL_DIR}"
+#cmakedefine SYSCONFDIR "${SYSCONFDIR}"
 
 #cmakedefine WITH_LOG4C 1
+#cmakedefine DEVELOPER 1
