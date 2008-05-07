7b6be113ea58b18113cbe0c8996d96480bd0d3c1
Andreas Schneider <mail@cynapses.org>
2008-05-07 10:56:36 +0200

    Add commandline options support for testcases.
    This adds a simple possibility to run the test not in the fork mode.

---
+++ tests/cmdline.c
@@ -0,0 +1,60 @@
+#include <argp.h>
+
+#include "support.h"
+
+const char *argp_program_version = "check test 0.1";
+const char *argp_program_bug_address = "<csync-devel@csync.org>";
+
+static char **cmdline;
+
+/* Program documentation. */
+static char doc[] = "check test";
+
+/* The options we understand. */
+static struct argp_option options[] = {
+  {
+    .name  = "no-fork",
+    .key   = 'n',
+    .arg   = NULL,
+    .flags = 0,
+    .doc   = "Don't fork the testcases",
+    .group = 0
+  },
+  {NULL, 0, NULL, 0, NULL, 0}
+};
+
+/* Parse a single option. */
+static error_t parse_opt (int key, char *arg, struct argp_state *state) {
+  /* Get the input argument from argp_parse, which we
+   * know is a pointer to our arguments structure.
+   */
+  struct argument_s *arguments = state->input;
+
+  switch (key) {
+    case 'n':
+      arguments->nofork = 1;
+      break;
+    case ARGP_KEY_ARG:
+      /* End processing here. */
+      cmdline = &state->argv [state->next - 1];
+      state->next = state->argc;
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+
+  return 0;
+}
+
+/* Our argp parser. */
+/* static struct argp argp = {options, parse_opt, args_doc, doc, NULL, NULL, NULL}; */
+static struct argp argp = {options, parse_opt, NULL, doc, NULL, NULL, NULL};
+
+void cmdline_parse(int argc, char **argv, struct argument_s *arguments) {
+  /*
+   * Parse our arguments; every option seen by parse_opt will
+   * be reflected in arguments.
+   */
+  argp_parse(&argp, argc, argv, 0, 0, arguments);
+}
+
--- tests/support.h
+++ tests/support.h
@@ -7,11 +7,20 @@
 
 #include "csync_private.h"
 
+/* Used by main to communicate with parse_opt. */
+struct argument_s {
+  char *args[2]; /* SOURCE and DESTINATION */
+  int nofork;
+};
+
+void cmdline_parse(int argc, char **argv, struct argument_s *arguments);
+
 /* create_case() with timeout of 30seconds (default) */
 void create_case(Suite *s, const char *name, TFun function);
 
 /* create_case() with timeout of 30seconds (default) and fixture */
-void create_case_fixture(Suite *s, const char *name, TFun function, void (*setup)(void), void (*teardown)(void));
+void create_case_fixture(Suite *s, const char *name, TFun function,
+    void (*setup)(void), void (*teardown)(void));
 
 /*
  * create_case_timeout() allow to specific a specific timeout - intended for
