8f523483154ced0aaa153b1351e1178e7a0cc312
Andreas Schneider <mail@cynapses.org>
2008-06-20 10:10:38 +0200

    Improve the authentication calls.
    Enter will use the old username so you don't have to type it.

--- client/csync_client.c
+++ client/csync_client.c
@@ -154,8 +154,20 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
 }
 
 static void csync_auth_fn(char *usr, size_t usrlen, char *pwd, size_t pwdlen) {
+  char tmp[256] = {0};
+
   /* get username */
-  csync_text_prompt("Username: ", usr, usrlen);
+  snprintf(tmp, 255, "Username: [%s] ", usr);
+  csync_text_prompt(tmp, tmp, 255);
+
+  if (tmp[strlen(tmp) - 1] == '\n') {
+    tmp[strlen(tmp) - 1] = '\0';
+  }
+
+  if (tmp[0] != '\0') {
+    strncpy(usr, tmp, usrlen - 1);
+  }
+
   /* get password */
   csync_password_prompt("Password: ", pwd, pwdlen, 0);
 }
