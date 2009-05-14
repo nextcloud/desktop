338370514ebc2d5e0295211b879c263bb8f2ce17
Andreas Schneider <mail@cynapses.org>
2009-05-14 11:22:38 +0200

    Don't force the build type or we can't overwrite it.

--- cmake/Modules/DefineCMakeDefaults.cmake
+++ cmake/Modules/DefineCMakeDefaults.cmake
@@ -23,6 +23,5 @@ if (NOT CMAKE_BUILD_TYPE)
   set(CMAKE_BUILD_TYPE RelWithDebInfo
     CACHE STRING
       "Choose the type of build, options are: None Debug Release RelWithDebInfo MinSizeRel."
-    FORCE
   )
 endif (NOT CMAKE_BUILD_TYPE)
