0af772f5240cb68c535cc03731cec306e9354c44
Duncan Mac-Vicar P <duncan@kde.org>
2011-02-17 15:10:03 +0100

    fix defines

--- src/mirall/constants.h
+++ src/mirall/constants.h
@@ -3,7 +3,7 @@
 #define MIRALL_CONSTANTS_H
 
 #define FOLDER_ICON "/usr/share/icons/oxygen/48x48/places/folder-favorites.png"
-#define FOLDER_SYNC_ICON = "/usr/share/icons/oxygen/48x48/actions/folder-sync.png"
-#define FOLDER_SYNC_ERROR = "/usr/share/icons/oxygen/48x48/places/folder-important.png"
+#define FOLDER_SYNC_ICON "/usr/share/icons/oxygen/48x48/actions/folder-sync.png"
+#define FOLDER_SYNC_ERROR "/usr/share/icons/oxygen/48x48/places/folder-important.png"
 
 #endif
