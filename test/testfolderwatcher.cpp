9ac0397ff9bdb682372c34ddb2ec10869c6aece8
Duncan Mac-Vicar P <duncan@kde.org>
2011-03-31 20:26:32 +0200

    unneeded

--- test/testfolderwatcher.cpp
+++ test/testfolderwatcher.cpp
@@ -10,8 +10,6 @@
 #include "mirall/temporarydir.h"
 #include "testfolderwatcher.h"
 
-static char dir_template[] = "/tmp/miralXXXXXX";
-
 void TestFolderWatcher::initTestCase()
 {
     Mirall::INotify::initialize();
