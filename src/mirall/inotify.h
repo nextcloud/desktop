9a6a87d9afd22031bc4924b2a6ed17a384e23c3c
Duncan Mac-Vicar P <duncan@kde.org>
2011-04-06 11:45:05 +0200

    add proper licensing

--- src/mirall/inotify.h
+++ src/mirall/inotify.h
@@ -1,11 +1,18 @@
-/**
-Based on example by:
-Copyright (c) Ashish Shukla
-
-This is licensed under GNU GPL v2 or later.
-For license text, Refer to the the file COPYING or visit
-http://www.gnu.org/licenses/gpl.txt .
-*/
+/*
+ * Copyright (C) by Duncan Mac-Vicar P. <duncan@kde.org>
+ *
+ * Originally based on example copyright (c) Ashish Shukla
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
 
 #ifndef MIRALL_INOTIFY_H
 #define MIRALL_INOTIFY_H
