41c9e908528421f8841e52d57082a519770670d2
Juan Carlos Cornejo <jc2@paintblack.com>
2011-11-04 15:01:42 -0400

    Implemented a trick to allow only one instance of the application to run. This should hopefully prevent corruption of the sync directory.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <QtGui/QApplication>
 #include "SyncWindow.h"
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
+    // Technique for having only one application running at the same time per
+    // computer.
+    QSharedMemory sharedMemory;
+    sharedMemory.setKey("8SyGEyz2kcI0UNrqxc7wcuKLjD1pSL8GezlBM1W0QckgpjMon0");
+    if(sharedMemory.attach()) {
+            return -1;
+    }
+
+    if (!sharedMemory.create(1)) {
+            return -1; // Exit already a process running
+    }
+
+    // Great. No other instance is running, so we can run the program.
     QApplication a(argc, argv);
     SyncWindow w;
     w.show();
