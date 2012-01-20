128223553c66f841136d87af71c00d4c5035d7ba
Klaas Freitag <freitag@opensuse.org>
2012-01-20 10:21:49 +0100

    added a windows compatible implementation

--- src/mirall/temporarydir.cpp
+++ src/mirall/temporarydir.cpp
@@ -18,6 +18,7 @@
 
 #include <QDebug>
 #include <QDir>
+#include <QDateTime>
 
 #include "mirall/temporarydir.h"
 #include "mirall/fileutils.h"
@@ -25,14 +26,35 @@
 namespace Mirall
 {
 
-static QString dirTemplate = QDir::tempPath() + "/mirall-XXXXXX";
+static const QString dirTemplate = QDir::tempPath() + "/mirall-XXXXXX";
+
+/*
+ * This class creates a temporary directory as Qt 4.x does not provide an
+ * implementation yet. It will come with Qt5, don't forget to PORT
+ *
+ * This class is only used in the test suite of mirall.
+ * As the mkdtemp function is problematic on non linux platforms, I change
+ * the filename generation to something more predictable using the timestamp.
+ *
+ * Note: This is not allowed for cases where the dir name needs to be
+ * unpredictable for security reasons.
+ */
 
 TemporaryDir::TemporaryDir()
 {
+#ifdef Q_WS_WIN
+  QDateTime dt = QDateTime::currentDateTime();
+  QString p( QDir::tempPath() + "/mirall-" + dt.toString( Qt::ISODate ) );
+  QDir dir( p );
+  if( dir.mkpath( p ) ) {
+    _path = p;
+  }
+#else
     char *buff = ::strdup(dirTemplate.toLocal8Bit().data());
     char *tmp = ::mkdtemp(buff);
     _path = QString((const char *) tmp);
     ::free(buff);
+#endif
 }
 
 TemporaryDir::~TemporaryDir()
