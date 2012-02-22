cd2c75e6024f4164062f3a7d29c5c9eb665427ed
Andreas Schneider <asn@cryptomilk.org>
2012-02-22 18:25:41 +0100

    Increase version numbers for beta release.

--- src/csync.h
+++ src/csync.h
@@ -45,8 +45,8 @@ extern "C" {
 
 /* csync version */
 #define LIBCSYNC_VERSION_MAJOR  0
-#define LIBCSYNC_VERSION_MINOR  45
-#define LIBCSYNC_VERSION_MICRO  0
+#define LIBCSYNC_VERSION_MINOR  49
+#define LIBCSYNC_VERSION_MICRO  9
 
 #define LIBCSYNC_VERSION_INT CSYNC_VERSION_INT(LIBCSYNC_VERSION_MAJOR, \
                                            LIBCSYNC_VERSION_MINOR, \
