1b12e6590f8563af5572cb093fd91bd24528e615
Klaas Freitag <freitag@owncloud.com>
2012-03-14 15:26:22 +0100

    Built a portable version of FindCSync that finds dev setup stuff.

--- cmake/modules/FindCsync.cmake
+++ cmake/modules/FindCsync.cmake
@@ -1,8 +1,18 @@
+IF(EXISTS "${CMAKE_SOURCE_DIR}/../csync/src/csync.h")
+  SET(CSYNC_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/../csync/src/)
+ELSE(EXISTS "${CMAKE_SOURCE_DIR}/../csync/src/csync.h")
+  FIND_PATH(CSYNC_INCLUDE_PATH NAMES csync.h HINTS /usr/include/csync)
+ENDIF(EXISTS "${CMAKE_SOURCE_DIR}/../csync/src/csync.h")
 
-FIND_LIBRARY(CSYNC_LIBRARY NAMES csync 
-	HINTS "${CMAKE_SOURCE_DIR}/../buildcsync/src" )
-FIND_PATH(CSYNC_INCLUDE_PATH csync.h
-	HINTS "${CMAKE_SOURCE_DIR}/../csync/src" "/usr/include/csync")
+IF(EXISTS "${CMAKE_SOURCE_DIR}/../buildcsync/src/")
+  IF( WIN32 )
+     SET(CSYNC_LIBRARY ${CMAKE_SOURCE_DIR}/../win/buildcsync/src/libcsync.dll)
+  ELSE( WIN32 )
+     SET(CSYNC_LIBRARY ${CMAKE_SOURCE_DIR}/../buildcsync/src/libcsync.so)
+  ENDIF( WIN32 )
+ELSE(EXISTS "${CMAKE_SOURCE_DIR}/../buildcsync/src/")
+  FIND_LIBRARY(CSYNC_LIBRARY NAMES csync )
+ENDIF(EXISTS "${CMAKE_SOURCE_DIR}/../buildcsync/src/")
 
 SET(CSYNC_INCLUDE_DIR ${CSYNC_INCLUDE_PATH})
 
@@ -12,6 +22,4 @@ include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Csync DEFAULT_MSG
   CSYNC_LIBRARY CSYNC_INCLUDE_PATH)
 
-MARK_AS_ADVANCED(
-  CSYNC_INCLUDE_PATH
-  CSYNC_LIBRARY)
+MARK_AS_ADVANCED( CSYNC_INCLUDE_PATH CSYNC_LIBRARY )
