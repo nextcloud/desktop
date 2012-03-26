3979492914c5e0234360043d39421e08a3920744
Klaas Freitag <freitag@owncloud.com>
2012-03-26 09:53:46 +0200

    Version to 0.50.3 for a next beta

--- src/csync.h
+++ src/csync.h
@@ -49,8 +49,8 @@ extern "C" {
 
 /* csync version */
 #define LIBCSYNC_VERSION_MAJOR  0
-#define LIBCSYNC_VERSION_MINOR  49
-#define LIBCSYNC_VERSION_MICRO  9
+#define LIBCSYNC_VERSION_MINOR  50 
+#define LIBCSYNC_VERSION_MICRO  3
 
 #define LIBCSYNC_VERSION_INT CSYNC_VERSION_INT(LIBCSYNC_VERSION_MAJOR, \
                                            LIBCSYNC_VERSION_MINOR, \
