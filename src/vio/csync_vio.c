02b617b53621073547e194321313950066e35e9a
Dominik Schmidt <dev@dominik-schmidt.de>
2012-04-15 14:13:07 +0200

    Load plugins on MacOSX.

--- src/vio/csync_vio.c
+++ src/vio/csync_vio.c
@@ -41,6 +41,10 @@
 #define MODULE_EXTENSION "so"
 #endif
 
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#endif
+
 #include "csync_log.h"
 
 int csync_vio_init(CSYNC *ctx, const char *module, const char *args) {
@@ -65,6 +69,24 @@ int csync_vio_init(CSYNC *ctx, const char *module, const char *args) {
   if (lstat(path, &sb) < 0) {
     SAFE_FREE(path);
     if (asprintf(&path, "modules/csync_%s.%s", module, MODULE_EXTENSION) < 0) {
+      return -1;
+    }
+  }
+#endif
+
+#ifdef __APPLE__
+  if (lstat(path, &sb) < 0) {
+    SAFE_FREE(path);
+
+    char path_tmp[1024];
+    uint32_t size = sizeof(path_tmp);
+    if (_NSGetExecutablePath(path_tmp, &size) == 0)
+        printf("executable path is %s\n", path_tmp);
+
+    char* path2 = NULL;
+    path2 = c_dirname(path_tmp);
+
+    if (asprintf(&path, "%s/../Plugins/csync_%s.%s", path2, module, MODULE_EXTENSION) < 0) {
       return -1;
     }
   }
