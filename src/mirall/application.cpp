cb99ccf75d7192b2c2e68564b32aa72c98825244
Klaas Freitag <freitag@owncloud.com>
2012-06-18 15:50:17 +0200

    Load translations correctly.

--- src/mirall/application.cpp
+++ src/mirall/application.cpp
@@ -98,9 +98,14 @@ Application::Application(int &argc, char **argv) :
 #ifdef Q_OS_LINUX
     // FIXME - proper path!
     mirallTranslator->load("mirall_" + QLocale::system().name(), QLatin1String("/usr/share/mirall/i18n/"));
-#else
+#endif
+#ifdef Q_OS_MAC
+    mirallTranslator->load("mirall_" + QLocale::system().name(), applicationDirPath()+QLatin1String("/translations") ); // path defaults to app dir.
+#endif
+#ifdef Q_QS_WIN32
     mirallTranslator->load("mirall_" + QLocale::system().name()); // path defaults to app dir.
 #endif
+
     installTranslator(mirallTranslator);
 
     // create folder manager for sync folder management
