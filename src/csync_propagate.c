97a7e46595a22ac94ea29dfb633e41ef55e19e95
Klaas Freitag <freitag@owncloud.com>
2012-08-23 17:18:35 +0300

    Set to INSTRUCTION_UPDATED only for NONE instructions.

--- src/csync_propagate.c
+++ src/csync_propagate.c
@@ -1118,7 +1118,7 @@ static int _csync_correct_id(CSYNC *ctx) {
               } else {
                   tfs = c_rbtree_node_data(node);
                   tfs->md5 = _get_md5(ctx, path);
-                  if( tfs->instruction != CSYNC_INSTRUCTION_UPDATED ) {
+                  if( tfs->instruction == CSYNC_INSTRUCTION_NONE ) {
                       /* set instruction for the statedb merger */
                       tfs->instruction = CSYNC_INSTRUCTION_UPDATED;
                   }
