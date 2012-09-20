7a70a6fa69afe3c8bf0be49a8ccdb9176325c1eb
Klaas Freitag <freitag@owncloud.com>
2012-09-20 17:57:58 +0300

    Update version to 0.50.10

--- src/csync.h
+++ src/csync.h
@@ -51,7 +51,7 @@ extern "C" {
 /* csync version */
 #define LIBCSYNC_VERSION_MAJOR  0
 #define LIBCSYNC_VERSION_MINOR  50 
-#define LIBCSYNC_VERSION_MICRO  9
+#define LIBCSYNC_VERSION_MICRO  10 
 
 #define LIBCSYNC_VERSION_INT CSYNC_VERSION_INT(LIBCSYNC_VERSION_MAJOR, \
                                            LIBCSYNC_VERSION_MINOR, \
