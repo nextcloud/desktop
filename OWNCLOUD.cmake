a05a4c3741826a2f8dcfeaa9784531a1f81184b8
Daniel Molkentin <danimo@owncloud.com>
2012-09-21 13:09:33 +0200

    More templatization.

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -2,5 +2,6 @@ set( APPLICATION_SHORTNAME  "owncloud-client" )
 set( APPLICATION_NAME       "ownCloud Client" )
 set( APPLICATION_EXECUTABLE "owncloud"        )
 set( APPLICATION_DOMAIN     "owncloud.org"    )
+set( APPLICATION_VENDOR     "ownCloud.org"    )
 set( THEME_CLASS            "ownCloudTheme"   )
 
