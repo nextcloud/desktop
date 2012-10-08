6459f574cc139f1fc47a1d4d7327f13e6d313437
Daniel Molkentin <danimo@owncloud.com>
2012-10-09 01:38:07 +0200

    Prepare for Mac package signing.

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -4,4 +4,5 @@ set( APPLICATION_EXECUTABLE "owncloud"        )
 set( APPLICATION_DOMAIN     "owncloud.org"    )
 set( APPLICATION_VENDOR     "ownCloud.org"    )
 set( THEME_CLASS            "ownCloudTheme"   )
+SET( APPLICATION_REV_DOMAIN "org.owncloud.desktopclient" )
 
