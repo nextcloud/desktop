48eec357d7d07de7f17af4a9a9110163e01aae96
Klaas Freitag <freitag@owncloud.com>
2012-10-10 12:09:36 +0200

    Push version to 0.60.0 - final release for owncloud client 1.1.0

--- src/csync.h
+++ src/csync.h
@@ -50,8 +50,8 @@ extern "C" {
 
 /* csync version */
 #define LIBCSYNC_VERSION_MAJOR  0
-#define LIBCSYNC_VERSION_MINOR  50 
-#define LIBCSYNC_VERSION_MICRO  11
+#define LIBCSYNC_VERSION_MINOR  60 
+#define LIBCSYNC_VERSION_MICRO  0 
 
 #define LIBCSYNC_VERSION_INT CSYNC_VERSION_INT(LIBCSYNC_VERSION_MAJOR, \
                                            LIBCSYNC_VERSION_MINOR, \
