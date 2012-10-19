eded4915e9c44bdfa5abaa0e64d0ea15e50a8fb5
Andreas Schneider <asn@cryptomilk.org>
2012-10-19 10:57:40 +0200

    tests: Migrate check_csync_init to cmocka.

--- tests/csync_tests/check_csync_init.c
+++ tests/csync_tests/check_csync_init.c
@@ -1,88 +1,97 @@
 #include <string.h>
 
-#include "support.h"
+#include "torture.h"
 
 #include "csync_private.h"
 
-CSYNC *csync;
+static void setup(void **state) {
+    CSYNC *csync;
+    int rc;
 
-static void setup(void) {
-  fail_if(system("mkdir -p /tmp/check_csync1") < 0, "Setup failed");
-  fail_if(system("mkdir -p /tmp/check_csync2") < 0, "Setup failed");
-  fail_if(csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2") < 0, "Setup failed");
-  fail_if(csync_set_config_dir(csync, "/tmp/check_csync") < 0, "Setup failed");
+    rc = system("mkdir -p /tmp/check_csync1");
+    assert_int_equal(rc, 0);
+
+    rc = system("mkdir -p /tmp/check_csync2");
+    assert_int_equal(rc, 0);
+
+    rc = csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2");
+    assert_int_equal(rc, 0);
+
+    rc = csync_set_config_dir(csync, "/tmp/check_csync");
+    assert_int_equal(rc, 0);
+
+    *state = csync;
 }
 
-static void setup_module(void) {
-  fail_if(system("mkdir -p /tmp/check_csync1") < 0, "Setup failed");
-  fail_if(system("mkdir -p /tmp/check_csync2") < 0, "Setup failed");
-  fail_if(csync_create(&csync, "/tmp/check_csync1", "dummy://foo/bar") < 0, "Setup failed");
-  fail_if(csync_set_config_dir(csync, "/tmp/check_csync") < 0, "Setup failed");
+static void setup_module(void **state) {
+    CSYNC *csync;
+    int rc;
+
+    rc = system("mkdir -p /tmp/check_csync1");
+    assert_int_equal(rc, 0);
+
+    rc = system("mkdir -p /tmp/check_csync2");
+    assert_int_equal(rc, 0);
+
+    rc = csync_create(&csync, "/tmp/check_csync1", "dummy://foo/bar");
+    assert_int_equal(rc, 0);
+
+    rc = csync_set_config_dir(csync, "/tmp/check_csync");
+    assert_int_equal(rc, 0);
+
+    *state = csync;
 }
 
-static void teardown(void) {
-  fail_if(csync_destroy(csync) < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync") < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync1") < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync2") < 0, "Teardown failed");
+static void teardown(void **state) {
+    CSYNC *csync = *state;
+    int rc;
+
+    rc = csync_destroy(csync);
+
+    rc = system("rm -rf /tmp/check_csync");
+    assert_int_equal(rc, 0);
+
+    rc = system("rm -rf /tmp/check_csync1");
+    assert_int_equal(rc, 0);
+
+    rc = system("rm -rf /tmp/check_csync2");
+    assert_int_equal(rc, 0);
+
+    *state = NULL;
 }
 
-START_TEST (check_csync_init_null)
+static void check_csync_init_null(void **state)
 {
-  fail_unless(csync_init(NULL) < 0, NULL);
-}
-END_TEST
+    int rc;
 
-START_TEST (check_csync_init)
+    (void) state; /* unused */
+
+    rc = csync_init(NULL);
+    assert_int_equal(rc, -1);
+}
+
+static void check_csync_init(void **state)
 {
-  fail_unless(csync_init(csync) == 0, NULL);
+    CSYNC *csync = *state;
+    int rc;
 
-  fail_unless((csync->status & CSYNC_STATUS_INIT) == 1, NULL);
+    rc = csync_init(csync);
+    assert_int_equal(rc, 0);
 
-  fail_unless(csync_init(csync) == 1, NULL);
+    assert_int_equal(csync->status & CSYNC_STATUS_INIT, 1);
+
+    rc = csync_init(csync);
+    assert_int_equal(rc, 1);
 }
-END_TEST
 
-START_TEST (check_csync_init_module)
+int torture_run_tests(void)
 {
-  fail_unless(csync_init(csync) == 0, NULL);
+    const UnitTest tests[] = {
+        unit_test_setup_teardown(check_csync_init_null, setup, teardown),
+        unit_test_setup_teardown(check_csync_init, setup, teardown),
+        unit_test_setup_teardown(check_csync_init, setup_module, teardown),
+    };
 
-  fail_unless((csync->status & CSYNC_STATUS_INIT) == 1, NULL);
-
-  fail_unless(csync_init(csync) == 1, NULL);
-}
-END_TEST
-
-static Suite *make_csync_suite(void) {
-  Suite *s = suite_create("csync");
-
-  create_case_fixture(s, "check_csync_init_null", check_csync_init_null, setup, teardown);
-  create_case_fixture(s, "check_csync_init", check_csync_init, setup, teardown);
-  create_case_fixture(s, "check_csync_init_module", check_csync_init_module, setup_module, teardown);
-
-  return s;
-}
-
-int main(int argc, char **argv) {
-  Suite *s = NULL;
-  SRunner *sr = NULL;
-  struct argument_s arguments;
-  int nf;
-
-  ZERO_STRUCT(arguments);
-
-  cmdline_parse(argc, argv, &arguments);
-
-  s = make_csync_suite();
-
-  sr = srunner_create(s);
-  if (arguments.nofork) {
-    srunner_set_fork_status(sr, CK_NOFORK);
-  }
-  srunner_run_all(sr, CK_VERBOSE);
-  nf = srunner_ntests_failed(sr);
-  srunner_free(sr);
-
-  return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    return run_tests(tests);
 }
 
