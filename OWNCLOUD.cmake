4fbfe7bfef7baaaea56c78720b120bfe4a2907bc
Daniel Molkentin <danimo@owncloud.com>
2012-11-02 16:14:33 +0100

    Installer: Only show license on special request
    GPL is a distribution license, and should thus
    not be required to be accepted upon installation.

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -4,6 +4,4 @@ set( APPLICATION_EXECUTABLE "owncloud"        )
 set( APPLICATION_DOMAIN     "owncloud.org"    )
 set( APPLICATION_VENDOR     "ownCloud.org"    )
 set( THEME_CLASS            "ownCloudTheme"   )
-SET( APPLICATION_REV_DOMAIN "org.owncloud.desktopclient" )
-SET( APPLICATION_LICENSE    "COPYING" )
-
+set( APPLICATION_REV_DOMAIN "org.owncloud.desktopclient" )
