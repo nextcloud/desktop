0a9cca8441b704af9a424b5da1618cf9c0a9c6ed
Andreas Schneider <asn@cryptomilk.org>
2012-12-04 13:50:14 +0100

    modules: Fix csync_owncloud module license header.

--- modules/csync_owncloud.c
+++ modules/csync_owncloud.c
@@ -4,19 +4,19 @@
  * Copyright (c) 2011      by Andreas Schneider <asn@cryptomilk.org>
  * Copyright (c) 2012      by Klaas Freitag <freitag@owncloud.com>
  *
- * This program is free software = NULL, you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation = NULL, either version 2
+ * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY = NULL, without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program = NULL, if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include <errno.h>
