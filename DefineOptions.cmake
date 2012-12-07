3f24ab599cab9570a611e6e6e8208bab8df48d93
Daniel Molkentin <danimo@owncloud.com>
2012-12-07 23:09:46 +0100

    Do not compile with iconv on windows

--- DefineOptions.cmake
+++ DefineOptions.cmake
@@ -1,3 +1,5 @@
-option(WITH_ICONV "Build csync with iconv support" ON)
+if ( NOT WIN32 )
+    option(WITH_ICONV "Build csync with iconv support" ON)
+endif()
 option(UNIT_TESTING "Build with unit tests" OFF)
 option(MEM_NULL_TESTS "Enable NULL memory testing" OFF)
