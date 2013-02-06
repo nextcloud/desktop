5baf362af0d7fc38febd0e4b6303b979ed877c7b
Andreas Schneider <asn@cryptomilk.org>
2013-02-06 18:18:04 +0100

    Revert "WIP"
    This reverts commit acd70803293fa20dfe7cce12eba2cd6a2dd1199e.
    
    Oops :)

--- src/csync_log.c
+++ src/csync_log.c
@@ -35,7 +35,6 @@
 
 CSYNC_THREAD int csync_log_level;
 CSYNC_THREAD csync_log_callback csync_log_cb;
-CSYNC_THREAD void *csync_log_userdata;
 
 static int current_timestring(int hires, char *buf, size_t len)
 {
@@ -102,12 +101,17 @@ static void csync_log_function(CSYNC *ctx,
 }
 
 
-void csync_log(int verbosity,
+void csync_log(CSYNC *ctx,
+               int verbosity,
                const char *function,
                const char *format, ...)
 {
     char buffer[1024];
     va_list va;
+
+    if (ctx == NULL) {
+        return;
+    }
 
     if (verbosity <= csync_get_log_level()) {
         va_start(va, format);
@@ -145,12 +149,3 @@ csync_log_callback csync_get_log_callback(void) {
   return csync_log_cb;
 }
 
-int csync_set_log_userdata(void *data)
-{
-    csync_log_userdata = data;
-}
-
-void *csync_get_log_userdata(void)
-{
-    return csync_log_userdata;
-}
