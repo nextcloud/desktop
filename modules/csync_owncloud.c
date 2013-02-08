1be65be68e47f9a04f2915d461e1dc35806d9c99
Olivier Goffart <ogoffart@woboq.com>
2013-02-08 17:27:06 +0100

    Fix crash in owncloud_mkdir
    ne_simple_request destroy the request, we cannot access the error code
    after it.

--- modules/csync_owncloud.c
+++ modules/csync_owncloud.c
@@ -1850,7 +1850,6 @@ static csync_vio_file_stat_t *owncloud_readdir(csync_vio_method_handle_t *dhandl
 static int owncloud_mkdir(const char *uri, mode_t mode) {
     int rc = NE_OK;
     int len = 0;
-    ne_request *req = NULL;
 
     char *path = _cleanPath( uri );
     (void) mode; /* unused */
@@ -1874,16 +1873,12 @@ static int owncloud_mkdir(const char *uri, mode_t mode) {
         }
         DEBUG_WEBDAV("MKdir on %s", path );
 
-        req = ne_request_create(dav_session.ctx, "MKCOL", path);
-        rc = ne_simple_request(dav_session.ctx, req);
-
+        rc = ne_mkcol(dav_session.ctx, path );
+        set_errno_from_neon_errcode(rc);
         /* Special for mkcol: it returns 405 if the directory already exists.
          * To keep csync vio_mkdirs working errno EEXIST has to be returned. */
-        if( ne_get_status(req)->code == 405 ) {
+        if (errno == EPERM && http_result_code_from_session() == 405)
             errno = EEXIST;
-        } else {
-            set_errno_from_neon_errcode(rc);
-        }
     }
     SAFE_FREE( path );
 
