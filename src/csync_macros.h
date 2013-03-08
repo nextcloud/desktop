f8f82a9af2c3b7c3b9d9b4a7e19130ff8b0a5f73
Klaas Freitag <freitag@owncloud.com>
2013-03-08 18:44:22 +0100

    Added custom errnos for use with csync in addition to the system errnos.
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- src/csync_macros.h
+++ src/csync_macros.h
@@ -28,5 +28,24 @@
 /* How many elements there are in a static array */
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof(a[0]))
 
+/* Some special custom errno values to report bugs properly. The BASE value
+ * should always be larger than the highest system errno. */
+#define CSYNC_CUSTOM_ERRNO_BASE 10000
+
+#define ERRNO_GENERAL_ERROR          CSYNC_CUSTOM_ERRNO_BASE+2
+#define ERRNO_LOOKUP_ERROR           CSYNC_CUSTOM_ERRNO_BASE+3
+#define ERRNO_USER_UNKNOWN_ON_SERVER CSYNC_CUSTOM_ERRNO_BASE+4
+#define ERRNO_PROXY_AUTH             CSYNC_CUSTOM_ERRNO_BASE+5
+#define ERRNO_CONNECT                CSYNC_CUSTOM_ERRNO_BASE+6
+#define ERRNO_TIMEOUT                CSYNC_CUSTOM_ERRNO_BASE+7
+#define ERRNO_PRECONDITION           CSYNC_CUSTOM_ERRNO_BASE+8
+#define ERRNO_RETRY                  CSYNC_CUSTOM_ERRNO_BASE+9
+#define ERRNO_REDIRECT               CSYNC_CUSTOM_ERRNO_BASE+10
+#define ERRNO_WRONG_CONTENT          CSYNC_CUSTOM_ERRNO_BASE+11
+#define ERRNO_TIMEDELTA              CSYNC_CUSTOM_ERRNO_BASE+12
+#define ERRNO_ERROR_STRING           CSYNC_CUSTOM_ERRNO_BASE+13
+#define ERRNO_SERVICE_UNAVAILABLE    CSYNC_CUSTOM_ERRNO_BASE+14
+#define ERRNO_QUOTA_EXCEEDED         CSYNC_CUSTOM_ERRNO_BASE+15
+
 #endif /* _CSYNC_MACROS_H */
 /* vim: set ft=c.doxygen ts=8 sw=2 et cindent: */
