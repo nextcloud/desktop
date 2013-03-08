2cc291c520d8db097aa815543bc0d2cc37cf93c7
Klaas Freitag <freitag@owncloud.com>
2013-03-08 15:47:18 +0100

    tests: Add treewalk test.
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/csync_tests/check_csync_treewalk.c
+++ tests/csync_tests/check_csync_treewalk.c
@@ -1,7 +1,6 @@
-#define _GNU_SOURCE /* asprintf */
 #include <string.h>
 
-#include "support.h"
+#include "torture.h"
 
 #define CSYNC_TEST 1
 #include "csync_config.c"
@@ -9,49 +8,49 @@
 CSYNC *csync;
 int    file_count;
 
-static void setup_local(void) {
-  fail_if(system("mkdir -p /tmp/check_csync1") < 0, "Setup failed");
-  fail_if(system("mkdir -p /tmp/check_csync2") < 0, "Setup failed");
-  fail_if(system("echo \"This is test data\" > /tmp/check_csync1/testfile1.txt") < 0, NULL);
-  fail_if(system("echo \"This is also test data\" > /tmp/check_csync1/testfile2.txt") < 0, NULL);
+static void setup_local(void **state) {
+  assert_int_equal(system("mkdir -p /tmp/check_csync1"), 0);
+  assert_int_equal(system("mkdir -p /tmp/check_csync2"), 0);
+  assert_int_equal(system("mkdir -p /tmp/check_csync"), 0);
+  assert_int_equal(system("echo \"This is test data\" > /tmp/check_csync1/testfile1.txt"), 0);
+  assert_int_equal(system("echo \"This is also test data\" > /tmp/check_csync1/testfile2.txt"), 0);
 
-  fail_if(csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2") < 0, "Setup failed");
-  fail_if(csync_init(csync) < 0, "Init failed");
+  assert_int_equal(csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2"), 0);
+  assert_int_equal(csync_set_config_dir(csync, "/tmp/check_csync/"), 0);
+  assert_int_equal(csync_init(csync), 0);
   
   file_count = 0;
   printf("********** setting up local!\n");
 }
 
-static void teardown_local(void) {
-  fail_if(csync_destroy(csync) < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync1") < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync2") < 0, "Teardown failed");
-  
+static void teardown_local(void **state) {
+  assert_int_equal(csync_destroy(csync),0 );
+  assert_int_equal(system("rm -rf /tmp/check_csync1"), 0);
+  assert_int_equal(system("rm -rf /tmp/check_csync2"), 0);
+  assert_int_equal(system("rm -rf /tmp/check_csync"), 0);
   printf("********** tearing down local\n");
 }
 
-static void setup_remote(void) {
-  fail_if(system("mkdir -p /tmp/check_csync1") < 0, "Setup failed");
-  fail_if(system("mkdir -p /tmp/check_csync2") < 0, "Setup failed");
-  fail_if(system("echo \"This is test data\" > /tmp/check_csync1/testfile1.txt") < 0, NULL);
-  fail_if(system("echo \"This is also test data\" > /tmp/check_csync1/testfile2.txt") < 0, NULL);
+static void setup_remote(void **state) {
+  assert_int_equal(system("mkdir -p /tmp/check_csync1"), 0);
+  assert_int_equal(system("mkdir -p /tmp/check_csync2"), 0);
+  assert_int_equal(system("echo \"This is test data\" > /tmp/check_csync1/testfile1.txt"), 0);
+  assert_int_equal(system("echo \"This is also test data\" > /tmp/check_csync1/testfile2.txt"), 0);
 
-  fail_if(csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2") < 0, "Setup failed");
-  fail_if(csync_init(csync) < 0, "Init failed");
-  fail_if(csync_update(csync) < 0, "Update failed");
-  fail_if(csync_reconcile(csync) < 0, "Reconcile failed");
-  fail_if(csync_propagate(csync) < 0, "Propagate failed");
+  assert_int_equal(csync_create(&csync, "/tmp/check_csync1", "/tmp/check_csync2"), 0);
+  assert_int_equal(csync_set_config_dir(csync, "/tmp/check_csync/"), 0);
+  assert_int_equal(csync_init(csync), 0);
+  assert_int_equal(csync_update(csync), 0);
+  assert_int_equal(csync_reconcile(csync), 0);
+  assert_int_equal(csync_propagate(csync), 0);
   
   file_count = 0;
-  printf("********** setting up remote!\n");
 }
 
-static void teardown_remote(void) {
-  fail_if(csync_destroy(csync) < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync1") < 0, "Teardown failed");
-  fail_if(system("rm -rf /tmp/check_csync2") < 0, "Teardown failed");
-  
-  printf("********** tearing down remote\n");
+static void teardown_remote(void **state) {
+  assert_int_equal(csync_destroy(csync), 0);
+  assert_int_equal(system("rm -rf /tmp/check_csync1"), 0);
+  assert_int_equal(system("rm -rf /tmp/check_csync2"), 0);
 }
 
 
@@ -59,86 +58,62 @@ static void teardown_remote(void) {
 static int visitor(TREE_WALK_FILE* file, void *userdata)
 {
     if( userdata ) printf("Userdata is set!\n");
-    fail_if(file == NULL, "Invalid TREE_WALK_FILE structure");
+    assert_non_null(file);
     printf("Found path: %s\n", file->path);
     file_count++;
     return 0;
 }
 
-START_TEST (check_csync_treewalk_local)
+static void check_csync_treewalk_local(void **state)
 {
-    fail_if(csync_walk_local_tree(csync, &visitor, 0) < 0, "Local walk needs update first");
-    fail_if(file_count != 0, "Local File count not correct (without update)");
-    fail_if(csync_update(csync) < 0, "Update failed");
-    fail_if(csync_walk_local_tree(csync, &visitor, 0) < 0, "Local walk");
-    fail_if(file_count != 2, "Local File count not correct");
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+    assert_int_equal(file_count, 0);
+    assert_int_equal(csync_update(csync), 0);
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+    assert_int_equal(file_count, 2 );
 }
-END_TEST
 
-START_TEST (check_csync_treewalk_remote)
+static void check_csync_treewalk_remote(void **state)
 {
-    fail_if(csync->remote.tree == NULL, "Csync remote tree zero" );
+    assert_non_null(csync->remote.tree);
     
-    fail_if(csync_walk_remote_tree(csync, &visitor, 0) < 0, "Local walk needs update first");
-    fail_if(file_count != 0, "Remote File count not correct: %d (without update)", file_count);
+    assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
+    assert_int_equal(file_count, 0);
     /* reconcile doesn't update the tree */
-    fail_if(csync_update(csync) < 0, "Update failed");
+    assert_int_equal(csync_update(csync), 0);
     
-    fail_if(csync_walk_remote_tree(csync, &visitor, 0) < 0, "Remote walk finds nothing");
+    assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
     
-    fail_if(file_count != 2, "Remote File count not correct: %d", file_count);
+    assert_int_equal(file_count, 2);
 }
-END_TEST
 
-START_TEST (check_csync_treewalk_local_with_filter)
+static void check_csync_treewalk_local_with_filter(void **state)
 {
-    fail_if(csync_walk_local_tree(csync, &visitor, 0) < 0, "Local walk needs update first");
-    fail_if(file_count != 0, "Local File count not correct (without update)");
-    fail_if(csync_update(csync) < 0, "Update failed");
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+    assert_int_equal(file_count, 0);
+    assert_int_equal(csync_update(csync), 0);
     
-    fail_if(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_EVAL) < 0, "Local walk");
-    fail_if(file_count != 2, "Local File count filtered (EVAL) not correct: %d", file_count);
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW), 0);
+    assert_int_equal(file_count, 2 );
+
     file_count = 0;
-    fail_if(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_EVAL | CSYNC_INSTRUCTION_REMOVE) < 0, "Local walk");
-    fail_if(file_count != 2, "Local File count filtered (EVAL) not correct: %d", file_count);
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW | CSYNC_INSTRUCTION_REMOVE), 0);
+    assert_int_equal(file_count, 2 );
+
     file_count = 0;
-    fail_if(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_RENAME) < 0, "Local walk");
-    fail_if(file_count != 0, "Local File count filtered (RENAME) not correct");
+    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_RENAME), 0);
+    assert_int_equal(file_count, 0);
 
 }
-END_TEST
 
+int torture_run_tests(void)
+{
+  const UnitTest tests[] = {
+    unit_test_setup_teardown(check_csync_treewalk_local, setup_local, teardown_local ),
+    unit_test_setup_teardown(check_csync_treewalk_remote, setup_remote, teardown_remote),
+    unit_test_setup_teardown(check_csync_treewalk_local_with_filter, setup_local, teardown_local)
+  };
 
-static Suite *make_csync_suite(void) {
-  Suite *s = suite_create("csync_treewalk");
-
-  create_case_fixture(s, "check_csync_treewalk_local", check_csync_treewalk_local, setup_local, teardown_local);
-  create_case_fixture(s, "check_csync_treewalk_remote", check_csync_treewalk_remote, setup_remote, teardown_remote);
-  create_case_fixture(s, "check_csync_treewalk_local_with_filter", check_csync_treewalk_local_with_filter, setup_local, teardown_local);
-
-  return s;
-}
-
-int main(int argc, char **argv) {
-  Suite *s = NULL;
-  SRunner *sr = NULL;
-  struct argument_s arguments;
-  int nf;
-
-  ZERO_STRUCT(arguments);
-
-  cmdline_parse(argc, argv, &arguments);
-
-  s = make_csync_suite();
-
-  sr = srunner_create(s);
-  if (arguments.nofork) {
-    srunner_set_fork_status(sr, CK_NOFORK);
-  }
-  srunner_run_all(sr, CK_VERBOSE);
-  nf = srunner_ntests_failed(sr);
-  srunner_free(sr);
-
-  return (nf == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+  return run_tests(tests);
 }
 
