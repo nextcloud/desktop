f4ee205fe99bfdd5d23b7673bfe9df9b1458ace3
Klaas Freitag <freitag@owncloud.com>
2013-03-12 17:21:22 +0100

    Minor code cleanups, warning avoidings and indentation.
    Reviewed-by: Andreas Schneider <asn@cryptomilk.org>

--- tests/csync_tests/check_csync_treewalk.c
+++ tests/csync_tests/check_csync_treewalk.c
@@ -9,6 +9,7 @@ CSYNC *csync;
 int    file_count;
 
 static void setup_local(void **state) {
+  (void) state; /* unused */
   assert_int_equal(system("mkdir -p /tmp/check_csync1"), 0);
   assert_int_equal(system("mkdir -p /tmp/check_csync2"), 0);
   assert_int_equal(system("mkdir -p /tmp/check_csync"), 0);
@@ -24,6 +25,7 @@ static void setup_local(void **state) {
 }
 
 static void teardown_local(void **state) {
+  (void) state; /* unused */
   assert_int_equal(csync_destroy(csync),0 );
   assert_int_equal(system("rm -rf /tmp/check_csync1"), 0);
   assert_int_equal(system("rm -rf /tmp/check_csync2"), 0);
@@ -32,6 +34,7 @@ static void teardown_local(void **state) {
 }
 
 static void setup_remote(void **state) {
+  (void) state; /* unused */
   assert_int_equal(system("mkdir -p /tmp/check_csync1"), 0);
   assert_int_equal(system("mkdir -p /tmp/check_csync2"), 0);
   assert_int_equal(system("echo \"This is test data\" > /tmp/check_csync1/testfile1.txt"), 0);
@@ -48,6 +51,7 @@ static void setup_remote(void **state) {
 }
 
 static void teardown_remote(void **state) {
+  (void) state; /* unused */
   assert_int_equal(csync_destroy(csync), 0);
   assert_int_equal(system("rm -rf /tmp/check_csync1"), 0);
   assert_int_equal(system("rm -rf /tmp/check_csync2"), 0);
@@ -57,52 +61,55 @@ static void teardown_remote(void **state) {
 
 static int visitor(TREE_WALK_FILE* file, void *userdata)
 {
-    if( userdata ) printf("Userdata is set!\n");
-    assert_non_null(file);
-    printf("Found path: %s\n", file->path);
-    file_count++;
-    return 0;
+  if( userdata ) printf("Userdata is set!\n");
+  assert_non_null(file);
+  printf("Found path: %s\n", file->path);
+  file_count++;
+  return 0;
 }
 
 static void check_csync_treewalk_local(void **state)
 {
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
-    assert_int_equal(file_count, 0);
-    assert_int_equal(csync_update(csync), 0);
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
-    assert_int_equal(file_count, 2 );
+  (void) state; /* unused */
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+  assert_int_equal(file_count, 0);
+  assert_int_equal(csync_update(csync), 0);
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+  assert_int_equal(file_count, 2 );
 }
 
 static void check_csync_treewalk_remote(void **state)
 {
-    assert_non_null(csync->remote.tree);
-    
-    assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
-    assert_int_equal(file_count, 0);
-    /* reconcile doesn't update the tree */
-    assert_int_equal(csync_update(csync), 0);
-    
-    assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
-    
-    assert_int_equal(file_count, 2);
+  (void) state; /* unused */
+  assert_non_null(csync->remote.tree);
+
+  assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
+  assert_int_equal(file_count, 0);
+  /* reconcile doesn't update the tree */
+  assert_int_equal(csync_update(csync), 0);
+
+  assert_int_equal(csync_walk_remote_tree(csync, &visitor, 0), 0);
+
+  assert_int_equal(file_count, 2);
 }
 
 static void check_csync_treewalk_local_with_filter(void **state)
 {
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
-    assert_int_equal(file_count, 0);
-    assert_int_equal(csync_update(csync), 0);
-    
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW), 0);
-    assert_int_equal(file_count, 2 );
+  (void) state; /* unused */
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, 0), 0);
+  assert_int_equal(file_count, 0);
+  assert_int_equal(csync_update(csync), 0);
 
-    file_count = 0;
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW | CSYNC_INSTRUCTION_REMOVE), 0);
-    assert_int_equal(file_count, 2 );
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW), 0);
+  assert_int_equal(file_count, 2 );
 
-    file_count = 0;
-    assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_RENAME), 0);
-    assert_int_equal(file_count, 0);
+  file_count = 0;
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_NEW | CSYNC_INSTRUCTION_REMOVE), 0);
+  assert_int_equal(file_count, 2 );
+
+  file_count = 0;
+  assert_int_equal(csync_walk_local_tree(csync, &visitor, CSYNC_INSTRUCTION_RENAME), 0);
+  assert_int_equal(file_count, 0);
 
 }
 
