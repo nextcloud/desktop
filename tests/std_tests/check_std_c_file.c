a3f29967f7df06e4003d02e38f58824e38299b71
Klaas Freitag <freitag@owncloud.com>
2013-03-22 13:54:46 +0100

    Test added for c_compare_file.

--- tests/std_tests/check_std_c_file.c
+++ tests/std_tests/check_std_c_file.c
@@ -100,10 +100,31 @@ static void check_c_compare_file(void **state)
   assert_int_equal(rc, 0);
   rc = system("echo \"hallo52\" > /tmp/check/bar.txt");
   assert_int_equal(rc, 0);
-
   rc = c_compare_file( check_src_file, check_dst_file );
   assert_int_equal(rc, 0);
 
+  /* Create two 1MB random files */
+  rc = system("dd if=/dev/urandom of=/tmp/check/foo.txt bs=1024 count=1024");
+  assert_int_equal(rc, 0);
+  rc = system("dd if=/dev/urandom of=/tmp/check/bar.txt bs=1024 count=1024");
+  assert_int_equal(rc, 0);
+  rc = c_compare_file( check_src_file, check_dst_file );
+  assert_int_equal(rc, 0);
+
+  /* Create two 1MB random files with different size */
+  rc = system("dd if=/dev/urandom of=/tmp/check/foo.txt bs=1024 count=1024");
+  assert_int_equal(rc, 0);
+  rc = system("dd if=/dev/urandom of=/tmp/check/bar.txt bs=1024 count=1020");
+  assert_int_equal(rc, 0);
+  rc = c_compare_file( check_src_file, check_dst_file );
+  assert_int_equal(rc, 0);
+
+  /* compare two big files which are equal */
+  rc = c_copy(check_src_file, check_dst_file, 0644);
+  assert_int_equal(rc, 0);
+
+  rc = c_compare_file( check_src_file, check_dst_file );
+  assert_int_equal(rc, 1);
 }
 
 int torture_run_tests(void)
