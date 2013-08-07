6a6bfd1c89256abfaf9d6d5948154d98be470528
Daniel Molkentin <danimo@owncloud.com>
2013-08-07 16:58:09 +0200

    Use proper compiler switch

--- cmake/Modules/DefineCompilerFlags.cmake
+++ cmake/Modules/DefineCompilerFlags.cmake
@@ -16,7 +16,7 @@ if (${CMAKE_C_COMPILER_ID} MATCHES "(GNU|Clang)")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -Wshadow -Wmissing-prototypes -Wdeclaration-after-statement")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wunused -Wfloat-equal -Wpointer-arith -Wwrite-strings -Wformat-security")
     set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wmissing-format-attribute")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wmissing-format-attribute -D__USE_GNU")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wmissing-format-attribute -D_GNU_SOURCE")
 
     set(CSYNC_STRICT ON CACHE BOOL "Strict error checking, enabled -Werror and friends")
     if (CSYNC_STRICT)
