82fb8c49cfacf48ec2e123b69a2702c03324dadd
Olivier Goffart <ogoffart@woboq.com>
2013-08-08 16:05:05 +0200

    Do not clear the pending event in the polling anymore
    This is useless,  and can even cause problems now since polling only do
    a sync when the server has actually changed

--- src/mirall/folder.cpp
+++ src/mirall/folder.cpp
@@ -236,8 +236,7 @@ void Folder::evaluateSync(const QStringList &/*pathList*/)
 
 void Folder::slotPollTimerTimeout()
 {
-    qDebug() << "* Polling" << alias() << "for changes. Ignoring all pending events until now (time since next sync:" << (_timeSinceLastSync.elapsed() / 1000) << "s)";
-    _watcher->clearPendingEvents();
+    qDebug() << "* Polling" << alias() << "for changes. (time since next sync:" << (_timeSinceLastSync.elapsed() / 1000) << "s)";
 
     if (quint64(_timeSinceLastSync.elapsed()) > MirallConfigFile().forceSyncInterval()) {
         qDebug() << "* Force Sync now";
