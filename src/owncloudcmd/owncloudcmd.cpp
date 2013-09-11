3c5c432e1d2a050a249455ff6acab2bf71fb3004
Klaas Freitag <freitag@owncloud.com>
2013-09-11 10:22:47 +0200

    Minor code cleanups-

--- src/owncloudcmd/owncloudcmd.cpp
+++ src/owncloudcmd/owncloudcmd.cpp
@@ -93,7 +93,6 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    //csync_set_log_callback(   _csync_ctx, csyncLogCatcher );
     csync_set_log_level(11);
     csync_enable_conflictcopys(_csync_ctx);
 
