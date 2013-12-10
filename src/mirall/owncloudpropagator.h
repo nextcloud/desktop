6707e93f318c331dfe2e286bff551f6b683c79e3
Klaas Freitag <freitag@owncloud.com>
2013-12-10 15:27:48 +0100

    Add NE_LFS define for correct size of ne_off_t

--- src/mirall/owncloudpropagator.h
+++ src/mirall/owncloudpropagator.h
@@ -15,6 +15,8 @@
 #ifndef OWNCLOUDPROPAGATOR_H
 #define OWNCLOUDPROPAGATOR_H
 
+#define NE_LFS // never remove this. otherwise NEON will behave badly.
+
 #include <neon/ne_request.h>
 #include <QHash>
 #include <QObject>
