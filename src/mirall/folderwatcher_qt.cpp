a12288ff216f6f2382292285ea3798a2eba11b2d
Klaas Freitag <freitag@owncloud.com>
2014-01-11 20:32:06 +0100

    Use folderwatcher based on Qt for Linux.

---
+++ src/mirall/folderwatcher_qt.cpp
@@ -0,0 +1,119 @@
+/*
+ * Copyright (C) by Daniel Molkentin <danimo@owncloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "mirall/folderwatcher.h"
+#include "mirall/folderwatcher_qt.h"
+
+#include <QDir>
+#include <QFileInfo>
+#include <QDebug>
+
+namespace Mirall {
+
+FolderWatcherPrivate::FolderWatcherPrivate()
+    :QObject(), _parent(0)
+{
+
+}
+
+FolderWatcherPrivate::FolderWatcherPrivate(FolderWatcher *p)
+    : QObject(), _parent(p)
+
+{
+    _watcher.reset(new QFileSystemWatcher);
+
+    QObject::connect(_watcher.data(), SIGNAL(directoryChanged(QString)),
+                     this, SLOT(slotDirectoryChanged(QString)));
+}
+
+// attention: result list passed by reference!
+bool FolderWatcherPrivate::findFoldersBelow( const QDir& dir, QStringList& fullList )
+{
+    bool ok = true;
+    if( !(dir.exists() && dir.isReadable()) ) {
+        qDebug() << "Non existing path coming in: " << dir.absolutePath();
+        ok = false;
+    } else {
+        QStringList nameFilter;
+        nameFilter << QLatin1String("*");
+        QDir::Filters filter = QDir::Dirs | QDir::NoDotAndDotDot|QDir::NoSymLinks;
+        const QStringList pathes = dir.entryList(nameFilter, filter);
+
+        QStringList::const_iterator constIterator;
+        for (constIterator = pathes.constBegin(); constIterator != pathes.constEnd();
+               ++constIterator) {
+            const QString fullPath(dir.path()+QLatin1String("/")+(*constIterator));
+            fullList.append(fullPath);
+            ok = findFoldersBelow(QDir(fullPath), fullList);
+        }
+    }
+
+    return ok;
+}
+
+void FolderWatcherPrivate::slotAddFolderRecursive(const QString &path)
+{
+    int subdirs = 0;
+    qDebug() << "(+) Watcher:" << path;
+
+    _watcher->addPath(path);
+    const QStringList watchedFolders(_watcher->directories());
+
+    QStringList allSubfolders;
+    if( !findFoldersBelow(QDir(path), allSubfolders)) {
+        qDebug() << "Could not traverse all sub folders";
+    }
+    // qDebug() << "currently watching " << watchedFolders;
+    QStringListIterator subfoldersIt(allSubfolders);
+    while (subfoldersIt.hasNext()) {
+        QString subfolder = subfoldersIt.next();
+        // qDebug() << "  (**) subfolder: " << subfolder;
+        QDir folder (subfolder);
+        if (folder.exists() && !watchedFolders.contains(folder.path())) {
+            subdirs++;
+            // check that it does not match the ignore list
+            foreach ( const QString& pattern, _parent->ignores()) {
+                QRegExp regexp(pattern);
+                regexp.setPatternSyntax(QRegExp::Wildcard);
+                if ( regexp.exactMatch(folder.path()) ) {
+                    qDebug() << "* Not adding" << folder.path();
+                    continue;
+                }
+
+            }
+            _watcher->addPath(folder.path());
+        }
+        else
+            qDebug() << "    `-> discarded:" << folder.path();
+    }
+    if (subdirs >0)
+        qDebug() << "    `-> and" << subdirs << "subdirectories";
+}
+
+void FolderWatcherPrivate::slotDirectoryChanged( const QString& path )
+{
+    if( path != _lastPath ) {
+        qDebug() << "OOOooo change detected: " << path;
+        _parent->changeDetected(path);
+        _lastPath = path;
+    }
+}
+
+void FolderWatcherPrivate::removePath(const QString &path )
+{
+    _watcher->removePath(path);
+}
+
+
+} // namespace Mirall
