9c300b83bde7a2b6371f5e8e8bf7756cd94f7f42
Klaas Freitag <freitag@owncloud.com>
2014-01-23 16:15:29 +0100

    Add FindINotify cmake module.

---
+++ cmake/modules/FindINotify.cmake
@@ -0,0 +1,19 @@
+# This module defines
+#  INOTIFY_INCLUDE_DIR, where to find inotify.h, etc.
+#  INOTIFY_FOUND, If false, do not try to use inotify.
+# also defined, but not for general use are
+#  INOTIFY_LIBRARY, where to find the inotify library.
+
+find_path(INOTIFY_INCLUDE_DIR sys/inotify.h 
+          HINTS /usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
+mark_as_advanced(INOTIFY_INCLUDE_DIR)
+
+# all listed variables are TRUE
+# handle the QUIETLY and REQUIRED arguments and set INOTIFY_FOUND to TRUE if
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(INOTIFY DEFAULT_MSG INOTIFY_INCLUDE_DIR)
+
+IF(INOTIFY_FOUND)
+  SET(INotify_INCLUDE_DIRS ${INOTIFY_INCLUDE_DIR})
+ENDIF(INOTIFY_FOUND)
+
