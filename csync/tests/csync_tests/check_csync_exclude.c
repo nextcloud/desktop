2eb9962f4c06d94ee157d060ae3ecabda0ff2bde
Olivier Goffart <ogoffart@woboq.com>
2014-01-26 18:57:22 +0100

    Adapt the test to the changes in the exclude list
    The exclude list was removed from csync in commit
    f32d90165302b2ec0eb38111a7f9836bc815f7e6
    Adapt the test.

--- csync/tests/csync_tests/check_csync_exclude.c
+++ csync/tests/csync_tests/check_csync_exclude.c
@@ -31,7 +31,7 @@ static void setup_init(void **state) {
     csync->options.config_dir = c_strdup("/tmp/check_csync1/");
     assert_non_null(csync->options.config_dir);
 
-    rc = csync_exclude_load(csync, BINARYDIR "/config/" CSYNC_EXCLUDE_FILE);
+    rc = csync_exclude_load(csync, SOURCEDIR "/../sync-exclude.lst");
     assert_int_equal(rc, 0);
 
     *state = csync;
@@ -64,10 +64,10 @@ static void check_csync_exclude_load(void **state)
     CSYNC *csync = *state;
     int rc;
 
-    rc = csync_exclude_load(csync, BINARYDIR "/config/" CSYNC_EXCLUDE_FILE);
+    rc = csync_exclude_load(csync,  SOURCEDIR "/../sync-exclude.lst");
     assert_int_equal(rc, 0);
 
-    assert_string_equal(csync->excludes->vector[0], ".beagle");
+    assert_string_equal(csync->excludes->vector[0], "*.filepart");
 }
 
 static void check_csync_excluded(void **state)
@@ -79,37 +79,37 @@ static void check_csync_excluded(void **state)
     assert_int_equal(rc, CSYNC_NOT_EXCLUDED);
     rc = csync_excluded(csync, ".kde/share/config/kwin.eventsrc", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_NOT_EXCLUDED);
-    rc = csync_excluded(csync, ".kde4/cache-maximegalon/cache1.txt", CSYNC_FTW_TYPE_FILE);
+    rc = csync_excluded(csync, ".htaccess/cache-maximegalon/cache1.txt", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
-    rc = csync_excluded(csync, ".mozilla/plugins", CSYNC_FTW_TYPE_DIR);
+    rc = csync_excluded(csync, "mozilla/.htaccess", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
 
     /*
      * Test for patterns in subdirs. '.beagle' is defined as a pattern and has
      * to be found in top dir as well as in directories underneath.
      */
-    rc = csync_excluded(csync, ".beagle", CSYNC_FTW_TYPE_DIR);
+    rc = csync_excluded(csync, ".apdisk", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
-    rc = csync_excluded(csync, "foo/.beagle", CSYNC_FTW_TYPE_DIR);
+    rc = csync_excluded(csync, "foo/.apdisk", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
-    rc = csync_excluded(csync, "foo/bar/.beagle", CSYNC_FTW_TYPE_DIR);
+    rc = csync_excluded(csync, "foo/bar/.apdisk", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
 
     /*
      * Pattern: .java/
      * A file wont be excluded but a directory .java will be.
      */
-    rc = csync_excluded(csync, ".java", CSYNC_FTW_TYPE_DIR);
+ /*   rc = csync_excluded(csync, ".java", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
     rc = csync_excluded(csync, ".java", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_NOT_EXCLUDED);
-
+*/
     /* Files in the ignored dir .java will also be ignored. */
-    rc = csync_excluded(csync, ".java/totally_amazing.jar", CSYNC_FTW_TYPE_FILE);
+    rc = csync_excluded(csync, ".apdisk/totally_amazing.jar", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
 
     /* and also in subdirs */
-    rc = csync_excluded(csync, "projects/.java/totally_amazing.jar", CSYNC_FTW_TYPE_FILE);
+    rc = csync_excluded(csync, "projects/.apdisk/totally_amazing.jar", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
 
     /* csync-journal is ignored in general silently. */
@@ -121,21 +121,21 @@ static void check_csync_excluded(void **state)
     assert_int_equal(rc, CSYNC_FILE_SILENTLY_EXCLUDED);
 
     /* pattern ]*.directory - ignore and remove */
-    rc = csync_excluded(csync, "my.directory", CSYNC_FTW_TYPE_FILE);
+    rc = csync_excluded(csync, "my.~directory", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_AND_REMOVE);
 
-    rc = csync_excluded(csync, "/a_folder/my.directory", CSYNC_FTW_TYPE_FILE);
+    rc = csync_excluded(csync, "/a_folder/my.~directory", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_AND_REMOVE);
 
     /* Not excluded because the pattern .netscape/cache requires directory. */
     rc = csync_excluded(csync, ".netscape/cache", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_NOT_EXCLUDED);
-
+/*
     rc = csync_excluded(csync, ".netscape/cache", CSYNC_FTW_TYPE_DIR);
-    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
+    assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);*/
 
     /* Excluded because the parent dir .netscape/cache is ingored. */
-    rc = csync_excluded(csync, ".netscape/cache/foo", CSYNC_FTW_TYPE_FILE);
+/*    rc = csync_excluded(csync, ".netscape/cache/foo", CSYNC_FTW_TYPE_FILE);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
 
     rc = csync_excluded(csync, ".netscape/cache/bar.txt", CSYNC_FTW_TYPE_FILE);
@@ -143,7 +143,7 @@ static void check_csync_excluded(void **state)
 
     rc = csync_excluded(csync, ".netscape/cache/longrun", CSYNC_FTW_TYPE_DIR);
     assert_int_equal(rc, CSYNC_FILE_EXCLUDE_LIST);
-
+*/
 
 }
 
