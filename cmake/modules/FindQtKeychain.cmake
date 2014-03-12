28cf9ce1a4df167d22b483466ebba866e7f5ad92
Daniel Molkentin <danimo@owncloud.com>
2014-03-12 17:55:30 +0100

    Properly abort if QtKeychain is not found
    Fixes #1486

--- cmake/modules/FindQtKeychain.cmake
+++ cmake/modules/FindQtKeychain.cmake
@@ -1,8 +1,8 @@
-# - Try to find QtKeyChain
+# - Try to find QtKeychain
 # Once done this will define
-#  QTKEYCHAIN_FOUND - System has QtKeyChain
-#  QTKEYCHAIN_INCLUDE_DIRS - The QtKeyChain include directories
-#  QTKEYCHAIN_LIBRARIES - The libraries needed to use QtKeyChain
+#  QTKEYCHAIN_FOUND - System has QtKeychain
+#  QTKEYCHAIN_INCLUDE_DIRS - The QtKeychain include directories
+#  QTKEYCHAIN_LIBRARIES - The libraries needed to use QtKeychain
 #  QTKEYCHAIN_DEFINITIONS - Compiler switches required for using LibXml2
 
 find_path(QTKEYCHAIN_INCLUDE_DIR qtkeychain/keychain.h)
@@ -12,7 +12,7 @@ find_library(QTKEYCHAIN_LIBRARY NAMES libqtkeychain qtkeychain)
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set QTKEYCHAIN_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(QtKeyChain  DEFAULT_MSG
+find_package_handle_standard_args(QtKeychain  DEFAULT_MSG
 	QTKEYCHAIN_LIBRARY QTKEYCHAIN_INCLUDE_DIR)
 
 mark_as_advanced(QTKEYCHAIN_INCLUDE_DIR QTKEYCHAIN_LIBRARY )
