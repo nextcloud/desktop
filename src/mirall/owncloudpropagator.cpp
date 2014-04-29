f159b028b486b96f2d1a33258848ef2dc97e9790
Olivier Goffart <ogoffart@woboq.com>
2014-04-29 11:39:46 +0200

    Add a compile option to not blacklist error code 5xx

--- src/mirall/owncloudpropagator.cpp
+++ src/mirall/owncloudpropagator.cpp
@@ -45,6 +45,11 @@ void PropagateItemJob::done(SyncFileItem::Status status, const QString &errorStr
     if( _item._httpErrorCode == 403 ||_item._httpErrorCode == 413 || _item._httpErrorCode == 415 ) {
         qDebug() << "Fatal Error condition" << _item._httpErrorCode << ", forbid retry!";
         retries = -1;
+#ifdef OWNCLOUD_5XX_NO_BLACKLIST
+    } else if (_item._httpErrorCode / 100 == 5) {
+        // In this configuration, never blacklist error 5xx
+        qDebug() << "Do not blacklist error " << _item._httpErrorCode;
+#endif
     } else {
         static QAtomicInt defaultRetriesCount(qgetenv("OWNCLOUD_BLACKLIST_COUNT").toInt());
         if (defaultRetriesCount.fetchAndAddAcquire(0) <= 0) {
