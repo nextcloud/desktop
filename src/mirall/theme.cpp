nextcloud/desktop

commit 6ff38d80053e469ee0a5d11498555d5a2fd913c0

Cleanup member initialization in Theme
This fixes coverity issue 12925

src/mirall/theme.cpp
@@ -36,6 +36,7 @@ Theme* Theme::_instance = 0;
 Theme* Theme::instance() {
     if (!_instance) {
         _instance = new THEME_CLASS;
+        // some themes may not call the base ctor
         _instance->_mono = false;
     }
     return _instance;
@@ -150,6 +151,13 @@ QIcon Theme::themeIcon( const QString& name, bool sysTray ) const
         }
     }
     return icon;
+}
+
+Theme::Theme() :
+    QObject(0)
+    ,_mono(false)
+{
+
 }
 #endif
 

src/mirall/owncloudtheme.cpp
@@ -31,7 +31,8 @@
 
 namespace Mirall {
 
-ownCloudTheme::ownCloudTheme()
+ownCloudTheme::ownCloudTheme() :
+    Theme()
 {
     // qDebug() << " ** running ownCloud theme!";
 }

src/mirall/theme.h
@@ -181,7 +181,7 @@ public:
 
 protected:
     QIcon themeIcon(const QString& name, bool sysTray = false) const;
-    Theme() {}
+    Theme();
 
 signals:
     void systrayUseMonoIconsChanged(bool);

commit 3477ea0eeb928f299d5f5ae6b4b89433b3c55e7b

Fix build with TOKEN_ONLY_AUTH
It was broken by commit 6ff38d80053e469ee0a5d11498555d5a2fd913c0

src/mirall/theme.cpp
@@ -153,13 +153,14 @@ QIcon Theme::themeIcon( const QString& name, bool sysTray ) const
     return icon;
 }
 
+#endif
+
 Theme::Theme() :
     QObject(0)
     ,_mono(false)
 {
 
 }
-#endif
 
 // if this option return true, the client only supports one folder to sync.
 // The Add-Button is removed accoringly.