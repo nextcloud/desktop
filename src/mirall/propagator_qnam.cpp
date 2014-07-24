208011f6ab891d5b774b5d203fdc9c6a25c5cdb8
Olivier Goffart <ogoffart@woboq.com>
2014-07-24 11:17:59 +0200

    propagator_qnam: add debug output in cae the file changed localy
    In order to debug https://github.com/owncloud/core/issues/9781

--- src/mirall/propagator_qnam.cpp
+++ src/mirall/propagator_qnam.cpp
@@ -303,7 +303,7 @@ void PropagateUploadFileQNAM::slotPutFinished()
         }
 
         if (Utility::qDateTimeToTime_t(fi.lastModified()) != _item._modtime) {
-            /* Uh oh:  The local file has changed during upload */
+            qDebug() << "The local file has changed during upload:" << _item._modtime << "!=" << Utility::qDateTimeToTime_t(fi.lastModified())  << fi.lastModified();
             _propagator->_activeJobs--;
             done(SyncFileItem::SoftError, tr("Local file changed during sync."));
             // FIXME:  the legacy code was retrying for a few seconds.
