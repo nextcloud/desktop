ddfc7c75df0dc86ef70be59634333aebddae6c5c
hefee <hefee@netzguerilla.net>
2014-08-31 00:32:37 +0200

    Fix minor Python styling issues
    * Do not use sapces inside parenthesis
    * make indend PEP8
    * empty lines should not contain whitespaces

--- shell_integration/nautilus/ownCloud.py
+++ shell_integration/nautilus/ownCloud.py
@@ -7,7 +7,7 @@ import socket
 from gi.repository import GObject, Nautilus
 
 class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoProvider):
-    
+
     nautilusVFSFile_table = {}
     registered_paths = {}
     remainder = ''
@@ -17,36 +17,35 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
     def __init__(self):
         self.connectToOwnCloud
         if not self.connected:
-          # try again in 5 seconds - attention, logic inverted!
-          GObject.timeout_add(5000, self.connectToOwnCloud)
+            # try again in 5 seconds - attention, logic inverted!
+            GObject.timeout_add(5000, self.connectToOwnCloud)
 
     def port(self):
         return 34001 # Fixme, read from config file.
 
     def connectToOwnCloud(self):
         try:
-          self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-          self.sock.connect(("localhost", self.port()))
-          self.sock.settimeout(5)
-          self.connected = True
-          self.watch_id = GObject.io_add_watch(self.sock, GObject.IO_IN, self.handle_notify)
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect(("localhost", self.port()))
+            self.sock.settimeout(5)
+            self.connected = True
+            self.watch_id = GObject.io_add_watch(self.sock, GObject.IO_IN, self.handle_notify)
         except:
-          print( "Connect could not be established, try again later!" )
-          self.sock.close()
+            print("Connect could not be established, try again later!")
+            self.sock.close()
         return not self.connected
-        
+
     def sendCommand(self, cmd):
         if self.connected:
-          try:
-            self.sock.send(cmd)
-          except:
-            print( "Sending failed." )
-            GObject.source_remove( self.watch_id )
-            self.connected = False
-            GObject.timeout_add(5000, self.connectToOwnCloud)
+            try:
+                self.sock.send(cmd)
+            except:
+                print("Sending failed.")
+                GObject.source_remove(self.watch_id)
+                self.connected = False
+                GObject.timeout_add(5000, self.connectToOwnCloud)
 
-    def find_item_for_file( self, path ):
+    def find_item_for_file(self, path):
         if path in self.nautilusVFSFile_table:
             return self.nautilusVFSFile_table[path]
         else:
@@ -55,14 +54,14 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
     def askForOverlay(self, file):
         if os.path.isdir(file):
             folderStatus = self.sendCommand("RETRIEVE_FOLDER_STATUS:"+file+"\n");
-            
+
         if os.path.isfile(file):
             fileStatus = self.sendCommand("RETRIEVE_FILE_STATUS:"+file+"\n");
 
-    def invalidate_items_underneath( self, path ):
+    def invalidate_items_underneath(self, path):
         update_items = []
         for p in self.nautilusVFSFile_table:
-            if p == path or p.startswith( path ):
+            if p == path or p.startswith(path):
                 item = self.nautilusVFSFile_table[p]
                 update_items.append(item)
 
@@ -85,7 +84,7 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
                     'NOP'       : 'oC_error'
                   }
 
-        print( "Server response: "+l )
+        print("Server response: "+l)
         parts = l.split(':')
         if len(parts) > 0:
             action = parts[0]
@@ -102,21 +101,21 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
             elif action == 'UPDATE_VIEW':
                 # Search all items underneath this path and invalidate them
                 if parts[1] in self.registered_paths:
-                    self.invalidate_items_underneath( parts[1] )
+                    self.invalidate_items_underneath(parts[1])
 
             elif action == 'REGISTER_PATH':
                 self.registered_paths[parts[1]] = 1
-                self.invalidate_items_underneath( parts[1] )
+                self.invalidate_items_underneath(parts[1])
             elif action == 'UNREGISTER_PATH':
                 del self.registered_paths[parts[1]]
-                self.invalidate_items_underneath( parts[1] )
+                self.invalidate_items_underneath(parts[1])
 
                 # check if there are non pathes any more, if so, its usual
                 # that mirall went away. Try reconnect.
                 if not self.registered_paths:
                     self.sock.close()
                     self.connected = False
-                    GObject.source_remove( self.watch_id )
+                    GObject.source_remove(self.watch_id)
                     GObject.timeout_add(5000, self.connectToOwnCloud)
 
             else:
@@ -140,12 +139,12 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
                 data = data[:lastNL]
 
             for l in data.split('\n'):
-                self.handle_server_response( l )
+                self.handle_server_response(l)
         else:
             return False
 
         return True # run again
-            
+
     def get_local_path(self, path):
         return path.replace("file://", "")
 
@@ -160,9 +159,9 @@ class ownCloudExtension(GObject.GObject, Nautilus.ColumnProvider, Nautilus.InfoP
         for reg_path in self.registered_paths:
             if filename.startswith(reg_path):
                 self.nautilusVFSFile_table[filename] = item
-        
+
                 # item.add_string_attribute('share_state', "share state")
                 self.askForOverlay(filename)
                 break
             else:
-                print( "Not in scope:"+filename )
+                print("Not in scope:"+filename)
