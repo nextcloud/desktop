661fe5df66fcc809754095c9559aa15fca353b90
Olivier Goffart <ogoffart@woboq.com>
2014-10-10 10:12:08 +0200

    csync_statedb: fix inverted logic in testing if the sqlite was compiled with threads

--- csync/src/csync_statedb.c
+++ csync/src/csync_statedb.c
@@ -76,7 +76,7 @@ static int _csync_check_db_integrity(sqlite3 *db) {
         c_strlist_destroy(result);
     }
 
-    if( sqlite3_threadsafe() != 0 ) {
+    if( sqlite3_threadsafe() == 0 ) {
         CSYNC_LOG(CSYNC_LOG_PRIORITY_WARN, "* WARNING: SQLite module is not threadsafe!");
     }
 
