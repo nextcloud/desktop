fba9020950271f8ea5e594fa412f645fb0575636
Roeland Jago Douma <roeland@famdouma.nl>
2015-02-25 18:32:25 +0100

    Added copyright header

--- src/gui/sharedialog.cpp
+++ src/gui/sharedialog.cpp
@@ -1,3 +1,16 @@
+/*
+ * Copyright (C) by Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
 #include "sharedialog.h"
 #include "ui_sharedialog.h"
 #include "networkjobs.h"
--- src/gui/sharedialog.h
+++ src/gui/sharedialog.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) by
+ * Copyright (C) by Roeland Jago Douma <roeland@famdouma.nl>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
