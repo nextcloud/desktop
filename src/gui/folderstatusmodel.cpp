fc23e810fd462830641da2784537c1ed99d5cc6d
Olivier Goffart <ogoffart@woboq.com>
2015-06-02 20:01:13 +0200

    folderstatusmodel: clean includes

--- src/gui/folderstatusmodel.cpp
+++ src/gui/folderstatusmodel.cpp
@@ -19,11 +19,9 @@
 #include <theme.h>
 #include <account.h>
 
-#include <QtCore>
-#include <QtGui>
-#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
-#include <QtWidgets>
-#endif
+#include <QFileIconProvider>
+#include <QPainter>
+#include <QApplication>
 
 Q_DECLARE_METATYPE(QPersistentModelIndex)
 
