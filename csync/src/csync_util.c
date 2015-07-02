3aed7fc0beab805f20ea9dc288454a7574dc6b8f
Klaas Freitag <freitag@owncloud.com>
2015-07-02 14:28:39 +0200

    Removed dead code from csync_utils. Functionality moved to Qt based code.

--- csync/src/csync_util.c
+++ csync/src/csync_util.c
@@ -103,31 +103,6 @@ void csync_memstat_check(void) {
   CSYNC_LOG(CSYNC_LOG_PRIORITY_INFO, "Memory: %dK total size, %dK resident, %dK shared",
                  m.size * 4, m.resident * 4, m.shared * 4);
 }
-#if 0
-void csync_win32_set_file_hidden( const char *file, bool h ) {
-#ifdef _WIN32
-  const mbchar_t *fileName;
-  DWORD dwAttrs;
-  if( !file ) return;
-
-  fileName = c_utf8_to_locale( file );
-  dwAttrs = GetFileAttributesW(fileName);
-
-  if (dwAttrs != INVALID_FILE_ATTRIBUTES) {
-      if (h && !(dwAttrs & FILE_ATTRIBUTE_HIDDEN)) {
-          SetFileAttributesW(fileName, dwAttrs | FILE_ATTRIBUTE_HIDDEN );
-      } else if (!h && (dwAttrs & FILE_ATTRIBUTE_HIDDEN)) {
-          SetFileAttributesW(fileName, dwAttrs & ~FILE_ATTRIBUTE_HIDDEN );
-      }
-  }
-
-  c_free_locale_string(fileName);
-#else
-    (void) h;
-    (void) file;
-#endif
-}
-#endif
 
 bool (*csync_file_locked_or_open_ext) (const char*) = 0; // filled in by library user
 void set_csync_file_locked_or_open_ext(bool (*f) (const char*));
