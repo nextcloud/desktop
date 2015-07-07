ccc91a52a83c351f3b3be99c9b1e5ad4bab9e454
Klaas Freitag <freitag@owncloud.com>
2015-07-07 21:31:15 +0200

    vio_local: Give strncpy and strncat enough space to copy closing zero.

--- csync/src/vio/csync_vio_local.c
+++ csync/src/vio/csync_vio_local.c
@@ -68,8 +68,8 @@ csync_vio_handle_t *csync_vio_local_opendir(const char *name) {
 
       // alloc an enough large buffer to take the name + '/*' + the closing zero.
       h = c_malloc(len_name+3);
-      strncpy( h, name, len_name);
-      strncat(h, "/*",2);
+      strncpy( h, name, 1+len_name);
+      strncat(h, "/*",3);
 
       dirname = c_utf8_path_to_locale(h);
       SAFE_FREE(h);
