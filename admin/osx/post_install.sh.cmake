cad33de8249909f43fcba9a13291dae102148910
Markus Goetz <markus@woboq.com>
2015-08-05 15:10:00 +0200

    OS X: Try harder to enable 10.10 plugin #3463

--- admin/osx/post_install.sh.cmake
+++ admin/osx/post_install.sh.cmake
@@ -10,6 +10,9 @@ EOF
 
 # Always enable the new 10.10 finder plugin if available
 if [ -x "$(command -v pluginkit)" ]; then
+    # add it to DB. This happens automatically too but we try to push it a bit harder for issue #3463
+    pluginkit -a  "/Applications/@APPLICATION_EXECUTABLE@.app/Contents/PlugIns/FinderSyncExt.appex/"
+    # enable it
     pluginkit -e use -i @APPLICATION_REV_DOMAIN@.FinderSyncExt
 fi
 
