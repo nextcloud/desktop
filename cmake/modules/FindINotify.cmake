7786f1f9a9b64844098d98f9f09db97a8411aa14
Kirill Bychkov <kirby@humppastara.linklevel.loc>
2015-08-13 09:44:55 +0300

    Fix linking with libinotify on OpenBSD.

--- cmake/modules/FindINotify.cmake
+++ cmake/modules/FindINotify.cmake
@@ -4,20 +4,27 @@
 
 # This module defines
 #  INOTIFY_INCLUDE_DIR, where to find inotify.h, etc.
+#  INOTIFY_LIBRARY_DIR, the directory holding the inotify library.
 #  INOTIFY_FOUND, If false, do not try to use inotify.
 # also defined, but not for general use are
 #  INOTIFY_LIBRARY, where to find the inotify library.
 
 find_path(INOTIFY_INCLUDE_DIR sys/inotify.h 
-          HINTS /usr/include/${CMAKE_LIBRARY_ARCHITECTURE})
+          PATH_SUFFIXES inotify)
 mark_as_advanced(INOTIFY_INCLUDE_DIR)
+
+find_library(INOTIFY_LIBRARY inotify PATH_SUFFIXES lib/inotify)
+
+get_filename_component(INOTIFY_LIBRARY_DIR ${INOTIFY_LIBRARY} DIRECTORY)
+mark_as_advanced(INOTIFY_LIBRARY_DIR)
 
 # all listed variables are TRUE
 # handle the QUIETLY and REQUIRED arguments and set INOTIFY_FOUND to TRUE if
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(INOTIFY DEFAULT_MSG INOTIFY_INCLUDE_DIR)
+find_package_handle_standard_args(INOTIFY DEFAULT_MSG INOTIFY_INCLUDE_DIR INOTIFY_LIBRARY_DIR)
 
 IF(INOTIFY_FOUND)
   SET(INotify_INCLUDE_DIRS ${INOTIFY_INCLUDE_DIR})
+  SET(INotify_LIBRARY_DIRS ${INOTIFY_LIBRARY_DIR})
 ENDIF(INOTIFY_FOUND)
 
