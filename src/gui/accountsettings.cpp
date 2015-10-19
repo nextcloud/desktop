nextcloud/desktop

commit 027a865fbc1dbee52bb100e488c869383b1e92df

Account Settings: Show selective sync buttons after list load
Especially nice when having a slower network.
For #3839

src/gui/accountsettings.cpp
@@ -91,6 +91,8 @@ AccountSettings::AccountSettings(AccountState *accountState, QWidget *parent) :
     connect(_model, SIGNAL(suggestExpand(QModelIndex)), ui->_folderList, SLOT(expand(QModelIndex)));
     connect(_model, SIGNAL(dirtyChanged()), this, SLOT(refreshSelectiveSyncStatus()));
     refreshSelectiveSyncStatus();
+    connect(_model, SIGNAL(rowsInserted(QModelIndex,int,int)),
+            this, SLOT(refreshSelectiveSyncStatus()));
 
     QAction *resetFolderAction = new QAction(this);
     resetFolderAction->setShortcut(QKeySequence(Qt::Key_F5));
@@ -517,6 +519,13 @@ AccountSettings::~AccountSettings()
 
 void AccountSettings::refreshSelectiveSyncStatus()
 {
+    if (_model->_folders.first()._subs.count() == 0) {
+        // Don't show when there is no items in model yet.
+        // It will be called again via rowsInserted()
+        ui->selectiveSyncStatus->setVisible(false);
+        return;
+    }
+
     bool shouldBeVisible = _model->isDirty();
     QStringList undecidedFolder;
     for (int i = 0; !shouldBeVisible && i < _model->rowCount(); ++i) {

commit 374f29c4d32949c7caf6f9cc0ec6de792682165a

Account Settings: Fix crash for acocunts withour folder
Was broken by  027a865fbc1dbee52bb100e488c869383b1e92df

src/gui/accountsettings.cpp
@@ -519,18 +519,13 @@ AccountSettings::~AccountSettings()
 
 void AccountSettings::refreshSelectiveSyncStatus()
 {
-    if (_model->_folders.first()._subs.count() == 0) {
-        // Don't show when there is no items in model yet.
-        // It will be called again via rowsInserted()
-        ui->selectiveSyncStatus->setVisible(false);
-        return;
-    }
-
     bool shouldBeVisible = _model->isDirty();
     QStringList undecidedFolder;
     for (int i = 0; !shouldBeVisible && i < _model->rowCount(); ++i) {
-        if (ui->_folderList->isExpanded(_model->index(i)))
+        auto index = _model->index(i);
+        if (ui->_folderList->isExpanded(index) && _model->rowCount(index) > 0) {
             shouldBeVisible = true;
+        }
     }
 
     foreach (Folder *folder, FolderMan::instance()->map().values()) {