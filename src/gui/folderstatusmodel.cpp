nextcloud/desktop

commit d610693af104e98f934878f228f6da57a2380fb2

SelectiveSync: Apply excludes #3876

src/gui/folderstatusmodel.cpp
@@ -496,21 +496,31 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
         return;
     }
 
-    auto list = list_;
-    list.removeFirst(); // remove the parent item
-
     if (parentInfo->_hasError) {
         beginRemoveRows(idx, 0 ,0);
         parentInfo->_hasError = false;
         endRemoveRows();
     }
 
-    beginInsertRows(idx, 0, list.count() - 1);
+    auto list = list_;
+    list.removeFirst(); // remove the parent item
 
     QUrl url = parentInfo->_folder->remoteUrl();
     QString pathToRemove = url.path();
     if (!pathToRemove.endsWith('/'))
         pathToRemove += '/';
+
+    // Drop the folder base path and check for excludes.
+    QMutableListIterator<QString> it(list);
+    while (it.hasNext()) {
+        it.next();
+        if (parentInfo->_folder->isFileExcluded(it.value())) {
+            it.remove();
+        }
+        it.value().remove(pathToRemove);
+    }
+
+    beginInsertRows(idx, 0, list.count() - 1);
 
     parentInfo->_fetched = true;
     parentInfo->_fetching = false;
@@ -531,7 +541,6 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
         newInfo._pathIdx << i++;
         auto size = job ? job->_sizes.value(path) : 0;
         newInfo._size = size;
-        path.remove(pathToRemove);
         newInfo._path = path;
         newInfo._name = path.split('/', QString::SkipEmptyParts).last();
 

src/gui/selectivesyncdialog.cpp
@@ -156,7 +156,7 @@ void SelectiveSyncTreeView::recursiveInsert(QTreeWidgetItem* parent, QStringList
     }
 }
 
-void SelectiveSyncTreeView::slotUpdateDirectories(const QStringList&list)
+void SelectiveSyncTreeView::slotUpdateDirectories(QStringList list)
 {
     auto job = qobject_cast<LsColJob *>(sender());
 
@@ -173,6 +173,22 @@ void SelectiveSyncTreeView::slotUpdateDirectories(const QStringList&list)
     pathToRemove.append(_folderPath);
     if (!_folderPath.isEmpty())
         pathToRemove.append('/');
+
+    // Check for excludes.
+    //
+    // We would like to use Folder::isFileExcluded, but the folder doesn't
+    // exist yet. So we just create one temporarily...
+    FolderDefinition def;
+    def.localPath = pathToRemove;
+    def.ignoreHiddenFiles = FolderMan::instance()->ignoreHiddenFiles();
+    Folder f(def);
+    QMutableListIterator<QString> it(list);
+    while (it.hasNext()) {
+        it.next();
+        if (f.isFileExcluded(it.value())) {
+            it.remove();
+        }
+    }
 
     // Since / cannot be in the blacklist, expand it to the actual
     // list of top-level folders as soon as possible.

src/gui/selectivesyncdialog.h
@@ -48,7 +48,7 @@ public:
 
     QSize sizeHint() const Q_DECL_OVERRIDE;
 private slots:
-    void slotUpdateDirectories(const QStringList &);
+    void slotUpdateDirectories(QStringList);
     void slotItemExpanded(QTreeWidgetItem *);
     void slotItemChanged(QTreeWidgetItem*,int);
     void slotLscolFinishedWithError(QNetworkReply*);

commit d657c00b11bf834e64ffd7893022a0a577af52bd

FolderStatusModel: fix getting the size of the folder in the selective sync (#3986)
Regressed since d610693af104e98f934878f228f6da57a2380fb2. The problem
is that the _size vector contains the pathToRemove and that it was removed
before.
Reorganize a bit the code so there is only one loop that has still all the
 information.

src/gui/folderstatusmodel.cpp
@@ -509,7 +509,7 @@ void FolderStatusModel::fetchMore(const QModelIndex& parent)
     QTimer::singleShot(1000, this, SLOT(slotShowFetchProgress()));
 }
 
-void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
+void FolderStatusModel::slotUpdateDirectories(const QStringList &list)
 {
     auto job = qobject_cast<LsColJob *>(sender());
     Q_ASSERT(job);
@@ -529,25 +529,10 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
     parentInfo->_fetching = false;
     parentInfo->_fetched = true;
 
-    auto list = list_;
-    list.removeFirst(); // remove the parent item
-
     QUrl url = parentInfo->_folder->remoteUrl();
     QString pathToRemove = url.path();
     if (!pathToRemove.endsWith('/'))
         pathToRemove += '/';
-
-    // Drop the folder base path and check for excludes.
-    QMutableListIterator<QString> it(list);
-    while (it.hasNext()) {
-        it.next();
-        it.value().remove(pathToRemove);
-        if (parentInfo->_folder->isFileExcludedRelative(it.value())) {
-            it.remove();
-        }
-    }
-
-    beginInsertRows(idx, 0, list.count() - 1);
 
     QStringList selectiveSyncBlackList;
     if (parentInfo->_checked == Qt::PartiallyChecked) {
@@ -555,20 +540,28 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
     }
     auto selectiveSyncUndecidedList = parentInfo->_folder->journalDb()->getSelectiveSyncList(SyncJournalDb::SelectiveSyncUndecidedList);
 
-    QVarLengthArray<int> undecidedIndexes;
+    QVarLengthArray<int, 10> undecidedIndexes;
 
-    int i = 0;
-    foreach (QString path, list) {
+    QVector<SubFolderInfo> newSubs;
+    newSubs.reserve(list.size() - 1);
+    for (int i = 1;  // skip the parent item (first in the list)
+            i < list.size(); ++i) {
+        const QString &path = list.at(i);
+        auto relativePath = path.mid(pathToRemove.size());
+        if (parentInfo->_folder->isFileExcludedRelative(relativePath)) {
+            continue;
+        }
+
         SubFolderInfo newInfo;
         newInfo._folder = parentInfo->_folder;
         newInfo._pathIdx = parentInfo->_pathIdx;
-        newInfo._pathIdx << i++;
+        newInfo._pathIdx << newSubs.size();
         auto size = job ? job->_sizes.value(path) : 0;
         newInfo._size = size;
-        newInfo._path = path;
-        newInfo._name = path.split('/', QString::SkipEmptyParts).last();
+        newInfo._path = relativePath;
+        newInfo._name = relativePath.split('/', QString::SkipEmptyParts).last();
 
-        if (path.isEmpty())
+        if (relativePath.isEmpty())
             continue;
 
         if (parentInfo->_checked == Qt::Unchecked) {
@@ -577,25 +570,27 @@ void FolderStatusModel::slotUpdateDirectories(const QStringList &list_)
             newInfo._checked = Qt::Checked;
         } else {
             foreach(const QString &str , selectiveSyncBlackList) {
-                if (str == path || str == QLatin1String("/")) {
+                if (str == relativePath || str == QLatin1String("/")) {
                     newInfo._checked = Qt::Unchecked;
                     break;
-                } else if (str.startsWith(path)) {
+                } else if (str.startsWith(relativePath)) {
                     newInfo._checked = Qt::PartiallyChecked;
                 }
             }
         }
 
         foreach(const QString &str , selectiveSyncUndecidedList) {
-            if (str == path) {
+            if (str == relativePath) {
                 newInfo._isUndecided = true;
-            } else if (str.startsWith(path)) {
+            } else if (str.startsWith(relativePath)) {
                 undecidedIndexes.append(newInfo._pathIdx.last());
             }
         }
-        parentInfo->_subs.append(newInfo);
+        newSubs.append(newInfo);
     }
 
+    beginInsertRows(idx, 0, newSubs.size() - 1);
+    parentInfo->_subs = std::move(newSubs);
     endInsertRows();
 
     for (auto it = undecidedIndexes.begin(); it != undecidedIndexes.end(); ++it) {