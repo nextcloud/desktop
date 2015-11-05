6901fc9e3891c0fd1995c2eea5f3c167ffff54b3
Markus Goetz <markus@woboq.com>
2015-11-05 18:15:45 +0100

    macdeployqt: Also copy bearer plugins
    See #3449

--- admin/osx/macdeployqt.py
+++ admin/osx/macdeployqt.py
@@ -38,6 +38,8 @@ QT_PLUGINS = [
     'imageformats/libqgif.dylib',
     'imageformats/libqico.dylib',
     'imageformats/libqjpeg.dylib',
+    'bearer/libqcorewlanbearer.dylib',
+    'bearer/libqgenericbearer.dylib'
     'imageformats/libqsvg.dylib',
     'imageformats/libqmng.dylib',
 ]
