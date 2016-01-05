60a6b2b0c38e8ea54a0d47735b1a7ec18d2460c4
Markus Goetz <markus@woboq.com>
2016-01-05 13:32:46 +0100

    OS X: Also show desktop notifications when app is active

--- src/gui/systray.mm
+++ src/gui/systray.mm
@@ -2,6 +2,19 @@
 #include <QDebug>
 #import <Cocoa/Cocoa.h>
 
+@interface NotificationCenterDelegate : NSObject
+@end
+@implementation NotificationCenterDelegate
+// Always show, even if app is active at the moment.
+- (BOOL)userNotificationCenter:(NSUserNotificationCenter *)center
+     shouldPresentNotification:(NSUserNotification *)notification
+{
+    Q_UNUSED(center);
+    Q_UNUSED(notification);
+    return YES;
+}
+@end
+
 namespace OCC {
 
 bool canOsXSendUserNotification()
@@ -14,6 +27,12 @@ void sendOsXUserNotification(const QString &title, const QString &message)
     qDebug() << Q_FUNC_INFO << title << message;
     Class cuserNotificationCenter = NSClassFromString(@"NSUserNotificationCenter");
     id userNotificationCenter = [cuserNotificationCenter defaultUserNotificationCenter];
+
+    static dispatch_once_t once;
+    dispatch_once(&once, ^{
+            id delegate = [[NotificationCenterDelegate alloc] init];
+            [userNotificationCenter setDelegate:delegate];
+    });
 
     Class cuserNotification = NSClassFromString(@"NSUserNotification");
     id notification = [[cuserNotification alloc] init];
