ae3b9f112cd2dc8944736abc0edede8b45b8794d
Klaas Freitag <freitag@owncloud.com>
2016-02-09 12:59:09 +0100

    Linux shell integration: Do not call sed from absolute path.
    sed is in /bin/ rather than in /usr/bin on some systems, which
    makes the scripts fail in the build environment. sed should be
    in the PATH tough.

--- shell_integration/nautilus/createnemoplugin.sh
+++ shell_integration/nautilus/createnemoplugin.sh
@@ -3,4 +3,4 @@
 # this script creates a plugin for nemo, just be replacing
 # all occurences of Nautilus with Nemo.
 
-/usr/bin/sed -i.org -e 's/autilus/emo/g' syncstate_nemo.py
+sed -i.org -e 's/autilus/emo/g' syncstate_nemo.py
--- shell_integration/nautilus/setappname.sh
+++ shell_integration/nautilus/setappname.sh
@@ -3,4 +3,4 @@
 # this script replaces the line
 #  appname = 'ownCloud'
 # with the correct branding name in the syncstate.py script
-/usr/bin/sed -i.org -e 's/appname\s*=\s*'"'"'ownCloud'"'/appname = '$1'/" syncstate.py
+sed -i.org -e 's/appname\s*=\s*'"'"'ownCloud'"'/appname = '$1'/" syncstate.py
