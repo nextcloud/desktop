0e1b9a346d54e81d5008d0a74c2fb28f6e05dbcd
Klaas Freitag <freitag@owncloud.com>
2016-03-18 15:28:00 +0100

    Fix plural translation handling, remove the superflous arg()

--- src/libsync/utility.cpp
+++ src/libsync/utility.cpp
@@ -442,7 +442,7 @@ QString Utility::timeAgoInWords(const QDateTime& dt, const QDateTime& from)
 
     if( dt.daysTo(now)>0 ) {
         int dtn = dt.daysTo(now);
-        return QObject::tr("%n day(s) ago", "", dtn).arg(dtn);
+        return QObject::tr("%n day(s) ago", "", dtn);
     } else {
         qint64 secs = dt.secsTo(now);
         if( secs < 0 ) {
@@ -450,7 +450,7 @@ QString Utility::timeAgoInWords(const QDateTime& dt, const QDateTime& from)
         }
         if( floor(secs / 3600.0) > 0 ) {
             int hours = floor(secs/3600.0);
-            return( QObject::tr("%n hour(s) ago", "", hours).arg(hours));
+            return( QObject::tr("%n hour(s) ago", "", hours) );
         } else {
             int minutes = qRound(secs/60.0);
             if( minutes == 0 ) {
@@ -460,7 +460,7 @@ QString Utility::timeAgoInWords(const QDateTime& dt, const QDateTime& from)
                     return QObject::tr("Less than a minute ago");
                 }
             }
-            return( QObject::tr("%n minute(s) ago", "", minutes).arg(minutes));
+            return( QObject::tr("%n minute(s) ago", "", minutes) );
         }
     }
     return QObject::tr("Some time ago");
