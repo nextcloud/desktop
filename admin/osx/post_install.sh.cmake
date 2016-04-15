6a9655aab6b0b06dc4085dcb01ff7ef4131100ee
Markus Goetz <markus@woboq.com>
2016-04-15 16:56:54 +0200

    OS X: Fix overlay installation on El Capitan #4650
    The sleep is somehow needed, probably for pkg to rebuild its DB?
    The case fix fixes a warning but did not fix the issue itself.

--- admin/osx/post_install.sh.cmake
+++ admin/osx/post_install.sh.cmake
@@ -12,6 +12,8 @@ EOF
 if [ -x "$(command -v pluginkit)" ]; then
     # add it to DB. This happens automatically too but we try to push it a bit harder for issue #3463
     pluginkit -a  "/Applications/@APPLICATION_EXECUTABLE@.app/Contents/PlugIns/FinderSyncExt.appex/"
+    # Since El Capitan we need to sleep #4650
+    sleep 10s
     # enable it
     pluginkit -e use -i @APPLICATION_REV_DOMAIN@.FinderSyncExt
 fi
