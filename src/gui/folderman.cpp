3335c733d7bb41c68be2bea4550b3db8d1953d0e
arthurchiao <arthurchiao@hotmail.com>
2016-07-08 15:53:11 +0800

    be case insensitive for MacOSX, too

--- src/gui/folderman.cpp
+++ src/gui/folderman.cpp
@@ -849,7 +849,7 @@ Folder *FolderMan::folderForPath(const QString &path)
     foreach(Folder* folder, this->map().values()) {
         const QString folderPath = folder->cleanPath()+QLatin1Char('/');
 
-        if(absolutePath.startsWith(folderPath, Utility::isWindows()?
+        if(absolutePath.startsWith(folderPath, (Utility::isWindows() || Utility::isMac())?
                     Qt::CaseInsensitive : Qt::CaseSensitive)) {
             //qDebug() << "found folder: " << folder->path() << " for " << absolutePath;
             return folder;
