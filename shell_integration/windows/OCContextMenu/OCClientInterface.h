b575ded464a4c55558cc5b8d0d1c6ccace38fff4
Daniel Molkentin <danimo@owncloud.com>
2016-07-26 16:53:11 +0200

    More GmbH -> Inc Copyright header fixes

--- shell_integration/windows/OCContextMenu/OCClientInterface.h
+++ shell_integration/windows/OCContextMenu/OCClientInterface.h
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2015 ownCloud, Inc. All rights reserved.
+* Copyright (c) 2015 ownCloud GmbH. All rights reserved.
 *
 * This library is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Lesser General Public License as published by the Free
@@ -12,7 +12,7 @@
 */
 
 /**
-* Copyright (c) 2014 ownCloud, Inc. All rights reserved.
+* Copyright (c) 2014 ownCloud GmbH. All rights reserved.
 *
 * This library is free software; you can redistribute it and/or modify it under
 * the terms of the GNU Lesser General Public License as published by the Free
