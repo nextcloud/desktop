a83c5598e850730c277f365e38a2348b825351a9
Daniel Molkentin <danimo@owncloud.com>
2016-07-26 16:47:44 +0200

    Update owner to be ownCloud GmbH
    Fixes #5079

--- src/libsync/theme.cpp
+++ src/libsync/theme.cpp
@@ -286,7 +286,7 @@ QString Theme::about() const
             .arg(MIRALL_VERSION_STRING).arg("http://" MIRALL_STRINGIFY(APPLICATION_DOMAIN))
             .arg(MIRALL_STRINGIFY(APPLICATION_DOMAIN));
 
-    re += tr("<p>Copyright ownCloud, Incorporated</p>");
+    re += tr("<p>Copyright ownCloud GmbH</p>");
     re += tr("<p>Distributed by %1 and licensed under the GNU General Public License (GPL) Version 2.0.<br/>"
              "%2 and the %2 logo are registered trademarks of %1 in the "
              "United States, other countries, or both.</p>")
