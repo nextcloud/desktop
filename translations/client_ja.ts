f2c6669224bb15858941fcf3c67bf7732c4ececb
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-08-29 02:18:30 +0200

    [tx-robot] updated from transifex

--- translations/client_ja.ts
+++ translations/client_ja.ts
@@ -2550,7 +2550,7 @@ It is not advisable to use it.</source>
     <message>
         <location filename="../src/gui/creds/shibboleth/shibbolethwebview.cpp" line="91"/>
         <source>SSL Chipher Debug View</source>
-        <translation type="unfinished"/>
+        <translation>SSL暗号デバッグ表示</translation>
     </message>
     <message>
         <location filename="../src/gui/creds/shibboleth/shibbolethwebview.cpp" line="99"/>
