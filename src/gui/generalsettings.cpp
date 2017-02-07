1102ac20acb794d3b3b8ca387502fc720652bd64
Markus Goetz <markus@woboq.com>
2017-02-07 19:40:32 +0100

    Improve previous fix
    After comment on https://github.com/owncloud/client/commit/78a798eef3eb8d04360c6d9542db55469f59cacd

--- src/gui/generalsettings.cpp
+++ src/gui/generalsettings.cpp
@@ -106,6 +106,7 @@ void GeneralSettings::loadMiscSettings()
 {
 #if QT_VERSION < QT_VERSION_CHECK( 5, 4, 0 )
     QScopedValueRollback<bool> scope(_currentlyLoading);
+    _currentlyLoading = true;
 #else
     QScopedValueRollback<bool> scope(_currentlyLoading, true);
 #endif
