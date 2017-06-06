7eb8219419fe9251ce62ded3bdae197fd796a66e
Christian Kamm <mail@ckamm.de>
2017-06-06 12:59:57 +0200

    Add extra logging for SQLITE_CANTOPEN errors #5633
    (cherry picked from commit 05c1dcef9b35a18d358ab307922eceff312e3304)

--- src/libsync/ownsql.cpp
+++ src/libsync/ownsql.cpp
@@ -57,6 +57,10 @@ bool SqlDatabase::openHelper( const QString& filename, int sqliteFlags )
 
     if( _errId != SQLITE_OK ) {
         qDebug() << "Error:" << _error << "for" << filename;
+        if (_errId == SQLITE_CANTOPEN) {
+            qDebug() << "CANTOPEN extended errcode: " << sqlite3_extended_errcode(_db);
+            qDebug() << "CANTOPEN system errno: " << sqlite3_system_errno(_db);
+        }
         close();
         return false;
     }
