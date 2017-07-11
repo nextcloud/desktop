e13b618a6574f3a47c12b12af6586dfc4e3efb69
Christian Kamm <mail@ckamm.de>
2017-07-11 15:50:56 +0200

    Add ElidedLabel
    A label that adjusts its text based on Qt::TextElideMode.

---
+++ src/gui/elidedlabel.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) by Christian Kamm <mail@ckamm.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef ELIDEDLABEL_H
+#define ELIDEDLABEL_H
+
+#include <QLabel>
+
+namespace OCC {
+
+/// Label that can elide its text
+class ElidedLabel : public QLabel
+{
+    Q_OBJECT
+public:
+    explicit ElidedLabel(const QString &text, QWidget *parent = 0);
+
+    void setText(const QString &text);
+    const QString &text() const { return _text; }
+
+    void setElideMode(Qt::TextElideMode elideMode);
+    Qt::TextElideMode elideMode() const { return _elideMode; }
+
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private:
+    QString _text;
+    Qt::TextElideMode _elideMode;
+};
+}
+
+#endif
