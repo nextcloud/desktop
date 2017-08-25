5b51346e83b01dfc1922ab506688d29625f95baa
Tomaz Canabrava <tcanabrava@kde.org>
2017-08-25 14:59:13 +0200

    [CSE] New Network Job: DeleteApiJob
    This network job does a DELETE http request on a URL. It's the
    second class that does basically the same, but this one returns
    the http return code, and it's set to do a api call.

--- src/libsync/networkjobs.cpp
+++ src/libsync/networkjobs.cpp
@@ -820,6 +820,7 @@ bool JsonApiJob::finished()
     return true;
 }
 
+
 DetermineAuthTypeJob::DetermineAuthTypeJob(AccountPtr account, QObject *parent)
     : AbstractNetworkJob(account, QString(), parent)
     , _redirects(0)
@@ -907,4 +908,36 @@ bool SimpleNetworkJob::finished()
     return true;
 }
 
+DeleteApiJob::DeleteApiJob(AccountPtr account, const QString &path, QObject *parent)
+    : AbstractNetworkJob(account, path, parent)
+{
+
+}
+
+void DeleteApiJob::start()
+{
+    QNetworkRequest req;
+    req.setRawHeader("OCS-APIREQUEST", "true");
+    QUrl url = Utility::concatUrlPath(account()->url(), path());
+    sendRequest("DELETE", url, req);
+    AbstractNetworkJob::start();
+}
+
+bool DeleteApiJob::finished()
+{
+    qCInfo(lcJsonApiJob) << "JsonApiJob of" << reply()->request().url() << "FINISHED WITH STATUS"
+                         << reply()->error()
+                         << (reply()->error() == QNetworkReply::NoError ? QLatin1String("") : errorString());
+
+    int statusCode = 0;
+
+    if (reply()->error() != QNetworkReply::NoError) {
+        qCWarning(lcJsonApiJob) << "Network error: " << path() << errorString() << reply()->attribute(QNetworkRequest::HttpStatusCodeAttribute);
+        emit result(statusCode);
+        return true;
+    }
+
+    const auto replyData = QString::fromUtf8(reply()->readAll());
+    qCInfo(lcJsonApiJob()) << "TMX Delete Job" << replyData;
+}
 } // namespace OCC
