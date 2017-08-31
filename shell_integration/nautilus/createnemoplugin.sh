61ca5eb1dbf03357a9648c64a5e932e097512cf9
Jürgen Weigert <jnweiger@gmail.com>
2017-08-31 13:16:34 +0200

    fix branded nemo and caja shell integration (#5966)
    * Update createcajaplugin.sh
    
    * first apply branding, then create copies. Better.
    Fixes https://github.com/owncloud/ownbrander/issues/753
    
    * fix https://github.com/owncloud/ownbrander/issues/752 too.

--- shell_integration/nautilus/createnemoplugin.sh
+++ shell_integration/nautilus/createnemoplugin.sh
@@ -3,4 +3,5 @@
 # this script creates a plugin for nemo, just be replacing
 # all occurences of Nautilus with Nemo.
 
+cp syncstate.py syncstate_nemo.py
 sed -i.org -e 's/autilus/emo/g' syncstate_nemo.py
