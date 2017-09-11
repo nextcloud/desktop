3f4d915a177ef8dc3bfdd0a3d59cc56760fac591
Tomaz Canabrava <tcanabrava@kde.org>
2017-09-11 15:00:01 +0200

    [cse] Add files to handle client side encryption
    This will be the responsible for encryption,
    decryption and talking with the server.

---
+++ src/libsync/clientsideencryption.cpp
@@ -0,0 +1,4 @@
+#include "clientsideencryption.h"
+
+namespace ClientSideEncryption {
+}
---
+++ src/libsync/clientsideencryption.h
@@ -0,0 +1,10 @@
+#ifndef CLIENTSIDEENCRYPTION_H
+#define CLIENTSIDEENCRYPTION_H
+
+#include <QString>
+
+namespace ClientSideEncryption {
+    QString baseUrl = QStringLiteral("ocs/v2.php/apps/client_side_encryption/api/v1/");
+}
+
+#endif
