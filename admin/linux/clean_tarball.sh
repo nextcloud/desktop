de8ab35cb3587a1c216d82f4ece0c35aa860f450
Markus Goetz <markus@woboq.com>
2017-09-11 18:53:51 +0200

    Add clean_tarball.sh #6005

---
+++ admin/linux/clean_tarball.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -vrf admin/
+rm -vrf src/3rdparty/sqlite3
+rm -vrf binary/
+rm -vrf src/3rdparty/libcrashreporter-qt
+rm -vrf shell_integration/windows
+rm -vrf shell_integration/MacOSX
