4f7265c04f258d4e8e8acb82f7068d2a43d0a5f0
Tomaz Canabrava <tcanabrava@kde.org>
2017-09-14 22:50:56 +0200

    [aes] Create the Decrypt function.

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -36,11 +36,15 @@ namespace {
         abort();
     }
 
-    int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+    int encrypt(unsigned char *plaintext,
+                int plaintext_len,
+                unsigned char *key,
+                unsigned char *iv,
+                unsigned char *ciphertext)
     {
-        EVP_CIPHER_CTX *ctx;
-        int len;
-        int ciphertext_len;
+        EVP_CIPHER_CTX *ctx = nullptr;
+        int len = 0;
+        int ciphertext_len = 0;
 
         /* Create and initialise the context */
         if(!(ctx = EVP_CIPHER_CTX_new())) {
@@ -72,6 +76,50 @@ namespace {
         EVP_CIPHER_CTX_free(ctx);
 
         return ciphertext_len;
+    }
+
+    int decrypt(unsigned char *ciphertext,
+                int ciphertext_len,
+                unsigned char *key,
+                unsigned char *iv,
+                unsigned char *plaintext)
+    {
+        EVP_CIPHER_CTX *ctx = nullptr;
+
+        int len = 0;
+
+        int plaintext_len = 0;
+
+        /* Create and initialise the context */
+        if(!(ctx = EVP_CIPHER_CTX_new())) {
+            handleErrors();
+        }
+
+        /* Initialise the decryption operation. IMPORTANT - ensure you use a key
+        * and IV size appropriate for your cipher
+        * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+        * IV size for *most* modes is the same as the block size. For AES this
+        * is 128 bits */
+        if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, key, iv)) {
+            handleErrors();
+        }
+
+        // Verify if we need to call this more than one time
+        if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len)) {
+            handleErrors();
+        }
+        plaintext_len = len;
+
+        // Finalize Decrypt
+        if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len)) {
+            handleErrors();
+        }
+        plaintext_len += len;
+
+        /* Clean up */
+        EVP_CIPHER_CTX_free(ctx);
+
+        return plaintext_len;
     }
 }
 
