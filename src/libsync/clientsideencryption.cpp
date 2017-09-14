6b53b4f25735f5dedbdc231072e8a6e04dd3db1c
Tomaz Canabrava <tcanabrava@kde.org>
2017-09-14 22:45:12 +0200

    [cse] Add the crypt method, based on OpenSSL Wiki

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -6,6 +6,7 @@
 #include <openssl/rsa.h>
 #include <openssl/evp.h>
 #include <openssl/pem.h>
+#include <openssl/err.h>
 
 #include <map>
 #include <string>
@@ -27,6 +28,52 @@ Q_LOGGING_CATEGORY(lcCse, "sync.clientsideencryption", QtInfoMsg)
 
 QString baseUrl = QStringLiteral("ocs/v2.php/apps/client_side_encryption/api/v1/");
 QString baseDirectory = QDir::homePath() + QStringLiteral("/.nextcloud-keys/");
+
+namespace {
+    void handleErrors(void)
+    {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+
+    int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key, unsigned char *iv, unsigned char *ciphertext)
+    {
+        EVP_CIPHER_CTX *ctx;
+        int len;
+        int ciphertext_len;
+
+        /* Create and initialise the context */
+        if(!(ctx = EVP_CIPHER_CTX_new())) {
+            handleErrors();
+        }
+
+        /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+        * and IV size appropriate for your cipher
+        * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+        * IV size for *most* modes is the same as the block size. For AES this
+        * is 128 bits */
+        if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, key, iv)) {
+            handleErrors();
+        }
+
+        // Figure out if I need to call this multiple times.
+        if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len)) {
+            handleErrors();
+        }
+        ciphertext_len = len;
+
+        // Finalize Encryption
+        if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) {
+            handleErrors();
+        }
+        ciphertext_len += len;
+
+        /* Clean up */
+        EVP_CIPHER_CTX_free(ctx);
+
+        return ciphertext_len;
+    }
+}
 
 ClientSideEncryption::ClientSideEncryption()
 {
@@ -278,7 +325,11 @@ void ClientSideEncryption::encryptPrivateKey(EVP_PKEY *keyPair)
     qCInfo(lcCse()) << "Return of the PKCS5" << ret;
     qCInfo(lcCse()) << "Result String" << secretKey;
 
-    const EVP_CIPHER *cipher = EVP_get_cipherbyname("aes-256-cbc");
+    //: FIRST TRY A SILLY PHRASE.
+    //: Hardcoed IV, really bad.
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+    const EVP_CIPHER *cipher = EVP_aes_256_gcm ();
+
     // Now, Try to encrypt it.
 }
 
