f64519bfbd601d1cfbf733c1d267ec05e6da7c8e
Christian Kamm <mail@ckamm.de>
2017-09-22 10:16:26 +0200

    Move the --version output text to Theme #5957
    Extracted and adjusted from packaging.diff by @jnweiger and @dschmidt

--- src/cmd/cmd.cpp
+++ src/cmd/cmd.cpp
@@ -196,8 +196,7 @@ void help()
 
 void showVersion()
 {
-    const char *binaryName = APPLICATION_EXECUTABLE "cmd";
-    std::cout << binaryName << " version " << qPrintable(Theme::instance()->version()) << std::endl;
+    std::cout << qPrintableUtf8(Theme::instance()->versionSwitchOutput());
     exit(0);
 }
 
