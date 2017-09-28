eb1a96c1649ba8d95d01cf89e3a2d02b5aabfea8
Jürgen Weigert <jnweiger@gmail.com>
2017-09-28 21:09:43 +0200

    Update clean_tarball.sh
    warning against removing sqlite. it is used with Linux with cmake switch on CentOS6
    USE_OUR_OWN_SQLITE3=TRUE

--- admin/linux/clean_tarball.sh
+++ admin/linux/clean_tarball.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -vrf admin/
-rm -vrf src/3rdparty/sqlite3
+rm -vrf src/3rdparty/sqlite3   # FIXME: For CentOS6 we have to use our bundled sqlite
 rm -vrf binary/
 rm -vrf src/3rdparty/libcrashreporter-qt
 rm -vrf shell_integration/windows
