494ae31de94080795f0525bea68dd3e528f27a4a
Tomaz Canabrava <tcanabrava@kde.org>
2017-12-19 23:09:39 +0100

    [CSE] Fix correct json object

--- src/libsync/clientsideencryption.cpp
+++ src/libsync/clientsideencryption.cpp
@@ -1277,7 +1277,7 @@ void FolderMetadata::setupEmptyMetadata() {
     };
 
     QJsonObject metaObject = {
-      {"metadata", metadataKeys},
+      {"metadata", metadata},
       {"files", files}
     };
 
