a2bdd5b9a534717fc2c788daee6e92c1df8c4690
Olivier Goffart <ogoffart@woboq.com>
2018-01-25 16:25:11 +0100

    Placeholders: Fixup clicking on placeholder after previous change
    Now that the name in the db is the name of the placeholder file,
    we need to adjust the call to downloadPlaceholder

--- src/gui/application.h
+++ src/gui/application.h
@@ -75,6 +75,7 @@ public slots:
     void slotCrash();
     /**
      * Will download a placeholder file, and open the result.
+     * The argument is the filename of the placeholder file (including the extension)
      */
     void openPlaceholder(const QString &filename);
 
