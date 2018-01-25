de929eb566eb6b7769a701d1bd7ec1662f7aa4ca
Olivier Goffart <ogoffart@woboq.com>
2018-01-25 14:35:21 +0100

    Adjust the Move To Trash patch
    (PR #6265)
    
     - Remove the UI completely
     - Move the #ifdef inside the FileSystem::moveToTrash function, so it is easier to
       implement on other platforms
     - Q_OS_UNIX includes mac, so we need to disable it. (not using Q_OS_LINUX because of
       other BSD that uses XDG spec as well
     - Translate the error messages
     - Add a couple of doc comments

--- src/common/filesystembase.h
+++ src/common/filesystembase.h
@@ -109,9 +109,11 @@ namespace FileSystem {
      */
     bool OCSYNC_EXPORT remove(const QString &fileName, QString *errorString = 0);
 
-#ifdef Q_OS_UNIX
+    /**
+     * Move the specified file or folder to the trash. (Only implemented on linux)
+     */
     bool OCSYNC_EXPORT moveToTrash(const QString &filename, QString *errorString);
-#endif
+
     /**
      * Replacement for QFile::open(ReadOnly) followed by a seek().
      * This version sets a more permissive sharing mode on Windows.
