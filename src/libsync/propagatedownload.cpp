718843ffce4f76131262f728ae214fa252213bc2
Olivier Goffart <ogoffart@woboq.com>
2018-04-17 15:04:36 +0200

    Download: Use the <s:message> from the reply in the error message if any
    Issue: #6459

--- src/libsync/propagatedownload.cpp
+++ src/libsync/propagatedownload.cpp
@@ -657,7 +657,7 @@ void PropagateDownloadFile::slotGetFinished()
                 &propagator()->_anotherSyncNeeded);
         }
 
-        done(status, job->errorString());
+        done(status,_item->_httpErrorCode >= 400 ? job->errorStringParsingBody() : job->errorString());
         return;
     }
 
