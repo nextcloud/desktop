e2a8975ad842011ef6e175af5b802be6b9f9a8dc
Dominik Schmidt <dev@dominik-schmidt.de>
2018-06-13 22:41:57 +0200

    Extract filename of updateFile without leading slash

--- src/gui/updater/ocupdater.cpp
+++ src/gui/updater/ocupdater.cpp
@@ -323,7 +323,7 @@ void NSISUpdater::versionInfoArrived(const UpdateInfo &info)
             showDialog(info);
         }
         if (!url.isEmpty()) {
-            _targetFile = cfg.configPath() + url.mid(url.lastIndexOf('/'));
+            _targetFile = cfg.configPath() + url.mid(url.lastIndexOf('/')+1);
             if (QFile(_targetFile).exists()) {
                 setDownloadState(DownloadComplete);
             } else {
