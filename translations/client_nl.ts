00674b12a7999a6264e2c99a708178fc91c0fd52
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2018-08-29 02:18:38 +0200

    [tx-robot] updated from transifex

--- translations/client_nl.ts
+++ translations/client_nl.ts
@@ -272,7 +272,7 @@
     <message>
         <location filename="../src/gui/accountsettings.cpp" line="671"/>
         <source>Obtaining authorization from the browser. &lt;a href=&apos;%1&apos;&gt;Click here&lt;/a&gt; to re-open the browser.</source>
-        <translation>Probeert autorisatie te krijgen van de browser. 1 Klik hier 1 om de browser opnieuw te openen.</translation>
+        <translation>Probeert autorisatie te krijgen van de browser. &lt;a href=&apos;%1&apos;&gt; Klik hier &lt;/a&gt; om de browser opnieuw te openen.</translation>
     </message>
     <message>
         <location filename="../src/gui/accountsettings.cpp" line="675"/>
