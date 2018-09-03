198b65e9fcf1265f0ef724166367b72e997a511b
Olivier Goffart <ogoffart@woboq.com>
2018-09-03 11:47:15 +0200

    Fix a compilation error on windows

--- src/csync/vio/csync_vio_local_win.cpp
+++ src/csync/vio/csync_vio_local_win.cpp
@@ -33,8 +33,6 @@
 #include "c_lib.h"
 #include "c_utf8.h"
 #include "csync_util.h"
-#include "csync_vio.h"
-
 #include "vio/csync_vio_local.h"
 
 Q_LOGGING_CATEGORY(lcCSyncVIOLocal, "nextcloud.sync.csync.vio_local", QtInfoMsg)
