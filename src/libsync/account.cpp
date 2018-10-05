15eab07866d5feb179e25125673dce51c1dda4fa
Olivier Goffart <ogoffart@woboq.com>
2018-10-05 19:45:43 +0200

    OAuth2: Store 'Account::davUser' in the config, and use that user for connecting
    We need to use the user id to check if we are connected to the right account.
    These might be different from the HTTP Basic Auth login. (LDAP setups)
    
    When the account was configured as an oauth2 account form the wisard, the
    http_user was already set correctly to the user id. But when the server is
    upgrading from basic auth to oauth2, we need to pick the right login.
    
    Note that Account::davUser() already defaults to the HTTP user when none
    is set, so this means the upgrade will be fine if this is not set in the
    config.
    
    Issues:
    https://github.com/owncloud/oauth2/issues/109
    https://github.com/owncloud/enterprise/issues/2781

--- src/libsync/account.cpp
+++ src/libsync/account.cpp
@@ -111,7 +111,10 @@ QString Account::davUser() const
 
 void Account::setDavUser(const QString &newDavUser)
 {
+    if (_davUser == newDavUser)
+        return;
     _davUser = newDavUser;
+    emit wantsAccountSaved(this);
 }
 
 #ifndef TOKEN_AUTH_ONLY
