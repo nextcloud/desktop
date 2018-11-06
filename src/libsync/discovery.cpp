69de2d51800fa9e85e498ed68f7b8a055d08aea1
Olivier Goffart <ogoffart@woboq.com>
2018-11-06 10:30:33 +0100

    Sync: optimize by removing setFileRecordMetadata
    Inh most case we already have a record from before, so avoid doing a useless
    lookup in the database.
    In owncloudpropagator.cpp, directories do not have a checksum so no need
    to call a function that preserves it

--- src/libsync/discovery.cpp
+++ src/libsync/discovery.cpp
@@ -884,7 +884,7 @@ void ProcessDirectoryJob::processFileConflict(const SyncFileItemPtr &item, Proce
             rec._fileSize = serverEntry.size;
             rec._remotePerm = serverEntry.remotePerm;
             rec._checksumHeader = serverEntry.checksumHeader;
-            _discoveryData->_statedb->setFileRecordMetadata(rec);
+            _discoveryData->_statedb->setFileRecord(rec);
         }
         return;
     }
