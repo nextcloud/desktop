5f974b91ac754648a7f3f4a35a653b88313954b5
Camila San <hello@camila.codes>
2018-11-13 19:05:56 +0100

    Refactores selective sync.
    - Integrates sync with currrent FUSE code - still needs fixing.
    - Fixes return of getProcessName function by using QString.
    
    Signed-off-by: Camila San <hello@camila.codes>

--- src/gui/syncwrapper.h
+++ src/gui/syncwrapper.h
@@ -16,21 +16,27 @@ public:
     static SyncWrapper *instance();
     ~SyncWrapper() {}
 
-    QString initSyncMode(const QString path);
     void openFileAtPath(const QString path);
     void releaseFileAtPath(const QString path);
     void writeFileAtPath(const QString path);
-    void updateLocalFileTree(const QString &path, csync_instructions_e instruction);
-    void sync(const QString path);
+    bool syncDone(const QString path);
+
+    void updateLocalFileTree(const QString &path, csync_instructions_e instruction = CSYNC_INSTRUCTION_NONE);
+    void initSyncMode(const QString path);
+    void initSync(const QString path, csync_instructions_e instruction = CSYNC_INSTRUCTION_SYNC);
+    void startSync();
 
 private:
-    bool shouldSync(const QString path);
-    QString removeSlash(QString path);
     QDateTime lastAccess(const QString path);
     int updateLastAccess(const QString path);
 
+    QString removeSlash(QString path);
+
     int syncMode(const QString path);
     int syncModeDownload(const QString path);
+    bool shouldSync(const QString path);
+
+    QMap<QString, bool> _syncDone;
 
 public slots:
     void updateSyncQueue(const QString path, bool syncing);
