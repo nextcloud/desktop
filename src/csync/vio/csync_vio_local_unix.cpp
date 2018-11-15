852f5b1a31f5881d47249bac66673cbb8a020d9c
Christian Kamm <mail@ckamm.de>
2018-11-15 09:27:35 +0100

    vfs: Document ignored return value.

--- src/csync/vio/csync_vio_local_unix.cpp
+++ src/csync/vio/csync_vio_local_unix.cpp
@@ -121,8 +121,11 @@ std::unique_ptr<csync_file_stat_t> csync_vio_local_readdir(csync_vio_handle_t *h
   }
 
   // Override type for virtual files if desired
-  if (vfs)
+  if (vfs) {
+      // Directly modifiest file_stat->type.
+      // We can ignore the return value since we're done here anyway.
       vfs->statTypeVirtualFile(file_stat.get(), nullptr);
+  }
 
   return file_stat;
 }
