f86774059f5922b68aa1ffbe5a26857ef933bfa5
Dominik Schmidt <dev@dominik-schmidt.de>
2019-06-06 11:48:44 +0200

    SocketApi: Avoid unneccessary conversions

--- src/gui/socketapi.cpp
+++ src/gui/socketapi.cpp
@@ -1173,7 +1173,7 @@ void SocketApi::command_ASYNC_GET_WIDGET_PROPERTY(const QSharedPointer<SocketApi
         }
     }
 
-    job->resolve(value.toUtf8().constData());
+    job->resolve(value);
 }
 
 void SocketApi::command_ASYNC_SET_WIDGET_PROPERTY(const QSharedPointer<SocketApiJob> &job)
@@ -1187,7 +1187,8 @@ void SocketApi::command_ASYNC_SET_WIDGET_PROPERTY(const QSharedPointer<SocketApi
         return;
     }
     widget->setProperty(arguments["property"].toString().toUtf8().constData(),
-                        arguments["value"].toString().toUtf8().constData());
+                        arguments["value"]);
+
     job->resolve();
 }
 
