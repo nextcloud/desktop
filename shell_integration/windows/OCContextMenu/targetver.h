a26f2a7359d65d296b56004df7bdf85734a97c2e
Dominique Fuchs <32204802+DominiqueFuchs@users.noreply.github.com>
2019-09-07 21:26:40 +0200

    Removed redundant (and wrong in terms of it's value) definitions for WINVER/_WIN32_WINNT
    Signed-off-by: Dominique Fuchs <32204802+DominiqueFuchs@users.noreply.github.com>
    (cherry picked from commit 69a11a7ec11b720f3c70b40259d56ede785c9b94)
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- shell_integration/windows/OCContextMenu/targetver.h
+++ shell_integration/windows/OCContextMenu/targetver.h
@@ -5,6 +5,8 @@
 // If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
 // set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
 
-#define WINVER 0x0501
-#define _WIN32_WINNT 0x0501
+// Note: Here was a #define for windows target version
+// e.g. WINVER / _WIN32_WINNT, see https://devblogs.microsoft.com/oldnewthing/20070411-00/?p=27283
+// Unnecessary because we define both in desktop/CMakeLists.txt
+
 #include <SDKDDKVer.h>
