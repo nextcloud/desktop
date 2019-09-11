80e812399a8db9cf644daa08494b3909a28c75b2
Christian Kamm <mail@ckamm.de>
2019-09-11 14:55:58 +0200

    Discovery: Change local job signal signatures
    To make it more explicit that data a copy of the data is transfered
    between threads. (though Qt will make a copy of the arguments anyway)

--- src/libsync/discoveryphase.h
+++ src/libsync/discoveryphase.h
@@ -91,11 +91,11 @@ public:
 
     void run() Q_DECL_OVERRIDE;
 signals:
-    void finished(const QVector<LocalInfo> &result);
-    void finishedFatalError(const QString &errorString);
-    void finishedNonFatalError(const QString &errorString);
+    void finished(QVector<LocalInfo> result);
+    void finishedFatalError(QString errorString);
+    void finishedNonFatalError(QString errorString);
 
-    void itemDiscovered(const SyncFileItemPtr &item);
+    void itemDiscovered(SyncFileItemPtr item);
     void childIgnored(bool b);
 private slots:
 private:
