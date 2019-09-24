09062721cde0e159326e03635da995a0597793f9
Dominique Fuchs <32204802+DominiqueFuchs@users.noreply.github.com>
2019-09-24 18:16:27 +0200

    Added dummy implementation of eventfilter for macOS/Linux
    Signed-off-by: Dominique Fuchs <32204802+DominiqueFuchs@users.noreply.github.com>

---
+++ src/gui/eventfilter_mac.cpp
@@ -0,0 +1,16 @@
+#include "eventfilter.h"
+
+#include "../common/utility.h"
+#include "../libsync/configfile.h"
+#include "theme.h"
+
+#include <QDebug>
+
+namespace OCC {
+
+static inline void processPlatformEventMsg(void *message)
+{
+    return;
+}
+
+} //OCC
