a9a731dfc0e3e3bfd4a66294d8271aa2cc9be4c0
Sergey Zolotarev <sryze@protonmail.com>
2019-10-05 22:37:46 +0600

    Don't run connection wizard when quitting the application
    Signed-off-by: Sergey Zolotarev <sryze@protonmail.com>

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -108,6 +108,7 @@ Application::Application(int &argc, char **argv)
     , _userTriggeredConnect(false)
     , _debugMode(false)
     , _backgroundMode(false)
+    , _isQuitting(false)
 {
     _startedAt.start();
 
@@ -285,7 +286,7 @@ void Application::slotAccountStateRemoved(AccountState *accountState)
     }
 
     // if there is no more account, show the wizard.
-    if (AccountManager::instance()->accounts().isEmpty()) {
+    if (!_isQuitting && AccountManager::instance()->accounts().isEmpty()) {
         // allow to add a new account if there is non any more. Always think
         // about single account theming!
         OwncloudSetupWizard::runWizard(this, SLOT(slotownCloudWizardDone(int)));
@@ -308,6 +309,8 @@ void Application::slotAccountStateAdded(AccountState *accountState)
 
 void Application::slotCleanup()
 {
+    _isQuitting = true;
+
     AccountManager::instance()->save();
     FolderMan::instance()->unloadAndDeleteAllFolders();
 
