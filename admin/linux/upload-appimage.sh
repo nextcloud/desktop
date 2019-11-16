cf0082f92fcbad1c43c0b50b7d3a25eb414ea23f
Michael Schuster <michael@schuster.ms>
2019-11-16 21:36:59 +0100

    Make AppImage upload optional and add timeout
    Moves the upload into a separate Drone command and adds a timeout for curl
    to fail after 15 minutes.
    
    Returns zero to keep Drone from failing.
    
    Signed-off-by: Michael Schuster <michael@schuster.ms>
    (cherry picked from commit 4fc8936553dfae28edc0e424408d4366081a7045)
    Signed-off-by: Michael Schuster <michael@schuster.ms>

---
+++ admin/linux/upload-appimage.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -xe
+
+# Upload AppImage
+APPIMAGE=$(readlink -f ./Nextcloud*.AppImage)
+BASENAME=$(basename ${APPIMAGE})
+
+if curl --max-time 900 --upload-file ${APPIMAGE} https://transfer.sh/${BASENAME}
+then
+    echo
+    echo "Get the AppImage at the link above!"
+else
+    echo
+    echo "Upload failed, however this is an optional step."
+fi
+
+# Don't let the Drone build fail
+exit 0
