f17b30515167d3e679f30118e52c41385787faba
Hannah von Reth <hannah.vonreth@owncloud.com>
2019-12-04 11:04:35 +0100

    [CMAKE] Do not create a deployable mac bundle during build
    This simplifies the cmake scripts and reduces build times

--- OwnCloudCPack.cmake
+++ OwnCloudCPack.cmake
@@ -15,30 +15,6 @@ set( CPACK_PACKAGE_VERSION_PATCH  ${MIRALL_VERSION_PATCH} )
 set( CPACK_PACKAGE_VERSION_BUILD  ${MIRALL_VERSION_BUILD} )
 set( CPACK_PACKAGE_VERSION  ${MIRALL_VERSION_FULL}${MIRALL_VERSION_SUFFIX} )
 
-if(APPLE)
-    set( CPACK_GENERATOR "DragNDrop" )
-    set( CPACK_SOURCE_GENERATOR "")
-    set( CPACK_PACKAGE_FILE_NAME ${APPLICATION_SHORTNAME}-${CPACK_PACKAGE_VERSION} )
-    set( CPACK_PACKAGE_ICON ${CMAKE_BINARY_DIR}/src/gui/ownCloud.icns)
-
-    set( CPACK_DMG_DS_STORE "${CMAKE_SOURCE_DIR}/admin/osx/DS_Store.in")
-#    set( CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/admin/osx/DMGBackground.png" )
-
-    set( CPACK_DMG_FORMAT "UDBZ" )
-    set( CPACK_DMG_VOLUME_NAME "${APPLICATION_SHORTNAME}")
-
-    # did not work with cmake 2.8.7, so we override MacOSXBundleInfo.plist.in
-    #set( CPACK_BUNDLE_PLIST ${CMAKE_SOURCE_DIR}/admin/osx/Info.plist )
-
-    # do we need these?
-    #set( CPACK_SYSTEM_NAME "OSX" )
-    #set( CPACK_PACKAGE_NAME "FOO" )
-    #set( CPACK_BUNDLE_NAME "BAR" )
-endif()
-
-
-
-
 if(WIN32)
     set( CPACK_PACKAGE_FILE_NAME  ${APPLICATION_SHORTNAME}-${CPACK_PACKAGE_VERSION}-setup )
     # Package file name without extension. Also a directory of installer  cmake-2.5.0-Linux-i686
