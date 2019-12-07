1b550b976fb7c57628ae8874cd3c06d080434a76
Michael Schuster <michael@schuster.ms>
2019-12-08 00:00:02 +0100

    Remote Wipe: Implement hack to allow deleting all keychain entries (SSL certs & keys)
    Client SSL certificates and keys cannot be deleted at this time because there is
    no UI for selecting them on re-login.
    
    We introduce this dirty hack here, to allow deleting them upon Remote Wipe.
    
    Signed-off-by: Michael Schuster <michael@schuster.ms>
    (cherry picked from commit 0c5f4a15251c64848f3ab1071f8b10c9cb9ad651)
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- src/libsync/account.h
+++ src/libsync/account.h
@@ -323,6 +323,22 @@ private:
     bool _wroteAppPassword = false;
 
     friend class AccountManager;
+
+    /* IMPORTANT - remove later - FIXME MS@2019-12-07 -->
+     * TODO: For "Log out" & "Remove account": Remove client CA certs and KEY!
+     *
+     *       Disabled as long as selecting another cert is not supported by the UI.
+     *
+     *       Being able to specify a new certificate is important anyway: expiry etc.
+     *
+     *       We introduce this dirty hack here, to allow deleting them upon Remote Wipe.
+    */
+    public:
+        void setRemoteWipeRequested_HACK() { _isRemoteWipeRequested_HACK = true; }
+        bool isRemoteWipeRequested_HACK() { return _isRemoteWipeRequested_HACK; }
+    private:
+        bool _isRemoteWipeRequested_HACK = false;
+    // <-- FIXME MS@2019-12-07
 };
 }
 
