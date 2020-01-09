4a1e8202a0ee6b8ed9780464f6a2c059a00a7a85
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-01-09 15:40:54 +0100

    [CMAKE] Use ecm for autotests

--- test/owncloud_add_test.cmake
+++ test/owncloud_add_test.cmake
@@ -1,26 +1,24 @@
 find_package(Qt5 COMPONENTS Core Test Xml Network REQUIRED)
 
-macro(owncloud_add_test test_class additional_cpp)
+include(ECMAddTests)
+
+function(owncloud_add_test test_class additional_cpp)
     set(OWNCLOUD_TEST_CLASS ${test_class})
     string(TOLOWER "${OWNCLOUD_TEST_CLASS}" OWNCLOUD_TEST_CLASS_LOWERCASE)
+    list(APPEND additional_cpp test${OWNCLOUD_TEST_CLASS_LOWERCASE}.cpp)
 
-    add_executable(${OWNCLOUD_TEST_CLASS}Test test${OWNCLOUD_TEST_CLASS_LOWERCASE}.cpp ${additional_cpp})
-    ecm_mark_nongui_executable(${OWNCLOUD_TEST_CLASS}Test)
-
-    target_link_libraries(${OWNCLOUD_TEST_CLASS}Test
+    ecm_add_test("${additional_cpp}"
+        TEST_NAME "${OWNCLOUD_TEST_CLASS}Test"
+        LINK_LIBRARIES
         updater
         ${APPLICATION_EXECUTABLE}sync
         Qt5::Core Qt5::Test Qt5::Xml Qt5::Network
     )
 
-    add_definitions(-DOWNCLOUD_TEST)
-    add_definitions(-DOWNCLOUD_BIN_PATH="${CMAKE_BINARY_DIR}/bin")
-    add_test(NAME ${OWNCLOUD_TEST_CLASS}Test
-        COMMAND ${OWNCLOUD_TEST_CLASS}Test
-        WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+    target_compile_definitions(${OWNCLOUD_TEST_CLASS}Test PRIVATE OWNCLOUD_TEST OWNCLOUD_BIN_PATH="${CMAKE_BINARY_DIR}/bin")
 
     target_include_directories(${OWNCLOUD_TEST_CLASS}Test PRIVATE "${CMAKE_SOURCE_DIR}/test/")
-endmacro()
+endfunction()
 
 macro(owncloud_add_benchmark test_class additional_cpp)
     set(OWNCLOUD_TEST_CLASS ${test_class})
@@ -34,7 +32,5 @@ macro(owncloud_add_benchmark test_class additional_cpp)
         ${APPLICATION_EXECUTABLE}sync
         Qt5::Core Qt5::Test Qt5::Xml Qt5::Network
     )
-
-    add_definitions(-DOWNCLOUD_TEST)
-    add_definitions(-DOWNCLOUD_BIN_PATH="${CMAKE_BINARY_DIR}/bin")
+    target_compile_definitions(${OWNCLOUD_TEST_CLASS}Bench PRIVATE OWNCLOUD_TEST OWNCLOUD_BIN_PATH="${CMAKE_BINARY_DIR}/bin")
 endmacro()
