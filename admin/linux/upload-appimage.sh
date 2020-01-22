c34b4a72dab9376c650e1be105b64447e9809318
tobiasKaminsky <tobias@kaminsky.me>
2020-01-22 09:50:39 +0100

    add appimage link to pr as comment

--- admin/linux/upload-appimage.sh
+++ admin/linux/upload-appimage.sh
@@ -1,17 +1,20 @@
 #! /bin/bash
 
-set -xe
-
 cd /build
 
 # Upload AppImage
 APPIMAGE=$(readlink -f ./Nextcloud*.AppImage)
 BASENAME=$(basename ${APPIMAGE})
 
-if curl --max-time 900 --upload-file ${APPIMAGE} https://transfer.sh/${BASENAME}
+URL=$(curl --max-time 900 --upload-file ${APPIMAGE} https://transfer.sh/${BASENAME})
+STATUS=$?
+
+if [[ $STATUS -eq 0 ]] 
 then
     echo
     echo "Get the AppImage at the link above!"
+    
+    curl -u $2:$3 -X POST https://api.github.com/repos/nextcloud/desktop/issues/$1/comments -d "{ \"body\" : \"AppImage: $URL<br/><br/>To test this change/fix you can simply download above AppImage, install and test it. \" }"
 else
     echo
     echo "Upload failed, however this is an optional step."
