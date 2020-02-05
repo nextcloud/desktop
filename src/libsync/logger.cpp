376522bc34b77685f52e2c3d12b564051bbd1a61
Olivier Goffart <ogoffart@woboq.com>
2020-02-05 12:37:58 +0100

    Logger: Remove the thread pointer from the log.
    We don't do discovery in a thread or use many threads anymore so this is
    just noise.
    
    Also use of QString::sprintf is deprecated, and C cast.

--- src/libsync/logger.cpp
+++ src/libsync/logger.cpp
@@ -18,7 +18,6 @@
 
 #include <QDir>
 #include <QStringList>
-#include <QThread>
 #include <QtGlobal>
 #include <qmetaobject.h>
 
@@ -105,7 +104,6 @@ void Logger::log(Log log)
         msg = log.timeStamp.toString(QLatin1String("MM-dd hh:mm:ss:zzz")) + QLatin1Char(' ');
     }
 
-    msg += QString().sprintf("%p ", (void *)QThread::currentThread());
     msg += log.message;
     // _logs.append(log);
     // std::cout << qPrintable(log.message) << std::endl;
