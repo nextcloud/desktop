be0944ee7c02e9fc9ff247de2cec35307fa2cdd1
Nicolas Fella <nicolas.fella@gmx.de>
2020-03-21 00:20:51 +0100

    Add desktop-entry hint to notifications
    This allows the notification server to map a notification to the program's desktop file, which allows the server to do extra things. For example KDE Plasma adds the Nextcloud icon to the notification and allows to configure notifications from Nextcloud.
    
    This is a standard hint from https://specifications.freedesktop.org/notification-spec/notification-spec-latest.html#hints
    
    Signed-off-by: Nicolas Fella <nicolas.fella@gmx.de>
    (cherry picked from commit 2f54e911b7d23653ca4aa89041e5749726a8c3c5)
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- src/gui/systray.cpp
+++ src/gui/systray.cpp
@@ -31,9 +31,10 @@ namespace OCC {
 void Systray::showMessage(const QString &title, const QString &message, MessageIcon icon, int millisecondsTimeoutHint)
 {
 #ifdef USE_FDO_NOTIFICATIONS
-    if(QDBusInterface(NOTIFICATIONS_SERVICE, NOTIFICATIONS_PATH, NOTIFICATIONS_IFACE).isValid()) {
+    if (QDBusInterface(NOTIFICATIONS_SERVICE, NOTIFICATIONS_PATH, NOTIFICATIONS_IFACE).isValid()) {
+        const QVariantMap hints = {{QStringLiteral("desktop-entry"), QStringLiteral("com.nextcloud.desktopclient.nextcloud")}};
         QList<QVariant> args = QList<QVariant>() << APPLICATION_NAME << quint32(0) << APPLICATION_ICON_NAME
-                                                 << title << message << QStringList() << QVariantMap() << qint32(-1);
+                                                 << title << message << QStringList() << hints << qint32(-1);
         QDBusMessage method = QDBusMessage::createMethodCall(NOTIFICATIONS_SERVICE, NOTIFICATIONS_PATH, NOTIFICATIONS_IFACE, "Notify");
         method.setArguments(args);
         QDBusConnection::sessionBus().asyncCall(method);
