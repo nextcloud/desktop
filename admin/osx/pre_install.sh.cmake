f268bb706899ddba15ceb30aa0da9aadc0e795aa
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-03-30 11:55:55 +0200

    Mac Package: Restart client after update
    Issue: #3922

--- admin/osx/pre_install.sh.cmake
+++ admin/osx/pre_install.sh.cmake
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# kill the old version. see issue #2044
-killall @APPLICATION_EXECUTABLE@
+OC_INSTANCE=$(ps aux | grep "/Applications/@APPLICATION_EXECUTABLE@.app/Contents/MacOS/@APPLICATION_EXECUTABLE@")
+
+if [[ "${OC_INSTANCE}" != "" ]]; then
+   kill $(echo ${OC_INSTANCE} | awk '{print $2}')
+   touch $INSTALLER_TEMP/OC_RESTART_NEEDED
+fi
 
 exit 0
