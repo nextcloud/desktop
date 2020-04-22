5f38cff092efba52e647f7fb57fa9b9792209215
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-04-22 12:08:30 +0200

    Use Q_ENUM_NS for PinState

--- src/common/common.cmake
+++ src/common/common.cmake
@@ -10,6 +10,7 @@ set(common_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/utility.cpp
     ${CMAKE_CURRENT_LIST_DIR}/remotepermissions.cpp
     ${CMAKE_CURRENT_LIST_DIR}/vfs.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/pinstate.cpp
     ${CMAKE_CURRENT_LIST_DIR}/plugin.cpp
     ${CMAKE_CURRENT_LIST_DIR}/syncfilestatus.cpp
 )
