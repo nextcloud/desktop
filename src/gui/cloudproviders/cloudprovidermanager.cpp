b305302b0323ab8bd8de03eaec7123625f48e100
Stephan Beyer <s-beyer@gmx.net>
2020-05-16 01:10:34 +0200

    Fix leak in CloudProviderManager
    The member _map, allocated in the constructor, was leaking.
    
    The ad-hoc way to fix the leak would have been to add a destructor
    that deletes _map.  However, there is no good reason why _map has
    to be a pointer to a QMap (instead of a QMap).
    Hence this commit changes the type of the _map member from a QMap
    pointer to a QMap.
    
    Signed-off-by: Stephan Beyer <s-beyer@gmx.net>
    Signed-off-by: Daniel Vedder <d.vedder@web.de>

--- src/gui/cloudproviders/cloudprovidermanager.cpp
+++ src/gui/cloudproviders/cloudprovidermanager.cpp
@@ -51,29 +51,28 @@ void CloudProviderManager::registerSignals()
 
 CloudProviderManager::CloudProviderManager(QObject *parent) : QObject(parent)
 {
-    _map = new QMap<QString, CloudProviderWrapper*>();
     _folder_index = 0;
     g_bus_own_name (G_BUS_TYPE_SESSION, LIBCLOUDPROVIDERS_DBUS_BUS_NAME, G_BUS_NAME_OWNER_FLAGS_NONE, nullptr, on_name_acquired, nullptr, this, nullptr);
 }
 
 void CloudProviderManager::slotFolderListChanged(const Folder::Map &folderMap)
 {
-    QMapIterator<QString, CloudProviderWrapper*> i(*_map);
+    QMapIterator<QString, CloudProviderWrapper*> i(_map);
     while (i.hasNext()) {
         i.next();
         if (!folderMap.contains(i.key())) {
             cloud_providers_provider_exporter_remove_account(_providerExporter, i.value()->accountExporter());
-            delete _map->find(i.key()).value();
-            _map->remove(i.key());
+            delete _map.find(i.key()).value();
+            _map.remove(i.key());
         }
     }
 
     Folder::MapIterator j(folderMap);
     while (j.hasNext()) {
         j.next();
-        if (!_map->contains(j.key())) {
+        if (!_map.contains(j.key())) {
             auto *cpo = new CloudProviderWrapper(this, j.value(), _folder_index++, _providerExporter);
-            _map->insert(j.key(), cpo);
+            _map.insert(j.key(), cpo);
         }
     }
 }
