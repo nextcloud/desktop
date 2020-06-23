3bbf015a37d350efeb0813c55c50c5184fff8503
Andreas Heimberger <andreasheimberger@bbv.ch>
2020-06-23 11:23:43 +0200

    Issue #2117: some reordering / classifying of cmake variables
    Signed-off-by: aheimberger <andreasheimberger@gmx.at>

--- NEXTCLOUD.cmake
+++ NEXTCLOUD.cmake
@@ -9,29 +9,34 @@ set( APPLICATION_ICON_NAME  "Nextcloud" CACHE STRING "Name of the application ic
 set( APPLICATION_SERVER_URL "" CACHE STRING "URL for the server to use. If entered the server can only connect to this instance" )
 set( APPLICATION_REV_DOMAIN "com.nextcloud.desktopclient" CACHE STRING "Name of the application rev-domain" )
 
+
+# linux specific
 set( LINUX_PACKAGE_SHORTNAME "nextcloud" CACHE STRING "Short name of the linux package" )
 set( LINUX_APPLICATION_ID "${APPLICATION_REV_DOMAIN}.${LINUX_PACKAGE_SHORTNAME}")
 
-set( THEME_CLASS            "NextcloudTheme" CACHE STRING "Name of the theme class" )
+
+# windows specific
 set( WIN_SETUP_BITMAP_PATH  "${CMAKE_SOURCE_DIR}/admin/win/nsi" )
 
+
+# mac specific
 set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
 
-# set( THEME_INCLUDE          "${OEM_THEME_DIR}/mytheme.h" )
-# set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
 
-option( WITH_CRASHREPORTER "Build crashreporter" OFF )
-#set( CRASHREPORTER_SUBMIT_URL "https://crash-reports.owncloud.com/submit" CACHE STRING "URL for crash reporter" )
-#set( CRASHREPORTER_ICON ":/owncloud-icon.png" )
+# crash reporter options
+option( WITH_CRASHREPORTER      "Build crashreporter" OFF)
+#set( CRASHREPORTER_SUBMIT_URL  "https://crash-reports.owncloud.com/submit" CACHE STRING "URL for crash reporter" )
+#set( CRASHREPORTER_ICON        ":/owncloud-icon.png" )
 
-## Updater options
-option( BUILD_UPDATER "Build updater" OFF )
 
+## updater options
+option( BUILD_UPDATER  "Build updater" OFF )
 option( WITH_PROVIDERS "Build with providers list" ON )
 
 
-## Theming options
+## theming options
+set( THEME_CLASS                                "NextcloudTheme" CACHE STRING "Name of the theme class" )
+#set( THEME_INCLUDE                             "${OEM_THEME_DIR}/mytheme.h" )
 set( APPLICATION_WIZARD_HEADER_BACKGROUND_COLOR "#0082c9" CACHE STRING "Hex color of the wizard header background")
-set( APPLICATION_WIZARD_HEADER_TITLE_COLOR "#ffffff" CACHE STRING "Hex color of the text in the wizard header")
-option( APPLICATION_WIZARD_USE_CUSTOM_LOGO "Use the logo from ':/client/theme/colored/wizard_logo.png' else the default application icon is used" ON )
-
+set( APPLICATION_WIZARD_HEADER_TITLE_COLOR      "#ffffff" CACHE STRING "Hex color of the text in the wizard header")
+option( APPLICATION_WIZARD_USE_CUSTOM_LOGO       "Use the logo from ':/client/theme/colored/wizard_logo.png' else the default application icon is used" ON )
