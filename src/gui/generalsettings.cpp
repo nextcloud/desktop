a3a3223b8de80b638842c3503a9758ef501c1b01
Camila <hello@camila.codes>
2020-06-25 17:49:00 +0200

    Fix missing functions declaration and implementations from GeneralSettings.
    This is a consequence of the rebase of the branch of the virtual
    drive: errors commited while fixing merging conflicts.
    
    Signed-off-by: Camila <hello@camila.codes>

--- src/gui/generalsettings.cpp
+++ src/gui/generalsettings.cpp
@@ -20,10 +20,14 @@
 #include "application.h"
 #include "configfile.h"
 #include "owncloudsetupwizard.h"
+#include "accountmanager.h"
 #include "synclogdialog.h"
 
+#if defined(BUILD_UPDATER)
 #include "updater/updater.h"
 #include "updater/ocupdater.h"
+#endif
+
 #include "ignorelisteditor.h"
 #include "common/utility.h"
 
@@ -34,7 +38,12 @@
 #include <QNetworkProxy>
 #include <QDir>
 #include <QScopedValueRollback>
-#include <QStandardPaths>
+
+#define QTLEGACY (QT_VERSION < QT_VERSION_CHECK(5,9,0))
+
+#if !(QTLEGACY)
+#include <QOperatingSystemVersion>
+#endif
 
 #ifdef Q_OS_WIN
 #include "vfs_windows.h"
@@ -44,24 +53,20 @@
 #include "vfs_mac.h"
 #endif
 
-#define QTLEGACY (QT_VERSION < QT_VERSION_CHECK(5,9,0))
-
-#if !(QTLEGACY)
-#include <QOperatingSystemVersion>
-#endif
-
 namespace OCC {
 
 GeneralSettings::GeneralSettings(QWidget *parent)
     : QWidget(parent)
     , _ui(new Ui::GeneralSettings)
-    , _currentlyLoading(false)
 {
     _ui->setupUi(this);
 
     connect(_ui->serverNotificationsCheckBox, &QAbstractButton::toggled,
         this, &GeneralSettings::slotToggleOptionalServerNotifications);
     _ui->serverNotificationsCheckBox->setToolTip(tr("Server notifications that require attention."));
+
+    connect(_ui->showInExplorerNavigationPaneCheckBox, &QAbstractButton::toggled, this, &GeneralSettings::slotShowInExplorerNavigationPane);
+
 
     #ifndef defined(Q_OS_UNIX)
     _ui->virtualFileSystemCheckbox->show();
@@ -71,7 +76,12 @@ GeneralSettings::GeneralSettings(QWidget *parent)
     _ui->virtualFileSystemCheckbox->setToolTip(tr("Sync files on demand."));
     #endif
 
-    connect(_ui->showInExplorerNavigationPaneCheckBox, &QAbstractButton::toggled, this, &GeneralSettings::slotShowInExplorerNavigationPane);
+    // Rename 'Explorer' appropriately on non-Windows
+#ifdef Q_OS_MAC
+    QString txt = _ui->showInExplorerNavigationPaneCheckBox->text();
+    txt.replace(QString::fromLatin1("Explorer"), QString::fromLatin1("Finder"));
+    _ui->showInExplorerNavigationPaneCheckBox->setText(txt);
+#endif
 
     _ui->autostartCheckBox->setChecked(Utility::hasLaunchOnStartup(Theme::instance()->appName()));
     connect(_ui->autostartCheckBox, &QAbstractButton::toggled, this, &GeneralSettings::slotToggleLaunchOnStartup);
@@ -86,7 +96,8 @@ GeneralSettings::GeneralSettings(QWidget *parent)
     connect(_ui->legalNoticeButton, &QPushButton::clicked, this, &GeneralSettings::slotShowLegalNotice);
 
     loadMiscSettings();
-    slotUpdateInfo();
+    // updater info now set in: customizeStyle
+    //slotUpdateInfo();
 
     // misc
     connect(_ui->monoIconsCheckBox, &QAbstractButton::toggled, this, &GeneralSettings::saveMiscSettings);
@@ -108,12 +119,18 @@ GeneralSettings::GeneralSettings(QWidget *parent)
         if (QOperatingSystemVersion::current() < QOperatingSystemVersion::Windows10)
     #endif
             _ui->showInExplorerNavigationPaneCheckBox->setVisible(false);
+#else
+    // Hide on non-Windows
+    _ui->showInExplorerNavigationPaneCheckBox->setVisible(false);
 #endif
 
     /* Set the left contents margin of the layout to zero to make the checkboxes
      * align properly vertically , fixes bug #3758
      */
-    int m0, m1, m2, m3;
+    int m0 = 0;
+    int m1 = 0;
+    int m2 = 0;
+    int m3 = 0;
     _ui->horizontalLayout_3->getContentsMargins(&m0, &m1, &m2, &m3);
     _ui->horizontalLayout_3->setContentsMargins(0, m1, m2, m3);
 
@@ -125,6 +142,8 @@ GeneralSettings::GeneralSettings(QWidget *parent)
 
     // accountAdded means the wizard was finished and the wizard might change some options.
     connect(AccountManager::instance(), &AccountManager::accountAdded, this, &GeneralSettings::loadMiscSettings);
+
+    customizeStyle();
 }
 
 GeneralSettings::~GeneralSettings()
@@ -135,7 +154,10 @@ GeneralSettings::~GeneralSettings()
 
 QSize GeneralSettings::sizeHint() const
 {
-    return QSize(ownCloudGui::settingsDialogSize().width(), QWidget::sizeHint().height());
+    return {
+        ownCloudGui::settingsDialogSize().width(),
+        QWidget::sizeHint().height()
+    };
 }
 
 void GeneralSettings::loadMiscSettings()
@@ -153,10 +175,11 @@ void GeneralSettings::loadMiscSettings()
     _ui->monoIconsCheckBox->setChecked(cfgFile.monoIcons());
 }
 
+#if defined(BUILD_UPDATER)
 void GeneralSettings::slotUpdateInfo()
 {
     // Note: the sparkle-updater is not an OCUpdater
-    OCUpdater *updater = qobject_cast<OCUpdater *>(Updater::instance());
+    auto *updater = qobject_cast<OCUpdater *>(Updater::instance());
     if (ConfigFile().skipUpdateCheck()) {
         updater = nullptr; // don't show update info if updates are disabled
     }
@@ -165,14 +188,47 @@ void GeneralSettings::slotUpdateInfo()
         connect(updater, &OCUpdater::downloadStateChanged, this, &GeneralSettings::slotUpdateInfo, Qt::UniqueConnection);
         connect(_ui->restartButton, &QAbstractButton::clicked, updater, &OCUpdater::slotStartInstaller, Qt::UniqueConnection);
         connect(_ui->restartButton, &QAbstractButton::clicked, qApp, &QApplication::quit, Qt::UniqueConnection);
+        connect(_ui->updateButton, &QAbstractButton::clicked, this, &GeneralSettings::slotUpdateCheckNow, Qt::UniqueConnection);
+        connect(_ui->autoCheckForUpdatesCheckBox, &QAbstractButton::toggled, this, &GeneralSettings::slotToggleAutoUpdateCheck);
 
-        _ui->updateStateLabel->setText(updater->statusString());
+        QString status = updater->statusString();
+        Theme::replaceLinkColorStringBackgroundAware(status);
+        _ui->updateStateLabel->setText(status);
+
         _ui->restartButton->setVisible(updater->downloadState() == OCUpdater::DownloadComplete);
+
+        _ui->updateButton->setEnabled(updater->downloadState() != OCUpdater::CheckingServer &&
+                                      updater->downloadState() != OCUpdater::Downloading &&
+                                      updater->downloadState() != OCUpdater::DownloadComplete);
+
+        _ui->autoCheckForUpdatesCheckBox->setChecked(ConfigFile().autoUpdateCheck());
     } else {
         // can't have those infos from sparkle currently
         _ui->updatesGroupBox->setVisible(false);
     }
 }
+
+void GeneralSettings::slotUpdateCheckNow()
+{
+    auto *updater = qobject_cast<OCUpdater *>(Updater::instance());
+    if (ConfigFile().skipUpdateCheck()) {
+        updater = nullptr; // don't show update info if updates are disabled
+    }
+
+    if (updater) {
+        _ui->updateButton->setEnabled(false);
+
+        updater->checkForUpdate();
+    }
+}
+
+void GeneralSettings::slotToggleAutoUpdateCheck()
+{
+    ConfigFile cfgFile;
+    bool isChecked = _ui->autoCheckForUpdatesCheckBox->isChecked();
+    cfgFile.setAutoUpdateCheck(isChecked, QString());
+}
+#endif // defined(BUILD_UPDATER)
 
 void GeneralSettings::saveMiscSettings()
 {
@@ -199,6 +255,53 @@ void GeneralSettings::slotToggleOptionalServerNotifications(bool enable)
 {
     ConfigFile cfgFile;
     cfgFile.setOptionalServerNotifications(enable);
+}
+
+void GeneralSettings::slotShowInExplorerNavigationPane(bool checked)
+{
+    ConfigFile cfgFile;
+    cfgFile.setShowInExplorerNavigationPane(checked);
+    // Now update the registry with the change.
+    FolderMan::instance()->navigationPaneHelper().setShowInExplorerNavigationPane(checked);
+}
+
+void GeneralSettings::slotIgnoreFilesEditor()
+{
+    if (_ignoreEditor.isNull()) {
+        ConfigFile cfgFile;
+        _ignoreEditor = new IgnoreListEditor(this);
+        _ignoreEditor->setAttribute(Qt::WA_DeleteOnClose, true);
+        _ignoreEditor->open();
+    } else {
+        ownCloudGui::raiseDialog(_ignoreEditor);
+    }
+}
+
+void GeneralSettings::slotShowLegalNotice()
+{
+    auto notice = new LegalNotice();
+    notice->exec();
+    delete notice;
+}
+
+void GeneralSettings::slotStyleChanged()
+{
+    customizeStyle();
+}
+
+void GeneralSettings::customizeStyle()
+{
+    // setup about section
+    QString about = Theme::instance()->about();
+    Theme::replaceLinkColorStringBackgroundAware(about);
+    _ui->aboutLabel->setText(about);
+
+#if defined(BUILD_UPDATER)
+    // updater info
+    slotUpdateInfo();
+#else
+    _ui->updatesGroupBox->setVisible(false);
+#endif
 }
 
 void GeneralSettings::slotToggleOptionalVirtualFileSystem(bool enable)
@@ -252,34 +355,6 @@ void GeneralSettings::slotToggleOptionalVirtualFileSystem(bool enable)
 		VfsWindows::instance()->unmount();
 	}
 #endif
-}
-
-
-void GeneralSettings::slotShowInExplorerNavigationPane(bool checked)
-{
-    ConfigFile cfgFile;
-    cfgFile.setShowInExplorerNavigationPane(checked);
-    // Now update the registry with the change.
-    FolderMan::instance()->navigationPaneHelper().setShowInExplorerNavigationPane(checked);
-}
-
-void GeneralSettings::slotIgnoreFilesEditor()
-{
-    if (_ignoreEditor.isNull()) {
-        ConfigFile cfgFile;
-        _ignoreEditor = new IgnoreListEditor(this);
-        _ignoreEditor->setAttribute(Qt::WA_DeleteOnClose, true);
-        _ignoreEditor->open();
-    } else {
-        ownCloudGui::raiseDialog(_ignoreEditor);
-    }
-}
-
-void GeneralSettings::slotShowLegalNotice()
-{
-    auto notice = new LegalNotice();
-    notice->exec();
-    delete notice;
 }
 
 } // namespace OCC
--- src/gui/generalsettings.h
+++ src/gui/generalsettings.h
@@ -47,7 +47,6 @@ private slots:
     void saveMiscSettings();
     void slotToggleLaunchOnStartup(bool);
     void slotToggleOptionalServerNotifications(bool);
-	void slotToggleOptionalVirtualFileSystem(bool);
     void slotShowInExplorerNavigationPane(bool);
     void slotIgnoreFilesEditor();
     void loadMiscSettings();
@@ -57,6 +56,7 @@ private slots:
     void slotUpdateCheckNow();
     void slotToggleAutoUpdateCheck();
 #endif
+	void slotToggleOptionalVirtualFileSystem(bool);
 
 signals:
 	void mountVirtualDrive(AccountState *accountState);
