nextcloud/desktop

commit 07bede8a562a07555ab093458d596260ca8c9749

Use properties to get user details
Convert imperative QML code to declarative code using property bindings

Signed-off-by: Nicolas Fella <nicolas.fella@gmx.de>

src/gui/tray/UserModel.cpp
@@ -42,6 +42,9 @@ User::User(AccountStatePtr &account, const bool &isCurrent, QObject *parent)
             [=]() { if (isConnected()) {slotRefresh();} });
     connect(_account.data(), &AccountState::hasFetchedNavigationApps,
         this, &User::slotRebuildNavigationAppList);
+    connect(_account->account().data(), &Account::accountChangedDisplayName, this, &User::nameChanged);
+
+    connect(FolderMan::instance(), &FolderMan::folderListChanged, this, &User::hasLocalFolderChanged);
 }
 
 void User::slotBuildNotificationDisplay(const ActivityList &list)
@@ -149,6 +152,7 @@ void User::slotRefreshNotifications()
 
 void User::slotRebuildNavigationAppList()
 {
+    emit serverHasTalkChanged();
     // Rebuild App list
     UserAppsModel::instance()->buildAppList();
 }
@@ -399,7 +403,7 @@ void User::setCurrentUser(const bool &isCurrent)
     _isCurrentUser = isCurrent;
 }
 
-Folder *User::getFolder()
+Folder *User::getFolder() const
 {
     foreach (Folder *folder, FolderMan::instance()->map()) {
         if (folder->accountState() == _account.data()) {
@@ -470,6 +474,11 @@ QImage User::avatar(bool whiteBg) const
     } else {
         return img;
     }
+}
+
+bool User::hasLocalFolder() const
+{
+    return getFolder() != nullptr;
 }
 
 bool User::serverHasTalk() const
@@ -544,7 +553,7 @@ Q_INVOKABLE int UserModel::numUsers()
     return _users.size();
 }
 
-Q_INVOKABLE int UserModel::currentUserId()
+Q_INVOKABLE int UserModel::currentUserId() const
 {
     return _currentUserId;
 }
@@ -577,15 +586,6 @@ Q_INVOKABLE QImage UserModel::currentUserAvatar()
 QImage UserModel::avatarById(const int &id)
 {
     return _users[id]->avatar(true);
-}
-
-Q_INVOKABLE QString UserModel::currentUserName()
-{
-    if (_users.count() >= 1) {
-        return _users[_currentUserId]->name();
-    } else {
-        return QString("No users");
-    }
 }
 
 Q_INVOKABLE QString UserModel::currentUserServer()
@@ -780,6 +780,11 @@ AccountAppList UserModel::appList() const
     } else {
         return AccountAppList();
     }
+}
+
+User *UserModel::currentUser() const
+{
+    return _users[currentUserId()];
 }
 
 /*-------------------------------------------------------------------------------------*/

src/gui/tray/UserModel.h
@@ -17,6 +17,10 @@ namespace OCC {
 class User : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QString name READ name NOTIFY nameChanged)
+    Q_PROPERTY(QString server READ server CONSTANT)
+    Q_PROPERTY(bool hasLocalFolder READ hasLocalFolder NOTIFY hasLocalFolderChanged)
+    Q_PROPERTY(bool serverHasTalk READ serverHasTalk NOTIFY serverHasTalkChanged)
 public:
     User(AccountStatePtr &account, const bool &isCurrent = false, QObject* parent = 0);
 
@@ -25,11 +29,12 @@ public:
     bool isConnected() const;
     bool isCurrentUser() const;
     void setCurrentUser(const bool &isCurrent);
-    Folder *getFolder();
+    Folder *getFolder() const;
     ActivityListModel *getActivityModel();
     void openLocalFolder();
     QString name() const;
     QString server(bool shortened = true) const;
+    bool hasLocalFolder() const;
     bool serverHasTalk() const;
     bool hasActivities() const;
     AccountAppList appList() const;
@@ -41,6 +46,9 @@ public:
 
 signals:
     void guiLog(const QString &, const QString &);
+    void nameChanged();
+    void hasLocalFolderChanged();
+    void serverHasTalkChanged();
 
 public slots:
     void slotItemCompleted(const QString &folder, const SyncFileItemPtr &item);
@@ -79,6 +87,7 @@ private:
 class UserModel : public QAbstractListModel
 {
     Q_OBJECT
+    Q_PROPERTY(User* currentUser READ currentUser NOTIFY newUserSelected)
 public:
     static UserModel *instance();
     virtual ~UserModel() {};
@@ -92,18 +101,19 @@ public:
 
     QImage avatarById(const int &id);
 
+    User *currentUser() const;
+
     Q_INVOKABLE void fetchCurrentActivityModel();
     Q_INVOKABLE void openCurrentAccountLocalFolder();
     Q_INVOKABLE void openCurrentAccountTalk();
     Q_INVOKABLE void openCurrentAccountServer();
     Q_INVOKABLE QImage currentUserAvatar();
     Q_INVOKABLE int numUsers();
-    Q_INVOKABLE QString currentUserName();
     Q_INVOKABLE QString currentUserServer();
     Q_INVOKABLE bool currentUserHasActivities();
     Q_INVOKABLE bool currentUserHasLocalFolder();
     Q_INVOKABLE bool currentServerHasTalk();
-    Q_INVOKABLE int currentUserId();
+    Q_INVOKABLE int currentUserId() const;
     Q_INVOKABLE bool isUserConnected(const int &id);
     Q_INVOKABLE void switchCurrentUser(const int &id);
     Q_INVOKABLE void login(const int &id);

src/gui/tray/Window.qml
@@ -30,10 +30,6 @@ Window {
     onVisibleChanged: {
         currentAccountAvatar.source = ""
         currentAccountAvatar.source = "image://avatars/currentUser"
-        currentAccountUser.text = UserModel.currentUserName();
-        currentAccountServer.text = UserModel.currentUserServer();
-        openLocalFolderButton.visible = UserModel.currentUserHasLocalFolder();
-        trayWindowTalkButton.visible = UserModel.currentServerHasTalk();
         currentAccountStateIndicator.source = ""
         currentAccountStateIndicator.source = UserModel.isUserConnected(UserModel.currentUserId()) ? "qrc:///client/theme/colored/state-ok.svg" : "qrc:///client/theme/colored/state-offline.svg"
 
@@ -48,15 +44,11 @@ Window {
         onRefreshCurrentUserGui: {
             currentAccountAvatar.source = ""
             currentAccountAvatar.source = "image://avatars/currentUser"
-            currentAccountUser.text = UserModel.currentUserName();
-            currentAccountServer.text = UserModel.currentUserServer();
             currentAccountStateIndicator.source = ""
             currentAccountStateIndicator.source = UserModel.isUserConnected(UserModel.currentUserId()) ? "qrc:///client/theme/colored/state-ok.svg" : "qrc:///client/theme/colored/state-offline.svg"
         }
         onNewUserSelected: {
             accountMenu.close();
-            openLocalFolderButton.visible = UserModel.currentUserHasLocalFolder();
-            trayWindowTalkButton.visible = UserModel.currentServerHasTalk();
         }
     }
 
@@ -327,8 +319,9 @@ Window {
                             Layout.leftMargin: 6
                             Label {
                                 id: currentAccountUser
+
                                 width: Style.currentAccountLabelWidth
-                                text: UserModel.currentUserName()
+                                text: UserModel.currentUser.name
                                 elide: Text.ElideRight
                                 color: "white"
                                 font.pixelSize: Style.topLinePixelSize
@@ -337,7 +330,7 @@ Window {
                             Label {
                                 id: currentAccountServer
                                 width: Style.currentAccountLabelWidth
-                                text: UserModel.currentUserServer()
+                                text: UserModel.currentUser.server
                                 elide: Text.ElideRight
                                 color: "white"
                                 font.pixelSize: Style.subLinePixelSize
@@ -364,7 +357,7 @@ Window {
                 HeaderButton {
                     id: openLocalFolderButton
 
-                    visible: UserModel.currentUserHasLocalFolder()
+                    visible: UserModel.currentUser.hasLocalFolder
                     icon.source: "qrc:///client/theme/white/folder.svg"
                     onClicked: UserModel.openCurrentAccountLocalFolder()
                 }
@@ -372,7 +365,7 @@ Window {
                 HeaderButton {
                     id: trayWindowTalkButton
 
-                    visible: UserModel.currentServerHasTalk()
+                    visible: UserModel.currentUser.serverHasTalk
                     icon.source: "qrc:///client/theme/white/talk-app.svg"
                     onClicked: UserModel.openCurrentAccountTalk()
                 }

commit d59cb962573564b6afc1546305920bc74c9ab4f0

Fix crash in UserModel::currentUser() and add more sanity checks
Commit 07bede8 (PR #1892) introduced a new helper method currentUser()
that didn't check for _users.count() thus causing to throw an
"index out of range" exception when no accounts are configured.

This commit uses the opportunity to add more sanity checks to UserModel.

Signed-off-by: Michael Schuster <michael@schuster.ms>

src/gui/tray/UserModel.cpp
@@ -560,17 +560,15 @@ Q_INVOKABLE int UserModel::currentUserId() const
 
 Q_INVOKABLE bool UserModel::isUserConnected(const int &id)
 {
-    if (!_users.isEmpty()) {
-        return _users[id]->isConnected();
-    } else {
+    if (_users.isEmpty())
         return false;
-    }
 
+    return _users[id]->isConnected();
 }
 
 Q_INVOKABLE QImage UserModel::currentUserAvatar()
 {
-    if (_users.count() >= 1) {
+    if (!_users.isEmpty()) {
         return _users[_currentUserId]->avatar();
     } else {
         QImage image(128, 128, QImage::Format_ARGB32);
@@ -585,25 +583,26 @@ Q_INVOKABLE QImage UserModel::currentUserAvatar()
 
 QImage UserModel::avatarById(const int &id)
 {
+    if (_users.isEmpty())
+        return {};
+
     return _users[id]->avatar(true);
 }
 
 Q_INVOKABLE QString UserModel::currentUserServer()
 {
-    if (_users.count() >= 1) {
-        return _users[_currentUserId]->server();
-    } else {
-        return QString("");
-    }
+    if (_users.isEmpty())
+        return {};
+
+    return _users[_currentUserId]->server();
 }
 
 Q_INVOKABLE bool UserModel::currentServerHasTalk()
 {
-    if (_users.count() >= 1) {
-        return _users[_currentUserId]->serverHasTalk();
-    } else {
+    if (_users.isEmpty())
         return false;
-    }
+
+    return _users[_currentUserId]->serverHasTalk();
 }
 
 void UserModel::addUser(AccountStatePtr &user, const bool &isCurrent)
@@ -636,11 +635,17 @@ int UserModel::currentUserIndex()
 
 Q_INVOKABLE void UserModel::openCurrentAccountLocalFolder()
 {
+    if (_users.isEmpty())
+        return;
+
     _users[_currentUserId]->openLocalFolder();
 }
 
 Q_INVOKABLE void UserModel::openCurrentAccountTalk()
 {
+    if (_users.isEmpty())
+        return;
+
     QString url = _users[_currentUserId]->server(false) + "/apps/spreed";
     if (!(url.contains("http://") || url.contains("https://"))) {
         url = "https://" + _users[_currentUserId]->server(false) + "/apps/spreed";
@@ -651,7 +656,9 @@ Q_INVOKABLE void UserModel::openCurrentAccountTalk()
 Q_INVOKABLE void UserModel::openCurrentAccountServer()
 {
     // Don't open this URL when the QML appMenu pops up on click (see Window.qml)
-    if(appList().count() > 0)
+    if (appList().count() > 0)
+        return;
+    if (_users.isEmpty())
         return;
 
     QString url = _users[_currentUserId]->server(false);
@@ -663,6 +670,9 @@ Q_INVOKABLE void UserModel::openCurrentAccountServer()
 
 Q_INVOKABLE void UserModel::switchCurrentUser(const int &id)
 {
+    if (_users.isEmpty())
+        return;
+
     _users[_currentUserId]->setCurrentUser(false);
     _users[id]->setCurrentUser(true);
     _currentUserId = id;
@@ -672,18 +682,27 @@ Q_INVOKABLE void UserModel::switchCurrentUser(const int &id)
 
 Q_INVOKABLE void UserModel::login(const int &id)
 {
+    if (_users.isEmpty())
+        return;
+
     _users[id]->login();
     emit refreshCurrentUserGui();
 }
 
 Q_INVOKABLE void UserModel::logout(const int &id)
 {
+    if (_users.isEmpty())
+        return;
+
     _users[id]->logout();
     emit refreshCurrentUserGui();
 }
 
 Q_INVOKABLE void UserModel::removeAccount(const int &id)
 {
+    if (_users.isEmpty())
+        return;
+
     QMessageBox messageBox(QMessageBox::Question,
         tr("Confirm Account Removal"),
         tr("<p>Do you really want to remove the connection to the account <i>%1</i>?</p>"
@@ -755,35 +774,47 @@ QHash<int, QByteArray> UserModel::roleNames() const
 
 ActivityListModel *UserModel::currentActivityModel()
 {
+    if (_users.isEmpty())
+        return nullptr;
+
     return _users[currentUserIndex()]->getActivityModel();
 }
 
 bool UserModel::currentUserHasActivities()
 {
+    if (_users.isEmpty())
+        return false;
+
     return _users[currentUserIndex()]->hasActivities();
 }
 
 bool UserModel::currentUserHasLocalFolder()
 {
+    if (_users.isEmpty())
+        return false;
+
     return _users[currentUserIndex()]->getFolder() != nullptr;
 }
 
 void UserModel::fetchCurrentActivityModel()
 {
-    _users[currentUserId()]->slotRefresh();
+    if (!_users.isEmpty())
+        _users[currentUserId()]->slotRefresh();
 }
 
 AccountAppList UserModel::appList() const
 {
-    if (_users.count() > 0) {
-        return _users[_currentUserId]->appList();
-    } else {
+    if (_users.isEmpty())
         return AccountAppList();
-    }
+
+    return _users[_currentUserId]->appList();
 }
 
 User *UserModel::currentUser() const
 {
+    if (_users.isEmpty())
+        return nullptr;
+
     return _users[currentUserId()];
 }
 
@@ -832,10 +863,10 @@ void UserAppsModel::buildAppList()
         endRemoveRows();
     }
 
-    if(UserModel::instance()->appList().count() > 0) {
-        foreach(AccountApp *app, UserModel::instance()->appList()) {
+    if (UserModel::instance()->appList().count() > 0) {
+        foreach (AccountApp *app, UserModel::instance()->appList()) {
             // Filter out Talk because we have a dedicated button for it
-            if(app->id() == QLatin1String("spreed"))
+            if (app->id() == QLatin1String("spreed"))
                 continue;
 
             beginInsertRows(QModelIndex(), rowCount(), rowCount());