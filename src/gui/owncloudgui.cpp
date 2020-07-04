6976580e93eac96bfc54e0b487cfaa5936897160
Michael Schuster <michael@schuster.ms>
2020-07-04 05:07:02 +0200

    Modify ownCloudGui::slotOpenMainDialog to show the appropriate window(s)
    The previous implementation only considered showing the Tray window, which may lead
    to undesired behaviour:
    
    - If there are no accounts configured, the wizard should be shown instead
    - Other open windows would not be shown on second app invocation (Settings, ShareDialog)
    
    This commit simply changes the call to ownCloudGui::slotTrayClicked, so if the user
    starts the app a second time (e.g. clicking on the Dock icon), the outcome will be like
    a click on the Tray icon.
    
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- src/gui/owncloudgui.cpp
+++ src/gui/owncloudgui.cpp
@@ -163,9 +163,7 @@ void ownCloudGui::slotOpenSettingsDialog()
 
 void ownCloudGui::slotOpenMainDialog()
 {
-    if (!_tray->isOpen()) {
-        _tray->showWindow();
-    }
+    slotTrayClicked(QSystemTrayIcon::Trigger);
 }
 
 void ownCloudGui::slotTrayClicked(QSystemTrayIcon::ActivationReason reason)
@@ -187,7 +185,6 @@ void ownCloudGui::slotTrayClicked(QSystemTrayIcon::ActivationReason reason)
             } else {
                 _tray->showWindow();
             }
-
         }
     }
     // FIXME: Also make sure that any auto updater dialogue https://github.com/owncloud/client/issues/5613
