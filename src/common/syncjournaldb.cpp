3caf8276a290da23c4f1af05013dc4f7cc56d2e9
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-08-11 10:30:19 +0200

    Remove pointless warning
    We also don't have a version entry in the db if thne db is new.
    As I don't expect regular updates from 1.5 this message can just be removed.
    
    Fixes: #8004

--- src/common/syncjournaldb.cpp
+++ src/common/syncjournaldb.cpp
@@ -474,7 +474,7 @@ bool SyncJournalDb::checkConnect()
                         "name TEXT UNIQUE"
                         ");");
     if (!createQuery.exec()) {
-        return sqlFail(QStringLiteral("Create table version"), createQuery);
+        return sqlFail(QStringLiteral("Create table checksumtype"), createQuery);
     }
 
     // create the datafingerprint table.
@@ -519,8 +519,6 @@ bool SyncJournalDb::checkConnect()
 
     SqlQuery versionQuery("SELECT major, minor, patch FROM version;", _db);
     if (!versionQuery.next().hasData) {
-        // If there was no entry in the table, it means we are likely upgrading from 1.5
-        qCInfo(lcDb) << "possibleUpgradeFromMirall_1_5 detected!";
         forceRemoteDiscovery = true;
 
         createQuery.prepare("INSERT INTO version VALUES (?1, ?2, ?3, ?4);");
