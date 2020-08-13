a5b4922a0e7b60cdcb2ea4a380c1be9f75285464
Kevin Ottens <kevin.ottens@nextcloud.com>
2020-08-13 14:23:18 +0200

    Remove unused method
    Signed-off-by: Kevin Ottens <kevin.ottens@nextcloud.com>

--- src/gui/folderstatusmodel.cpp
+++ src/gui/folderstatusmodel.cpp
@@ -409,24 +409,6 @@ FolderStatusModel::SubFolderInfo *FolderStatusModel::infoForIndex(const QModelIn
     }
 }
 
-/* Recursivelly traverse the file info looking for the id */
-FolderStatusModel::SubFolderInfo *FolderStatusModel::infoForFileId(const QByteArray& fileId, SubFolderInfo* info) const
-{
-  const QVector<SubFolderInfo>& infoVec = info ? info->_subs : _folders;
-  for(int i = 0, end = infoVec.size(); i < end; i++) {
-    auto *info = const_cast<SubFolderInfo *>(&infoVec[i]);
-    if (info->_fileId == fileId) {
-      return info;
-    } else if (info->_subs.size()) {
-      if (auto *subInfo = infoForFileId(fileId, info)) {
-        return subInfo;
-      }
-    }
-  }
-
-  return nullptr;
-}
-
 QModelIndex FolderStatusModel::indexForPath(Folder *f, const QString &path) const
 {
     if (!f) {
