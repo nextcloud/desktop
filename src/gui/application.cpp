1642efd84c201e372599c632d4a44e8f43c6aafe
Kevin Ottens <kevin.ottens@nextcloud.com>
2020-10-05 19:34:46 +0200

    Make sure --logfile has precedence on config
    Since we changed the default in the config file and since log dir had
    precedence on log file, the --logfile command line option wasn't doing
    anything anymore.
    
    We make sure it has an effect again overriding --logdir or the logDir
    config entry.
    
    Signed-off-by: Kevin Ottens <kevin.ottens@nextcloud.com>

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -408,7 +408,9 @@ void Application::setupLogging()
     // might be called from second instance
     auto logger = Logger::instance();
     logger->setLogFile(_logFile);
-    logger->setLogDir(!_logDir.isEmpty() ? _logDir : ConfigFile().logDir());
+    if (_logFile.isEmpty()) {
+        logger->setLogDir(_logDir.isEmpty() ? ConfigFile().logDir() : _logDir);
+    }
     logger->setLogExpire(_logExpire > 0 ? _logExpire : ConfigFile().logExpire());
     logger->setLogFlush(_logFlush || ConfigFile().logFlush());
     logger->setLogDebug(_logDebug || ConfigFile().logDebug());
