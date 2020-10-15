6979b12488bd3841c2e7135bdb9a39e16e69b9c0
Michael Schuster <michael@schuster.ms>
2020-10-15 02:23:49 +0200

    [stable-3.0] Update comments
    Everyone uses their private mail, so ensure people can get in touch.
    
    Signed-off-by: Michael Schuster <michael@schuster.ms>

--- src/gui/headerbanner.h
+++ src/gui/headerbanner.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) by Michael Schuster <michael@nextcloud.com>
+ * Copyright (C) by Michael Schuster <michael@schuster.ms>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
