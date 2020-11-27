5927beb08d3e96238df573b630057ea0da44de8e
Hannah von Reth <hannah.vonreth@owncloud.com>
2020-11-27 10:03:54 +0100

    Fix potential null pointer access
    https://sentry.io/organizations/owncloud/issues/1529161263/events/02509984b5ca42ffb3960d9c9e161414/?project=79001&statsPeriod=14d

--- src/libsync/discoveryphase.h
+++ src/libsync/discoveryphase.h
@@ -157,7 +157,7 @@ class DiscoveryPhase : public QObject
 
     friend class ProcessDirectoryJob;
 
-    ProcessDirectoryJob *_currentRootJob = nullptr;
+    QPointer<ProcessDirectoryJob> _currentRootJob;
 
     /** Maps the db-path of a deleted item to its SyncFileItem.
      *
