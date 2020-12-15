owncloud/client

VirtualFiles: Ensure the target location supports vfs
Fixes: #8131

src/gui/socketapi.cpp
@@ -1028,7 +1028,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
 
     // File availability actions
     if (folder
-        && folder->supportsVirtualFiles()
+        && folder->virtualFilesEnabled()
         && folder->vfs().socketApiPinStateActionsShown()) {
         OC_ENFORCE(!files.isEmpty());
 

nextcloud/desktop

src/gui/socketapi.cpp
@@ -1107,7 +1107,7 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
 
     // File availability actions
     if (syncFolder
-        && syncFolder->supportsVirtualFiles()
+        && syncFolder->virtualFilesEnabled()
         && syncFolder->vfs().socketApiPinStateActionsShown()) {
         ENFORCE(!files.isEmpty());
 