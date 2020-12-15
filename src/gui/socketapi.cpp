Bhanditz/client

Vfs: Add option to hide socketapi pin actions
Because some plugins provide alternative ui.

src/gui/socketapi.cpp
@@ -903,7 +903,9 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
     }
 
     // File availability actions
-    if (folder && folder->supportsVirtualFiles()) {
+    if (folder
+        && folder->supportsVirtualFiles()
+        && folder->vfs().socketApiPinStateActionsShown()) {
         bool hasAlwaysLocal = false;
         bool hasOnlineOnly = false;
         bool hasHydratedOnlineOnly = false;

nextcloud/desktop

src/gui/socketapi.cpp
@@ -1011,7 +1011,9 @@ void SocketApi::command_GET_MENU_ITEMS(const QString &argument, OCC::SocketListe
     }
 
     // File availability actions
-    if (syncFolder && syncFolder->supportsVirtualFiles()) {
+    if (syncFolder
+        && syncFolder->supportsVirtualFiles()
+        && syncFolder->vfs().socketApiPinStateActionsShown()) {
         bool hasAlwaysLocal = false;
         bool hasOnlineOnly = false;
         bool hasHydratedOnlineOnly = false;