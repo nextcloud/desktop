owncloud/client

Ui: Hide selective sync settings if vfs is active
Fixes: #7976

src/gui/accountsettings.cpp
@@ -589,6 +589,7 @@ void AccountSettings::slotEnableVfsCurrentFolder()
             FolderMan::instance()->scheduleFolder(folder);
 
             ui->_folderList->doItemsLayout();
+            ui->selectiveSyncStatus->setVisible(false);
         };
 
         if (folder->isSyncRunning()) {

nextcloud/desktop

src/gui/accountsettings.cpp
@@ -703,6 +703,7 @@ void AccountSettings::slotEnableVfsCurrentFolder()
             FolderMan::instance()->scheduleFolder(folder);
 
             _ui->_folderList->doItemsLayout();
+            _ui->selectiveSyncStatus->setVisible(false);
         };
 
         if (folder->isSyncRunning()) {