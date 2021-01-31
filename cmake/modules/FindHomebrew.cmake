55079108003c2ee49bae27d2c192ac4ca861d4cd
Elsie Hupp <github@elsiehupp.com>
2021-01-31 18:07:48 -0500

    Moved Homebrew into its own CMake script
    Signed-off-by: Elsie Hupp <github@elsiehupp.com>

---
+++ cmake/modules/FindHomebrew.cmake
@@ -0,0 +1,12 @@
+if (APPLE AND NOT HOMEBREW_EXECUTABLE)
+    find_program(HOMEBREW_EXECUTABLE brew)
+    mark_as_advanced(FORCE HOMEBREW_EXECUTABLE)
+    if (HOMEBREW_EXECUTABLE)
+        # Detected a Homebrew install, query for its install prefix.
+        execute_process(COMMAND ${HOMEBREW_EXECUTABLE} --prefix
+            OUTPUT_VARIABLE HOMEBREW_INSTALL_PREFIX
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        message(STATUS "Detected Homebrew with install prefix: "
+            "${HOMEBREW_INSTALL_PREFIX}, adding to CMake search paths.")
+    list(APPEND CMAKE_PREFIX_PATH "${HOMEBREW_INSTALL_PREFIX}")
+endif()
