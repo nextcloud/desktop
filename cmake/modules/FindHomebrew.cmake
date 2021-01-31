26f90e8983485d458ed8d2ce48e2891f29af6165
Elsie Hupp <github@elsiehupp.com>
2021-01-31 18:53:41 -0500

    Commented out debug messages
    Signed-off-by: Elsie Hupp <github@elsiehupp.com>

--- cmake/modules/FindHomebrew.cmake
+++ cmake/modules/FindHomebrew.cmake
@@ -6,7 +6,7 @@ if (APPLE AND NOT HOMEBREW_EXECUTABLE)
         execute_process(COMMAND ${HOMEBREW_EXECUTABLE} --prefix
             OUTPUT_VARIABLE HOMEBREW_INSTALL_PREFIX
             OUTPUT_STRIP_TRAILING_WHITESPACE)
-        message(STATUS "Detected Homebrew with install prefix: "
+        # message(STATUS "Detected Homebrew with install prefix: "
             "${HOMEBREW_INSTALL_PREFIX}, adding to CMake search paths.")
         list(APPEND CMAKE_PREFIX_PATH "${HOMEBREW_INSTALL_PREFIX}")
     endif()
