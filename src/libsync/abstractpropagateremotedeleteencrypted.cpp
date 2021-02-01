856f0567a7488fc34eb79af705bb25b92f3f6a99
Kevin Ottens <kevin.ottens@nextcloud.com>
2021-02-01 16:50:45 +0100

    Make sure we pass a full remote path to LSCOL jobs
    Indeed the path we have is supposedly not fully qualified in case of a
    sync folder which doesn't point to / on the remote end. But LSCOL works
    with absolute paths on the server so make sure this is what we give it
    out.
    
    Signed-off-by: Kevin Ottens <kevin.ottens@nextcloud.com>

--- src/libsync/abstractpropagateremotedeleteencrypted.cpp
+++ src/libsync/abstractpropagateremotedeleteencrypted.cpp
@@ -58,7 +58,7 @@ void AbstractPropagateRemoteDeleteEncrypted::storeFirstErrorString(const QString
 void AbstractPropagateRemoteDeleteEncrypted::startLsColJob(const QString &path)
 {
     qCDebug(ABSTRACT_PROPAGATE_REMOVE_ENCRYPTED) << "Folder is encrypted, let's get the Id from it.";
-    auto job = new LsColJob(_propagator->account(), path, this);
+    auto job = new LsColJob(_propagator->account(), _propagator->fullRemotePath(path), this);
     job->setProperties({"resourcetype", "http://owncloud.org/ns:fileid"});
     connect(job, &LsColJob::directoryListingSubfolders, this, &AbstractPropagateRemoteDeleteEncrypted::slotFolderEncryptedIdReceived);
     connect(job, &LsColJob::finishedWithError, this, &AbstractPropagateRemoteDeleteEncrypted::taskFailed);
