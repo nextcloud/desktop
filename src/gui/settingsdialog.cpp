068ca4108fe777dd227689d4871f3361f8a930fb
Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>
2021-05-08 17:35:40 -0400

    Allow Maximizing Settings Window
    Signed-off-by: Elsie Hupp <9206310+elsiehupp@users.noreply.github.com>

--- src/gui/settingsdialog.cpp
+++ src/gui/settingsdialog.cpp
@@ -158,7 +158,7 @@ SettingsDialog::SettingsDialog(ownCloudGui *gui, QWidget *parent)
 
     customizeStyle();
 
-    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint & Qt::Window);
     cfg.restoreGeometry(this);
 }
 
