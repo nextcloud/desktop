74b2c5761d5e268cf637827522dbd0aad15addbc
Felix Weilbach <felix.weilbach@nextcloud.com>
2021-06-07 13:43:15 +0200

    Check compiler against CMAKE_CXX_COMPILER_ID
    Signed-off-by: Felix Weilbach <felix.weilbach@nextcloud.com>

--- cmake/modules/Warnings.cmake
+++ cmake/modules/Warnings.cmake
@@ -5,7 +5,7 @@
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
 
     # Use this only for Clang
-    if (CMAKE_CXX_COMPILER MATCHES "Clang")
+    if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -pedantic -Wno-long-long -Wno-gnu-zero-variadic-macro-arguments")
     endif()
 
