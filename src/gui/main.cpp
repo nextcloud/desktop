24830a1cf3765214549c8030b26eef0c09dbfbfe
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2021-09-17 09:54:25 +0200

    set default format for QSurface to handle NVidia context loss events
    should fix wrong OpenGL rendering after suspend on Linux with NVidia
    binary OpenGL driver
    
    close #3759
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -36,6 +36,7 @@
 #include <QDebug>
 #include <QQuickStyle>
 #include <QQuickWindow>
+#include <QSurfaceFormat>
 
 using namespace OCC;
 
@@ -100,6 +101,10 @@ int main(int argc, char **argv)
         QQuickWindow::setTextRenderType(QQuickWindow::NativeTextRendering);
     }
 #endif
+
+    auto surfaceFormat = QSurfaceFormat::defaultFormat();
+    surfaceFormat.setOption(QSurfaceFormat::ResetNotification);
+    QSurfaceFormat::setDefaultFormat(surfaceFormat);
 
 // check a environment variable for core dumps
 #ifdef Q_OS_UNIX
