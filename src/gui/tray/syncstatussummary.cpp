fb3c7401291f4f2fe436eb63fcbd9f57c06cdc9e
rakekniven <2069590+rakekniven@users.noreply.github.com>
2021-10-16 21:15:18 +0200

    Changed wording of status message
    The files themselves have no problem. The sync process has problems. 
    
    Signed-off-by: rakekniven <2069590+rakekniven@users.noreply.github.com>

--- src/gui/tray/syncstatussummary.cpp
+++ src/gui/tray/syncstatussummary.cpp
@@ -166,7 +166,7 @@ void SyncStatusSummary::setSyncStateForFolder(const Folder *folder)
     case SyncResult::Problem:
     case SyncResult::Undefined:
         setSyncing(false);
-        setSyncStatusString(tr("Some files had problems during the sync!"));
+        setSyncStatusString(tr("There is a problem with some files during the synchronisation!"));
         setSyncStatusDetailString(tr("See below for warnings"));
         setSyncIcon(Theme::instance()->syncStatusWarning());
         markFolderAsError(folder);
