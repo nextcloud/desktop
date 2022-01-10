9dd3e9aad35df85d1685ca832493929a46ccbcb9
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-01-10 12:14:14 +0100

    Happy new year 2022

--- cmake/modules/FindLibcloudproviders.cmake
+++ cmake/modules/FindLibcloudproviders.cmake
@@ -1,4 +1,4 @@
-# (c) 2019-2021 Copyright ownCloud GmbH
+# (c) 2019-2022 Copyright ownCloud GmbH
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING* file.
 
