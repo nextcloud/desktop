6c764735b0d2ad0a1374121b485f644a037b3f2c
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-01-14 10:26:36 +0100

    Remove legacy defines

--- src/csync/config_csync.h.cmake
+++ src/csync/config_csync.h.cmake
@@ -1,8 +1,4 @@
 #include "ocsynclib.h"
-#cmakedefine PACKAGE "${APPLICATION_NAME}"
-#cmakedefine VERSION "${APPLICATION_VERSION}"
-#cmakedefine LOCALEDIR "${LOCALE_INSTALL_DIR}"
-#cmakedefine LIBDIR "${LIBDIR}"
 #cmakedefine SYSCONFDIR "${SYSCONFDIR}"
 
 #cmakedefine HAVE_UTIMES 1
