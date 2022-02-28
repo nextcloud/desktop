541447723a46a1265a53eaec041472fe03b69ef5
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2022-02-28 14:02:58 +0100

    remove all usage of QDateTime::toTime_t() to avoid overflow
    QDateTime::toTime_t() can overflow if called with a date outside its
    domain of working
    
    this method is obsolete and QDateTime::toSecsSinceEpoch() is the
    replacement
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/libsync/discoveryphase.cpp
+++ src/libsync/discoveryphase.cpp
@@ -407,7 +407,10 @@ static void propertyMapToRemoteInfo(const QMap<QString, QString> &map, RemoteInf
         } else if (property == QLatin1String("getlastmodified")) {
             const auto date = QDateTime::fromString(value, Qt::RFC2822Date);
             Q_ASSERT(date.isValid());
-            result.modtime = date.toTime_t();
+            result.modtime = 0;
+            if (date.toSecsSinceEpoch() > 0) {
+                result.modtime = date.toSecsSinceEpoch();
+            }
         } else if (property == QLatin1String("getcontentlength")) {
             // See #4573, sometimes negative size values are returned
             bool ok = false;
