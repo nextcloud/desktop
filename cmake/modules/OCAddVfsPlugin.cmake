2b81fb2df2c766c675e8c683d4fb1bffe3d6b55f
Hannah von Reth <vonreth@kde.org>
2022-05-05 12:35:02 +0200

    Use function to add vfs plugins
    Ensure that owncloud depends on the plugins, this simplifies work in IDE's

---
+++ cmake/modules/OCAddVfsPlugin.cmake
@@ -0,0 +1,26 @@
+
+function(add_vfs_plugin)
+    set(options "")
+    set(oneValueArgs NAME)
+    set(multiValueArgs SRC LIBS)
+    cmake_parse_arguments(__PLUGIN "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+    add_library(vfs_${__PLUGIN_NAME} MODULE
+        ${__PLUGIN_SRC}
+    )
+
+    set_target_properties(vfs_${__PLUGIN_NAME} PROPERTIES OUTPUT_NAME "${synclib_NAME}_vfs_${__PLUGIN_NAME}")
+
+    target_link_libraries(vfs_${__PLUGIN_NAME}
+        libsync
+        ${__PLUGIN_LIBS}
+    )
+    INSTALL(TARGETS vfs_${__PLUGIN_NAME} DESTINATION "${KDE_INSTALL_PLUGINDIR}")
+
+    if (TARGET owncloud)
+        add_dependencies(owncloud vfs_${__PLUGIN_NAME})
+    endif()
+    if (TARGET cmd)
+        add_dependencies(cmd vfs_${__PLUGIN_NAME})
+    endif()
+endfunction()
