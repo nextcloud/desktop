20ee506b71f4576163e6c6ce7b625012e98dec51
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2022-05-22 22:26:34 +0200

    port away from deprecated API from QFontMetricsF
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/wizard/postfixlineedit.cpp
+++ src/gui/wizard/postfixlineedit.cpp
@@ -32,7 +32,7 @@ void PostfixLineEdit::setPostfix(const QString &postfix)
     _postfix = postfix;
     QFontMetricsF fm(font());
     QMargins tm = textMargins();
-    tm.setRight(tm.right() + qRound(fm.width(_postfix)) + verticalMargin);
+    tm.setRight(tm.right() + qRound(fm.horizontalAdvance(_postfix)) + verticalMargin);
     setTextMargins(tm);
 }
 
@@ -63,7 +63,7 @@ void PostfixLineEdit::paintEvent(QPaintEvent *pe)
     //
     p.setPen(palette().color(QPalette::Disabled, QPalette::Text));
     QFontMetricsF fm(font());
-    int start = rect().right() - qRound(fm.width(_postfix));
+    int start = rect().right() - qRound(fm.horizontalAdvance(_postfix));
     QStyleOptionFrame panel;
     initStyleOption(&panel);
     QRect r = style()->subElementRect(QStyle::SE_LineEditContents, &panel, this);
