ee648ab99b86580c8b81b128573795bbf2fa348b
Hannah von Reth <hannah.vonreth@owncloud.com>
2022-07-27 13:26:41 +0200

    Remove pointless benchmark

--- test/owncloud_add_test.cmake
+++ test/owncloud_add_test.cmake
@@ -19,18 +19,3 @@ function(owncloud_add_test test_class)
         set_property(TEST ${OWNCLOUD_TEST_CLASS}Test PROPERTY ENVIRONMENT "QT_QPA_PLATFORM=offscreen")
     endif()
 endfunction()
-
-macro(owncloud_add_benchmark test_class)
-    set(OWNCLOUD_TEST_CLASS ${test_class})
-    string(TOLOWER "${OWNCLOUD_TEST_CLASS}" OWNCLOUD_TEST_CLASS_LOWERCASE)
-
-    add_executable(${OWNCLOUD_TEST_CLASS}Bench benchmarks/bench${OWNCLOUD_TEST_CLASS_LOWERCASE}.cpp ${additional_cpp})
-    ecm_mark_nongui_executable(${OWNCLOUD_TEST_CLASS}Bench)
-
-    target_link_libraries(${OWNCLOUD_TEST_CLASS}Bench
-        libsync
-        syncenginetestutils
-        Qt5::Core Qt5::Test Qt5::Xml Qt5::Network
-    )
-    target_compile_definitions(${OWNCLOUD_TEST_CLASS}Bench PRIVATE OWNCLOUD_BIN_PATH="${CMAKE_BINARY_DIR}/bin")
-endmacro()
