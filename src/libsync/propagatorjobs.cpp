b5c8c4329179a3535072191190bbb2e7a5baf386
allexzander <blackslayer4@gmail.com>
2022-08-04 16:22:35 +0300

    Fix unit tests.
    Signed-off-by: allexzander <blackslayer4@gmail.com>

--- src/libsync/propagatorjobs.cpp
+++ src/libsync/propagatorjobs.cpp
@@ -257,6 +257,7 @@ void PropagateLocalRename::start()
     if (!propagator()->_journal->deleteFileRecord(_item->_originalFile)) {
         qCWarning(lcPropagateLocalRename) << "could not delete file from local DB" << _item->_originalFile;
         done(SyncFileItem::NormalError, tr("could not delete file %1 from local DB").arg(_item->_originalFile));
+        return;
     }
 
     auto &vfs = propagator()->syncOptions()._vfs;
