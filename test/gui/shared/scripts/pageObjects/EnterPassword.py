ff66b8a7612b5fadc870d71e0995392b54d0e84c
Saw-jan <saw.jan.grg3e@gmail.com>
2022-08-19 16:56:22 +0545

    adapt to changing password entry ui

--- test/gui/shared/scripts/pageObjects/EnterPassword.py
+++ test/gui/shared/scripts/pageObjects/EnterPassword.py
@@ -4,17 +4,17 @@ import squish
 
 class EnterPassword:
     PASSWORD_BOX = {
-        "container": names.enter_Password_QInputDialog,
+        "container": names.loginRequiredDialog_contentWidget_QStackedWidget, 
+        "name": "passwordLineEdit",
         "type": "QLineEdit",
-        "unnamed": 1,
-        "visible": 1,
+        "visible": 1
     }
-    OK_BUTTON = {
-        "text": "OK",
+    LOGIN_BUTTON = {
+        "text": "Log in",
         "type": "QPushButton",
         "unnamed": 1,
         "visible": 1,
-        "window": names.enter_Password_QInputDialog,
+        "window": names.loginRequiredDialog_OCC_LoginRequiredDialog
     }
 
     def __init__(self):
@@ -24,6 +24,6 @@ class EnterPassword:
         try:
             squish.waitForObject(self.PASSWORD_BOX, 10000)
             squish.type(squish.waitForObject(self.PASSWORD_BOX), password)
-            squish.clickButton(squish.waitForObject(self.OK_BUTTON))
+            squish.clickButton(squish.waitForObject(self.LOGIN_BUTTON))
         except LookupError:
             pass
