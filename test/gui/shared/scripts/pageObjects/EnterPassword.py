7af3de95ad1eb57a3c3ced44ded2af5a719fd954
Saw-jan <saw.jan.grg3e@gmail.com>
2022-08-22 15:47:10 +0545

    throw error if failed on password entry
    check with reduced image

--- test/gui/shared/scripts/pageObjects/EnterPassword.py
+++ test/gui/shared/scripts/pageObjects/EnterPassword.py
@@ -21,9 +21,9 @@ class EnterPassword:
         pass
 
     def enterPassword(self, password):
-        try:
-            squish.waitForObject(self.PASSWORD_BOX, 10000)
-            squish.type(squish.waitForObject(self.PASSWORD_BOX), password)
-            squish.clickButton(squish.waitForObject(self.LOGIN_BUTTON))
-        except LookupError:
-            pass
+        # try:
+        squish.waitForObject(self.PASSWORD_BOX, 10000)
+        squish.type(squish.waitForObject(self.PASSWORD_BOX), password)
+        squish.clickButton(squish.waitForObject(self.LOGIN_BUTTON))
+        # except LookupError:
+        #     pass
