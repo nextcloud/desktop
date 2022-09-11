Fuseteam/nextcloud-desktop

Use proper online status for user ('dnd', 'online', 'invisible', etc.) to enable or disable desktop notifications.
Signed-off-by: alex-z <blackslayer4@gmail.com>

src/gui/accountstate.cpp
@@ -21,6 +21,7 @@
 #include "logger.h"
 #include "configfile.h"
 #include "ocsnavigationappsjob.h"
+#include "ocsuserstatusconnector.h"
 #include "pushnotifications.h"
 
 #include <QSettings>
@@ -58,6 +59,8 @@ AccountState::AccountState(AccountPtr account)
         this, &AccountState::slotCredentialsAsked);
     connect(account.data(), &Account::pushNotificationsReady,
             this, &AccountState::slotPushNotificationsReady);
+    connect(account.data(), &Account::userOnlineStatusChanged, this,
+        &AccountState::slotUserOnlineStatusChanged);
 
     connect(this, &AccountState::isConnectedChanged, [=]{
         // Get the Apps available on the server if we're now connected.
@@ -556,6 +559,11 @@ void AccountState::slotPushNotificationsReady()
     if (state() != AccountState::State::Connected) {
         setState(AccountState::State::Connected);
     }
+}
+
+void AccountState::slotUserOnlineStatusChanged()
+{
+    setDesktopNotificationsAllowed(_account->userStatusConnector()->userStatus().state() != UserStatus::OnlineStatus::DoNotDisturb);
 }
 
 void AccountState::slotNavigationAppsFetched(const QJsonDocument &reply, int statusCode)

src/libsync/account.cpp
@@ -635,9 +635,12 @@ void Account::setupUserStatusConnector()
     connect(_userStatusConnector.get(), &UserStatusConnector::userStatusFetched, this, [this](const UserStatus &) {
         emit userStatusChanged();
     });
+    connect(_userStatusConnector.get(), &UserStatusConnector::userOnlineStatusChanged, this, &Account::userOnlineStatusChanged);
     connect(_userStatusConnector.get(), &UserStatusConnector::messageCleared, this, [this] {
         emit userStatusChanged();
     });
+
+    _userStatusConnector->fetchUserStatus();
 }
 
 QString Account::serverVersion() const

src/libsync/ocsuserstatusconnector.cpp
@@ -256,8 +256,14 @@ void OcsUserStatusConnector::onUserStatusFetched(const QJsonDocument &json, int 
         return;
     }
 
+    const auto oldOnlineState = _userStatus.state();
     _userStatus = jsonToUserStatus(json);
+
     emit userStatusFetched(_userStatus);
+
+    if (oldOnlineState != _userStatus.state()) {
+        emit userOnlineStatusChanged();
+    }
 }
 
 void OcsUserStatusConnector::startFetchPredefinedStatuses()
@@ -396,7 +402,9 @@ void OcsUserStatusConnector::setUserStatus(const UserStatus &userStatus)
         return;
     }
 
-    setUserStatusOnlineStatus(userStatus.state());
+    if (userStatus.state() != _userStatus.state()) {
+        setUserStatusOnlineStatus(userStatus.state());
+    }
     setUserStatusMessage(userStatus);
 }
 
@@ -407,6 +415,15 @@ void OcsUserStatusConnector::onUserStatusOnlineStatusSet(const QJsonDocument &js
     if (statusCode != 200) {
         emit error(Error::CouldNotSetUserStatus);
         return;
+    }
+
+    const auto oldOnlineState = _userStatus.state();
+    _userStatus.setState(jsonToUserStatus(json).state());
+
+    emit userStatusSet();
+
+    if (oldOnlineState != _userStatus.state()) {
+        emit userOnlineStatusChanged();
     }
 }
 
@@ -449,7 +466,10 @@ void OcsUserStatusConnector::onMessageCleared(const QJsonDocument &json, int sta
         return;
     }
 
+    const auto onlineState = _userStatus.state();
+
     _userStatus = {};
+    _userStatus.setState(onlineState);
     emit messageCleared();
 }
 }

nextcloud/desktop

src/gui/accountstate.cpp
@@ -21,6 +21,7 @@
 #include "logger.h"
 #include "configfile.h"
 #include "ocsnavigationappsjob.h"
+#include "ocsuserstatusconnector.h"
 #include "pushnotifications.h"
 
 #include <QSettings>
@@ -58,6 +59,8 @@ AccountState::AccountState(AccountPtr account)
         this, &AccountState::slotCredentialsAsked);
     connect(account.data(), &Account::pushNotificationsReady,
             this, &AccountState::slotPushNotificationsReady);
+    connect(account.data(), &Account::serverUserStatusChanged, this,
+        &AccountState::slotServerUserStatusChanged);
 
     connect(this, &AccountState::isConnectedChanged, [=]{
         // Get the Apps available on the server if we're now connected.
@@ -556,6 +559,11 @@ void AccountState::slotPushNotificationsReady()
     if (state() != AccountState::State::Connected) {
         setState(AccountState::State::Connected);
     }
+}
+
+void AccountState::slotServerUserStatusChanged()
+{
+    setDesktopNotificationsAllowed(_account->userStatusConnector()->userStatus().state() != UserStatus::OnlineStatus::DoNotDisturb);
 }
 
 void AccountState::slotNavigationAppsFetched(const QJsonDocument &reply, int statusCode)

src/libsync/account.cpp
@@ -635,9 +635,12 @@ void Account::setupUserStatusConnector()
     connect(_userStatusConnector.get(), &UserStatusConnector::userStatusFetched, this, [this](const UserStatus &) {
         emit userStatusChanged();
     });
+    connect(_userStatusConnector.get(), &UserStatusConnector::serverUserStatusChanged, this, &Account::serverUserStatusChanged);
     connect(_userStatusConnector.get(), &UserStatusConnector::messageCleared, this, [this] {
         emit userStatusChanged();
     });
+
+    _userStatusConnector->fetchUserStatus();
 }
 
 QString Account::serverVersion() const

src/libsync/ocsuserstatusconnector.cpp
@@ -256,8 +256,14 @@ void OcsUserStatusConnector::onUserStatusFetched(const QJsonDocument &json, int 
         return;
     }
 
+    const auto oldOnlineState = _userStatus.state();
     _userStatus = jsonToUserStatus(json);
+
     emit userStatusFetched(_userStatus);
+
+    if (oldOnlineState != _userStatus.state()) {
+        emit serverUserStatusChanged();
+    }
 }
 
 void OcsUserStatusConnector::startFetchPredefinedStatuses()
@@ -396,7 +402,9 @@ void OcsUserStatusConnector::setUserStatus(const UserStatus &userStatus)
         return;
     }
 
-    setUserStatusOnlineStatus(userStatus.state());
+    if (userStatus.state() != _userStatus.state()) {
+        setUserStatusOnlineStatus(userStatus.state());
+    }
     setUserStatusMessage(userStatus);
 }
 
@@ -407,6 +415,15 @@ void OcsUserStatusConnector::onUserStatusOnlineStatusSet(const QJsonDocument &js
     if (statusCode != 200) {
         emit error(Error::CouldNotSetUserStatus);
         return;
+    }
+
+    const auto oldOnlineState = _userStatus.state();
+    _userStatus.setState(jsonToUserStatus(json).state());
+
+    emit userStatusSet();
+
+    if (oldOnlineState != _userStatus.state()) {
+        emit serverUserStatusChanged();
     }
 }
 
@@ -449,7 +466,10 @@ void OcsUserStatusConnector::onMessageCleared(const QJsonDocument &json, int sta
         return;
     }
 
+    const auto onlineState = _userStatus.state();
+
     _userStatus = {};
+    _userStatus.setState(onlineState);
     emit messageCleared();
 }
 }