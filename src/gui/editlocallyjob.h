68a2e7d179460418b7c56df5ab30e618b3e53f51
Claudio Cambra <claudio.cambra@nextcloud.com>
2022-12-07 13:46:21 +0100

    Make fileLockTimeRemainingMinutes a static method
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/editlocallyjob.h
+++ src/gui/editlocallyjob.h
@@ -88,7 +88,7 @@ private:
     const QString getRelativePathToRemoteRootForFile() const; // returns either '/' or a (relative path - Folder::remotePath()) for folders pointing to a non-root remote path e.g. '/subfolder' instead of '/'
     const QString getRelativePathParent() const;
 
-    [[nodiscard]] int fileLockTimeRemainingMinutes(const int lockTime, const int lockTimeOut);
+    [[nodiscard]] static int fileLockTimeRemainingMinutes(const int lockTime, const int lockTimeOut);
 
     bool _tokenVerified = false;
 
