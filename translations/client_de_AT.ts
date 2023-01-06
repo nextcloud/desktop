26bc239f43e6319f8b6f1002b418773b35ab3017
ownClouders <devops@owncloud.com>
2023-01-06 03:06:47 +0000

    [tx] updated client translations from transifex [skip ci]

--- translations/client_de_AT.ts
+++ translations/client_de_AT.ts
@@ -504,7 +504,7 @@
     <message>
         <location filename="../src/gui/accountsettings.cpp" line="221"/>
         <source>Reconnect</source>
-        <translation type="unfinished"/>
+        <translation>Erneut verbinden</translation>
     </message>
     <message>
         <location filename="../src/gui/accountsettings.cpp" line="375"/>
