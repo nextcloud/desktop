f7ad94f526af032bc6f2c11126ff89cadf806f39
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-04-06 13:47:32 +0200

    Don't set QT_NO_DEPRECATED_WARNINGS, it was removed in Qt 5.13

--- cmake/modules/OCApplyCommonSettings.cmake
+++ cmake/modules/OCApplyCommonSettings.cmake
@@ -24,7 +24,6 @@ function(apply_common_target_settings targetName)
             QT_NO_CAST_FROM_BYTEARRAY
             QT_USE_QSTRINGBUILDER
             QT_MESSAGELOGCONTEXT  # enable function name and line number in debug output
-            QT_DEPRECATED_WARNINGS
             QT_NO_FOREACH
     )
 endfunction()
