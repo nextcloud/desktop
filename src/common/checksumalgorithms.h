90bd14237d756b6d698b77c5b1e6a27d125d4247
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-04-11 15:48:24 +0200

    Silence some possible loss of data warnings

--- src/common/checksumalgorithms.h
+++ src/common/checksumalgorithms.h
@@ -68,7 +68,11 @@ namespace CheckSums {
     inline QString toQString(Algorithm algo)
     {
         const auto n = toString(algo);
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+        return QString::fromUtf8(n.data(), static_cast<int>(n.size()));
+#else
         return QString::fromUtf8(n.data(), n.size());
+#endif
     }
 
     constexpr auto pair(Algorithm a)
