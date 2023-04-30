be4b8066c3cca9542217c1a012accdf9d80803f2
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-04-30 18:22:43 +0800

    Add static discoveredLegacyConfigPath member to configfile
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/libsync/configfile.h
+++ src/libsync/configfile.h
@@ -219,6 +219,10 @@ public:
     /// Add the system and user exclude file path to the ExcludedFiles instance.
     static void setupDefaultExcludeFilePaths(ExcludedFiles &excludedFiles);
 
+    /// Set during first time migration of legacy accounts in AccountManager
+    [[nodiscard]] static QString discoveredLegacyConfigPath();
+    static void setDiscoveredLegacyConfigPath(const QString &discoveredLegacyConfigPath);
+
 protected:
     [[nodiscard]] QVariant getPolicySetting(const QString &policy, const QVariant &defaultValue = QVariant()) const;
     void storeData(const QString &group, const QString &key, const QVariant &value);
@@ -237,6 +241,7 @@ private:
     using SharedCreds = QSharedPointer<AbstractCredentials>;
 
     static QString _confDir;
+    static QString _discoveredLegacyConfigPath;
 };
 }
 #endif // CONFIGFILE_H
