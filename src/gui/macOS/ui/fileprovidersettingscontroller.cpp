8643acfdaa10e9e0698721cc2dedec32083522e5
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-05-17 17:01:48 +0800

    Add a start fileprovidersettingscontroller
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ src/gui/macOS/ui/fileprovidersettingscontroller.cpp
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2023 by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "fileprovidersettingscontroller.h"
+
+namespace OCC {
+
+namespace Mac {
+
+FileProviderSettingsController::FileProviderSettingsController(QObject *parent)
+    : QObject{parent}
+{
+}
+
+} // Mac
+
+} // OCC
