4aa9fff1eca5903185fe74f600aee5d12809eedf
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-06-02 14:52:16 +0800

    Ensure all Finder extensions are enabled post-install
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/post_install.sh.cmake
+++ admin/osx/post_install.sh.cmake
@@ -4,10 +4,13 @@
 if [ -x "$(command -v pluginkit)" ]; then
     # add it to DB. This happens automatically too but we try to push it a bit harder for issue #3463
     pluginkit -a  "/Applications/@APPLICATION_NAME@.app/Contents/PlugIns/FinderSyncExt.appex/"
+    pluginkit -a  "/Applications/@APPLICATION_NAME@.app/Contents/PlugIns/FileProviderExt.appex/"
     # Since El Capitan we need to sleep #4650
     sleep 10s
     # enable it
     pluginkit -e use -i @APPLICATION_REV_DOMAIN@.FinderSyncExt
+    pluginkit -e use -i @APPLICATION_REV_DOMAIN@.FileProviderExt
+
 fi
 
 exit 0
