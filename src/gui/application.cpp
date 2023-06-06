1929c488529eaf4c3d532c78eb3193f6f7baca93
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-06-06 22:01:35 +0800

    Revert "Merge pull request #5742 from nextcloud/backport/5707/stable-3.9"
    This reverts commit 5118bcfaba1595e52a1eabd30b4012b875bc1fde, reversing
    changes made to d9603fc84cd81b44da085bd824321167c935125b.
    
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -403,7 +403,7 @@ Application::Application(int &argc, char **argv)
         }
     }
 
-#if defined(BUILD_FILE_PROVIDER_MODULE)
+#if defined(Q_OS_MACOS)
     _fileProvider.reset(new Mac::FileProvider);
 #endif
 
