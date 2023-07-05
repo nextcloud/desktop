29cc9c06f98478b499d13807482b91bcc2db5999
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-07-05 17:17:28 +0800

    Handle WHITELIST_FOLDER and BLACKLIST_FOLDER verbs in activitylistmodel
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/tray/activitylistmodel.cpp
+++ src/gui/tray/activitylistmodel.cpp
@@ -836,6 +836,12 @@ void ActivityListModel::slotTriggerAction(const int activityIndex, const int act
                (activity._syncFileItemStatus == SyncFileItem::Conflict || activity._syncFileItemStatus == SyncFileItem::FileNameClash)) {
         slotTriggerDefaultAction(activityIndex);
         return;
+    } else if (action._verb == "WHITELIST_FOLDER" && !activity._folder.isEmpty()) {
+        FolderMan::instance()->whitelistFolderPath(activity._folder);
+        return;
+    } else if (action._verb == "BLACKLIST_FOLDER" && !activity._folder.isEmpty()) {
+        FolderMan::instance()->blacklistFolderPath(activity._folder);
+        return;
     }
 
     emit sendNotificationRequest(activity._accName, action._link, action._verb, activityIndex);
