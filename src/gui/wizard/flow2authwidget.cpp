a1efbef19468a736cebba479502b312c739557a5
Ankur Bavisa <Ankur.Bavisa@t-systems.com>
2023-07-06 18:35:14 +0530

    update runwizard introduction screen
    Signed-off-by: Ankur Bavisa <Ankur.Bavisa@t-systems.com>

--- src/gui/wizard/flow2authwidget.cpp
+++ src/gui/wizard/flow2authwidget.cpp
@@ -42,8 +42,8 @@ Flow2AuthWidget::Flow2AuthWidget(QWidget *parent)
     WizardCommon::initErrorLabel(_ui.errorLabel);
     _ui.errorLabel->setTextFormat(Qt::RichText);
 
-    connect(_ui.openLinkLabel, &LinkLabel::clicked, this, &Flow2AuthWidget::slotOpenBrowser);
-    connect(_ui.copyLinkLabel, &LinkLabel::clicked, this, &Flow2AuthWidget::slotCopyLinkToClipboard);
+    connect(_ui.openLinkLabel, &QPushButton::clicked, this, &Flow2AuthWidget::slotOpenBrowser);
+    connect(_ui.copyLinkLabel, &QPushButton::clicked, this, &Flow2AuthWidget::slotCopyLinkToClipboard);
 
     auto sizePolicy = _progressIndi->sizePolicy();
     sizePolicy.setRetainSizeWhenHidden(true);
@@ -218,11 +218,9 @@ void Flow2AuthWidget::customizeStyle()
         }
     }
 
-    _ui.openLinkLabel->setText(tr("Reopen Browser"));
-    _ui.openLinkLabel->setAlignment(Qt::AlignCenter);
+    _ui.openLinkLabel->setText(tr("Open Browser"));
 
     _ui.copyLinkLabel->setText(tr("Copy Link"));
-    _ui.copyLinkLabel->setAlignment(Qt::AlignCenter);
 
     WizardCommon::customizeHintLabel(_ui.statusLabel);
 }
