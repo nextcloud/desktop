ec3eb853bebc3cea848d4bd21840a7532aa2ff43
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-07-21 12:13:51 +0200

    Update .run-clang-tidy.ps1

--- .github/workflows/.run-clang-tidy.ps1
+++ .github/workflows/.run-clang-tidy.ps1
@@ -1,2 +1,7 @@
 cd "${env:GITHUB_WORKSPACE}"
+
+# allow errors as some files can't be compiled on all platforms
+$oldErrorActio = $ErrorActionPreference
+$ErrorActionPreference="Continue"
 git ls-files *.cpp | ForEach-Object -Parallel {clang-tidy -p $env:BUILD_DIR/compile_commands.json $_ } -ThrottleLimit 5 | Tee-Object -Path $env:TMPDIR/clang-tidy.log
+$ErrorActionPreference = $oldErrorActio
