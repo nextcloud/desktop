9c471865c919efbbb8a2e11070910995d86369d6
Hannah von Reth <hannah.vonreth@owncloud.com>
2023-08-04 13:25:26 +0200

    Ci: Update sonarcloud

--- .github/workflows/.sonar.ps1
+++ .github/workflows/.sonar.ps1
@@ -1,4 +1,4 @@
-$env:SONAR_SCANNER_VERSION = "4.7.0.2747"
+$env:SONAR_SCANNER_VERSION = "5.0.0.2966"
 $env:SONAR_DIRECTORY = [System.IO.Path]::Combine($(get-location).Path,".sonar")
 $env:SONAR_SCANNER_HOME = "$env:SONAR_DIRECTORY/sonar-scanner-$env:SONAR_SCANNER_VERSION-windows"
 rm $env:SONAR_SCANNER_HOME -Force -Recurse -ErrorAction SilentlyContinue
