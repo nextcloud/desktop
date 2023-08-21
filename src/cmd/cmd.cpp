30867a40bfb09ad85f5d135c3cbbd4c2ebb6115f
internet-memme <142409719+internet-memme@users.noreply.github.com>
2023-08-21 10:17:44 +0200

    Update src/cmd/cmd.cpp
    Co-authored-by: Claudio Cambra <developer@claudiocambra.com>
    Signed-off-by: internet-memme <142409719+internet-memme@users.noreply.github.com>

--- src/cmd/cmd.cpp
+++ src/cmd/cmd.cpp
@@ -392,17 +392,14 @@ int main(int argc, char **argv)
         if (password.isEmpty()) {
             password = queryPassword(user);
         }
-    }else{
-       //else use ENV?
-      if(user.isEmpty()){
-	user = std::getenv("NC_USER");
-      }
-      if(password.isEmpty()){
-	password = std::getenv("NC_PASSWORD");
-      }
-
+    } else {
+        if (user.isEmpty()) {
+            user = std::getenv("NC_USER");
+        }
+        if (password.isEmpty()) {
+            password = std::getenv("NC_PASSWORD");
+        }
     }
-   
 
     // Find the folder and the original owncloud url
 
