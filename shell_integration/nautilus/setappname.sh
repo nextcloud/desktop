46976779c8474551ff65e4958569984127f326de
Fabian Müller <fmueller@owncloud.com>
2023-09-04 12:04:47 +0200

    Remove shell integration plugin for GNOME's Nautilus
    ... and its derivatives.
    
    Moved to https://github.com/owncloud/client-desktop-shell-integration-nautilus.

--- shell_integration/nautilus/setappname.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# this script replaces the line
-#  appname = 'ownCloud'
-# with the correct branding name in the syncstate.py script
-# It also replaces the occurences in the class name so several
-# branding can be loaded (see #6524)
-sed -i.org -e "s/ownCloud/$1/g" syncstate.py
