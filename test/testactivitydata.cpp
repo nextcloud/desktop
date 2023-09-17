09e60744c227a5ad8c976c041b55f68e9b5c86e1
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2023-09-17 20:28:50 +0200

    e proper data type in data for activity model automated test data
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- test/testactivitydata.cpp
+++ test/testactivitydata.cpp
@@ -49,10 +49,10 @@ public:
         const auto path = QStringLiteral("path/test.").append(fileFormat);
         const auto fileName = QStringLiteral("test.").append(fileFormat);
         const auto activityType = QStringLiteral("file");
-        const auto activityId = QStringLiteral("90000");
+        const auto activityId = 90000;
         const auto message = QString();
-        const auto objectName = QStringLiteral("test.").append(fileFormat);
-        const auto link = account->url().toString().append(QStringLiteral("/f/")).append(activityId);
+        const auto objectName = QStringLiteral("test.%1").arg(fileFormat);
+        const auto link = account->url().toString().append(QStringLiteral("/f/%1").arg(activityId));
         const auto datetime = QDateTime::currentDateTime().toString(Qt::ISODate);
         const auto icon = account->url().toString().append(QStringLiteral("/apps/files/img/add-color.svg"));
 
