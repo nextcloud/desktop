8f9f7bd3a713060b130b687059949445ef09c7da
Camila <hello@camila.codes>
2023-09-20 14:41:59 +0200

    Update folder version.
    Signed-off-by: Camila <hello@camila.codes>

--- src/gui/folder.h
+++ src/gui/folder.h
@@ -82,8 +82,9 @@ public:
      * Version 2: introduction of metadata_parent hash in 2.6.0
      *            (version remains readable by 2.5.1)
      * Version 3: introduction of new windows vfs mode in 2.6.0
+     * Version 5: available in oC client 4.0.0 and 4.2.0
      */
-    static int maxSettingsVersion() { return 3; }
+    static int maxSettingsVersion() { return 5; }
 
     /// Ensure / as separator and trailing /.
     static QString prepareLocalPath(const QString &path);
