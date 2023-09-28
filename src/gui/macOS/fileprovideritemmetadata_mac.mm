a34a390790dca30e34a00e8c4a809abfb1c95179
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-09-29 00:38:12 +0800

    Add a convenience function to convert from nsnamecomponents to qstring
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ src/gui/macOS/fileprovideritemmetadata_mac.mm
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "fileprovideritemmetadata.h"
+
+#import <Foundation/Foundation.h>
+#import <FileProvider/NSFileProviderItem.h>
+#import <UniformTypeIdentifiers/UniformTypeIdentifiers.h>
+
+namespace {
+
+QString nsNameComponentsToLocalisedQString(NSPersonNameComponents *const nameComponents)
+{
+    if (nameComponents == nil) {
+        return {};
+    }
+
+    NSString *const name = [NSPersonNameComponentsFormatter localizedStringFromPersonNameComponents:nameComponents style:NSPersonNameComponentsFormatterStyleDefault options:0];
+    return QString::fromNSString(name);
+}
+
+}
