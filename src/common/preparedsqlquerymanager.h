042ad18ec82c52a738d3b4ce5fc981f1a24ddd5b
alex-z <blackslayer4@gmail.com>
2023-11-15 20:04:25 +0100

    Fixed incorrect move logic and tests.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/common/preparedsqlquerymanager.h
+++ src/common/preparedsqlquerymanager.h
@@ -107,13 +107,9 @@ public:
         GetE2EeLockedFolderQuery,
         GetE2EeLockedFoldersQuery,
         DeleteE2EeLockedFolderQuery,
-<<<<<<< HEAD
         ListAllTopLevelE2eeFoldersStatusLessThanQuery,
-=======
         MoveFilesInPathQuery,
->>>>>>> 6e3bb76cc (On folder move execute only one UPDATE query for all nested items.)
-
-        PreparedQueryCount
+        PreparedQueryCount,
     };
     PreparedSqlQueryManager() = default;
     /**
