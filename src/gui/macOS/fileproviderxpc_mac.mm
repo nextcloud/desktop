5cc8b6e7c2a3c376786b736403ce6758b15f335e
Claudio Cambra <claudio.cambra@nextcloud.com>
2023-11-21 17:38:01 +0800

    Add starter FileProviderXPC class on client side
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ src/gui/macOS/fileproviderxpc_mac.mm
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2022 by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+
+#include "fileproviderxpc.h"
+
+#include <QLoggingCategory>
+
+namespace OCC {
+
+namespace Mac {
+
+Q_LOGGING_CATEGORY(lcFileProviderXPC, "nextcloud.gui.macos.fileprovider.xpc", QtInfoMsg)
+
+FileProviderXPC::FileProviderXPC(QObject *parent)
+    : QObject{parent}
+{
+}
+
+} // namespace OCC
+
+} // namespace Mac
