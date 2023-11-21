947d56cb25a892e70bdf8736a59f52957cd68813
alex-z <blackslayer4@gmail.com>
2023-11-21 20:35:38 +0100

    Client status reporting feature. First iteration.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/common/common.cmake
+++ src/common/common.cmake
@@ -4,6 +4,7 @@
 set(common_SOURCES
     ${CMAKE_CURRENT_LIST_DIR}/checksums.cpp
     ${CMAKE_CURRENT_LIST_DIR}/checksumcalculator.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/clientstatusreportingrecord.cpp
     ${CMAKE_CURRENT_LIST_DIR}/filesystembase.cpp
     ${CMAKE_CURRENT_LIST_DIR}/ownsql.cpp
     ${CMAKE_CURRENT_LIST_DIR}/preparedsqlquerymanager.cpp
