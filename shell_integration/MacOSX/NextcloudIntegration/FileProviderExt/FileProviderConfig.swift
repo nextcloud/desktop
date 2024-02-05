f987bcd97aa58c12a6d785e2f221b76b6c7b0ede
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-05 18:15:45 +0800

    Add computed property to FileProviderConfig to get and set internal config
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/FileProviderConfig.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/FileProviderConfig.swift
@@ -15,4 +15,19 @@ struct FileProviderConfig {
 
     let domainIdentifier: NSFileProviderDomainIdentifier
 
+    private var internalConfig: [String: Any] {
+        get {
+            let defaults = UserDefaults.standard
+            if let settings = defaults.dictionary(forKey: domainIdentifier.rawValue) {
+                return settings
+            }
+            let dictionary: [String: Any] = [:]
+            defaults.setValue(dictionary, forKey: domainIdentifier.rawValue)
+            return dictionary
+        }
+        set {
+            let defaults = UserDefaults.standard
+            defaults.setValue(newValue, forKey: domainIdentifier.rawValue)
+        }
+    }
 }
