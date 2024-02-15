6b1683c3ae33edbacf5c1866a765f7e275eaa8fc
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-02-15 12:19:10 +0100

    Added more translations

--- translations/client_de_NMC.ts
+++ translations/client_de_NMC.ts
@@ -412,6 +412,131 @@
         <source>DATA_ANALYSIS</source>
         <translation>Analyse-Datenerfassung zur bedarfsgerechten Gestaltung</translation>
     </message>
+    <message>
+        <source>GENERAL_SETTINGS</source>
+        <translation>Allgemeine Einstellungen</translation>
+    </message>
+    <message>
+        <source>ADVANCED_SETTINGS</source>
+        <translation>Erweiterte Einstallungen</translation>
+    </message>
+    <message>
+        <source>UPDATES_SETTINGS</source>
+        <translation>Updates & Infos</translation>
+    </message>
+    <message>
+        <source>USED_STORAGE_%1</source>
+        <translation>Speicher zu %1% belegt</translation>
+    </message>
+<!-- From the original ts file -->
+    <message>
+        <source>%1 of %2 in use</source>
+        <translation>%1 von %2</translation>
+    </message>
+    <message>
+        <source>STORAGE_EXTENSION</source>
+        <translation>Speicherplatz erweitern</translation>
+    </message>
+    <message>
+        <source>LIVE_BACKUPS</source>
+        <translation>Live-Backups</translation>
+    </message>
+    <message>
+        <source>ADD_LIVE_BACKUP</source>
+        <translation>Live-Backup hinzufügen</translation>
+    </message>
+    <message>
+        <source>LIVE_BACKUPS_DESCRIPTION</source>
+        <translation>Synchronisieren Sie weitere beliebiege lokale Ordner in ihre MagentaCLOUD und schützen Sie damit ihre Onhalte kontienuierlich.</translation>
+    </message>
+    <message>
+        <source>YOUR_FOLDER_SYNC</source>
+        <translation>Ihre Ordner in Synchronisation</translation>
+    </message>
+    <message>
+        <source>E2E_ENCRYPTION</source>
+        <translation>E2E-Verschlüsselung</translation>
+    </message>
+    <message>
+        <source>More</source>
+        <translation>Mehr</translation>
+    </message>
+    <message>
+        <source>FOLDER_WIZARD_FOLDER_WARNING</source>
+        <translation>Der ausgewählte lokale Ordner auf Ihrem Computer liegt in einem übergeordneten Ordner, der bereits mit Ihrer MagentaCLOUD synchronisiert wird. Bitte wählen Sie einen anderen Ordner aus.</translation>
+    </message>
+    <message>
+        <source>ADD_SYNCHRONIZATION</source>
+        <translation>Synchronisierung hinzufügen</translation>
+    </message>
+    <message>
+        <source>ADD_LIVE_BACKUP_HEADLINE</source>
+        <translation>Live.Backup hinzufügen</translation>
+    </message>
+    <message>
+        <source>ADD_LIVE_BACKUP_PAGE1_DESCRIPTION</source>
+        <translation>Wählen Sie einen lokalen Ordner auf Ihrem Computer aus, den Sie mit MagentaCLOUD kontinuierlich synchronisieren und damit sichern möchten.</translation>
+    </message>
+    <message>
+        <source>ADD_LIVE_BACKUP_PAGE2_DESCRIPTION</source>
+        <translation>Wählen Sie bitte einen Ordner in Ihrer MagentaCLOUD aus, wo der lokale Ordner synchronisiert und gesichert werden soll. Sie können auch einen neuen Ordner erstellen und ihn entsprechend benennen.</translation>
+    </message>
+    <message>
+        <source>ADD_LIVE_BACKUP_PAGE3_DESCRIPTION</source>
+        <translation>Bitte wählen SIe die Unterordner ab, die nicht synchronisiert und gesichert werden sollen.</translation>
+    </message>
+    <message>
+        <source>START_NOW</source>
+        <translation>Jetzt starten</translation>
+    </message>
+    <message>
+        <source>ADVERT_DETAIL_TEXT_1</source>
+        <translation>Speichern SIe Ihre Fotos, Videos, Musik und Dokumente sicher in der MagentaCLOUD und greifen Sie jederzeit und von überall darauf zu - auch offline.</translation>
+    </message>
+    <message>
+        <source>ADVERT_HEADER_TEXT_1</source>
+        <translation>Sicher.Online.Speichern.</translation>
+    </message>
+    <message>
+        <source>ADVERT_HEADER_1</source>
+        <translation>MagentaCLOUD</translation>
+    </message>
+    <message>
+        <source>ADVERT_DETAIL_TEXT_3</source>
+        <translation>Teilen Sie auch Fotos und Videos ohne Größenbeschränkungen ganz einfach und bequem per Link mit Familie und Freunden.</translation>
+    </message>
+    <message>
+        <source>ADVERT_HEADER_TEXT_3</source>
+        <translation>Erlebnisse einfach teilen</translation>
+    </message>
+    <message>
+        <source>ADVERT_DETAIL_TEXT_2</source>
+        <translation>Machen Sie so viele Fotos und Videos wie Sie möchten und lassen Sie sich nicht von dem Speicherplatz auf Ihrem Gerät beschränken.</translation>
+    </message>
+    <message>
+        <source>ADVERT_HEADER_TEXT_2</source>
+        <translation>Fotos automatisch hochladen</translation>
+    </message>
+    <message>
+        <source>SETUP_HEADER_TEXT_1</source>
+        <translation>Melden Sie sich an um direkt loszulegen</translation>
+    </message>
+    <message>
+        <source>SETUP_DESCRIPTION_TEXT_1</source>
+        <translation>Wechseln SIe bitte zu Ihrem Browser und melden Sie sich dort an um ihr Konto zu verbinden. Oder Sie erstellen ein Konto mit dem für Sie passenden Tarif.</translation>
+    </message>
+    <message>
+        <source>SETUP_HEADER_TEXT_2</source>
+        <translation>Ihr lokaler Ordner für MagentaCLOUD</translation>
+    </message>
+    <message>
+        <source>SETUP_DESCRIPTION_TEXT_2</source>
+        <translation>Überprüfen Sie bitte den Speicherort und ändern Sie ihn, falls Sie schon einen bestehenden MagentaCLOUD Ordner aus einer früheren Installation wiederverwenden möchten.</translation>
+    </message>
+    <message>
+        <source>SETUP_CHANGE_STORAGE_LOCATION</source>
+        <translation>Speicherort ändern</translation>
+    </message>
 
 </context>
 </TS>
