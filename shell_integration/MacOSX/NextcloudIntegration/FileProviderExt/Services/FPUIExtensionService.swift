fd63f2ee3188af795ee5d058e8ba47cb60e7afcd
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-21 19:19:41 +0800

    Add starter FPUIExtensionService protocol
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/Services/FPUIExtensionService.swift
@@ -0,0 +1,16 @@
+//
+//  FPUIExtensionCommunicationProtocol.swift
+//  FileProviderExt
+//
+//  Created by Claudio Cambra on 21/2/24.
+//
+
+import Foundation
+
+let fpUiExtensionServiceName = NSFileProviderServiceName(
+    "com.nextcloud.desktopclient.FPUIExtensionService"
+)
+
+@objc protocol FPUIExtensionService {
+    
+}
