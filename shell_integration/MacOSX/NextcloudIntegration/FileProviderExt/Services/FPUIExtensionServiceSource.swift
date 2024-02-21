7aebdcd5c375ee43601787fe59c1555d01741ca4
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-21 20:29:03 +0800

    Add starter FPUIExtensionServiceSource
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/Services/FPUIExtensionServiceSource.swift
@@ -0,0 +1,41 @@
+//
+//  FPUIExtensionCommunicationService.swift
+//  FileProviderExt
+//
+//  Created by Claudio Cambra on 21/2/24.
+//
+
+import FileProvider
+import Foundation
+import OSLog
+
+class FPUIExtensionServiceSource: NSObject, NSFileProviderServiceSource, NSXPCListenerDelegate, FPUIExtensionService {
+    let listener = NSXPCListener.anonymous()
+    let serviceName = fpUiExtensionServiceName
+    let fpExtension: FileProviderExtension
+
+    init(fpExtension: FileProviderExtension) {
+        Logger.fpUiExtensionService.debug("Instantiating FPUIExtensionService service")
+        self.fpExtension = fpExtension
+        super.init()
+    }
+
+    func makeListenerEndpoint() throws -> NSXPCListenerEndpoint {
+        listener.delegate = self
+        listener.resume()
+        return listener.endpoint
+    }
+
+    func listener(
+        _ listener: NSXPCListener,
+        shouldAcceptNewConnection newConnection: NSXPCConnection
+    ) -> Bool {
+        newConnection.exportedInterface = NSXPCInterface(with: ClientCommunicationProtocol.self)
+        newConnection.exportedObject = self
+        newConnection.resume()
+        return true
+    }
+
+    //MARK: - FPUIExtensionService protocol methods
+
+}
