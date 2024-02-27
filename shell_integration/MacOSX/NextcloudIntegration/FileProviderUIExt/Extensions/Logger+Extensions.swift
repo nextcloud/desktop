9d741641c35c1a5d164bff0acad86ea67e204ed6
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-27 20:28:55 +0800

    Add item shares loading routine to ShareTableViewDataSource
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Extensions/Logger+Extensions.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Extensions/Logger+Extensions.swift
@@ -11,6 +11,7 @@ extension Logger {
     private static var subsystem = Bundle.main.bundleIdentifier!
 
     static let actionViewController = Logger(subsystem: subsystem, category: "actionViewController")
+    static let sharesDataSource = Logger(subsystem: subsystem, category: "sharesDataSource")
     static let shareViewController = Logger(subsystem: subsystem, category: "shareViewController")
 }
 
