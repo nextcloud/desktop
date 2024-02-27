7aa46324e4c399955e8b3f194c6f12483a7eb6af
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-27 14:44:15 +0800

    Add starter ItemSharesController
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/Shares/ItemSharesController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemSharesController.swift
+//  FileProviderExt
+//
+//  Created by Claudio Cambra on 27/2/24.
+//
+
+import FileProvider
+import Foundation
+
+class ItemSharesController {
+    let itemIdentifier: NSFileProviderItemIdentifier
+
+    init(itemIdentifier: NSFileProviderItemIdentifier) {
+        self.itemIdentifier = itemIdentifier
+    }
+}
