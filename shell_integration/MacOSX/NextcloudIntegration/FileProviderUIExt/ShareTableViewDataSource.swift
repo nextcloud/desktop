b421e378b8b3a791f568df05f198ac1953e519d2
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-27 20:28:09 +0800

    Add base properties to ShareTableViewDataSource
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareTableViewDataSource.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareTableViewDataSource.swift
@@ -5,4 +5,19 @@
 //  Created by Claudio Cambra on 27/2/24.
 //
 
-import Foundation
+import AppKit
+import FileProvider
+import NextcloudKit
+class ShareTableViewDataSource: NSObject, NSTableViewDataSource {
+    var sharesTableView: NSTableView? {
+        didSet {
+            sharesTableView?.dataSource = self
+            sharesTableView?.reloadData()
+        }
+    }
+    private var itemIdentifier: NSFileProviderItemIdentifier?
+    private var itemURL: URL?
+    private var shares: [NKShare] = [] {
+        didSet { sharesTableView?.reloadData() }
+    }
+}
