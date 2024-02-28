14ba5f589b61ff28303f77f8fec1c0dc3dadeee6
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-28 16:01:01 +0800

    Make share property central to display update of share table item view
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareTableItemView.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareTableItemView.swift
@@ -6,12 +6,25 @@
 //
 
 import AppKit
+import NextcloudKit
 
 class ShareTableItemView: NSTableCellView {
-    @IBOutlet weak var typeImageView: NSImageView!
-    @IBOutlet weak var label: NSTextField!
-    @IBOutlet weak var copyLinkButton: NSButton!
-    @IBOutlet weak var optionsButton: NSButton!
+    @IBOutlet private weak var typeImageView: NSImageView!
+    @IBOutlet private weak var label: NSTextField!
+    @IBOutlet private weak var copyLinkButton: NSButton!
+    @IBOutlet private weak var optionsButton: NSButton!
+
+    var share: NKShare? {
+        didSet {
+            guard let share = share else {
+                prepareForReuse()
+                return
+            }
+            typeImageView.image = share.typeImage
+            label.stringValue = share.displayString
+            copyLinkButton.isHidden = share.shareType != NKShare.ShareType.publicLink.rawValue
+        }
+    }
 
     override func prepareForReuse() {
         typeImageView.image = nil
