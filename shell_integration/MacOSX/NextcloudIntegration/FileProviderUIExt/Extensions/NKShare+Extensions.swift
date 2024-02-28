930106e7ac1cf0e9570e203276b4ec05d78870cc
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-02-28 15:59:53 +0800

    Add ShareType enum to NKShare extension
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Extensions/NKShare+Extensions.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Extensions/NKShare+Extensions.swift
@@ -8,4 +8,13 @@
 import NextcloudKit
 
 extension NKShare {
+    enum ShareType: Int {
+        case user = 0
+        case group = 1
+        case publicLink = 3
+        case email = 4
+        case federatedCloud = 6
+        case circle = 7
+        case talkConversation = 10
+    }
 }
