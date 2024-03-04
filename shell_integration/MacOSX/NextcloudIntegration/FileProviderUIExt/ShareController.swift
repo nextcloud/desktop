29262345cd3d20eeae133ea2f25f2f068a2e4f62
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-03-04 20:41:01 +0800

    Add functionality to share controller to save changes
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareController.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/ShareController.swift
@@ -5,15 +5,40 @@
 //  Created by Claudio Cambra on 4/3/24.
 //
 
+import Combine
 import Foundation
 import NextcloudKit
+import OSLog
 
-class ShareController {
-    let share: NKShare
+class ShareController: ObservableObject {
+    @Published private(set) var share: NKShare
     private let kit: NextcloudKit
 
     init(share: NKShare, kit: NextcloudKit) {
         self.share = share
         self.kit = kit
+    }
+
+    func save() async -> NKError? {
+        return await withCheckedContinuation { continuation in
+            kit.updateShare(
+                idShare: share.idShare,
+                password: share.password,
+                expireDate: share.expirationDateString ?? "",
+                permissions: share.permissions,
+                note: share.note,
+                label: share.label,
+                hideDownload: share.hideDownload,
+                attributes: share.attributes
+            ) { account, share, data, error in
+                Logger.shareController.info("Received update response: \(share?.url ?? "")")
+                defer { continuation.resume(returning: error) }
+                guard error == .success, let share = share else {
+                    Logger.shareController.error("Error updating save: \(error.errorDescription)")
+                    return
+                }
+                self.share = share
+            }
+        }
     }
 }
