238d1a45831cf821eaf6a239974aaa810eed01ce
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-03-27 19:25:37 +0800

    Re-run update sync paused state slot when folder list has changed
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/systray.cpp
+++ src/gui/systray.cpp
@@ -128,6 +128,7 @@ void Systray::create()
     hideWindow();
     emit activated(QSystemTrayIcon::ActivationReason::Unknown);
     slotUpdateSyncPausedState();
+    connect(FolderMan::instance(), &FolderMan::folderListChanged, this, &Systray::slotUpdateSyncPausedState);
 }
 
 void Systray::showWindow(WindowPosition position)
