22f4494c10b8f43d31394a475d6cf91154a9d495
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-03-27 19:25:11 +0800

    Make sure to emit syncIsPausedChanged in syncIsPaused setter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/systray.cpp
+++ src/gui/systray.cpp
@@ -576,6 +576,7 @@ void Systray::setSyncIsPaused(const bool syncIsPaused)
     } else {
         slotUnpauseAllFolders();
     }
+    emit syncIsPausedChanged();
 }
 
 /********************************************************************************************/
