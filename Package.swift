0e26fb7de315f643d42ace652404c535720a4bb7
Claudio Cambra <developer@claudiocambra.com>
2024-04-15 17:06:13 +0800

    Add dependencies and platforms to package def

--- Package.swift
+++ Package.swift
@@ -5,17 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "NextcloudFileProviderKit",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NextcloudFileProviderKit",
             targets: ["NextcloudFileProviderKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/nextcloud/NextcloudKit", .upToNextMajor(from: "2.9.9")),
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.33.0")),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NextcloudFileProviderKit"),
+            name: "NextcloudFileProviderKit",
+            dependencies: [
+                .product(name: "NextcloudKit", package: "NextcloudKit"),
+                .product(name: "RealmSwift", package: "realm-swift")]),
         .testTarget(
             name: "NextcloudFileProviderKitTests",
             dependencies: ["NextcloudFileProviderKit"]),
