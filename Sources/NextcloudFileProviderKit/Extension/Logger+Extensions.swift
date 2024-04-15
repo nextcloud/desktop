bb0cc80ebe81b9271c7de6df534d61474551619b
Claudio Cambra <developer@claudiocambra.com>
2024-04-15 17:02:49 +0800

    Add logger extension

---
+++ Sources/NextcloudFileProviderKit/Extension/Logger+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  Logger+Extensions.swift
+//  
+//
+//  Created by Claudio Cambra on 15/4/24.
+//
+
+import OSLog
+
+extension Logger {
+    static var subsystem = Bundle.main.bundleIdentifier!
+}
