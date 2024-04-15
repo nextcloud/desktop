094f5141f28409ac860143a12dbe108d29987485
Claudio Cambra <developer@claudiocambra.com>
2024-04-15 17:05:58 +0800

    Add account struct

---
+++ Sources/NextcloudFileProviderKit/Account.swift
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2022 by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+import Foundation
+
+fileprivate let AccountDictUsernameKey = "usernameKey"
+fileprivate let AccountDictPasswordKey = "passwordKey"
+fileprivate let AccountDictNcKitAccountKey = "ncKitAccountKey"
+fileprivate let AccountDictServerUrlKey = "serverUrlKey"
+fileprivate let AccountDictDavFilesUrlKey = "davFilesUrlKey"
+
+public struct Account: Equatable {
+    public static let webDavFilesUrlSuffix: String = "/remote.php/dav/files/"
+    public let username, password, ncKitAccount, serverUrl, davFilesUrl: String
+
+    public init(user: String, serverUrl: String, password: String) {
+        username = user
+        self.password = password
+        ncKitAccount = user + " " + serverUrl
+        self.serverUrl = serverUrl
+        davFilesUrl = serverUrl + Self.webDavFilesUrlSuffix + user
+    }
+
+    public init?(dictionary: Dictionary<String, String>) {
+        guard let username = dictionary[AccountDictUsernameKey],
+              let password = dictionary[AccountDictPasswordKey],
+              let ncKitAccount = dictionary[AccountDictNcKitAccountKey],
+              let serverUrl = dictionary[AccountDictServerUrlKey],
+              let davFilesUrl = dictionary[AccountDictDavFilesUrlKey]
+        else {
+            return nil
+        }
+
+        self.username = username
+        self.password = password
+        self.ncKitAccount = ncKitAccount
+        self.serverUrl = serverUrl
+        self.davFilesUrl = davFilesUrl
+    }
+
+    public func dictionary() -> Dictionary<String, String> {
+        return [
+            AccountDictUsernameKey: username,
+            AccountDictPasswordKey: password,
+            AccountDictNcKitAccountKey: ncKitAccount,
+            AccountDictServerUrlKey: serverUrl,
+            AccountDictDavFilesUrlKey: davFilesUrl
+        ]
+    }
+}
+
