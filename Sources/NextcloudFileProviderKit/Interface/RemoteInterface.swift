b5d93ac81cf088dfd74efaab3e891a17b196387b
Claudio Cambra <developer@claudiocambra.com>
2024-04-26 11:25:37 +0800

    Add RemoteInterface protocol

---
+++ Sources/NextcloudFileProviderKit/Interface/RemoteInterface.swift
@@ -0,0 +1,98 @@
+//
+//  RemoteInterface.swift
+//
+//
+//  Created by Claudio Cambra on 16/4/24.
+//
+
+import Alamofire
+import Foundation
+import NextcloudKit
+
+public enum EnumerateDepth: String {
+    case target = "0"
+    case targetAndDirectChildren = "1"
+    case targetAndAllChildren = "infinity"
+}
+
+public protocol RemoteInterface {
+
+    var account: Account { get }
+
+    func createFolder(
+        remotePath: String,
+        options: NKRequestOptions,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void
+    ) async -> (account: String, ocId: String?, date: NSDate?, error: Error?)
+
+    func upload(
+        remotePath: String,
+        localPath: String,
+        creationDate: Date?,
+        modificationDate: Date?,
+        options: NKRequestOptions,
+        requestHandler: @escaping (_ request: UploadRequest) -> Void,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void,
+        progressHandler: @escaping (_ progress: Progress) -> Void
+    ) async -> (
+        account: String,
+        ocId: String?,
+        etag: String?,
+        date: NSDate?,
+        size: Int64,
+        allHeaderFields: [AnyHashable: Any]?,
+        afError: AFError?, 
+        remoteError: Error?
+    )
+
+    func download(
+        remotePath: String,
+        localPath: String,
+        options: NKRequestOptions,
+        requestHandler: @escaping (_ request: DownloadRequest) -> Void,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void,
+        progressHandler: @escaping (_ progress: Progress) -> Void
+    ) async -> (
+        account: String,
+        etag: String?,
+        date: NSDate?,
+        length: Int64,
+        allHeaderFields: [AnyHashable: Any]?,
+        afError: AFError?,
+        remoteError: Error?
+    )
+
+    func enumerate(
+        remotePath: String,
+        depth: EnumerateDepth,
+        showHiddenFiles: Bool,
+        includeHiddenFiles: [String],
+        requestBody: Data?,
+        options: NKRequestOptions,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void
+    ) async -> (account: String, files: [NKFile], data: Data?, error: Error?)
+
+    func delete(
+        remotePath: String,
+        options: NKRequestOptions,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void
+    ) async -> (account: String, error: Error?)
+
+    func downloadThumbnail(
+        url: URL,
+        options: NKRequestOptions,
+        taskHandler: @escaping (_ task: URLSessionTask) -> Void
+    ) async -> (account: String, data: Data?, error: Error?)
+
+    func getInternalType(
+        fileName: String,
+        mimeType: String, directory: Bool
+    ) -> (
+        mimeType: String,
+        classFile: String,
+        iconName: String,
+        typeIdentifier: String,
+        fileNameWithoutExt: String,
+        ext: String
+    )
+}
