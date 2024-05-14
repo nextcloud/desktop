f62fb8ebae9481b4006144953ad658604a155908
Claudio Cambra <developer@claudiocambra.com>
2024-05-14 12:34:23 +0200

    Add test for item modification
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Tests/NextcloudFileProviderKitTests/ItemModifyTests.swift
@@ -0,0 +1,135 @@
+//
+//  ItemModifyTests.swift
+//
+//
+//  Created by Claudio Cambra on 13/5/24.
+//
+
+import FileProvider
+import NextcloudKit
+import RealmSwift
+import TestInterface
+import XCTest
+@testable import NextcloudFileProviderKit
+
+final class ItemModifyTests: XCTestCase {
+    static let account = Account(
+        user: "testUser", serverUrl: "https://mock.nc.com", password: "abcd"
+    )
+
+    lazy var rootItem = MockRemoteItem(
+        identifier: NSFileProviderItemIdentifier.rootContainer.rawValue,
+        name: "root",
+        remotePath: Self.account.davFilesUrl,
+        directory: true
+    )
+    static let dbManager = FilesDatabaseManager(realmConfig: .defaultConfiguration)
+
+    override func setUp() {
+        super.setUp()
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = name
+    }
+
+    override func tearDown() {
+        rootItem.children = []
+    }
+
+    func testModifyFileContents() async throws {
+        let remoteInterface = MockRemoteInterface(account: Self.account, rootItem: rootItem)
+        let remoteItem = MockRemoteItem(
+            identifier: "item",
+            versionIdentifier: "0",
+            name: "item.txt",
+            remotePath: Self.account.davFilesUrl + "/item.txt",
+            data: "Hello, World!".data(using: .utf8)
+        )
+        let remoteFolder = MockRemoteItem(
+            identifier: "folder",
+            name: "folder",
+            remotePath: Self.account.davFilesUrl + "/folder",
+            directory: true
+        )
+        rootItem.children = [remoteItem, remoteFolder]
+        remoteItem.parent = rootItem
+        remoteFolder.parent = rootItem
+
+        let folderMetadata = ItemMetadata()
+        folderMetadata.ocId = remoteFolder.identifier
+        folderMetadata.etag = remoteFolder.versionIdentifier
+        folderMetadata.directory = remoteFolder.directory
+        folderMetadata.name = remoteFolder.name
+        folderMetadata.fileName = remoteFolder.name
+        folderMetadata.fileNameView = remoteFolder.name
+        folderMetadata.serverUrl = Self.account.davFilesUrl
+        folderMetadata.urlBase = Self.account.serverUrl
+        folderMetadata.userId = Self.account.username
+        folderMetadata.user = Self.account.username
+
+        Self.dbManager.addItemMetadata(folderMetadata)
+
+        let itemMetadata = ItemMetadata()
+        itemMetadata.ocId = remoteItem.identifier
+        itemMetadata.etag = remoteItem.versionIdentifier
+        itemMetadata.name = remoteItem.name
+        itemMetadata.fileName = remoteItem.name
+        itemMetadata.fileNameView = remoteItem.name
+        itemMetadata.serverUrl = Self.account.davFilesUrl
+        itemMetadata.urlBase = Self.account.serverUrl
+        itemMetadata.userId = Self.account.username
+        itemMetadata.user = Self.account.username
+
+        Self.dbManager.addItemMetadata(itemMetadata)
+
+        let targetItemMetadata = ItemMetadata()
+        targetItemMetadata.ocId = remoteItem.identifier
+        targetItemMetadata.etag = remoteItem.identifier
+        targetItemMetadata.name = "item-renamed.txt" // Renamed
+        targetItemMetadata.fileName = "item-renamed.txt" // Renamed
+        targetItemMetadata.fileNameView = "item-renamed.txt" // Renamed
+        targetItemMetadata.serverUrl = Self.account.davFilesUrl + "/folder" // Move
+        targetItemMetadata.urlBase = Self.account.serverUrl
+        targetItemMetadata.userId = Self.account.username
+        targetItemMetadata.user = Self.account.username
+        targetItemMetadata.date = .init()
+
+        let item = Item(
+            metadata: itemMetadata,
+            parentItemIdentifier: .rootContainer,
+            remoteInterface: remoteInterface
+        )
+
+        let targetItem = Item(
+            metadata: targetItemMetadata,
+            parentItemIdentifier: .init(remoteFolder.identifier),
+            remoteInterface: remoteInterface
+        )
+        targetItem.dbManager = Self.dbManager
+
+        let newContents = "Hello, New World!".data(using: .utf8)
+        let newContentsUrl = FileManager.default.temporaryDirectory.appendingPathComponent("test")
+        try newContents?.write(to: newContentsUrl)
+
+        let (modifiedItemMaybe, error) = await item.modify(
+            itemTarget: targetItem,
+            changedFields: [.filename, .contents, .parentItemIdentifier, .contentModificationDate],
+            contents: newContentsUrl,
+            ncAccount: Self.account,
+            dbManager: Self.dbManager
+        )
+        XCTAssertNil(error)
+        let modifiedItem = try XCTUnwrap(modifiedItemMaybe)
+
+        XCTAssertEqual(modifiedItem.itemIdentifier, targetItem.itemIdentifier)
+        XCTAssertEqual(modifiedItem.filename, targetItem.filename)
+        XCTAssertEqual(modifiedItem.parentItemIdentifier, targetItem.parentItemIdentifier)
+        XCTAssertEqual(modifiedItem.contentModificationDate, targetItem.contentModificationDate)
+
+        XCTAssertFalse(remoteFolder.children.isEmpty)
+        XCTAssertEqual(remoteItem.data, newContents)
+        XCTAssertEqual(remoteItem.name, targetItemMetadata.fileName)
+        XCTAssertEqual(
+            remoteItem.remotePath, targetItemMetadata.serverUrl + "/" + targetItemMetadata.fileName
+        )
+
+    }
+}
