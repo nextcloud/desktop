c7caac02d697d9e9648da7444dea3fcf6e0625e2
Claudio Cambra <developer@claudiocambra.com>
2024-05-14 16:29:15 +0200

    Add test for enumerating a non-root folder
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/EnumeratorTests.swift
+++ Tests/NextcloudFileProviderKitTests/EnumeratorTests.swift
@@ -87,4 +87,91 @@ final class EnumeratorTests: XCTestCase {
         XCTAssertEqual(retrievedFolderItem.creationDate, remoteFolder.creationDate)
         XCTAssertEqual(retrievedFolderItem.contentModificationDate, remoteFolder.modificationDate)
     }
+
+    @MainActor
+    func testFolderEnumeration() async throws {
+        let remoteInterface = MockRemoteInterface(account: Self.account, rootItem: rootItem)
+        let remoteFolder = MockRemoteItem(
+            identifier: "folder",
+            versionIdentifier: "NEW",
+            name: "folder",
+            remotePath: Self.account.davFilesUrl + "/folder",
+            directory: true,
+            account: Self.account.ncKitAccount,
+            username: Self.account.username,
+            serverUrl: Self.account.serverUrl
+        )
+        let remoteItemA = MockRemoteItem(
+            identifier: "itemA",
+            name: "itemA",
+            remotePath: Self.account.davFilesUrl + "/folder/itemA",
+            account: Self.account.ncKitAccount,
+            username: Self.account.username,
+            serverUrl: Self.account.serverUrl
+        )
+        let remoteItemB = MockRemoteItem(
+            identifier: "itemB",
+            name: "itemB",
+            remotePath: Self.account.davFilesUrl + "/folder/itemB",
+            account: Self.account.ncKitAccount,
+            username: Self.account.username,
+            serverUrl: Self.account.serverUrl
+        )
+        rootItem.children = [remoteFolder]
+        remoteFolder.parent = rootItem
+        remoteFolder.children = [remoteItemA, remoteItemB]
+        remoteItemA.parent = remoteFolder
+        remoteItemB.parent = remoteFolder
+
+        let oldEtag = "OLD"
+        let folderMetadata = ItemMetadata()
+        folderMetadata.ocId = remoteFolder.identifier
+        folderMetadata.etag = oldEtag
+        folderMetadata.name = remoteFolder.name
+        folderMetadata.fileName = remoteFolder.name
+        folderMetadata.fileNameView = remoteFolder.name
+        folderMetadata.serverUrl = Self.account.davFilesUrl
+        folderMetadata.account = Self.account.ncKitAccount
+        folderMetadata.user = Self.account.username
+        folderMetadata.userId = Self.account.username
+        folderMetadata.urlBase = Self.account.serverUrl
+
+        Self.dbManager.addItemMetadata(folderMetadata)
+        XCTAssertNotNil(Self.dbManager.itemMetadataFromOcId(folderMetadata.ocId))
+
+        let enumerator = Enumerator(
+            enumeratedItemIdentifier: .init(remoteFolder.identifier),
+            ncAccount: Self.account,
+            remoteInterface: remoteInterface,
+            dbManager: Self.dbManager
+        )
+        let observer = MockEnumerationObserver(enumerator: enumerator)
+        try await observer.enumerateItems()
+        XCTAssertEqual(observer.items.count, 2)
+
+        // A pass of enumerating a target should update the target too. Let's check.
+        let dbFolderMetadata = try XCTUnwrap(
+            Self.dbManager.itemMetadataFromOcId(folderMetadata.ocId)
+        )
+        let storedFolderItem = try XCTUnwrap(
+            Item.storedItem(
+                identifier: .init(folderMetadata.ocId),
+                remoteInterface: remoteInterface,
+                dbManager: Self.dbManager
+            )
+        )
+        storedFolderItem.dbManager = Self.dbManager
+        XCTAssertEqual(dbFolderMetadata.etag, remoteFolder.versionIdentifier)
+        XCTAssertNotEqual(dbFolderMetadata.etag, oldEtag)
+        XCTAssertEqual(storedFolderItem.childItemCount?.intValue, remoteFolder.children.count)
+
+        let retrievedItemA = try XCTUnwrap(
+            observer.items.first(where: { $0.itemIdentifier.rawValue == remoteItemA.identifier })
+        )
+        XCTAssertEqual(retrievedItemA.itemIdentifier.rawValue, remoteItemA.identifier)
+        XCTAssertEqual(retrievedItemA.filename, remoteItemA.name)
+        XCTAssertEqual(retrievedItemA.parentItemIdentifier.rawValue, remoteFolder.identifier)
+        XCTAssertEqual(retrievedItemA.creationDate, remoteItemA.creationDate)
+        XCTAssertEqual(retrievedItemA.contentModificationDate, remoteItemA.modificationDate)
+    }
 }
