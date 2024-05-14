b9ed3c201df44e44073cd814a31234c57cc4fbc5
Claudio Cambra <developer@claudiocambra.com>
2024-05-14 15:15:18 +0200

    Add a MockEnumerationObserver
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Tests/Interface/MockEnumerationObserver.swift
@@ -0,0 +1,43 @@
+//
+//  MockEnumerationObserver.swift
+//
+//
+//  Created by Claudio Cambra on 14/5/24.
+//
+
+import FileProvider
+import Foundation
+
+public class MockEnumerationObserver: NSObject, NSFileProviderEnumerationObserver {
+    public var items: [NSFileProviderItem] = []
+    private var error: Error?
+    private var isComplete = false
+    private var enumerator: NSFileProviderEnumerator
+
+    public init(enumerator: NSFileProviderEnumerator) {
+        self.enumerator = enumerator
+    }
+
+    public func didEnumerate(_ items: [NSFileProviderItem]) {
+        self.items.append(contentsOf: items)
+    }
+
+    public func finishEnumerating(upTo nextPage: NSFileProviderPage?) {
+        isComplete = true
+    }
+
+    public func finishEnumeratingWithError(_ error: Error) {
+        self.error = error
+    }
+
+    public func enumerateItems() async throws {
+        let startPage = NSFileProviderPage.initialPageSortedByDate as NSFileProviderPage
+        enumerator.enumerateItems(for: self, startingAt: startPage)
+        while !isComplete {
+            try await Task.sleep(nanoseconds: 1_000_000)
+        }
+        if let error {
+            throw error
+        }
+    }
+}
