9821903d94e51fc89c1697f2e00c767f8c12fb6d
Claudio Cambra <developer@claudiocambra.com>
2024-05-15 15:46:17 +0200

    Fix handling of root container metadata
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Item/Item.swift
+++ Sources/NextcloudFileProviderKit/Item/Item.swift
@@ -157,8 +157,6 @@ public class Item: NSObject, NSFileProviderItem {
         metadata.account = ncKit.nkCommonInstance.account
         metadata.directory = true
         metadata.ocId = NSFileProviderItemIdentifier.rootContainer.rawValue
-        metadata.fileName = "root"
-        metadata.fileNameView = "root"
         metadata.serverUrl = ncKit.nkCommonInstance.urlBase
         metadata.classFile = NKCommon.TypeClassFile.directory.rawValue
         return Item(metadata: metadata, parentItemIdentifier: .rootContainer, ncKit: ncKit)
