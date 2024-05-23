c9dada4843f7c38be2aabfd027ac17a792a2394f
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-05-23 23:56:45 +0800

    Add starter EditLocallyVerificationJob
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ src/gui/editlocallyverificationjob.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "editlocallyverificationjob.h"
+
+#include <QLoggingCategory>
+
+namespace OCC
+{
+
+Q_LOGGING_CATEGORY(lcEditLocallyVerificationJob, "nextcloud.gui.editlocallyverificationjob", QtInfoMsg)
+
+EditLocallyVerificationJob::EditLocallyVerificationJob(const AccountStatePtr &accountState, const QString &relPath, const QString &token, QObject *const parent)
+    : QObject(parent)
+    , _accountState(accountState)
+    , _relPath(relPath)
+    , _token(token)
+{
+}
+
+}
---
+++ src/gui/editlocallyverificationjob.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) by Claudio Cambra <claudio.cambra@nextcloud.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#pragma once
+
+#include <QObject>
+
+#include "accountstate.h"
+
+namespace OCC {
+
+class EditLocallyVerificationJob : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit EditLocallyVerificationJob(const AccountStatePtr &accountState,
+                                        const QString &relPath,
+                                        const QString &token,
+                                        QObject *const parent = nullptr);
+
+signals:
+    void error(const QString &message, const QString &informativeText);
+    void finished();
+
+private:
+    AccountStatePtr _accountState;
+    QString _relPath; // full remote path for a file (as on the server)
+    QString _token;
+};
+
+}
