298a5ca4e386631b59d7c52381c5a26b86aebc34
Camila Ayres <hello@camilasan.com>
2024-06-03 22:14:00 +0200

    NextcloudDev is not a branded client.
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- src/libsync/theme.cpp
+++ src/libsync/theme.cpp
@@ -116,7 +116,7 @@ QString Theme::statusHeaderText(SyncResult::Status status) const
 
 bool Theme::isBranded() const
 {
-    return appNameGUI() != QStringLiteral("Nextcloud");
+    return (appNameGUI() != QStringLiteral("Nextcloud") && appNameGUI() != QStringLiteral("NextcloudDev"));
 }
 
 QString Theme::appNameGUI() const
