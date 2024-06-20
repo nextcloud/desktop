9a0095c84e5cdd4d700ed3b0359476271f693714
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-06-20 14:25:56 +0800

    Make installIfMissing throwing in mac crafter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/mac-crafter/Sources/Utils/Install.swift
+++ admin/osx/mac-crafter/Sources/Utils/Install.swift
@@ -14,18 +14,21 @@
 
 import Foundation
 
+enum InstallError: Error {
+    case failedToInstall(String)
+}
+
 func installIfMissing(
     _ command: String,
     _ installCommand: String,
     installCommandEnv: [String: String]? = nil
-) {
+) throws {
     if commandExists(command) {
         print("\(command) is installed.")
     } else {
         print("\(command) is missing. Installing...")
         guard shell(installCommand, env: installCommandEnv) == 0 else {
-            print("Failed to install \(command).")
-            exit(1)
+            throw InstallError.failedToInstall("Failed to install \(command).")
         }
         print("\(command) installed.")
     }
