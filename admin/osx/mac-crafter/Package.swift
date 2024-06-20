8fa86217cbda7fdd0eaf1ba48273a39ee3e16c73
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-06-20 14:58:02 +0800

    Fix dependencies for mac-crafter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/mac-crafter/Package.swift
+++ admin/osx/mac-crafter/Package.swift
@@ -14,6 +14,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(name: "mac-crafter"),
+        .executableTarget(
+            name: "mac-crafter",
+            dependencies: [.product(name: "ArgumentParser", package: "swift-argument-parser")]
+        ),
     ]
 )
