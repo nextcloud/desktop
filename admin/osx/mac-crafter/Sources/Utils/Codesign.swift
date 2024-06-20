922748755fe6a88a3a1ecc4e6342f86b7c4dfa40
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-06-20 19:38:33 +0800

    Extract app bundle codesigning procedure to separate file in mac crafter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/mac-crafter/Sources/Utils/Codesign.swift
+++ admin/osx/mac-crafter/Sources/Utils/Codesign.swift
@@ -18,6 +18,10 @@ enum CodeSigningError: Error {
     case failedToCodeSign(String)
 }
 
+enum AppBundleSigningError: Error {
+    case couldNotEnumeratePlugins(String)
+}
+
 func codesign(
     identity: String,
     path: String,
@@ -28,4 +32,33 @@ func codesign(
     guard shell(command) == 0 else {
         throw CodeSigningError.failedToCodeSign("Failed to code-sign \(path).")
     }
+}
+
+func codesignClientAppBundle(
+    at clientAppDir: String, withCodeSignIdentity codeSignIdentity: String
+) throws {
+    print("Code-signing Nextcloud Desktop Client libraries and frameworks...")
+
+    let clientFrameworksDir = "\(clientAppDir)/Contents/Frameworks"
+    let fm = FileManager.default
+    let clientLibs = try fm.contentsOfDirectory(atPath: clientFrameworksDir)
+    for lib in clientLibs {
+        guard isLibrary(lib) else { continue }
+        try codesign(identity: codeSignIdentity, path: "\(clientFrameworksDir)/\(lib)")
+    }
+
+    let clientPluginsDir = "\(clientAppDir)/Contents/PlugIns"
+    guard let clientPluginsEnumerator = fm.enumerator(atPath: clientPluginsDir) else {
+        throw AppBundleSigningError.couldNotEnumeratePlugins(
+            "Failed to list craft plugins directory at \(clientPluginsDir)."
+        )
+    }
+
+    for case let plugin as String in clientPluginsEnumerator {
+        guard isLibrary(plugin) else { continue }
+        try codesign(identity: codeSignIdentity, path: "\(clientPluginsDir)/\(plugin)")
+    }
+
+    print("Code-signing Nextcloud Desktop Client app bundle...")
+    try codesign(identity: codeSignIdentity, path: clientAppDir)
 }
