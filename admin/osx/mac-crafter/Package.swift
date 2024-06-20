89a113a436ac57772e4e6424486c81142f6d6eca
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-06-20 14:16:58 +0800

    Implement basic argumentparser in mac crafter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/mac-crafter/Package.swift
+++ admin/osx/mac-crafter/Package.swift
@@ -4,13 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "builder",
+    name: "mac-crafter",
     platforms: [
         .macOS(.v11),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.4.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(name: "builder"),
+        .executableTarget(name: "mac-crafter"),
     ]
 )
