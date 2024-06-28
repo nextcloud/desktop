823c57b7bca93a156c963c5761dd6090e6263e58
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-06-28 17:54:27 +0800

    Add proxy needs auth property to account
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/libsync/account.h
+++ src/libsync/account.h
@@ -94,6 +94,7 @@ class OWNCLOUDSYNC_EXPORT Account : public QObject
     Q_PROPERTY(QNetworkProxy::ProxyType proxyType READ proxyType WRITE setProxyType NOTIFY proxyTypeChanged)
     Q_PROPERTY(QString proxyHostName READ proxyHostName WRITE setProxyHostName NOTIFY proxyHostNameChanged)
     Q_PROPERTY(int proxyPort READ proxyPort WRITE setProxyPort NOTIFY proxyPortChanged)
+    Q_PROPERTY(bool proxyNeedsAuth READ proxyNeedsAuth WRITE setProxyNeedsAuth NOTIFY proxyNeedsAuthChanged)
 
 public:
     enum class AccountNetworkProxySetting {
@@ -368,6 +369,9 @@ public:
     [[nodiscard]] int proxyPort() const;
     void setProxyPort(int port);
 
+    [[nodiscard]] bool proxyNeedsAuth() const;
+    void setProxyNeedsAuth(bool needsAuth);
+
 public slots:
     /// Used when forgetting credentials
     void clearQNAMCache();
@@ -416,6 +420,7 @@ signals:
     void proxyTypeChanged();
     void proxyHostNameChanged();
     void proxyPortChanged();
+    void proxyNeedsAuthChanged();
 
 protected Q_SLOTS:
     void slotCredentialsFetched();
@@ -496,6 +501,7 @@ private:
     QNetworkProxy::ProxyType _proxyType = QNetworkProxy::NoProxy;
     QString _proxyHostName;
     int _proxyPort = 0;
+    bool _proxyNeedsAuth = false;
     /* IMPORTANT - remove later - FIXME MS@2019-12-07 -->
      * TODO: For "Log out" & "Remove account": Remove client CA certs and KEY!
      *
