4f63a5079873b2f064f1c8800ed384d40a1ae11f
Claudio Cambra <developer@claudiocambra.com>
2024-07-24 20:33:50 +0800

    Add test for getting correct contenttype of packages and bundles in item

--- Tests/NextcloudFileProviderKitTests/ItemPropertyTests.swift
+++ Tests/NextcloudFileProviderKitTests/ItemPropertyTests.swift
@@ -86,4 +86,52 @@ final class ItemPropertyTests: XCTestCase {
         )
         XCTAssertEqual(item.contentType, UTType.folder)
     }
+
+    func testMetadataPackageContentType() {
+        let metadata = ItemMetadata()
+        metadata.ocId = "test-id"
+        metadata.etag = "test-etag"
+        metadata.name = "test.zip"
+        metadata.fileName = "test.zip"
+        metadata.fileNameView = "test.zip"
+        metadata.serverUrl = Self.account.davFilesUrl
+        metadata.urlBase = Self.account.serverUrl
+        metadata.userId = Self.account.username
+        metadata.user = Self.account.username
+        metadata.date = .init()
+        metadata.size = 12
+        metadata.directory = true
+        metadata.contentType = UTType.package.identifier
+
+        let item = Item(
+            metadata: metadata,
+            parentItemIdentifier: .rootContainer,
+            remoteInterface: MockRemoteInterface(account: Self.account)
+        )
+        XCTAssertEqual(item.contentType, UTType.package)
+    }
+
+    func testMetadataBundleContentType() {
+        let metadata = ItemMetadata()
+        metadata.ocId = "test-id"
+        metadata.etag = "test-etag"
+        metadata.name = "test.app"
+        metadata.fileName = "test.key"
+        metadata.fileNameView = "test.key"
+        metadata.serverUrl = Self.account.davFilesUrl
+        metadata.urlBase = Self.account.serverUrl
+        metadata.userId = Self.account.username
+        metadata.user = Self.account.username
+        metadata.date = .init()
+        metadata.size = 12
+        metadata.directory = true
+        metadata.contentType = UTType.bundle.identifier
+
+        let item = Item(
+            metadata: metadata,
+            parentItemIdentifier: .rootContainer,
+            remoteInterface: MockRemoteInterface(account: Self.account)
+        )
+        XCTAssertEqual(item.contentType, UTType.bundle)
+    }
 }
