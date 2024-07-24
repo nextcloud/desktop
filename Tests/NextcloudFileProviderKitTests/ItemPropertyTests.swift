5c3b59008efaa9874f4eda4d83a2b98c93ae0b9c
Claudio Cambra <developer@claudiocambra.com>
2024-07-24 20:23:30 +0800

    Add basic test for getting content type from metadata in item

---
+++ Tests/NextcloudFileProviderKitTests/ItemPropertyTests.swift
@@ -0,0 +1,43 @@
+//
+//  ItemPropertyTests.swift
+//
+//
+//  Created by Claudio Cambra on 24/7/24.
+//
+
+import FileProvider
+import NextcloudKit
+import RealmSwift
+import TestInterface
+import UniformTypeIdentifiers
+import XCTest
+@testable import NextcloudFileProviderKit
+
+final class ItemPropertyTests: XCTestCase {
+    static let account = Account(
+        user: "testUser", serverUrl: "https://mock.nc.com", password: "abcd"
+    )
+
+    func testMetadataContentType() {
+        let metadata = ItemMetadata()
+        metadata.ocId = "test-id"
+        metadata.etag = "test-etag"
+        metadata.name = "test.txt"
+        metadata.fileName = "test.txt"
+        metadata.fileNameView = "test.txt"
+        metadata.serverUrl = Self.account.davFilesUrl
+        metadata.urlBase = Self.account.serverUrl
+        metadata.userId = Self.account.username
+        metadata.user = Self.account.username
+        metadata.date = .init()
+        metadata.contentType = UTType.text.identifier
+        metadata.size = 12
+
+        let item = Item(
+            metadata: metadata,
+            parentItemIdentifier: .rootContainer,
+            remoteInterface: MockRemoteInterface(account: Self.account)
+        )
+        XCTAssertEqual(item.contentType, UTType.text)
+    }
+}
