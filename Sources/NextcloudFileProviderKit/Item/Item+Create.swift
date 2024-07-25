e038ff55c08ace5494e8f9ed4622bfcc5506b087
Claudio Cambra <developer@claudiocambra.com>
2024-07-25 17:07:32 +0800

    Simplify post-create bundle remote scan by using enumerator method

--- Sources/NextcloudFileProviderKit/Item/Item+Create.swift
+++ Sources/NextcloudFileProviderKit/Item/Item+Create.swift
@@ -325,60 +325,30 @@ extension Item {
             }
         }
 
-        var directoryMetadatas = [ItemMetadata]()
         for remoteDirectoryPath in remoteDirectoriesPaths {
             // After everything, check into what the final state is of each folder now
-            let (_, files, _, readError) = await remoteInterface.enumerate(
-                remotePath: remoteDirectoryPath,
-                depth: .targetAndDirectChildren,
-                showHiddenFiles: true,
-                includeHiddenFiles: [],
-                requestBody: nil,
-                options: .init(),
-                taskHandler: { task in
-                    if let domain {
-                        NSFileProviderManager(for: domain)?.register(
-                            task,
-                            forItemWithIdentifier: rootItem.itemIdentifier,
-                            completionHandler: { _ in }
-                        )
-                    }
-                }
+            Self.logger.debug("Reading bpi folder at: \(remoteDirectoryPath, privacy: .public)")
+            let (_, _, _, _, readError) = await Enumerator.readServerUrl(
+                remoteDirectoryPath,
+                ncAccount: ncAccount,
+                remoteInterface: remoteInterface,
+                dbManager: dbManager
             )
 
-            guard readError == .success else {
+            if let readError, readError != .success {
                 Self.logger.error(
                     """
-                    Could not read new bpi folder at: \(remotePath, privacy: .public),
-                    received error: \(readError.errorCode, privacy: .public)
-                    \(readError.errorDescription, privacy: .public)
+                    Could not read bpi folder at: \(remotePath, privacy: .public),
+                    received error: \(readError.errorDescription, privacy: .public)
                     """
                 )
                 throw remoteErrorToThrow(readError)
             }
-
-            let (directoryMetadata, allMetadatas) = await withCheckedContinuation {
-                continuation in
-                let account = remoteInterface.account.ncKitAccount
-                return ItemMetadata.metadatasFromDirectoryReadNKFiles(
-                    files, account: account
-                ) { directoryMetadata, _, allMetadatas in
-                    continuation.resume(returning: (directoryMetadata, allMetadatas))
-                }
-            }
-
-            dbManager.addItemMetadata(directoryMetadata)
-            directoryMetadatas.append(ItemMetadata(value: directoryMetadata))
-
-            for metadata in allMetadatas {
-                // Skip dirs, we will scan child dirs anyway
-                guard !metadata.directory else { continue }
-                dbManager.addItemMetadata(metadata)
-                directoryMetadatas.append(ItemMetadata(value: metadata))
-            }
         }
 
-        guard let bundleRootMetadata = directoryMetadatas.first else {
+        guard let bundleRootMetadata = dbManager.itemMetadata(
+            account: ncAccount.ncKitAccount, locatedAtRemoteUrl: remotePath
+        ) else {
             Self.logger.error(
                 """
                 Could not find directory metadata for bundle or package at: \(contentsPath, privacy: .public)
