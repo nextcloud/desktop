9ee4502039cf5d3b10be75672698768a9c821e1f
Claudio Cambra <developer@claudiocambra.com>
2024-07-26 16:35:56 +0800

    Add method to download the internal contents of a directory when downloading a directory type item

--- Sources/NextcloudFileProviderKit/Item/Item+Fetch.swift
+++ Sources/NextcloudFileProviderKit/Item/Item+Fetch.swift
@@ -11,7 +11,90 @@ import NextcloudKit
 import OSLog
 
 public extension Item {
-    
+    private func fetchDirectoryContents(
+        directoryLocalPath: String,
+        directoryRemotePath: String,
+        domain: NSFileProviderDomain?,
+        progress: Progress
+    ) async throws {
+        // Download *everything* within this directory. What we do:
+        // 1. Enumerate the contents of the directory
+        // 2. Download everything within this directory
+        // 3. Detect child directories
+        // 4. Repeat 1 -> 3 for each child directory
+        var remoteDirectoryPaths = [directoryRemotePath]
+        while !remoteDirectoryPaths.isEmpty {
+            let remoteDirectoryPath = remoteDirectoryPaths.removeFirst()
+            let (metadatas, _, _, _, readError) = await Enumerator.readServerUrl(
+                remoteDirectoryPath,
+                ncAccount: remoteInterface.account,
+                remoteInterface: remoteInterface,
+                dbManager: dbManager
+            )
+
+            if let readError {
+                Self.logger.error(
+                    """
+                    Could not enumerate directory contents for
+                    \(self.metadata.fileName, privacy: .public)
+                    at \(remoteDirectoryPath, privacy: .public)
+                    error: \(readError.errorCode, privacy: .public)
+                    \(readError.errorDescription, privacy: .public)
+                    """
+                )
+                throw readError.fileProviderError ?? NSFileProviderError(.cannotSynchronize)
+            }
+
+            guard let metadatas else {
+                Self.logger.error(
+                    """
+                    Could not fetch directory contents for
+                    \(self.metadata.fileName, privacy: .public)
+                    at \(remoteDirectoryPath, privacy: .public), received nil metadatas
+                    """
+                )
+                throw NSFileProviderError(.cannotSynchronize)
+            }
+
+            for metadata in metadatas {
+                let remotePath = metadata.serverUrl + "/" + metadata.fileName
+                if metadata.directory {
+                    remoteDirectoryPaths.append(remotePath)
+                }
+                let relativePath =
+                    remotePath.replacingOccurrences(of: directoryRemotePath, with: "")
+                let childLocalPath = directoryLocalPath + relativePath
+                let (_, etag, date, _, _, _, error) = await remoteInterface.download(
+                    remotePath: remotePath,
+                    localPath: childLocalPath,
+                    options: .init(),
+                    requestHandler: { progress.setHandlersFromAfRequest($0) },
+                    taskHandler: { task in
+                        if let domain {
+                            NSFileProviderManager(for: domain)?.register(
+                                task,
+                                forItemWithIdentifier: self.itemIdentifier,
+                                completionHandler: { _ in }
+                            )
+                        }
+                    }, progressHandler: { _ in }
+                )
+
+                if error != .success {
+                    Self.logger.error(
+                        """
+                        Could not acquire contents of item: \(metadata.fileName, privacy: .public)
+                        at \(remotePath, privacy: .public)
+                        error: \(error.errorCode, privacy: .public)
+                        \(error.errorDescription, privacy: .public)
+                        """
+                    )
+                    throw error.fileProviderError ?? NSFileProviderError(.cannotSynchronize)
+                }
+            }
+        }
+    }
+
     func fetchContents(
         domain: NSFileProviderDomain? = nil,
         progress: Progress = .init(),
