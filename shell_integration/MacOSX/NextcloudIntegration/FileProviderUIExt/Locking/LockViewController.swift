73674bf6331764dafd94e8ce33759812d1088020
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-07-30 16:18:58 +0800

    Add basic LockViewController components
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Locking/LockViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LockViewController.swift
+//  FileProviderUIExt
+//
+//  Created by Claudio Cambra on 30/7/24.
+//
+
+import AppKit
+import FileProvider
+import NextcloudKit
+import OSLog
+import QuickLookThumbnailing
+
+class LockViewController: NSViewController {
+
+}
