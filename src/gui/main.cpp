690ebccbe36aeb673935a2894adcfb3aa9788c98
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2024-08-07 21:28:27 +0200

    set the windows native qml style on windows systems
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -73,24 +73,15 @@ int main(int argc, char **argv)
 
     auto style = QStringLiteral("Fusion");
 
-#ifdef Q_OS_MAC
+#if defined Q_OS_MAC
     style = QStringLiteral("macOS");
+#elif defined Q_OS_WIN
+    style = QStringLiteral("Windows");
 #endif
 
-    OCC::Application app(argc, argv);
-
-#ifdef Q_OS_WIN
-    // The Windows style still has pixelated elements with Qt 5.6,
-    // it's recommended to use the Fusion style in this case, even
-    // though it looks slightly less native. Check here after the
-    // QApplication was constructed, but before any QWidget is
-    // constructed.
-    if (app.devicePixelRatio() > 1) {
-        style = "Windows";
-    }
-#endif // Q_OS_WIN
-
     QQuickStyle::setStyle(style);
+
+    OCC::Application app(argc, argv);
 
 #ifndef Q_OS_WIN
     signal(SIGPIPE, SIG_IGN);
