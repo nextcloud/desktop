8577a73b8e469eef584b982085a292b741fcc22b
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-09-19 21:00:15 +0800

    Add flag to build pkg in mac-crafter
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- admin/osx/mac-crafter/Sources/main.swift
+++ admin/osx/mac-crafter/Sources/main.swift
@@ -86,6 +86,9 @@ struct Build: ParsableCommand {
     @Flag(help: "Run a full rebuild.")
     var fullRebuild = false
 
+    @Flag(help: "Create an installer package.")
+    var package = false
+
     mutating func run() throws {
         print("Configuring build tooling.")
 
@@ -175,6 +178,7 @@ struct Build: ParsableCommand {
 
         let allOptionsString = craftOptions.map({ "--options \"\($0)\"" }).joined(separator: " ")
 
+        let buildWorkPath = "\(buildPath)/\(craftTarget)/build"
         let clientBuildDir = "\(buildPath)/\(craftTarget)/build/\(craftBlueprintName)"
         if fullRebuild {
             do {
@@ -208,6 +212,11 @@ struct Build: ParsableCommand {
             try fm.removeItem(atPath: "\(productPath)/\(appName).app")
         }
         try fm.copyItem(atPath: clientAppDir, toPath: "\(productPath)/\(appName).app")
+
+        if package {
+            let packagePath =
+                try buildPackage(buildWorkPath: buildWorkPath, productPath: productPath)
+        }
 
         print("Done!")
     }
