c462bd46ffa409432a5a175327c0099951349f83
Erik Verbruggen <erik@verbruggen.consulting>
2024-09-24 16:17:22 +0200

    Fix count of synced spaces
    When a new account is added, the settings page for the account is
    created with the account in the disconnected state. At that point, it is
    not yet known if the server supports spaces, so there is no space
    manager yet, and the update signal is not connected. Moving the signal
    connect to `slotAccountStateChanged` for the `Connected` state fixes the
    issue.
    
    Fixes: #11889

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -76,9 +76,6 @@ AccountSettings::AccountSettings(const AccountStatePtr &accountState, QWidget *p
     ui->quickWidget->setOCContext(QUrl(QStringLiteral("qrc:/qt/qml/org/ownCloud/gui/qml/FolderDelegate.qml")), this);
 
     connect(FolderMan::instance(), &FolderMan::folderListChanged, _model, &FolderStatusModel::resetFolders);
-    if (accountsState()->supportsSpaces()) {
-        connect(accountsState()->account()->spacesManager(), &GraphApi::SpacesManager::updated, this, &AccountSettings::slotSpacesUpdated);
-    }
 
     ui->connectionStatusLabel->clear();
 
@@ -488,7 +485,8 @@ void AccountSettings::slotAccountStateChanged()
         }
         showConnectionLabel(tr("Connected"), icon, errors);
         if (accountsState()->supportsSpaces()) {
-            connect(accountsState()->account()->spacesManager(), &GraphApi::SpacesManager::updated, this, &AccountSettings::slotSpacesUpdated);
+            connect(accountsState()->account()->spacesManager(), &GraphApi::SpacesManager::updated, this, &AccountSettings::slotSpacesUpdated,
+                Qt::UniqueConnection);
             slotSpacesUpdated();
         }
         break;
