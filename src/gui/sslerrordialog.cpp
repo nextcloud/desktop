f6c3065274abf4b996cae342266bfd7d977bbfce
Claudio Luck <c.luck@datact.ch>
2024-09-30 16:38:31 +0200

    [stable-3.12] Feat: silence SSL errors
    Signed-off-by: Claudio Luck <c.luck@datact.ch>

--- src/gui/sslerrordialog.cpp
+++ src/gui/sslerrordialog.cpp
@@ -39,19 +39,61 @@ bool SslDialogErrorHandler::handleErrors(QList<QSslError> errors, const QSslConf
         return false;
     }
 
-    SslErrorDialog dlg(account);
     // whether the failing certs have previously been accepted
-    if (dlg.checkFailingCertsKnown(errors)) {
-        *certs = dlg.unknownCerts();
+    if (this->checkFailingCertsKnown(errors, account)) {
+        *certs = this->unknownCerts();
         return true;
     }
-    // whether the user accepted the certs
-    if (dlg.exec() == QDialog::Accepted) {
-        if (dlg.trustConnection()) {
-            *certs = dlg.unknownCerts();
-            return true;
+
+#if 1
+    QByteArray feature_test = qgetenv("NEXTCLOUD_TEST_SSL_SILENT");
+    if (feature_test.isEmpty() || feature_test.toInt() == 0) {
+        SslErrorDialog dlg(account);
+        dlg.showFailingCertsKnown(
+            errors,
+            _unknownCerts,
+            _errorStrings,
+            _additionalErrorStrings
+        );
+        // whether the user accepted the certs
+        if (dlg.exec() == QDialog::Accepted) {
+            if (dlg.trustConnection()) {
+                *certs = this->unknownCerts();
+                return true;
+            }
         }
     }
+#endif
+    return false;
+}
+
+bool SslDialogErrorHandler::checkFailingCertsKnown(const QList<QSslError> &errors, AccountPtr account)
+{
+    // check if unknown certs caused errors.
+    _unknownCerts.clear();
+    _errorStrings.clear();
+    _additionalErrorStrings.clear();
+
+    QList<QSslCertificate> trustedCerts = account->approvedCerts();
+
+    for (int i = 0; i < errors.count(); ++i) {
+        QSslError error = errors.at(i);
+        if (trustedCerts.contains(error.certificate()) || _unknownCerts.contains(error.certificate())) {
+            continue;
+        }
+        _errorStrings += error.errorString();
+        if (!error.certificate().isNull()) {
+            _unknownCerts.append(error.certificate());
+        } else {
+            _additionalErrorStrings.append(error.errorString());
+        }
+    }
+
+    // if there are no errors left, all Certs were known.
+    if (_errorStrings.isEmpty()) {
+        return true;
+    }
+
     return false;
 }
 
@@ -100,34 +142,16 @@ QString SslErrorDialog::styleSheet() const
 }
 #define QL(x) QLatin1String(x)
 
-bool SslErrorDialog::checkFailingCertsKnown(const QList<QSslError> &errors)
+bool SslErrorDialog::showFailingCertsKnown(
+    const QList<QSslError> &errors,
+    QList<QSslCertificate> unknownCerts,
+    QStringList errorStrings,
+    QStringList additionalErrorStrings
+    )
 {
-    // check if unknown certs caused errors.
-    _unknownCerts.clear();
-
-    QStringList errorStrings;
-
-    QStringList additionalErrorStrings;
-
-    QList<QSslCertificate> trustedCerts = _account->approvedCerts();
-
-    for (int i = 0; i < errors.count(); ++i) {
-        QSslError error = errors.at(i);
-        if (trustedCerts.contains(error.certificate()) || _unknownCerts.contains(error.certificate())) {
-            continue;
-        }
-        errorStrings += error.errorString();
-        if (!error.certificate().isNull()) {
-            _unknownCerts.append(error.certificate());
-        } else {
-            additionalErrorStrings.append(error.errorString());
-        }
-    }
-
-    // if there are no errors left, all Certs were known.
+    _unknownCerts = unknownCerts;
     if (errorStrings.isEmpty()) {
         _allTrusted = true;
-        return true;
     }
 
     QString msg = QL("<html><head>");
