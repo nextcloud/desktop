509b5729e3b899820af5339c3a64014b52ac9dc0
Claudio Cambra <claudio.cambra@nextcloud.com>
2024-10-21 14:56:14 +0800

    Simply pass AccountPtr to updateFileTagModel
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- src/gui/filedetails/filedetails.h
+++ src/gui/filedetails/filedetails.h
@@ -66,7 +66,7 @@ signals:
 private slots:
     void refreshFileDetails();
     void updateLockExpireString();
-    void updateFileTagModel(const OCC::Folder * const folder);
+    void updateFileTagModel(const OCC::AccountPtr &account);
 
 private:
     QString _localPath;
