81ad96c174ce788257f4ef49afdfdd5145e5a6bf
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2024-10-25 12:29:57 +0200

    use new in Qt6.8 windows fluent style
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
 #if defined Q_OS_MAC
     style = QStringLiteral("macOS");
 #elif defined Q_OS_WIN
-    style = QStringLiteral("Fusion");
+    style = QStringLiteral("FluentWinUI3");
 #endif
 
     QQuickStyle::setStyle(style);
