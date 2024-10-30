efe686992e32aedf670c5b286725451f639f7f15
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2024-10-30 16:37:42 +0100

    do not move to trash online only files/folders: going to fail
    ensure that even if move to trash is enabled, we do not erroneously try
    to move online only files or folders to trash bin
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/libsync/propagatorjobs.cpp
+++ src/libsync/propagatorjobs.cpp
@@ -117,7 +117,8 @@ void PropagateLocalRemove::start()
     }
 
     QString removeError;
-    if (_moveToTrash) {
+    const auto availability = propagator()->syncOptions()._vfs->availability(_item->_file, Vfs::AvailabilityRecursivity::RecursiveAvailability);
+    if (_moveToTrash && (!availability || (*availability != VfsItemAvailability::AllDehydrated && *availability != VfsItemAvailability::OnlineOnly && *availability != VfsItemAvailability::Mixed))) {
         if ((QDir(filename).exists() || FileSystem::fileExists(filename))
             && !FileSystem::moveToTrash(filename, &removeError)) {
             done(SyncFileItem::NormalError, removeError, ErrorCategory::GenericError);
