ad998034b4c4eac69001b777b60e90628611da1b
Camila Ayres <hello@camilasan.com>
2024-11-21 15:27:11 +0100

    Fix removal of file.
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- admin/linux/build-appimage.sh
+++ admin/linux/build-appimage.sh
@@ -90,16 +90,20 @@ export QML_SOURCES_PATHS=${DESKTOP_CLIENT_ROOT}/src/gui
 
 ./linuxdeploy-squashfs-root/AppRun --desktop-file=${DESKTOP_FILE} --icon-file=usr/share/icons/hicolor/512x512/apps/Nextcloud.png --executable=usr/bin/${EXECUTABLE_NAME} --appdir=AppDir --output appimage
 
-# Workaround issue #103 and #7231
+## Workaround issue #103 and #7231
 export APPIMAGETOOL="appimagetool"
-wget -O ${APPIMAGETOOL} --ca-directory=/etc/ssl/certs -c https://github.com/AppImage/AppImageKit/releases/download/13/appimagetool-x86_64.AppImage
-chmod a+x ${APPIMAGETOOL}
+wget -O ${APPIMAGETOOL} --ca-directory=/etc/ssl/certs -c "https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x ${APPIMAGE_NAME}
+./${APPIMAGE_NAME} --appimage-extract
+rm ./${APPIMAGETOOL}
+cp -r ./squashfs-root ./appimagetool-squashfs-root
 rm -rf ./squashfs-root
+
 APPIMAGE=$(ls *.AppImage)
 ./"${APPIMAGE}" --appimage-extract
 rm ./"${APPIMAGE}"
 rm ./squashfs-root/usr/lib/libglib-2.0.so.0
-./"${APPIMAGETOOL}" -n ./squashfs-root "${APPIMAGE}"
+./appimagetool-squashfs-root/urs/bin/appimagetool -n ./squashfs-root "${APPIMAGE}"
 
 #move AppImage
 if [ ! -z "$DRONE_COMMIT" ]
