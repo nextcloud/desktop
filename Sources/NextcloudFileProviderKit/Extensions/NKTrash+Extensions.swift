33dd402ec0e5503acc67d7ff39aa75fb6ff19d3d
Claudio Cambra <developer@claudiocambra.com>
2024-12-02 16:33:47 +0800

    Add extension to NKTrash to convert to an itemmetadata
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Sources/NextcloudFileProviderKit/Extensions/NKTrash+Extensions.swift
@@ -0,0 +1,29 @@
+//
+//  NKTrash+Extensions.swift
+//  NextcloudFileProviderKit
+//
+//  Created by Claudio Cambra on 2024-12-02.
+//
+
+import Foundation
+import NextcloudKit
+
+extension NKTrash {
+    func toItemMetadata(account: String) -> ItemMetadata {
+        let metadata = ItemMetadata()
+        metadata.account = account
+        metadata.contentType = contentType
+        metadata.date = date as Date
+        metadata.directory = directory
+        metadata.fileId = fileId
+        metadata.fileName = fileName
+        metadata.hasPreview = hasPreview
+        metadata.iconName = iconName
+        metadata.size = size
+        metadata.classFile = classFile
+        metadata.trashbinFileName = trashbinFileName
+        metadata.trashbinOriginalLocation = trashbinOriginalLocation
+        metadata.trashbinDeletionTime = trashbinDeletionTime as Date
+        return metadata
+    }
+}
