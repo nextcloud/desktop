e605a2c0cabf9e8950125bead0ce3a954ad2d4a1
Claudio Cambra <developer@claudiocambra.com>
2024-12-03 14:12:24 +0800

    Upgrade NextcloudKit minimum version to 5.0.1
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
             url: "https://github.com/claucambra/NextcloudCapabilitiesKit.git",
                 .upToNextMajor(from: "2.0.0")
         ),
-        .package(url: "https://github.com/nextcloud/NextcloudKit", .upToNextMajor(from: "2.9.9")),
+        .package(url: "https://github.com/nextcloud/NextcloudKit", from: "5.0.1"),
         .package(url: "https://github.com/realm/realm-swift.git", exact: "10.49.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
