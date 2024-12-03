0da53cc942b746ad8a747d6d0d5ca63cc17ddca0
Claudio Cambra <developer@claudiocambra.com>
2024-12-03 15:04:08 +0800

    Add ocId to NKTrash conversion
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Extensions/NKTrash+Extensions.swift
+++ Sources/NextcloudFileProviderKit/Extensions/NKTrash+Extensions.swift
@@ -11,6 +11,7 @@ import NextcloudKit
 extension NKTrash {
     func toItemMetadata(account: String) -> ItemMetadata {
         let metadata = ItemMetadata()
+        metadata.ocId = ocId
         metadata.account = account
         metadata.contentType = contentType
         metadata.date = date as Date
