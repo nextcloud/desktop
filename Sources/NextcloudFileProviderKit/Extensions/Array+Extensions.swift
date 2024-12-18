f4a1e736a7537f99d753f7dfde94d72436020e4d
Claudio Cambra <developer@claudiocambra.com>
2024-12-18 15:38:09 +0800

    Add array extension to chunk an array
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

---
+++ Sources/NextcloudFileProviderKit/Extensions/Array+Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Array+Extensions.swift
+//  NextcloudFileProviderKit
+//
+//  Created by Claudio Cambra on 2024-12-18.
+//
+
+extension Array {
+    func chunked(into size: Int) -> [ArraySlice<Element>] {
+        guard size > 0 else { return [] } // Avoid invalid chunk sizes
+        return stride(from: 0, to: count, by: size).map { startIndex in
+            self[startIndex..<Swift.min(startIndex + size, count)]
+        }
+    }
+}
