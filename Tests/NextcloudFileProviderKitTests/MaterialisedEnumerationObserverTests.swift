1b8ed67ad1722f01d0da8affae3fea5c3bfe8978
Claudio Cambra <developer@claudiocambra.com>
2024-12-20 16:34:52 +0800

    Add a test for the expected deleted ocIds in the materialised enumeration observer tests
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/MaterialisedEnumerationObserverTests.swift
+++ Tests/NextcloudFileProviderKitTests/MaterialisedEnumerationObserverTests.swift
@@ -23,19 +23,53 @@ final class MaterialisedEnumerationObserverTests: XCTestCase {
         Realm.Configuration.defaultConfiguration.inMemoryIdentifier = name
     }
 
-    func testMaterialisedFiles() async {
+    func testMaterialisedObserver() async {
         let dbManager = FilesDatabaseManager(realmConfig: .defaultConfiguration)
         let remoteInterface = MockRemoteInterface()
         let expect = XCTestExpectation(description: "Enumerator")
         let observer = MaterialisedEnumerationObserver(
             ncKitAccount: Self.account.ncKitAccount, dbManager: dbManager
         ) { deletedOcIds in
-            XCTAssertEqual(deletedOcIds, [])
+            XCTAssertTrue(deletedOcIds.isEmpty)
             expect.fulfill()
         }
         let enumerator = MockEnumerator(
             account: Self.account, dbManager: dbManager, remoteInterface: remoteInterface
         )
+        enumerator.enumerateItems(for: observer, startingAt: NSFileProviderPage(Data(count: 1)))
+        await fulfillment(of: [expect], timeout: 1)
+    }
+
+    func testMaterialisedFiles() async {
+        let itemA = ItemMetadata()
+        itemA.apply(account: Self.account)
+        itemA.apply(fileName: "itemA")
+        itemA.ocId = "itemA"
+        itemA.serverUrl = Self.account.davFilesUrl
+
+        let itemB = ItemMetadata()
+        itemB.apply(account: Self.account)
+        itemB.apply(fileName: "itemB")
+        itemB.ocId = "itemB"
+        itemB.serverUrl = Self.account.davFilesUrl
+
+        let dbManager = FilesDatabaseManager(realmConfig: .defaultConfiguration)
+        dbManager.addItemMetadata(itemA)
+        dbManager.addItemMetadata(itemB)
+
+        let remoteInterface = MockRemoteInterface()
+        let expect = XCTestExpectation(description: "Enumerator")
+        let observer = MaterialisedEnumerationObserver(
+            ncKitAccount: Self.account.ncKitAccount, dbManager: dbManager
+        ) { deletedOcIds in
+            XCTAssertEqual(deletedOcIds.count, 1) // Item B deleted
+            XCTAssertEqual(deletedOcIds.first, itemB.ocId)
+            expect.fulfill()
+        }
+        let enumerator = MockEnumerator(
+            account: Self.account, dbManager: dbManager, remoteInterface: remoteInterface
+        )
+        enumerator.enumeratorItems = [itemA] // No item B
         enumerator.enumerateItems(for: observer, startingAt: NSFileProviderPage(Data(count: 1)))
         await fulfillment(of: [expect], timeout: 1)
     }
