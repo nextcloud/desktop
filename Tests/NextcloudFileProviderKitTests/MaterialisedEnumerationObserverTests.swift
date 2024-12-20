3d40230e30627bb2f0c9ebeb0adc79a448dd6cca
Claudio Cambra <developer@claudiocambra.com>
2024-12-20 17:18:36 +0800

    Address and fix materialised enumeration observer test
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/MaterialisedEnumerationObserverTests.swift
+++ Tests/NextcloudFileProviderKitTests/MaterialisedEnumerationObserverTests.swift
@@ -46,30 +46,42 @@ final class MaterialisedEnumerationObserverTests: XCTestCase {
         itemA.apply(fileName: "itemA")
         itemA.ocId = "itemA"
         itemA.serverUrl = Self.account.davFilesUrl
+        itemA.downloaded = true
 
         let itemB = ItemMetadata()
         itemB.apply(account: Self.account)
         itemB.apply(fileName: "itemB")
         itemB.ocId = "itemB"
         itemB.serverUrl = Self.account.davFilesUrl
+        itemB.downloaded = false
+
+        let itemC = ItemMetadata()
+        itemC.apply(account: Self.account)
+        itemC.apply(fileName: "itemC")
+        itemC.ocId = "itemC"
+        itemC.serverUrl = Self.account.davFilesUrl
+        itemC.downloaded = true
 
         let dbManager = FilesDatabaseManager(realmConfig: .defaultConfiguration)
         dbManager.addItemMetadata(itemA)
         dbManager.addItemMetadata(itemB)
+        dbManager.addItemMetadata(itemC)
 
         let remoteInterface = MockRemoteInterface()
         let expect = XCTestExpectation(description: "Enumerator")
         let observer = MaterialisedEnumerationObserver(
             ncKitAccount: Self.account.ncKitAccount, dbManager: dbManager
         ) { deletedOcIds in
+            // itemA is downloaded, itemB is not, itemC is (and is new). Only the local copy of
+            // itemA should come up as deleted
             XCTAssertEqual(deletedOcIds.count, 1) // Item B deleted
-            XCTAssertEqual(deletedOcIds.first, itemB.ocId)
+            XCTAssertEqual(deletedOcIds.first, itemA.ocId)
             expect.fulfill()
         }
         let enumerator = MockEnumerator(
             account: Self.account, dbManager: dbManager, remoteInterface: remoteInterface
         )
-        enumerator.enumeratorItems = [itemA] // No item B
+        enumerator.enumeratorItems = [itemB, itemC]
         enumerator.enumerateItems(for: observer, startingAt: NSFileProviderPage(Data(count: 1)))
         await fulfillment(of: [expect], timeout: 1)
     }
