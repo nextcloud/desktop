9dc801f0ee3ded36a8b5cb01325b964926619966
Claudio Cambra <developer@claudiocambra.com>
2025-01-07 19:13:42 +0900

    Add simple test for upload convenience func chunked upload
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/UploadTests.swift
+++ Tests/NextcloudFileProviderKitTests/UploadTests.swift
@@ -47,4 +47,30 @@ final class UploadTests: XCTestCase {
         XCTAssertNotNil(result.ocId)
         XCTAssertNotNil(result.etag)
     }
+
+    func testChunkedUpload() async throws {
+        let fileUrl =
+            FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let data = Data(repeating: 1, count: 8)
+        try data.write(to: fileUrl)
+
+        let account = Account(user: "user", id: "id", serverUrl: "test.cloud.com", password: "1234")
+        let remoteInterface =
+            MockRemoteInterface(rootItem: MockRemoteItem.rootItem(account: account))
+        let remotePath = account.davFilesUrl + "/file.txt"
+        let result = await NextcloudFileProviderKit.upload(
+            fileLocatedAt: fileUrl,
+            toRemotePath: remotePath,
+            usingRemoteInterface: remoteInterface,
+            withAccount: account,
+            inChunksSized: 3
+        )
+
+        XCTAssertTrue(result.succeeded)
+        XCTAssertNotNil(result.chunks)
+        XCTAssertEqual(result.chunks?.count, 3)
+        XCTAssertEqual(result.size, Int64(data.count))
+        XCTAssertNotNil(result.ocId)
+        XCTAssertNotNil(result.etag)
+    }
 }
