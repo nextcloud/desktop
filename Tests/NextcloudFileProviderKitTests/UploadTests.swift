6cb61f14c0bee7058c8eb3e678e9d792c55f1885
Claudio Cambra <developer@claudiocambra.com>
2025-01-07 18:53:22 +0900

    Add basic test to check standard upload in upload convenience func
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/UploadTests.swift
+++ Tests/NextcloudFileProviderKitTests/UploadTests.swift
@@ -5,6 +5,7 @@
 //  Created by Claudio Cambra on 2025-01-07.
 //
 
+import TestInterface
 import XCTest
 @testable import NextcloudFileProviderKit
 
@@ -21,5 +22,29 @@ final class UploadTests: XCTestCase {
             remoteError: .success
         )
         XCTAssertTrue(uploadResult.succeeded)
+    }
+
+    func testStandardUpload() async throws {
+        let fileUrl =
+            FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let data = Data(repeating: 1, count: 8)
+        try data.write(to: fileUrl)
+
+        let account = Account(user: "user", id: "id", serverUrl: "test.cloud.com", password: "1234")
+        let remoteInterface =
+            MockRemoteInterface(rootItem: MockRemoteItem.rootItem(account: account))
+        let remotePath = account.davFilesUrl + "/file.txt"
+        let result = await NextcloudFileProviderKit.upload(
+            fileLocatedAt: fileUrl,
+            toRemotePath: remotePath,
+            usingRemoteInterface: remoteInterface,
+            withAccount: account
+        )
+
+        XCTAssertTrue(result.succeeded)
+        XCTAssertNil(result.chunks)
+        XCTAssertEqual(result.size, Int64(data.count))
+        XCTAssertNotNil(result.ocId)
+        XCTAssertNotNil(result.etag)
     }
 }
