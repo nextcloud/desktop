8dff7b20ccfc772a4bf776ac400b83dc54b3a0f7
Claudio Cambra <developer@claudiocambra.com>
2025-01-08 10:56:30 +0800

    Add test for resuming interrupted chunked uploads
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/NextcloudFileProviderKitTests/UploadTests.swift
+++ Tests/NextcloudFileProviderKitTests/UploadTests.swift
@@ -85,4 +85,59 @@ final class UploadTests: XCTestCase {
             Int(lastUploadedChunk.size), data.count - (lastUploadedChunkNameInt * chunkSize)
         )
     }
+
+    func testResumingInterruptedChunkedUpload() async throws {
+        let fileUrl =
+            FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString)
+        let data = Data(repeating: 1, count: 8)
+        try data.write(to: fileUrl)
+
+        let remoteInterface =
+            MockRemoteInterface(rootItem: MockRemoteItem.rootItem(account: account))
+        let chunkSize = 3
+        let uploadUuid = UUID().uuidString
+        let previousUploadedChunkNum = 1
+        let previousUploadedChunk = RemoteFileChunk(
+            fileName: String(previousUploadedChunkNum),
+            size: Int64(chunkSize),
+            remoteChunkStoreFolderName: uploadUuid
+        )
+        let previousUploadedChunks = [previousUploadedChunk]
+        remoteInterface.currentChunks = [uploadUuid: previousUploadedChunks]
+
+        let remotePath = account.davFilesUrl + "/file.txt"
+        var uploadedChunks = [RemoteFileChunk]()
+        let result = await NextcloudFileProviderKit.upload(
+            fileLocatedAt: fileUrl,
+            toRemotePath: remotePath,
+            usingRemoteInterface: remoteInterface,
+            withAccount: account,
+            inChunksSized: chunkSize,
+            usingChunkUploadId: uploadUuid,
+            dbManager: dbManager,
+            chunkUploadCompleteHandler: { uploadedChunks.append($0) }
+        )
+        let resultChunks = try XCTUnwrap(result.chunks)
+        let expectedChunkCount = Int(ceil(Double(data.count) / Double(chunkSize)))
+
+        XCTAssertEqual(result.remoteError, .success)
+        XCTAssertEqual(resultChunks.count, expectedChunkCount)
+        XCTAssertEqual(result.size, Int64(data.count))
+        XCTAssertNotNil(result.ocId)
+        XCTAssertNotNil(result.etag)
+
+        XCTAssertEqual(uploadedChunks.count, resultChunks.count - previousUploadedChunks.count)
+
+        let firstUploadedChunk = try XCTUnwrap(uploadedChunks.first)
+        let firstUploadedChunkNameInt = try XCTUnwrap(Int(firstUploadedChunk.fileName))
+        let lastUploadedChunk = try XCTUnwrap(uploadedChunks.last)
+        let lastUploadedChunkNameInt = try XCTUnwrap(Int(lastUploadedChunk.fileName))
+        XCTAssertEqual(firstUploadedChunkNameInt, previousUploadedChunkNum + 1)
+        XCTAssertEqual(lastUploadedChunkNameInt, previousUploadedChunkNum + 2)
+        print(uploadedChunks)
+        XCTAssertEqual(Int(firstUploadedChunk.size), chunkSize)
+        XCTAssertEqual(
+            Int(lastUploadedChunk.size), data.count - ((lastUploadedChunkNameInt - 1) * chunkSize)
+        )
+    }
 }
