cd59ca73b4b845c4ba4c72087b19cfa32de950b5
Claudio Cambra <developer@claudiocambra.com>
2025-01-08 11:28:43 +0800

    Apply appropriate chunkUploadId state when setting upload state in FilesDatabaseManager
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Database/FilesDatabaseManager.swift
+++ Sources/NextcloudFileProviderKit/Database/FilesDatabaseManager.swift
@@ -340,6 +340,9 @@ public class FilesDatabaseManager {
                     result.downloaded = false
                 } else if result.isUpload {
                     result.uploaded = false
+                    result.chunkUploadId = UUID().uuidString
+                } else if status == .normal {
+                    result.chunkUploadId = ""
                 }
 
                 Self.logger.debug(
