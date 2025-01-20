363aae340912933e50962b625cccbff2d5af50a0
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-01-20 08:50:36 +0100

    update legal notice copyright years for Nextcloud
    Close #7767
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/legalnotice.cpp
+++ src/gui/legalnotice.cpp
@@ -52,7 +52,7 @@ void LegalNotice::changeEvent(QEvent *e)
 
 void LegalNotice::customizeStyle()
 {
-    QString notice = tr("<p>Copyright 2017-2023 Nextcloud GmbH<br />"
+    QString notice = tr("<p>Copyright 2017-2025 Nextcloud GmbH<br />"
                         "Copyright 2012-2023 ownCloud GmbH</p>");
 
     notice += tr("<p>Licensed under the GNU General Public License (GPL) Version 2.0 or any later version.</p>");
