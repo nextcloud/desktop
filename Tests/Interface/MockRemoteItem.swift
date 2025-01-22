0d67db56c465a4fd92426c9ea80c1ebd1bb3665f
Claudio Cambra <developer@claudiocambra.com>
2025-01-22 10:13:22 +0800

    Properly apply lock state values on conversion of MockRemoteItem
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Tests/Interface/MockRemoteItem.swift
+++ Tests/Interface/MockRemoteItem.swift
@@ -189,13 +189,12 @@ public class MockRemoteItem: Equatable {
             note: "", // Placeholder as not set in original code
             ownerId: "", // Placeholder as not set in original code
             ownerDisplayName: "", // Placeholder as not set in original code
-            lock: false, // Default as not set in original code
-            lockOwner: "", // Placeholder as not set in original code
-            lockOwnerEditor: "", // Placeholder as not set in original code
-            lockOwnerType: 0, // Default as not set in original code
-            lockOwnerDisplayName: "", // Placeholder as not set in original code
+            lock: locked,
+            lockOwner: lockOwner,
+            lockOwnerType: lockOwner.isEmpty ? 0 : 1,
+            lockOwnerDisplayName: lockOwner == account.username ? account.username : "other user",
             lockTime: nil, // Default as not set in original code
-            lockTimeOut: nil, // Default as not set in original code
+            lockTimeOut: lockTimeOut,
             path: "", // Placeholder as not set in original code
             permissions: "", // Placeholder as not set in original code
             quotaUsedBytes: 0, // Default as not set in original code
