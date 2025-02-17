1723fa86a51e42f025aafb192afe215e7176e156
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-02-17 09:14:05 +0100

    detect the need to sign the terms of service during login web flow v2
    should avoid being blocked by terms_of_service blocking WebDAV access
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>
    Signed-off-by: Jyrki Gadinger <nilsding@nilsding.org>

--- src/gui/owncloudsetupwizard.cpp
+++ src/gui/owncloudsetupwizard.cpp
@@ -13,13 +13,6 @@
  * for more details.
  */
 
-#include <QAbstractButton>
-#include <QtCore>
-#include <QProcess>
-#include <QMessageBox>
-#include <QDesktopServices>
-#include <QApplication>
-
 #include "accessmanager.h"
 #include "account.h"
 #include "accountmanager.h"
@@ -35,10 +28,22 @@
 #include "sslerrordialog.h"
 #include "wizard/owncloudwizard.h"
 #include "wizard/owncloudwizardcommon.h"
+#include "connectionvalidator.h"
 
 #include "creds/credentialsfactory.h"
 #include "creds/abstractcredentials.h"
 #include "creds/dummycredentials.h"
+
+#ifdef BUILD_FILE_PROVIDER_MODULE
+#include "gui/macOS/fileprovider.h"
+#endif
+
+#include <QAbstractButton>
+#include <QtCore>
+#include <QProcess>
+#include <QMessageBox>
+#include <QDesktopServices>
+#include <QApplication>
 
 namespace OCC {
 
@@ -377,7 +382,14 @@ void OwncloudSetupWizard::testOwnCloudConnect()
     job->setFollowRedirects(false);
     job->setProperties(QList<QByteArray>() << "getlastmodified");
     connect(job, &PropfindJob::result, _ocWizard, &OwncloudWizard::successfulStep);
-    connect(job, &PropfindJob::finishedWithError, this, &OwncloudSetupWizard::slotAuthError);
+    connect(job, &PropfindJob::finishedWithError, this, [this] (QNetworkReply *reply) {
+        if (reply && reply->error() == QNetworkReply::ContentAccessDenied) {
+            testTermsOfService();
+        } else {
+            slotAuthError();
+        }
+    });
+
     job->start();
 }
 
@@ -416,6 +428,9 @@ void OwncloudSetupWizard::slotAuthError()
 
         // A 404 is actually a success: we were authorized to know that the folder does
         // not exist. It will be created later...
+    } else if (reply->error() == QNetworkReply::ContentAccessDenied) {
+        testTermsOfService();
+        return;
     } else if (reply->error() == QNetworkReply::ContentNotFoundError) {
         _ocWizard->successfulStep();
         return;
@@ -471,6 +486,14 @@ bool OwncloudSetupWizard::checkDowngradeAdvised(QNetworkReply *reply)
         return false;
     }
     return true;
+}
+
+void OwncloudSetupWizard::testTermsOfService()
+{
+    _termsOfServiceChecker = new TermsOfServiceChecker{_ocWizard->account(), this};
+
+    connect(_termsOfServiceChecker, &TermsOfServiceChecker::done, this, &OwncloudSetupWizard::termsOfServiceChecked);
+    _termsOfServiceChecker->start();
 }
 
 void OwncloudSetupWizard::slotCreateLocalAndRemoteFolders(const QString &localFolder, const QString &remoteFolder)
@@ -724,6 +747,17 @@ void OwncloudSetupWizard::slotSkipFolderConfiguration()
 
     // Accept to check connectivity, only skip folder setup
     emit ownCloudWizardDone(QDialog::Accepted);
+}
+
+void OwncloudSetupWizard::termsOfServiceChecked()
+{
+    if (_termsOfServiceChecker && _termsOfServiceChecker->needToSign()) {
+        QDesktopServices::openUrl(_ocWizard->account()->url());
+    } else {
+        _ocWizard->successfulStep();
+        delete _termsOfServiceChecker;
+        _termsOfServiceChecker = nullptr;
+    }
 }
 
 AccountState *OwncloudSetupWizard::applyAccountChanges()
