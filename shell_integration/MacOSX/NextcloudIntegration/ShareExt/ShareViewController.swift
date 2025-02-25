48608014785767979cd204650e88693cd9b32c1c
Claudio Cambra <claudio.cambra@nextcloud.com>
2025-02-25 16:36:31 +0800

    shell_extension/macOS/ShareExt: Add starter target
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

---
+++ shell_integration/MacOSX/NextcloudIntegration/ShareExt/ShareViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ShareViewController.swift
+//  ShareExt
+//
+//  Created by Claudio Cambra on 25/2/25.
+//
+
+import Cocoa
+
+class ShareViewController: NSViewController {
+
+    override var nibName: NSNib.Name? {
+        return NSNib.Name("ShareViewController")
+    }
+
+    override func loadView() {
+        super.loadView()
+    
+        // Insert code here to customize the view
+        let item = self.extensionContext!.inputItems[0] as! NSExtensionItem
+        if let attachments = item.attachments {
+            NSLog("Attachments = %@", attachments as NSArray)
+        } else {
+            NSLog("No Attachments")
+        }
+    }
+
+    @IBAction func send(_ sender: AnyObject?) {
+        let outputItem = NSExtensionItem()
+        // Complete implementation by setting the appropriate value on the output item
+    
+        let outputItems = [outputItem]
+        self.extensionContext!.completeRequest(returningItems: outputItems, completionHandler: nil)
+}
+
+    @IBAction func cancel(_ sender: AnyObject?) {
+        let cancelError = NSError(domain: NSCocoaErrorDomain, code: NSUserCancelledError, userInfo: nil)
+        self.extensionContext!.cancelRequest(withError: cancelError)
+    }
+
+}
