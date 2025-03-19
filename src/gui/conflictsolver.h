d47a98563287f96b6e621cf8fe7062ca32ba9aea
Jyrki Gadinger <nilsding@nilsding.org>
2025-03-19 09:39:15 +0100

    gui: add a "Yes to all" button when resolving multiple conflicts at once
    Fixes #7446
    
    Signed-off-by: Jyrki Gadinger <nilsding@nilsding.org>

--- src/gui/conflictsolver.h
+++ src/gui/conflictsolver.h
@@ -26,6 +26,9 @@ class ConflictSolver : public QObject
     Q_OBJECT
     Q_PROPERTY(QString localVersionFilename READ localVersionFilename WRITE setLocalVersionFilename NOTIFY localVersionFilenameChanged)
     Q_PROPERTY(QString remoteVersionFilename READ remoteVersionFilename WRITE setRemoteVersionFilename NOTIFY remoteVersionFilenameChanged)
+    Q_PROPERTY(bool isBulkSolution READ isBulkSolution WRITE setIsBulkSolution NOTIFY isBulkSolutionChanged)
+    Q_PROPERTY(bool yesToAllRequested READ yesToAllRequested WRITE setYesToAllRequested NOTIFY yesToAllRequestedChanged)
+
 public:
     enum Solution {
         KeepLocalVersion,
@@ -38,25 +41,34 @@ public:
 
     [[nodiscard]] QString localVersionFilename() const;
     [[nodiscard]] QString remoteVersionFilename() const;
+    [[nodiscard]] bool isBulkSolution() const;
+    [[nodiscard]] bool yesToAllRequested() const;
 
     bool exec(Solution solution);
 
 public slots:
     void setLocalVersionFilename(const QString &localVersionFilename);
     void setRemoteVersionFilename(const QString &remoteVersionFilename);
+    void setIsBulkSolution(bool isBulkSolution);
+    void setYesToAllRequested(bool yesToAllRequested);
 
 signals:
     void localVersionFilenameChanged();
     void remoteVersionFilenameChanged();
+    void isBulkSolutionChanged();
+    void yesToAllRequestedChanged();
 
 private:
     bool deleteLocalVersion();
     bool renameLocalVersion();
     bool overwriteRemoteVersion();
+    bool confirmDeletion();
 
     QWidget *_parentWidget;
     QString _localVersionFilename;
     QString _remoteVersionFilename;
+    bool _isBulkSolution = false;
+    bool _yesToAllRequested = false;
 };
 
 } // namespace OCC
