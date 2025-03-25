66ea9276adcc9fc0a188423568dbf3ea6124a2a2
Claudio Cambra <claudio.cambra@nextcloud.com>
2025-03-25 10:32:14 +0100

    shell_integration/macOS/FileProviderUIExt: Log eviction process
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Evicting/Eviction.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderUIExt/Evicting/Eviction.swift
@@ -7,20 +7,30 @@
 
 import FileProvider
 import Foundation
+import OSLog
 
 func evict(
     itemsWithIdentifiers identifiers: [NSFileProviderItemIdentifier],
     inDomain domain: NSFileProviderDomain
 ) async {
+    Logger.eviction.debug("Starting eviction process…")
     guard let manager = NSFileProviderManager(for: domain) else {
+        Logger.eviction.error(
+            "Could not get manager for domain: \(domain.identifier.rawValue, privacy: .public)"
+        )
         return;
     }
     do {
         for itemIdentifier in identifiers {
+            Logger.eviction.error(
+                "Evicting item: \(itemIdentifier.rawValue, privacy: .public)"
+            )
             try await manager.evictItem(identifier: itemIdentifier)
         }
     } catch let error {
-        
+        Logger.eviction.error(
+            "Error evicting item: \(error.localizedDescription, privacy: .public)"
+        )
     }
 }
 
