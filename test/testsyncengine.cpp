cba3701c5c4764ab4e30f9441a8ff712273c165a
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-04-08 17:15:14 +0200

    remove bogus check that tried to check for existing case clash conflict
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- test/testsyncengine.cpp
+++ test/testsyncengine.cpp
@@ -1849,12 +1849,6 @@ private slots:
             QVERIFY(conflictSolverCaseClashForFolderDone.wait());
             QVERIFY(fakeFolder.syncOnce());
 
-            // verify no case clash conflicts folder items are found
-            caseClashConflictFolderItemLower = completeSpy.findItem(diverseConflictsFolderPath + "/" + testLowerCaseFolder);
-            caseClashConflictFolderItemUpper = completeSpy.findItem(diverseConflictsFolderPath + "/" + testUpperCaseFolder);
-            QVERIFY((!caseClashConflictFolderItemLower || caseClashConflictFolderItemLower->_file.isEmpty())
-                    && (!caseClashConflictFolderItemUpper || caseClashConflictFolderItemUpper->_file.isEmpty()));
-
             // veriy invalid filename conflict folder item is still present
             invalidFilenameConflictFolderItem = completeSpy.findItem(diverseConflictsFolderPath + "/" + testInvalidCharFolder);
             completeSpy.clear();
