f035af2366fd2e2a663c46956062d3251348c97d
Claudio Cambra <developer@claudiocambra.com>
2025-04-17 12:56:12 +0800

    Move lock file creation handling to own static method
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Item/Item+Create.swift
+++ Sources/NextcloudFileProviderKit/Item/Item+Create.swift
@@ -477,118 +477,15 @@ extension Item {
         let itemTemplateIsFolder = itemTemplate.contentType?.conforms(to: .directory) ?? false
 
         guard !isLockFileName(itemTemplate.filename) || itemTemplateIsFolder else {
-            // Lock but don't upload, do not error
-            let (_, capabilitiesData, capabilitiesError) = await remoteInterface.fetchCapabilities(
+            return await Item.createLockFile(
+                basedOn: itemTemplate,
+                parentItemIdentifier: parentItemIdentifier,
+                parentItemRemotePath: parentItemRemotePath,
+                progress: progress,
+                domain: domain,
                 account: account,
-                options: .init(),
-                taskHandler: { task in
-                    if let domain {
-                        NSFileProviderManager(for: domain)?.register(
-                            task,
-                            forItemWithIdentifier: itemTemplate.itemIdentifier,
-                            completionHandler: { _ in }
-                        )
-                    }
-                }
-            )
-            guard capabilitiesError == .success,
-                  let capabilitiesData,
-                  let capabilities = Capabilities(data: capabilitiesData),
-                  capabilities.files?.locking != nil
-            else {
-                uploadLogger.info(
-                    """
-                    Received nil capabilities data.
-                        Received error: \(capabilitiesError.errorDescription, privacy: .public)
-                        Will not proceed with locking for \(itemTemplate.filename, privacy: .public)
-                    """
-                )
-                return (nil, nil)
-            }
-
-            Self.logger.info(
-                """
-                Item to create:
-                    \(itemTemplate.filename)
-                    is a lock file. Will handle by remotely locking the target file.
-                """
-            )
-            guard let targetFileName = originalFileName(
-                fromLockFileName: itemTemplate.filename
-            ) else {
-                Self.logger.error(
-                    """
-                    Could not get original filename from lock file filename
-                        \(itemTemplate.filename, privacy: .public)
-                        so will not lock target file.
-                    """
-                )
-                return (nil, nil)
-            }
-            let (_, _, error) = await remoteInterface.setLockStateForFile(
-                remotePath: parentItemRemotePath + "/" + targetFileName,
-                lock: true,
-                account: account,
-                options: .init(),
-                taskHandler: { task in
-                    if let domain {
-                        NSFileProviderManager(for: domain)?.register(
-                            task,
-                            forItemWithIdentifier: itemTemplate.itemIdentifier,
-                            completionHandler: { _ in }
-                        )
-                    }
-                }
-            )
-            if error != .success {
-                Self.logger.error(
-                    """
-                    Failed to lock target file \(targetFileName, privacy: .public)
-                        for lock file: \(itemTemplate.filename, privacy: .public)
-                        received error: \(error.errorDescription)
-                    """
-                )
-            }
-
-            let metadata = SendableItemMetadata(
-                ocId: itemTemplate.itemIdentifier.rawValue,
-                account: account.ncKitAccount,
-                classFile: "lock", // Indicates this metadata is for a locked file
-                contentType: itemTemplate.contentType?.preferredMIMEType ?? "",
-                creationDate: itemTemplate.creationDate as? Date ?? Date(),
-                date: Date(),
-                directory: false,
-                e2eEncrypted: false,
-                etag: "",
-                fileId: itemTemplate.itemIdentifier.rawValue,
-                fileName: itemTemplate.filename,
-                fileNameView: itemTemplate.filename,
-                hasPreview: false,
-                iconName: "lockIcon", // Custom icon for locked items
-                mountType: "",
-                ownerId: "",
-                ownerDisplayName: "",
-                path: parentItemRemotePath + "/" + targetFileName,
-                serverUrl: parentItemRemotePath,
-                size: 0,
-                status: Status.normal.rawValue,
-                downloaded: true,
-                uploaded: false,
-                urlBase: "",
-                user: "",
-                userId: ""
-            )
-            dbManager.addItemMetadata(metadata)
-
-            return (
-                Item(
-                    metadata: metadata,
-                    parentItemIdentifier: parentItemIdentifier,
-                    account: account,
-                    remoteInterface: remoteInterface,
-                    dbManager: dbManager
-                ),
-                error.fileProviderError
+                remoteInterface: remoteInterface,
+                dbManager: dbManager
             )
         }
 
