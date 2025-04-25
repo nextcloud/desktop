460f3a75ca8715eabb495cfc21c8fd3a481fb03c
Claudio Cambra <developer@claudiocambra.com>
2025-04-25 18:29:35 +0800

    Add modifyUnuploaded method that just modified items locally
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- Sources/NextcloudFileProviderKit/Item/Item+ModifyUnuploaded.swift
+++ Sources/NextcloudFileProviderKit/Item/Item+ModifyUnuploaded.swift
@@ -8,7 +8,73 @@
 import FileProvider
 
 extension Item {
-    // Creates a file that was previously unuploaded (e.g.a  previously ignored/lock file) on server
+    // Just modifies metadata
+    func modifyUnuploaded(
+        itemTarget: NSFileProviderItem,
+        baseVersion: NSFileProviderItemVersion = NSFileProviderItemVersion(),
+        changedFields: NSFileProviderItemFields,
+        contents newContents: URL?,
+        options: NSFileProviderModifyItemOptions = [],
+        request: NSFileProviderRequest = NSFileProviderRequest(),
+        ignoredFiles: IgnoredFilesMatcher? = nil,
+        domain: NSFileProviderDomain? = nil,
+        forcedChunkSize: Int? = nil,
+        progress: Progress = .init(),
+        dbManager: FilesDatabaseManager
+    ) -> Item? {
+        var modifiedParentItemIdentifier = parentItemIdentifier
+        var modifiedMetadata = metadata
+
+        if changedFields.contains(.filename) {
+            modifiedMetadata.fileName = itemTarget.filename
+            if !isLockFileName(modifiedMetadata.fileName) {
+                modifiedMetadata.classFile = ""
+                // Do the actual upload at the end, not yet
+            }
+        }
+        if changedFields.contains(.contents),
+           let newSize = try? newContents?.resourceValues(forKeys: [.fileSizeKey]).fileSize
+        {
+            modifiedMetadata.size = Int64(newSize)
+        }
+        if changedFields.contains(.parentItemIdentifier) {
+            guard let parentMetadata = dbManager.itemMetadata(
+                ocId: itemTarget.parentItemIdentifier.rawValue
+            ) else {
+                Self.logger.error(
+                    """
+                    Unable to find new parent item identifier during unuploaded item modification.
+                        Filename: \(self.filename, privacy: .public)
+                    """
+                )
+                return nil
+            }
+            modifiedMetadata.serverUrl = parentMetadata.serverUrl + "/" + parentMetadata.fileName
+            modifiedParentItemIdentifier = .init(parentMetadata.ocId)
+        }
+        if changedFields.contains(.creationDate),
+           let newCreationDate = itemTarget.creationDate,
+           let newCreationDate
+        {
+            modifiedMetadata.creationDate = newCreationDate
+        }
+        if changedFields.contains(.contentModificationDate),
+           let newModificationDate = itemTarget.contentModificationDate,
+           let newModificationDate
+        {
+            modifiedMetadata.date = newModificationDate
+        }
+
+        return Item(
+            metadata: modifiedMetadata,
+            parentItemIdentifier: modifiedParentItemIdentifier,
+            account: account,
+            remoteInterface: remoteInterface,
+            dbManager: dbManager
+        )
+    }
+
+    // Creates a file that was previously unuploaded (e.g. a previously ignored/lock file) on server
     func createUnuploadedRemotely(
         itemTarget: NSFileProviderItem,
         baseVersion: NSFileProviderItemVersion = NSFileProviderItemVersion(),
