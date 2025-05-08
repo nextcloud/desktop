cc4bc8fa980b817144dca871e07dcb4bb9569d52
Claudio Cambra <claudio.cambra@nextcloud.com>
2025-05-08 15:17:07 +0800

    feat(shell_integration/macOS/FileProviderExt): Pass ignored files matcher when creating/modifying/deleting items
    Signed-off-by: Claudio Cambra <claudio.cambra@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/FileProviderExtension.swift
+++ shell_integration/MacOSX/NextcloudIntegration/FileProviderExt/FileProviderExtension.swift
@@ -266,6 +266,7 @@ import OSLog
                 domain: self.domain,
                 account: ncAccount,
                 remoteInterface: ncKit,
+                ignoredFiles: ignoredFiles,
                 progress: progress,
                 dbManager: dbManager
             )
@@ -360,6 +361,7 @@ import OSLog
                 contents: newContents,
                 options: options,
                 request: request,
+                ignoredFiles: ignoredFiles,
                 domain: domain,
                 progress: progress,
                 dbManager: dbManager
@@ -435,7 +437,9 @@ import OSLog
         }
 
         Task {
-            let error = await item.delete(dbManager: dbManager)
+            let error = await item.delete(
+                domain: domain, ignoredFiles: ignoredFiles, dbManager: dbManager
+            )
             if error != nil {
                 insertErrorAction(actionId)
                 signalEnumerator(completionHandler: { _ in })
