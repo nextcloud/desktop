be6c34d0e4e3d01f231e5b4d046133bc2d1af0e4
Rello <Rello@users.noreply.github.com>
2025-06-24 21:46:55 +0200

    chore: add SPDX headers
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- src/gui/legacyaccountselectiondialog.cpp
+++ src/gui/legacyaccountselectiondialog.cpp
@@ -1,3 +1,9 @@
+
+/*
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
 #include "legacyaccountselectiondialog.h"
 
 #include <QCheckBox>
