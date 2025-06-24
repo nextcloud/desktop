c0d91660e6121c4d124f8b2424c9ec5c4b934b3f
Rello <Rello@users.noreply.github.com>
2025-06-24 21:34:42 +0200

    Add legacy account selection dialog

---
+++ src/gui/legacyaccountselectiondialog.cpp
@@ -0,0 +1,45 @@
+#include "legacyaccountselectiondialog.h"
+
+#include <QCheckBox>
+#include <QDialogButtonBox>
+#include <QLabel>
+#include <QVBoxLayout>
+
+namespace OCC
+{
+
+LegacyAccountSelectionDialog::LegacyAccountSelectionDialog(const QVector<AccountItem> &accounts, QWidget *parent)
+    : QDialog(parent)
+{
+    setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
+    setWindowTitle(tr("Legacy import"));
+
+    auto layout = new QVBoxLayout(this);
+    layout->addWidget(new QLabel(tr("Select the accounts to import from the legacy configuration:"), this));
+
+    for (const auto &acc : accounts) {
+        auto cb = new QCheckBox(acc.label, this);
+        cb->setChecked(true);
+        layout->addWidget(cb);
+        _checkBoxes.insert(acc.id, cb);
+    }
+
+    auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, this);
+    connect(buttonBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
+    layout->addWidget(buttonBox);
+}
+
+QStringList LegacyAccountSelectionDialog::selectedAccountIds() const
+{
+    QStringList res;
+    for (auto it = _checkBoxes.constBegin(); it != _checkBoxes.constEnd(); ++it) {
+        if (it.value()->isChecked()) {
+            res.push_back(it.key());
+        }
+    }
+    return res;
+}
+
+} // namespace OCC
+
