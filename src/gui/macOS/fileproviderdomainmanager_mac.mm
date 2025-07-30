a8fd4fc9b1a8996f0ca12fd86f7a8eb6284083c5
Iva Horn <iva.horn@icloud.com>
2025-07-30 16:37:28 +0200

    fix(vfs): Resolve provider sync status with domain id instead of account id.
    Problem: file provider settings controller was using the account identifier from the list of file provider enabled accounts to retrieve the sync status of a file provider domain. The identifier of a file provider domain can differ from their corresponding account identifier. The mapping was missing. Hence, the file provider domain "user3@localhost-8080" could not be resolved for account "user3@localhost:8080".
    Solution: Invoke a mapping from account id to file provider domain id.
    Other changes: Moved some generic utility code from fileproviderdomainmanager_mac.mm to fileproviderutils.mm so it is accessible independent from the domain manager type from different contexts.
    
    Signed-off-by: Iva Horn <iva.horn@icloud.com>

--- src/gui/macOS/fileproviderdomainmanager_mac.mm
+++ src/gui/macOS/fileproviderdomainmanager_mac.mm
@@ -14,6 +14,7 @@
 #include "config.h"
 #include "fileproviderdomainmanager.h"
 #include "fileprovidersettingscontroller.h"
+#include "fileproviderutils.h"
 
 #include "gui/accountmanager.h"
 #include "libsync/account.h"
@@ -28,54 +29,13 @@ Q_LOGGING_CATEGORY(lcMacFileProviderDomainManager, "nextcloud.gui.macfileprovide
 // are consistent throughout these classes
 namespace {
 
-static constexpr auto bundleExtensions = std::array{
-    QLatin1StringView(".app"),
-    QLatin1StringView(".framework"),
-    QLatin1StringView(".kext"),
-    QLatin1StringView(".plugin"),
-    QLatin1StringView(".docset"),
-    QLatin1StringView(".xpc"),
-    QLatin1StringView(".qlgenerator"),
-    QLatin1StringView(".component"),
-    QLatin1StringView(".saver"),
-    QLatin1StringView(".mdimporter")
-};
-static const QRegularExpression illegalChars("[:/]");
-
-inline bool hasBundleExtension(const QString &domainId)
-{
-    return std::any_of(bundleExtensions.begin(), bundleExtensions.end(), [&domainId](const auto &ext) {
-        return domainId.endsWith(ext);
-    });
-}
-
-inline bool illegalDomainIdentifier(const QString &domainId)
-{
-    return !domainId.isEmpty() && !illegalChars.match(domainId).hasMatch() && hasBundleExtension(domainId);
-}
-
 QString domainIdentifierForAccount(const OCC::Account * const account)
 {
     Q_ASSERT(account);
     auto domainId = account->userIdAtHostWithPort();
     Q_ASSERT(!domainId.isEmpty());
 
-    domainId.replace(illegalChars, "-");
-
-    // Some url domains like .app cause issues on macOS as these are also bundle extensions.
-    // Under the hood, fileproviderd will create a folder for the user to access the files named
-    // after the domain identifier. If the url domain is the same as a bundle extension, Finder
-    // will interpret this folder as a bundle and will not allow the user to access the files.
-    // Here we wrap the dot in the url domain extension to prevent this from happening.
-    for (const auto &ext : bundleExtensions) {
-        if (domainId.endsWith(ext)) {
-            domainId = domainId.left(domainId.length() - ext.length());
-            domainId += "(.)" + ext.right(ext.length() - 1);
-            break;
-        }
-    }
-
-    return domainId;
+    return OCC::Mac::FileProviderUtils::domainIdentifierForUserIdAtHostWithPort(domainId);
 }
 
 inline QString domainIdentifierForAccount(const OCC::AccountPtr account)
@@ -171,7 +131,7 @@ public:
                                                                << accountState->account()->displayName();
                         [domain retain];
 
-                        if (illegalDomainIdentifier(QString::fromNSString(domain.identifier))) {
+                        if (FileProviderUtils::illegalDomainIdentifier(QString::fromNSString(domain.identifier))) {
                             qCWarning(lcMacFileProviderDomainManager) << "Found existing file provider domain with illegal domain identifier:"
                                                                       << domain.identifier
                                                                       << "removing and recreating";
--- src/gui/macOS/fileproviderutils_mac.mm
+++ src/gui/macOS/fileproviderutils_mac.mm
@@ -6,6 +6,7 @@
 #include "fileproviderutils.h"
 
 #include <QLoggingCategory>
+#include <QRegularExpression>
 #include <QString>
 
 #import <FileProvider/FileProvider.h>
@@ -17,6 +18,58 @@ namespace Mac {
 namespace FileProviderUtils {
 
 Q_LOGGING_CATEGORY(lcMacFileProviderUtils, "nextcloud.gui.macfileproviderutils", QtInfoMsg)
+
+/**
+ * This list is not exhaustive already because third-party apps can define their own proprietary bundle types arbitrarily.
+ * This list only coveers the most common extensions.
+ */
+static constexpr auto bundleExtensions = std::array{
+    QLatin1StringView(".app"),
+    QLatin1StringView(".framework"),
+    QLatin1StringView(".kext"),
+    QLatin1StringView(".plugin"),
+    QLatin1StringView(".docset"),
+    QLatin1StringView(".xpc"),
+    QLatin1StringView(".qlgenerator"),
+    QLatin1StringView(".component"),
+    QLatin1StringView(".saver"),
+    QLatin1StringView(".mdimporter")
+};
+
+static const QRegularExpression illegalChars("[:/]");
+
+inline bool hasBundleExtension(const QString &domainId)
+{
+    return std::any_of(bundleExtensions.begin(), bundleExtensions.end(), [&domainId](const auto &ext) {
+        return domainId.endsWith(ext);
+    });
+}
+
+bool illegalDomainIdentifier(const QString &domainId)
+{
+    return !domainId.isEmpty() && !illegalChars.match(domainId).hasMatch() && hasBundleExtension(domainId);
+}
+
+QString domainIdentifierForUserIdAtHostWithPort(const QString userIdAtHostWithPort)
+{
+    auto domainId = userIdAtHostWithPort;
+    domainId.replace(illegalChars, "-");
+
+    // Some url domains like .app cause issues on macOS as these are also bundle extensions.
+    // Under the hood, fileproviderd will create a folder for the user to access the files named
+    // after the domain identifier. If the url domain is the same as a bundle extension, Finder
+    // will interpret this folder as a bundle and will not allow the user to access the files.
+    // Here we wrap the dot in the url domain extension to prevent this from happening.
+    for (const auto &ext : bundleExtensions) {
+        if (domainId.endsWith(ext)) {
+            domainId = domainId.left(domainId.length() - ext.length());
+            domainId += "(.)" + ext.right(ext.length() - 1);
+            break;
+        }
+    }
+
+    return domainId;
+}
 
 NSFileProviderDomain *domainForIdentifier(const QString &domainIdentifier)
 {
