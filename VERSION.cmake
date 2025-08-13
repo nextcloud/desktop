55f04ef44f1706551f648094a832eb3bac04758f
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-08-13 11:09:07 +0200

    chore: prepare for release 3.17.0
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- VERSION.cmake
+++ VERSION.cmake
@@ -6,11 +6,11 @@
 # Version information
 # ------------------------------------
 set(MIRALL_VERSION_MAJOR 3)
-set(MIRALL_VERSION_MINOR 16)
-set(MIRALL_VERSION_PATCH 83)
+set(MIRALL_VERSION_MINOR 17)
+set(MIRALL_VERSION_PATCH 0)
 set(MIRALL_VERSION_YEAR  2025)
 set(MIRALL_SOVERSION     0)
-set(MIRALL_PREVERSION_HUMAN "3.17.0 rc3")  # For preversions where PATCH>=50. Use version + alpha, rc1, rc2, etc.
+set(MIRALL_PREVERSION_HUMAN "3.17.0")  # For preversions where PATCH>=50. Use version + alpha, rc1, rc2, etc.
 set(NCEXT_BUILD_NUM 47)
 set(NCEXT_VERSION 3,0,0,${NCEXT_BUILD_NUM})
 
