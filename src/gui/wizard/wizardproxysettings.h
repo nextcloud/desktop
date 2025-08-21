2afe019bed4a5ec9b8ebbaef6116540adb70555b
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2025-08-21 14:00:44 +0200

    feat(wizard): makes entering proxy settings feasible for new accounts
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/wizard/wizardproxysettings.h
+++ src/gui/wizard/wizardproxysettings.h
@@ -6,16 +6,56 @@
 #pragma once
 
 #include <QDialog>
+#include <QNetworkProxy>
 
 #include "ui_proxysettings.h"
+
+namespace OCC {
 
 class WizardProxySettings : public QDialog
 {
     Q_OBJECT
 public:
-    explicit WizardProxySettings(QWidget *parent = nullptr);
+    enum class ProxyAuthentication {
+        AuthenticationRequired,
+        NoAuthentication,
+    };
+
+    explicit WizardProxySettings(QUrl serverURL, QWidget *parent = nullptr);
+
+Q_SIGNALS:
+    void proxySettingsAccepted(QString user,
+                               QString password,
+                               QString host,
+                               int port,
+                               WizardProxySettings::ProxyAuthentication needsAuth,
+                               QNetworkProxy::ProxyType proxyType);
+
+private Q_SLOTS:
+    /// Red marking of host field if empty and enabled
+    void checkEmptyProxyHost();
+
+    void checkEmptyProxyCredentials();
+
+    void checkAccountLocalhost();
+
+    void validateProxySettings();
+
+    void settingsDone();
 
 private:
     Ui_ProxySettings _ui{};
 
+    QUrl _serverURL;
+
+    bool _valid = false;
+
+    QString _user;
+    QString _password;
+    QString _host;
+    int _port;
+    ProxyAuthentication _needsAuth = ProxyAuthentication::NoAuthentication;
+    QNetworkProxy::ProxyType _proxyType = QNetworkProxy::NoProxy;
 };
+
+}
