fa055ba108bea0eae616b577496958161cf8f1ab
Iva Horn <iva.horn@nextcloud.com>
2025-10-29 17:09:07 +0100

    fix(macOS): Relocated socket files into sandboxed containers.
    Content of sandboxed apps must not be created on the root level of a container but lower in the hierarchy of it.
    
    Signed-off-by: Iva Horn <iva.horn@nextcloud.com>

--- src/gui/macOS/fileproviderutils_mac.mm
+++ src/gui/macOS/fileproviderutils_mac.mm
@@ -209,10 +209,12 @@ NSFileProviderManager *managerForDomainIdentifier(const QString &domainIdentifie
 QString groupContainerPath()
 {
     NSString *const groupId = (NSString *)[NSBundle.mainBundle objectForInfoDictionaryKey:@"NCFPKAppGroupIdentifier"];
+
     if (groupId == nil) {
         qCWarning(lcMacFileProviderUtils) << "No app group identifier found in Info.plist, cannot determine group container path.";
         return QString();
     }
+
     return QString::fromNSString([NSFileManager.defaultManager containerURLForSecurityApplicationGroupIdentifier:groupId].path);
 }
 
