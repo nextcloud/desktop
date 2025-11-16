4b77983a3d2a32fbd911a6013fa1abbf7ef2a030
Camila Ayres <hello@camilasan.com>
2025-11-16 16:08:09 +0100

    fix(credentials): remove emit signal call.
    It would trigger the call for saveAccountHelper() which in turn would call
    persist() again.
    
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- src/gui/creds/webflowcredentials.cpp
+++ src/gui/creds/webflowcredentials.cpp
@@ -224,7 +224,6 @@ void WebFlowCredentials::persist() {
     }
 
     _account->setCredentialSetting(userC, _user);
-    emit _account->wantsAccountSaved(_account->sharedFromThis());
 
     // write cert if there is one
     if (!_clientSslCertificate.isNull()) {
