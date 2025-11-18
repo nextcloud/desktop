5feb837ab92882981925b7af3ff6df3f2e302dd3
Tamás Bari <adaorcpp@gmail.com>
2025-11-18 13:58:06 +0100

    fix: Enforce enterprise update channel
    [issue 9004]
    
    Signed-off-by: Tamás Bari <adaorcpp@gmail.com>

---
+++ src/libsync/updatechannel.h
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2025 ownCloud GmbH
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef UPDATECHANNEL_H
+#define UPDATECHANNEL_H
+
+#include <QString>
+
+class UpdateChannel
+{
+public:
+    enum class ChannelName : unsigned int {
+        // Values are assigned by stability (higher means more stable).
+        invalid = 0,
+        daily = 1,
+        beta = 2,
+        stable = 3,
+        enterprise = 4
+    };
+
+    ChannelName channelName() const;
+    bool isValid() const;
+    QString toString() const;
+    std::strong_ordering operator<=>(const UpdateChannel &rhs) const;
+
+    static UpdateChannel fromString(const QString &channelName);
+
+private:
+    UpdateChannel();
+    UpdateChannel &setChannelName(ChannelName channelName);
+
+    ChannelName _channelName;
+};
+
+#endif // UPDATECHANNEL_H
