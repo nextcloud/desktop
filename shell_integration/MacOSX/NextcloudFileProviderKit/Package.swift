e48845350d007de08e0319c068bc6866b5380559
Iva Horn <iva.horn@nextcloud.com>
2026-03-26 11:51:36 +0100

    chore(dependencies): Updated Realm to 20.0.4 to fix builds with Xcode 26.4
    Signed-off-by: Iva Horn <iva.horn@nextcloud.com>

--- shell_integration/MacOSX/NextcloudFileProviderKit/Package.swift
+++ shell_integration/MacOSX/NextcloudFileProviderKit/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         .package(url: "https://github.com/nextcloud/NextcloudCapabilitiesKit.git", from: "2.5.0"),
         .package(url: "https://github.com/nextcloud/NextcloudKit", from: "7.2.3"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.55.0"),
-        .package(url: "https://github.com/realm/realm-swift.git", from: "20.0.1"),
+        .package(url: "https://github.com/realm/realm-swift.git", from: "20.0.4"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
     ],
     targets: [
