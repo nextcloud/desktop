caf2b6b756c031cdfc29cf22c99e5de29a2c9e7f
Iva Horn <iva.horn@nextcloud.com>
2026-04-16 13:55:47 +0200

    fix(macOS): Updated NextcloudKit dependency to fix NextcloudFileProviderKit tests
    Signed-off-by: Iva Horn <iva.horn@nextcloud.com>

--- shell_integration/MacOSX/NextcloudFileProviderKit/Sources/NextcloudFileProviderKit/Extensions/NKFile+Extensions.swift
+++ shell_integration/MacOSX/NextcloudFileProviderKit/Sources/NextcloudFileProviderKit/Extensions/NKFile+Extensions.swift
@@ -80,7 +80,7 @@ extension NKFile {
             sharePermissionsCloudMesh: sharePermissionsCloudMesh,
             shareType: shareType,
             size: size,
-            tags: tags,
+            tags: tags.map(\.name),
             uploaded: uploaded,
             trashbinFileName: trashbinFileName,
             trashbinOriginalLocation: trashbinOriginalLocation,
