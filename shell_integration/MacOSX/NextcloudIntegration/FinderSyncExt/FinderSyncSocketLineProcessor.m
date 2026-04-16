4a7e9e2c19308792389b5b079f4b119322240826
Iva Horn <iva.horn@nextcloud.com>
2026-04-16 16:24:11 +0200

    fix(macOS): FinderSync Crash because of Socket Reading
    Signed-off-by: Iva Horn <iva.horn@nextcloud.com>

--- shell_integration/MacOSX/NextcloudIntegration/FinderSyncExt/FinderSyncSocketLineProcessor.m
+++ shell_integration/MacOSX/NextcloudIntegration/FinderSyncExt/FinderSyncSocketLineProcessor.m
@@ -27,6 +27,10 @@
     NSLog(@"Command: %@", command);
     
     if([command isEqualToString:@"STATUS"]) {
+        if([split count] < 3) {
+            NSLog(@"ERROR: STATUS message too short: '%@'", line);
+            return;
+        }
         NSString *result = [split objectAtIndex:1];
         NSArray *pathSplit = [split subarrayWithRange:NSMakeRange(2, [split count] - 2)]; // Get everything after location 2
         NSString *path = [pathSplit componentsJoinedByString:@":"];
@@ -36,6 +40,10 @@
             [self.delegate setResult:result forPath:path];
         });
     } else if([command isEqualToString:@"UPDATE_VIEW"]) {
+        if([split count] < 2) {
+            NSLog(@"ERROR: UPDATE_VIEW message too short: '%@'", line);
+            return;
+        }
         NSString *path = [split objectAtIndex:1];
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -43,6 +51,10 @@
             [self.delegate reFetchFileNameCacheForPath:path];
         });
     } else if([command isEqualToString:@"REGISTER_PATH"]) {
+        if([split count] < 2) {
+            NSLog(@"ERROR: REGISTER_PATH message too short: '%@'", line);
+            return;
+        }
         NSString *path = [split objectAtIndex:1];
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -50,6 +62,10 @@
             [self.delegate registerPath:path];
         });
     } else if([command isEqualToString:@"UNREGISTER_PATH"]) {
+        if([split count] < 2) {
+            NSLog(@"ERROR: UNREGISTER_PATH message too short: '%@'", line);
+            return;
+        }
         NSString *path = [split objectAtIndex:1];
         
         dispatch_async(dispatch_get_main_queue(), ^{
@@ -60,6 +76,10 @@
         // BEGIN and END messages, do nothing.
         return;
     } else if([command isEqualToString:@"STRING"]) {
+        if([split count] < 3) {
+            NSLog(@"ERROR: STRING message too short: '%@'", line);
+            return;
+        }
         NSString *key = [split objectAtIndex:1];
         NSString *value = [split objectAtIndex:2];
         
@@ -68,6 +88,10 @@
             [self.delegate setString:key value:value];
         });
     } else if([command isEqualToString:@"GET_MENU_ITEMS"]) {
+        if([split count] < 2) {
+            NSLog(@"ERROR: GET_MENU_ITEMS message too short: '%@'", line);
+            return;
+        }
         if([[split objectAtIndex:1] isEqualToString:@"BEGIN"]) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 NSLog(@"Resetting menu items.");
@@ -78,6 +102,10 @@
             [self.delegate menuHasCompleted];
         }
     } else if([command isEqualToString:@"MENU_ITEM"]) {
+        if([split count] < 4) {
+            NSLog(@"ERROR: MENU_ITEM message too short: '%@'", line);
+            return;
+        }
         NSDictionary *item = @{@"command": [split objectAtIndex:1], @"flags": [split objectAtIndex:2], @"text": [split objectAtIndex:3]};
         
         dispatch_async(dispatch_get_main_queue(), ^{
