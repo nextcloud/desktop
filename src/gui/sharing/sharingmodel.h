71ee63366d60351d485e32f21d24da878455aacf
Jyrki Gadinger <nilsding@nilsding.org>
2026-04-28 15:54:34 +0200

    feat(sharing): keep track of changes in viewmodel
    Signed-off-by: Jyrki Gadinger <nilsding@nilsding.org>

--- src/gui/sharing/sharingmodel.h
+++ src/gui/sharing/sharingmodel.h
@@ -25,6 +25,7 @@ public:
         PropertyRole,
         TypeRole,
         PlaceholderRole,
+        ValueRole,
     };
 
     enum FieldTypes {
@@ -39,6 +40,8 @@ public:
 
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+    Qt::ItemFlags flags(const QModelIndex &index) const override;
     QHash<int, QByteArray> roleNames() const override;
 
     [[nodiscard]] AccountState *accountState() const;
@@ -49,6 +52,7 @@ Q_SIGNALS:
 
 private:
     AccountState *_accountState = nullptr;
+    QVariantMap _fieldValues;
 };
 
 }
