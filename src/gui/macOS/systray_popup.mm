795c1ce696984bac43fdc0df0eb15d622cf6111a
Camila Ayres <hello@camilasan.com>
2026-05-06 12:53:32 +0200

    chore: update copyright year in new file.
    Signed-off-by: Camila Ayres <hello@camilasan.com>

--- src/gui/macOS/systray_popup.mm
+++ src/gui/macOS/systray_popup.mm
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
