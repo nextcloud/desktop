bc03eb5dc7760901fe3d0d2bfab1aa2b40d4ba9e
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-05-29 11:15:46 +0200

    fix(linux/updater): do not freeze on start when running update check
    to detect if the installed binary was changed while the client is
    running, we used to synchronously try to run the binary to query its
    version
    
    this in addition with the need to prevent multiple running instances at
    the same time is creating a deadlock on start
    
    as an user, I do not expect my client to decide on its own to restart
    after my distribution updated the native package
    
    as a developer and administrator of a linux box, I expect the
    distribution tools to take care of restarting what should be restarted
    or at least notify about outdated running binaries
    
    this should not have been in scope of the client
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/common/utility.cpp
+++ src/common/utility.cpp
@@ -386,35 +386,6 @@ int Utility::convertSizeToInt(size_t &convertVar)
     return static_cast<int>(convertVar);
 }
 
-// read the output of the owncloud --version command from the owncloud
-// version that is on disk. This works for most versions of the client,
-// because clients that do not yet know the --version flag return the
-// version in the first line of the help output :-)
-//
-// This version only delivers output on linux, as Mac and Win get their
-// restarting from the installer.
-QByteArray Utility::versionOfInstalledBinary(const QString &command)
-{
-    QByteArray re;
-    if (isLinux()) {
-        QString binary(command);
-        if (binary.isEmpty()) {
-            binary = qApp->arguments()[0];
-        }
-        QStringList params;
-        params << QStringLiteral("--version");
-        QProcess process;
-        process.start(binary, params);
-        process.waitForFinished(); // sets current thread to sleep and waits for pingProcess end
-        re = process.readAllStandardOutput();
-        int newline = re.indexOf('\n');
-        if (newline > 0) {
-            re.truncate(newline);
-        }
-    }
-    return re;
-}
-
 QString Utility::timeAgoInWords(const QDateTime &dt, const QDateTime &from)
 {
     QDateTime now = QDateTime::currentDateTimeUtc();
