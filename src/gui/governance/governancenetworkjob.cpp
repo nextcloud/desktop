c42229a10f1e22d019183dbc555f0d951537a308
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-06-02 22:50:11 +0200

    feat(governance): add more job types for each requests
    also improve the available properties for the job parameters
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/governance/governancenetworkjob.cpp
+++ src/gui/governance/governancenetworkjob.cpp
@@ -13,4 +13,64 @@ GovernanceNetworkJob::GovernanceNetworkJob(QObject *parent)
 {
 }
 
+GovernanceNetworkJob::ApiVersion GovernanceNetworkJob::apiVersion() const
+{
+    return _apiVersion;
+}
+
+void GovernanceNetworkJob::setApiVersion(ApiVersion newApiVersion)
+{
+    if (_apiVersion == newApiVersion) {
+        return;
+    }
+
+    _apiVersion = newApiVersion;
+    Q_EMIT apiVersionChanged();
+}
+
+GovernanceNetworkJob::EntityType GovernanceNetworkJob::entityType() const
+{
+    return _entityType;
+}
+
+void GovernanceNetworkJob::setEntityType(EntityType newEntityType)
+{
+    if (_entityType == newEntityType) {
+        return;
+    }
+
+    _entityType = newEntityType;
+    Q_EMIT entityTypeChanged();
+}
+
+QString GovernanceNetworkJob::customEntityType() const
+{
+    return _customEntityType;
+}
+
+void GovernanceNetworkJob::setCustomEntityType(const QString &newCustomEntityType)
+{
+    if (_customEntityType == newCustomEntityType) {
+        return;
+    }
+
+    _customEntityType = newCustomEntityType;
+    Q_EMIT customEntityTypeChanged();
+}
+
+QString GovernanceNetworkJob::entityId() const
+{
+    return _entityId;
+}
+
+void GovernanceNetworkJob::setEntityId(const QString &newEntityId)
+{
+    if (_entityId == newEntityId) {
+        return;
+    }
+
+    _entityId = newEntityId;
+    Q_EMIT entityIdChanged();
+}
+
 } // namespace OCC
