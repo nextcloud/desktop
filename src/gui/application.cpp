4181b99f544e6398f0ef7a2a93e30951118e0e31
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-07-03 18:58:54 +0200

    fix(account/setup): fix automated account creation via CLI options
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -325,7 +325,7 @@ Application::Application(int &argc, char **argv)
 
     // try to migrate legacy accounts and folders from a previous client version
     // only copy the settings and check what should be skipped
-    if (!configVersionMigration()) {
+    if (!AccountSetupCommandLineManager::instance()->isCommandLineParsed() && !configVersionMigration()) {
         qCWarning(lcApplication) << "Config version migration was not possible.";
     }
 
@@ -386,6 +386,16 @@ Application::Application(int &argc, char **argv)
 #endif
 
     connect(this, &SharedTools::QtSingleApplication::messageReceived, this, &Application::slotParseMessage);
+
+    if (AccountSetupCommandLineManager::instance()->isCommandLineParsed()) {
+        _folderManager.reset(new FolderMan);
+        AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine();
+        _quitInstance = true;
+    }
+    AccountSetupCommandLineManager::destroy();
+    if (_quitInstance) {
+        return;
+    }
 
     // create accounts and folders from a legacy desktop client or from the current config file
     setupAccountsAndFolders();
@@ -460,11 +470,6 @@ Application::Application(int &argc, char **argv)
     _gui->createTray();
 
     handleEditLocallyFromOptions();
-
-    if (AccountSetupCommandLineManager::instance()->isCommandLineParsed()) {
-        AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine();
-    }
-    AccountSetupCommandLineManager::destroy();
 
 #if defined(BUILD_FILE_PROVIDER_MODULE)
     Mac::FileProvider::instance();
