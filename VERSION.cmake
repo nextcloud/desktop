dd51aa55d077775c35d03d77af98b860967616fb
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-07-08 13:01:18 +0200

    chore: prepare for 4.0.11 stable release
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- VERSION.cmake
+++ VERSION.cmake
@@ -7,7 +7,7 @@
 # ------------------------------------
 set(MIRALL_VERSION_MAJOR 4)
 set(MIRALL_VERSION_MINOR 0)
-set(MIRALL_VERSION_PATCH 10)
+set(MIRALL_VERSION_PATCH 11)
 set(MIRALL_VERSION_YEAR  2026)
 set(MIRALL_SOVERSION     0)
 set(MIRALL_PREVERSION_HUMAN "4.0.10")  # For preversions where PATCH>=50. Use version + alpha, rc1, rc2, etc.
