4374271e18f7289d003bbdc5ca072a67048a2875
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-07-22 10:38:46 +0200

    fix(governance): makes desktop UI closer to the files web UI
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/governance/governancelabelslistmodel.cpp
+++ src/gui/governance/governancelabelslistmodel.cpp
@@ -5,6 +5,9 @@
 
 #include "governancelabelslistmodel.h"
 
+#include "applygovernancelabel.h"
+#include "deletegovernancelabel.h"
+
 #include <QLoggingCategory>
 #include <QJsonDocument>
 #include <QJsonObject>
@@ -22,6 +25,51 @@ GovernanceLabelsListModel::GovernanceLabelsListModel(QObject *parent)
 {
 }
 
+AccountPtr GovernanceLabelsListModel::account() const
+{
+    return _account;
+}
+
+void GovernanceLabelsListModel::setAccount(AccountPtr newAccount)
+{
+    if (_account == newAccount) {
+        return;
+    }
+
+    _account = newAccount;
+    Q_EMIT accountChanged();
+}
+
+Governance::ApiVersion GovernanceLabelsListModel::apiVersion() const
+{
+    return _apiVersion;
+}
+
+void GovernanceLabelsListModel::setApiVersion(Governance::ApiVersion newApiVersion)
+{
+    if (_apiVersion == newApiVersion) {
+        return;
+    }
+
+    _apiVersion = newApiVersion;
+    Q_EMIT apiVersionChanged();
+}
+
+Governance::EntityType GovernanceLabelsListModel::entityType() const
+{
+    return _entityType;
+}
+
+void GovernanceLabelsListModel::setEntityType(Governance::EntityType newEntityType)
+{
+    if (_entityType == newEntityType) {
+        return;
+    }
+
+    _entityType = newEntityType;
+    Q_EMIT entityTypeChanged();
+}
+
 int GovernanceLabelsListModel::rowCount(const QModelIndex &parent) const
 {
     auto result = 0;
@@ -29,7 +77,7 @@ int GovernanceLabelsListModel::rowCount(const QModelIndex &parent) const
         return result;
     }
 
-    result = _data.count() + 1;
+    result = _data.count();
     return result;
 }
 
@@ -45,56 +93,33 @@ QVariant GovernanceLabelsListModel::data(const QModelIndex &index, int role) con
         return result;
     }
 
-    if (index.row() < 0 || index.row() >= _data.count() + 1) {
+    if (index.row() < 0 || index.row() >= _data.count()) {
         return result;
     }
 
     if (role >= Qt::UserRole + 1) {
         auto convertedRole = static_cast<LabelsListModelRoles>(role);
 
-        [[unlikely]] if (index.row() == _data.count()) {
-            switch (convertedRole)
-            {
-            case LabelsListModelRoles::IdRole:
-                result = -1;
-                break;
-            case LabelsListModelRoles::NameRole:
-                result = tr("None");
-                break;
-            case LabelsListModelRoles::PriorityRole:
-                result = -1;
-                break;
-            case LabelsListModelRoles::DescriptionRole:
-                result = tr("No label");
-                break;
-            case LabelsListModelRoles::ColorRole:
-                break;
-            case LabelsListModelRoles::SelectedRole:
-                result = false;
-                break;
-            }
-        } else {
-            switch (convertedRole)
-            {
-            case LabelsListModelRoles::IdRole:
-                result = _data[index.row()]._id;
-                break;
-            case LabelsListModelRoles::NameRole:
-                result = _data[index.row()]._name;
-                break;
-            case LabelsListModelRoles::PriorityRole:
-                result = _data[index.row()]._priority;
-                break;
-            case LabelsListModelRoles::DescriptionRole:
-                result = _data[index.row()]._description;
-                break;
-            case LabelsListModelRoles::ColorRole:
-                result = _data[index.row()]._color;
-                break;
-            case LabelsListModelRoles::SelectedRole:
-                result = _data[index.row()]._status == GovernanceLabelInfo::Status::Selected;
-                break;
-            }
+        switch (convertedRole)
+        {
+        case LabelsListModelRoles::IdRole:
+            result = _data[index.row()]._id;
+            break;
+        case LabelsListModelRoles::NameRole:
+            result = _data[index.row()]._name;
+            break;
+        case LabelsListModelRoles::PriorityRole:
+            result = _data[index.row()]._priority;
+            break;
+        case LabelsListModelRoles::DescriptionRole:
+            result = _data[index.row()]._description;
+            break;
+        case LabelsListModelRoles::ColorRole:
+            result = _data[index.row()]._color;
+            break;
+        case LabelsListModelRoles::SelectedRole:
+            result = _data[index.row()]._currentStatus == GovernanceLabelInfo::Status::Selected;
+            break;
         }
     }
 
@@ -163,46 +188,33 @@ void GovernanceLabelsListModel::setAvailableLabelsJsonData(const QJsonDocument &
     }
 }
 
-void GovernanceLabelsListModel::setExistingLabelsJsonData(const QJsonDocument &reply)
+void GovernanceLabelsListModel::toggleLabel(const int row)
 {
-    _existingLabelsReply = reply.object();
-
-    if (!validState()) {
-        qCWarning(lcGovernanceLabelsListModel()) << "setting data in an invalid model state";
-        return;
-    }
-
-    if (hasReceivedAllData()) {
-        refreshModel();
-    }
-}
-
-void GovernanceLabelsListModel::toggleLabel(const int index, const QString &labelId)
-{
-    if (_data.size() == index) {
-        for (const auto &oneLabel : std::as_const(_data)) {
-            if (oneLabel._status == GovernanceLabelInfo::Status::Selected) {
-                Q_EMIT removeLabel(oneLabel._id);
-            }
-        }
-    } else if (index <_data.size() && index >= 0) {
-        switch (_data[index]._status)
+    const auto toggleSingleLabel = [this] (const int row, auto &oneLabel) -> void
+    {
+        switch (oneLabel._currentStatus)
         {
         case GovernanceLabelInfo::Status::Selected:
-            Q_EMIT removeLabel(labelId);
+            oneLabel._currentStatus = GovernanceLabelInfo::Status::Available;
+            Q_EMIT dataChanged(index(row), index(row), {static_cast<int>(LabelsListModelRoles::SelectedRole)});
+            setHasPendingChanges(checksForPendingChanges());
             break;
         case GovernanceLabelInfo::Status::Available:
-            Q_EMIT addLabel(labelId);
+            oneLabel._currentStatus = GovernanceLabelInfo::Status::Selected;
+            Q_EMIT dataChanged(index(row), index(row), {static_cast<int>(LabelsListModelRoles::SelectedRole)});
+            setHasPendingChanges(checksForPendingChanges());
             break;
         case GovernanceLabelInfo::Status::UnknownStatus:
             break;
         }
-    }
-}
+    };
 
-void GovernanceLabelsListModel::toggleLabel(const QString &labelId)
-{
-    Q_EMIT addLabel(labelId);
+    if (row < _data.size() && row >= 0) {
+        auto &oneLabel = _data[row];
+        toggleSingleLabel(row, oneLabel);
+    } else {
+        qCWarning(lcGovernanceLabelsListModel()) << "impossible to toggle the selected state of a label with invalid index" << row;
+    }
 }
 
 void OCC::GovernanceLabelsListModel::etagChanged()
@@ -210,21 +222,77 @@ void OCC::GovernanceLabelsListModel::etagChanged()
     emitRefreshData();
 }
 
-void GovernanceLabelsListModel::labelWasModified()
+void GovernanceLabelsListModel::labelsWereModified()
 {
+    if (_applyingChanges || !_pendingRefreshLabelIds.isEmpty()) {
+        return;
+    }
+
+    setHasPendingChanges(false);
     emitRefreshData();
+    _busy = false;
+    Q_EMIT busyChanged();
+}
+
+void GovernanceLabelsListModel::reset()
+{
+    for (auto dataRow = 0; dataRow < _data.size(); ++dataRow) {
+        auto &oneLabel = _data[dataRow];
+        if (oneLabel._currentStatus != oneLabel._serverStatus) {
+            oneLabel._currentStatus = oneLabel._serverStatus;
+            Q_EMIT dataChanged(index(dataRow), index(dataRow), {static_cast<int>(LabelsListModelRoles::SelectedRole)});
+        }
+    }
+
+    setHasPendingChanges(false);
+}
+
+void GovernanceLabelsListModel::apply()
+{
+    _busy = true;
+    Q_EMIT busyChanged();
+    _applyingChanges = true;
+    auto hasPendingChanges = false;
+
+    for (auto dataRow = 0; dataRow < _data.size(); ++dataRow) {
+        auto &oneLabel = _data[dataRow];
+        if (oneLabel._currentStatus != oneLabel._serverStatus) {
+            hasPendingChanges = true;
+
+            switch (oneLabel._currentStatus)
+            {
+            case GovernanceLabelInfo::Status::Selected:
+                _pendingRefreshLabelIds.insert(oneLabel._id);
+                addLabel(oneLabel._id);
+                break;
+            case GovernanceLabelInfo::Status::Available:
+                _pendingRefreshLabelIds.insert(oneLabel._id);
+                removeLabel(oneLabel._id);
+                break;
+            case GovernanceLabelInfo::Status::UnknownStatus:
+                break;
+            }
+        }
+    }
+
+    _applyingChanges = false;
+    if (hasPendingChanges) {
+        labelsWereModified();
+    } else {
+        _busy = false;
+        Q_EMIT busyChanged();
+    }
 }
 
 void GovernanceLabelsListModel::emitRefreshData()
 {
     if (!validState()) {
+        qCWarning(lcGovernanceLabelsListModel()) << "cannot emit refreshData signal from an invalid model state";
         return;
     }
 
     _availableLabelsReply = {};
-    _existingLabelsReply = {};
     Q_EMIT refreshAvailableLabelsData(_labelType, _entityId);
-    Q_EMIT refreshExistingLabelsData(_entityId);
 }
 
 QJsonValue GovernanceLabelsListModel::readOcsReply(const QJsonObject &replyObject)
@@ -250,43 +318,34 @@ QJsonValue GovernanceLabelsListModel::readOcsReply(const QJsonObject &replyObjec
 
 bool GovernanceLabelsListModel::hasReceivedAllData() const
 {
-    return _availableLabelsReply.contains(u"ocs"_s) && _existingLabelsReply.contains(u"ocs"_s);
+    return _availableLabelsReply.contains(u"ocs"_s);
 }
 
 bool GovernanceLabelsListModel::validState() const
 {
-    return !_entityId.isEmpty() && _labelType != Governance::LabelType::InvalidLabelType;
+    return !_entityId.isEmpty() && _labelType != Governance::LabelType::InvalidLabelType && _account;
 }
 
 void GovernanceLabelsListModel::refreshModel()
 {
     const auto availableLabelsArray = readOcsReply(_availableLabelsReply).toArray();
-    const auto existingLabelsObject = readOcsReply(_existingLabelsReply).toObject();
 
-    const auto parseExistingSingleLabel = [this] (const int rowIndex, const QJsonObject &oneLabel) -> void{
-        if (_data[rowIndex]._id == oneLabel[u"id"_s]) {
-            if (_data[rowIndex]._status != GovernanceLabelInfo::Status::Selected) {
-                _data[rowIndex]._status = GovernanceLabelInfo::Status::Selected;
-                Q_EMIT dataChanged(index(rowIndex), index(rowIndex));
-            }
-        } else {
-            if (_data[rowIndex]._status != GovernanceLabelInfo::Status::Available) {
-                _data[rowIndex]._status = GovernanceLabelInfo::Status::Available;
-                Q_EMIT dataChanged(index(rowIndex), index(rowIndex));
-            }
-        }
-    };
+    qCDebug(lcGovernanceLabelsListModel()) << "label type:" << _labelType
+                                           << "entity id:" << _entityId
+                                           << "label behavior:" << _labelBehavior
+                                           << "data:" << availableLabelsArray;
 
     beginResetModel();
     _data.clear();
     for (const auto oneLabel : availableLabelsArray) {
-        const auto rowIndex = _data.size();
         const auto oneLabelObject = oneLabel.toObject();
         _data.emplaceBack(oneLabelObject.value(u"id"_s).toString(),
                           oneLabelObject.value(u"name"_s).toString(),
                           oneLabelObject.value(u"priority"_s).toInt(),
                           oneLabelObject.value(u"description"_s).toString(),
-                          oneLabelObject.value(u"color"_s).toString()
+                          oneLabelObject.value(u"color"_s).toString(),
+                          oneLabelObject.value(u"isAssigned"_s).toBool() ? GovernanceLabelInfo::Status::Selected : GovernanceLabelInfo::Status::Available,
+                          oneLabelObject.value(u"isAssigned"_s).toBool() ? GovernanceLabelInfo::Status::Selected : GovernanceLabelInfo::Status::Available
                           );
 
         auto labelKeyName = QString{};
@@ -305,20 +364,100 @@ void GovernanceLabelsListModel::refreshModel()
             break;
         }
 
-        qCDebug(lcGovernanceLabelsListModel()) << "checking existing" << labelKeyName << "labels";
-        if (!existingLabelsObject.contains(labelKeyName)) {
-            qCWarning(lcGovernanceLabelsListModel()) << "missing" << labelKeyName << "data in OCS reply";
+        qCDebug(lcGovernanceLabelsListModel()) << "label type:" << _labelType
+                                               << "entity id:" << _entityId
+                                               << "checking existing:" << labelKeyName
+                                               << "current label:" << oneLabelObject;
+    }
+
+    _availableLabelsReply = {};
+
+    endResetModel();
+    Q_EMIT isEmptyChanged();
+    setHasPendingChanges(false);
+}
+
+void GovernanceLabelsListModel::addLabel(const QString &labelId)
+{
+    auto addLabelJob = std::make_unique<ApplyGovernanceLabel>();
+
+    connect(addLabelJob.get(), &ApplyGovernanceLabel::finished,
+            addLabelJob.get(), [this, &labelId, addLabelJobPtr = addLabelJob.get()] ([[maybe_unused]] QJsonDocument reply) -> void
+            {
+                _pendingRefreshLabelIds.remove(labelId);
+                labelsWereModified();
+                addLabelJobPtr->deleteLater();
+            });
+
+    connect(addLabelJob.get(), &ApplyGovernanceLabel::finishedWithError,
+            addLabelJob.get(), [this, &labelId, addLabelJobPtr = addLabelJob.get()] ([[maybe_unused]] int errorCode, const QString &errorMessage) -> void
+            {
+                _pendingRefreshLabelIds.remove(labelId);
+                Q_EMIT displayError(errorMessage);
+                labelsWereModified();
+                addLabelJobPtr->deleteLater();
+            });
+
+    addLabelJob->setAccount(account());
+    addLabelJob->setLabelType(labelType());
+    addLabelJob->setEntityId(entityId());
+
+    addLabelJob->start(labelId);
+    addLabelJob.release();
+}
+
+void GovernanceLabelsListModel::removeLabel(const QString &labelId)
+{
+    auto addLabelJob = std::make_unique<DeleteGovernanceLabel>();
+
+    connect(addLabelJob.get(), &ApplyGovernanceLabel::finished,
+            addLabelJob.get(), [this, &labelId, addLabelJobPtr = addLabelJob.get()] ([[maybe_unused]] QJsonDocument reply) -> void
+            {
+                _pendingRefreshLabelIds.remove(labelId);
+                labelsWereModified();
+                addLabelJobPtr->deleteLater();
+            });
+
+    connect(addLabelJob.get(), &ApplyGovernanceLabel::finishedWithError,
+            addLabelJob.get(), [this, &labelId, addLabelJobPtr = addLabelJob.get()] ([[maybe_unused]] int errorCode, const QString &errorMessage) -> void
+            {
+                _pendingRefreshLabelIds.remove(labelId);
+                Q_EMIT displayError(errorMessage);
+                labelsWereModified();
+                addLabelJobPtr->deleteLater();
+            });
+
+    addLabelJob->setAccount(account());
+    addLabelJob->setLabelType(labelType());
+    addLabelJob->setEntityId(entityId());
+
+    addLabelJob->start(labelId);
+    addLabelJob.release();
+}
+
+void GovernanceLabelsListModel::setHasPendingChanges(bool newValue)
+{
+    if (_hasPendingChanges == newValue) {
+        return;
+    }
+
+    _hasPendingChanges = newValue;
+    Q_EMIT hasPendingChangesChanged();
+}
+
+bool GovernanceLabelsListModel::checksForPendingChanges() const
+{
+    auto hasPendingChanges = false;
+
+    for (auto dataRow = 0; dataRow < _data.size(); ++dataRow) {
+        auto &oneLabel = _data[dataRow];
+        if (oneLabel._currentStatus != oneLabel._serverStatus) {
+            hasPendingChanges = true;
             break;
         }
-
-        const auto &sensitivityLabels = existingLabelsObject[labelKeyName].toArray();
-
-        for (const auto &oneLabel : sensitivityLabels) {
-            const auto &oneLabelObject = oneLabel.toObject();
-            parseExistingSingleLabel(rowIndex, oneLabelObject);
-        }
     }
-    endResetModel();
+
+    return hasPendingChanges;
 }
 
 GovernanceLabelsListModel::LabelBehavior GovernanceLabelsListModel::labelBehavior() const
@@ -334,6 +473,21 @@ void GovernanceLabelsListModel::setLabelBehavior(LabelBehavior newLabelBehavior)
 
     _labelBehavior = newLabelBehavior;
     Q_EMIT labelBehaviorChanged();
+}
+
+bool GovernanceLabelsListModel::busy() const
+{
+    return _busy;
+}
+
+bool GovernanceLabelsListModel::isEmpty() const
+{
+    return _data.isEmpty();
+}
+
+bool GovernanceLabelsListModel::hasPendingChanges() const
+{
+    return _hasPendingChanges;
 }
 
 } // namespace OCC
