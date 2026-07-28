868136be70eded85c652a53f037a719172f15eb6
Claudio Cambra <developer@claudiocambra.com>
2026-07-28 21:48:31 +0800

    feat(sharing): adapt tests to new jobs
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- test/testunifiedsharing.cpp
+++ test/testunifiedsharing.cpp
@@ -7,11 +7,18 @@
 #include "gui/sharing/addsourcejob.h"
 #include "gui/sharing/createsharejob.h"
 #include "gui/sharing/destroysharejob.h"
+#include "gui/sharing/generatesecretjob.h"
+#include "gui/sharing/getsharejob.h"
+#include "gui/sharing/getsharesjob.h"
 #include "gui/sharing/recipientsearchmodel.h"
 #include "gui/sharing/removerecipientjob.h"
+#include "gui/sharing/removesourcejob.h"
 #include "gui/sharing/searchrecipientsjob.h"
 #include "gui/sharing/setpermissionjob.h"
 #include "gui/sharing/setpermissionpresetjob.h"
+#include "gui/sharing/setpropertyjob.h"
+#include "gui/sharing/setrecipientsecretjob.h"
+#include "gui/sharing/setsharestatejob.h"
 #include "gui/sharing/share.h"
 #include "gui/sharing/unifiedsharingrequest.h"
 #include "gui/sharing/updatesharejob.h"
@@ -20,6 +27,8 @@
 #include <QSignalSpy>
 #include <QTest>
 #include <QUrlQuery>
+
+#include <algorithm>
 
 using namespace OCC;
 using namespace OCC::Gui::Sharing;
@@ -36,10 +45,26 @@ private slots:
         auto requestCount = 0;
         auto requestPath = QString{};
         auto requestVerb = QByteArray{};
+        auto requestQuery = QList<QPair<QString, QString>>{};
+        auto requestBody = QJsonObject{};
+        auto requestContentType = QByteArray{};
 
-        fakeFolder.setServerOverride([&](FakeQNAM::Operation operation, const QNetworkRequest &request, QIODevice *) {
+        fakeFolder.setServerOverride([&](FakeQNAM::Operation operation, const QNetworkRequest &request, QIODevice *outgoingData) {
             ++requestCount;
             requestPath = request.url().path();
+            requestQuery = QUrlQuery{request.url()}.queryItems();
+            requestQuery.removeAll({"format"_L1, "json"_L1});
+            std::ranges::sort(requestQuery);
+            requestContentType = request.rawHeader("Content-Type");
+            if (outgoingData) {
+                if (!outgoingData->isOpen()) {
+                    outgoingData->open(QIODevice::ReadOnly);
+                }
+                requestBody = QJsonDocument::fromJson(outgoingData->peek(outgoingData->bytesAvailable())).object();
+                outgoingData->reset();
+            } else {
+                requestBody = {};
+            }
             requestVerb = request.attribute(QNetworkRequest::CustomVerbAttribute).toByteArray();
             if (requestVerb.isEmpty()) {
                 switch (operation) {
@@ -60,21 +85,27 @@ private slots:
                 }
             }
 
-            return new FakePayloadReply{operation,
-                                        request,
-                                        R"json({
+            auto statusCode = 200;
+            if (requestVerb == "POST" && requestPath.endsWith("/api/v1/share"_L1)) {
+                statusCode = 201;
+            } else if (requestVerb == "DELETE" && requestPath.endsWith("/api/v1/share/share-1"_L1)) {
+                statusCode = 204;
+            }
+            const auto response = QString{R"json({
                 "ocs": {
                     "meta": {
                         "status": "ok",
-                        "statuscode": 200,
+                        "statuscode": %1,
                         "message": "OK"
                     },
                     "data": {
                         "id": "share-1"
                     }
                 }
-            })json",
-                                        this};
+            })json"}
+                                      .arg(statusCode)
+                                      .toUtf8();
+            return new FakePayloadReply{operation, request, response, this};
         });
 
         const auto account = fakeFolder.account();
@@ -83,46 +114,110 @@ private slots:
         const auto verifyRequest = [&](UnifiedSharingRequest *job,
                                        const QByteArray &expectedVerb,
                                        const QString &expectedPath,
-                                       const QHash<QString, QString> &expectedParameters = {}) {
-            for (auto it = expectedParameters.cbegin(); it != expectedParameters.cend(); ++it) {
-                QCOMPARE(job->getParamValue(it.key()), it.value());
-            }
-
+                                       QList<QPair<QString, QString>> expectedQuery = {},
+                                       const QJsonObject &expectedBody = {}) {
             QSignalSpy finishedSpy{job, &UnifiedSharingRequest::jobFinished};
             job->start();
             QVERIFY(finishedSpy.wait());
             QCOMPARE(requestVerb, expectedVerb);
             QVERIFY2(requestPath.endsWith(expectedPath), qPrintable(requestPath));
+            std::ranges::sort(expectedQuery);
+            QCOMPARE(requestQuery, expectedQuery);
+            QCOMPARE(requestBody, expectedBody);
+            if (!expectedBody.isEmpty()) {
+                QCOMPARE(requestContentType, "application/json");
+            }
         };
 
+        verifyRequest(new SearchRecipientsJob{account, "ali"_L1, 10, 20, {"user-class"_L1, "group-class"_L1}},
+                      "GET",
+                      "/ocs/v2.php/apps/sharing/api/v1/recipients",
+                      {{"query"_L1, "ali"_L1},
+                       {"offset"_L1, "10"_L1},
+                       {"limit"_L1, "20"_L1},
+                       {"recipientTypeClasses%5B%5D"_L1, "user-class"_L1},
+                       {"recipientTypeClasses%5B%5D"_L1, "group-class"_L1}});
+        verifyRequest(new GenerateSecretJob{account}, "GET", "/ocs/v2.php/apps/sharing/api/v1/secret");
         verifyRequest(new CreateShareJob{account}, "POST", "/ocs/v2.php/apps/sharing/api/v1/share");
-        verifyRequest(new DestroyShareJob{account, "share-1"_L1}, "DELETE", "/ocs/v2.php/apps/sharing/api/v1/share/share-1");
+        verifyRequest(new SetShareStateJob{account, *share, Share::ShareState::Active},
+                      "PUT",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1/state",
+                      {},
+                      {{"state"_L1, "active"_L1}});
         verifyRequest(new AddSourceJob{account, *share, "42"_L1},
                       "POST",
                       "/ocs/v2.php/apps/sharing/api/v1/share/share-1/source",
+                      {},
                       {{"class"_L1, "OCA\\Files\\Sharing\\Source\\NodeShareSourceType"_L1}, {"value"_L1, "42"_L1}});
-        verifyRequest(new AddRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1},
+        verifyRequest(new RemoveSourceJob{account, *share, "42"_L1},
+                      "DELETE",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1/source",
+                      {{"class"_L1, "OCA\\Files\\Sharing\\Source\\NodeShareSourceType"_L1}, {"value"_L1, "42"_L1}});
+        verifyRequest(new AddRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1, "https://example.com"_L1},
                       "POST",
                       "/ocs/v2.php/apps/sharing/api/v1/share/share-1/recipient",
-                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}});
-        verifyRequest(new RemoveRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1},
+                      {},
+                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}, {"instance"_L1, "https://example.com"_L1}});
+        verifyRequest(new RemoveRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1, "https://example.com"_L1},
                       "DELETE",
                       "/ocs/v2.php/apps/sharing/api/v1/share/share-1/recipient",
-                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}});
-        verifyRequest(new SearchRecipientsJob{account, "ali"_L1, 10, 20},
-                      "GET",
-                      "/ocs/v2.php/apps/sharing/api/v1/recipients",
-                      {{"query"_L1, "ali"_L1}, {"offset"_L1, "10"_L1}, {"limit"_L1, "20"_L1}});
+                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}, {"instance"_L1, "https%3A%2F%2Fexample.com"_L1}});
+        verifyRequest(new SetRecipientSecretJob{account, *share, "recipient-class"_L1, "alice"_L1, "secret"_L1, "https://example.com"_L1},
+                      "PUT",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1/recipient/secret",
+                      {},
+                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}, {"secret"_L1, "secret"_L1}, {"instance"_L1, "https://example.com"_L1}});
+        verifyRequest(new SetPropertyJob{account, *share, "property-class"_L1, std::nullopt},
+                      "PUT",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1/property",
+                      {},
+                      {{"class"_L1, "property-class"_L1}, {"value"_L1, QJsonValue::Null}});
         verifyRequest(new SetPermissionJob{account, *share, "permission-class"_L1, true},
                       "PUT",
                       "/ocs/v2.php/apps/sharing/api/v1/share/share-1/permission",
-                      {{"class"_L1, "permission-class"_L1}, {"enabled"_L1, "true"_L1}});
+                      {},
+                      {{"class"_L1, "permission-class"_L1}, {"enabled"_L1, true}});
         verifyRequest(new SetPermissionPresetJob{account, *share, "preset-class"_L1},
                       "PUT",
                       "/ocs/v2.php/apps/sharing/api/v1/share/share-1/permission/preset",
-                      {{"permissionPreset"_L1, "preset-class"_L1}});
+                      {},
+                      {{"permissionPresetClass"_L1, "preset-class"_L1}});
+        verifyRequest(new DestroyShareJob{account, "share-1"_L1}, "DELETE", "/ocs/v2.php/apps/sharing/api/v1/share/share-1");
+        verifyRequest(new GetShareJob{account,
+                                      "share-1"_L1,
+                                      "secret"_L1,
+                                      QJsonObject{{"argument-class"_L1, QJsonObject{{"key"_L1, "value"_L1}}}}},
+                      "POST",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1",
+                      {},
+                      {{"secret"_L1, "secret"_L1}, {"arguments"_L1, QJsonObject{{"argument-class"_L1, QJsonObject{{"key"_L1, "value"_L1}}}}}});
+        verifyRequest(
+            new GetSharesJob{account, "source-class"_L1, "42"_L1, "share-0"_L1, 50},
+            "GET",
+            "/ocs/v2.php/apps/sharing/api/v1/shares",
+            {{"filterSourceTypeClass"_L1, "source-class"_L1}, {"filterSourceTypeValue"_L1, "42"_L1}, {"lastShareID"_L1, "share-0"_L1}, {"limit"_L1, "50"_L1}});
+        verifyRequest(new AddRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1},
+                      "POST",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1/recipient",
+                      {},
+                      {{"class"_L1, "recipient-class"_L1}, {"value"_L1, "alice"_L1}});
+        verifyRequest(new GetShareJob{account, "share-1"_L1},
+                      "POST",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1");
+        QCOMPARE(requestContentType, "application/x-www-form-urlencoded");
+        verifyRequest(new UnifiedSharingRequest{account,
+                                                "/ocs/v2.php/apps/sharing/api/v1/share/share-1"_L1,
+                                                "POST"_ba,
+                                                {.body = QJsonObject{}}},
+                      "POST",
+                      "/ocs/v2.php/apps/sharing/api/v1/share/share-1");
+        QCOMPARE(requestContentType, "application/json");
+        verifyRequest(new GetSharesJob{account},
+                      "GET",
+                      "/ocs/v2.php/apps/sharing/api/v1/shares",
+                      {{"limit"_L1, "100"_L1}});
 
-        QCOMPARE(requestCount, 8);
+        QCOMPARE(requestCount, 19);
         delete share;
     }
 
@@ -157,13 +252,68 @@ private slots:
         QCOMPARE(requestCount, 1);
     }
 
+    void operationSpecificStatusCodesAreEnforced()
+    {
+        FakeFolder fakeFolder{{}, {}, {}, false};
+        fakeFolder.setServerOverride([&](FakeQNAM::Operation operation, const QNetworkRequest &request, QIODevice *) {
+            return new FakePayloadReply{operation,
+                                        request,
+                                        R"json({
+                "ocs": {
+                    "meta": {
+                        "status": "ok",
+                        "statuscode": 200,
+                        "message": "OK"
+                    },
+                    "data": {}
+                }
+            })json",
+                                        this};
+        });
+
+        const auto jobs = QList<UnifiedSharingRequest *>{
+            new CreateShareJob{fakeFolder.account()},
+            new DestroyShareJob{fakeFolder.account(), "share-1"_L1},
+        };
+        for (const auto job : jobs) {
+            auto jobFinished = false;
+            auto ocsError = false;
+            connect(job, &UnifiedSharingRequest::jobFinished, this, [&](const QJsonDocument &, int) {
+                jobFinished = true;
+            });
+            connect(job, &UnifiedSharingRequest::ocsError, this, [&](int, const QString &) {
+                ocsError = true;
+            });
+
+            job->start();
+
+            QTRY_VERIFY(ocsError);
+            QVERIFY(!jobFinished);
+        }
+    }
+
     void typedJobsParseTheirResults()
     {
         FakeFolder fakeFolder{{}, {}, {}, false};
         fakeFolder.setServerOverride([&](FakeQNAM::Operation operation, const QNetworkRequest &request, QIODevice *) {
-            const auto data =
-                request.url().path().endsWith("/recipients"_L1) ? R"json([{"label":"Alice"}])json" : R"json({"id":"share-1","state":"active"})json";
-            const auto statusCode = operation == QNetworkAccessManager::DeleteOperation ? 204 : 200;
+            const auto path = request.url().path();
+            auto data = QByteArray{R"json({"id":"share-1","state":"active"})json"};
+            if (path.endsWith("/recipients"_L1)) {
+                data = R"json([{"label":"Alice"}])json";
+            } else if (path.endsWith("/secret"_L1)) {
+                data = R"json("generated-secret")json";
+            } else if (path.endsWith("/shares"_L1)) {
+                data = R"json([{"id":"share-1","state":"active"},{"id":"share-2","state":"draft"}])json";
+            }
+
+            auto statusCode = 200;
+            const auto customVerb = request.attribute(QNetworkRequest::CustomVerbAttribute).toByteArray();
+            if ((operation == QNetworkAccessManager::DeleteOperation || customVerb == "DELETE") && !path.endsWith("/source"_L1)
+                && !path.endsWith("/recipient"_L1)) {
+                statusCode = 204;
+            } else if (operation == QNetworkAccessManager::PostOperation && path.endsWith("/share"_L1)) {
+                statusCode = 201;
+            }
             const auto payload = QString{R"json({"ocs":{"meta":{"status":"ok","statuscode":%1,"message":"OK"},"data":%2}})json"}
                                      .arg(statusCode)
                                      .arg(QString::fromUtf8(data))
@@ -201,6 +351,33 @@ private slots:
         QTRY_COMPARE(recipients.size(), 1);
         QCOMPARE(recipients.at(0).toObject().value("label"_L1).toString(), "Alice"_L1);
 
+        auto generatedSecret = QString{};
+        const auto secretJob = new GenerateSecretJob{account};
+        connect(secretJob, &GenerateSecretJob::secretGenerated, this, [&](const QString &secret) {
+            generatedSecret = secret;
+        });
+        secretJob->start();
+        QTRY_COMPARE(generatedSecret, "generated-secret"_L1);
+
+        QPointer<Share> fetchedShare;
+        const auto getShareJob = new GetShareJob{account, "share-1"_L1};
+        connect(getShareJob, &GetShareJob::shareFetched, this, [&](QPointer<Share> share) {
+            fetchedShare = share;
+        });
+        getShareJob->start();
+        QTRY_VERIFY(fetchedShare);
+        QCOMPARE(fetchedShare->id(), "share-1"_L1);
+
+        auto fetchedShares = QList<QPointer<Share>>{};
+        const auto getSharesJob = new GetSharesJob{account};
+        connect(getSharesJob, &GetSharesJob::sharesFetched, this, [&](const QList<QPointer<Share>> &shares) {
+            fetchedShares = shares;
+        });
+        getSharesJob->start();
+        QTRY_COMPARE(fetchedShares.size(), 2);
+        QCOMPARE(fetchedShares.at(0)->id(), "share-1"_L1);
+        QCOMPARE(fetchedShares.at(1)->id(), "share-2"_L1);
+
         auto destroyed = false;
         const auto destroyJob = new DestroyShareJob{account, createdShare->id()};
         connect(destroyJob, &DestroyShareJob::jobFinished, this, [&](const QJsonDocument &, int) {
@@ -210,6 +387,8 @@ private slots:
         QTRY_VERIFY(destroyed);
 
         delete createdShare;
+        delete fetchedShare;
+        qDeleteAll(fetchedShares);
     }
 
     void ocsErrorsAreSeparateFromSuccessfulResults()
@@ -276,10 +455,14 @@ private slots:
         const auto share = Share::fromJson(QJsonDocument::fromJson(R"json({"ocs":{"data":{"id":"share-1","state":"draft"}}})json"), account);
         const auto jobs = QList<UpdateShareJob *>{
             new AddSourceJob{account, *share, "42"_L1},
+            new RemoveSourceJob{account, *share, "42"_L1},
             new AddRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1},
             new RemoveRecipientJob{account, *share, "recipient-class"_L1, "alice"_L1},
+            new SetRecipientSecretJob{account, *share, "recipient-class"_L1, "alice"_L1, "secret"_L1},
+            new SetPropertyJob{account, *share, "property-class"_L1, "value"_L1},
             new SetPermissionJob{account, *share, "permission-class"_L1, true},
             new SetPermissionPresetJob{account, *share, "preset-class"_L1},
+            new SetShareStateJob{account, *share, Share::ShareState::Active},
         };
 
         for (const auto job : jobs) {
