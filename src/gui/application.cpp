255ca72587c89e471923d2c51b486d5e2e4156d5
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-07-28 18:58:10 +0200

    chore(cmd): in config mode, provide meaningfull return code
    teh executable return code should be 0 in case it worked and non 0 for
    error cases
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/gui/application.cpp
+++ src/gui/application.cpp
@@ -416,7 +416,9 @@ Application::Application(int &argc, char **argv)
     setupAccountsAndFolders();
 
     if (AccountSetupCommandLineManager::instance()->isCommandLineParsed()) {
-        AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine();
+        if (!AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine()) {
+            qCWarning(lcApplication()) << "setup of the account had some issues and could not be completed";
+        }
         _quitInstance = true;
     }
     AccountSetupCommandLineManager::destroy();
@@ -683,7 +685,9 @@ void Application::parseOptionsRemoteCommand(const QStringList &options)
     handleUriFromOptions();
 
     if (AccountSetupCommandLineManager::instance()->isCommandLineParsed()) {
-        AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine();
+        if (!AccountSetupCommandLineManager::instance()->setupAccountFromCommandLine()) {
+            qCWarning(lcApplication()) << "setup of the account had some issues and could not be completed";
+        }
     }
     AccountSetupCommandLineManager::destroy();
 }
