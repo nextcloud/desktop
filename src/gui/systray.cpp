64d75063d1643353f1c61a7184f72ed51f4b79f8
Claudio Cambra <developer@claudiocambra.com>
2026-08-13 15:42:49 +0800

    fix(tray): Handle scroll-to-top on activation in QML
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- src/gui/systray.cpp
+++ src/gui/systray.cpp
@@ -30,7 +30,6 @@
 #include <QEvent>
 #include <QGuiApplication>
 #include <QMenu>
-#include <QMetaObject>
 #include <QMouseEvent>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
@@ -253,9 +252,6 @@ void Systray::showActivitiesWindow(int userIndex)
         existingWindow->show();
         existingWindow->raise();
         existingWindow->requestActivate();
-        if (!QMetaObject::invokeMethod(existingWindow.data(), "resetActivityList", Qt::QueuedConnection)) {
-            qCWarning(lcSystray) << "Could not reset the activities window after showing it";
-        }
         user->refreshActivities();
         return;
     }
