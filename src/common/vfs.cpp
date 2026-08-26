c08c5ebeaf871d12c0278972a77700e10221a6a0
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-08-26 15:28:32 +0200

    fix(openvfs): fix loading of the plug-in and make it default on linux
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/common/vfs.cpp
+++ src/common/vfs.cpp
@@ -52,6 +52,10 @@ Optional<Vfs::Mode> Vfs::modeFromString(const QString &str)
         return WithSuffix;
     } else if (str == QLatin1String("wincfapi")) {
         return WindowsCfApi;
+    } else if (str == QLatin1String("xattr")) {
+        return XAttr;
+    } else if (str == QLatin1String("openvfs")) {
+        return OpenVFS;
     }
     return {};
 }
@@ -141,16 +145,20 @@ VfsOff::~VfsOff() = default;
 
 static QString modeToPluginName(Vfs::Mode mode)
 {
-    if (mode == Vfs::WithSuffix) {
+    switch (mode) {
+    case Vfs::Off:
+        return {};
+    case Vfs::WithSuffix:
         return QStringLiteral("suffix");
-    }
-    if (mode == Vfs::WindowsCfApi) {
+    case Vfs::WindowsCfApi:
         return QStringLiteral("cfapi");
-    }
-    if (mode == Vfs::XAttr) {
+    case Vfs::XAttr:
         return QStringLiteral("xattr");
+    case Vfs::OpenVFS:
+        return QStringLiteral("openvfs");
     }
-    return QString();
+
+    return {};
 }
 
 Q_LOGGING_CATEGORY(lcPlugin, "plugins", QtInfoMsg)
@@ -203,6 +211,10 @@ Vfs::Mode OCC::bestAvailableVfsMode()
 {
     if (isVfsPluginAvailable(Vfs::WindowsCfApi)) {
         return Vfs::WindowsCfApi;
+    }
+
+    if (isVfsPluginAvailable(Vfs::OpenVFS)) {
+        return Vfs::OpenVFS;
     }
 
     if (isVfsPluginAvailable(Vfs::WithSuffix)) {
@@ -275,4 +287,9 @@ std::unique_ptr<Vfs> OCC::createVfsFromPlugin(Vfs::Mode mode)
 
     qCInfo(lcPlugin) << "Created VFS instance from plugin" << pluginPath;
     return vfs;
+}
+
+const FileSystem::Path &VfsSetupParams::root() const
+{
+    return rootPath;
 }
