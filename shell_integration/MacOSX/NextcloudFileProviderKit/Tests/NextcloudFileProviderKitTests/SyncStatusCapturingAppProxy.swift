075c57ecbc7144e5e7b064319cd649022ca275f6
Claudio Cambra <developer@claudiocambra.com>
2026-09-18 16:59:44 +0800

    fix(sharing): Address rebase issues in NCFPK test suite
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- shell_integration/MacOSX/NextcloudFileProviderKit/Tests/NextcloudFileProviderKitTests/SyncStatusCapturingAppProxy.swift
+++ shell_integration/MacOSX/NextcloudFileProviderKit/Tests/NextcloudFileProviderKitTests/SyncStatusCapturingAppProxy.swift
@@ -21,6 +21,7 @@ final class SyncStatusCapturingAppProxy: NSObject, AppProtocol {
 
     // The following are unused by the tests using this proxy but required for protocol conformance.
     func presentFileActions(_: String, path _: String, remoteItemPath _: String, withDomainIdentifier _: String) {}
+    func presentUnifiedSharing(forItem _: String, localPath _: String, remoteItemPath _: String, forDomainIdentifier _: String) {}
     func openItemInBrowser(_: String, remoteItemPath _: String, forDomainIdentifier _: String) {}
     func copyInternalLink(forItem _: String, remoteItemPath _: String, forDomainIdentifier _: String) {}
     func reportItemExcluded(fromSync _: String, fileName _: String, reason _: String, forDomainIdentifier _: String) {}
