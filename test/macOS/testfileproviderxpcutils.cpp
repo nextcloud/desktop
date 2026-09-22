338119d625277959aba919347c08a17c709a568d
Claudio Cambra <developer@claudiocambra.com>
2026-09-23 00:30:48 +0800

    fix(file-provider): Test retain state
    Signed-off-by: Claudio Cambra <developer@claudiocambra.com>

--- test/macOS/testfileproviderxpcutils.cpp
+++ test/macOS/testfileproviderxpcutils.cpp
@@ -5,6 +5,7 @@
 
 #include <QtTest>
 
+#include "common/utility.h"
 #include "macOS/fileproviderxpc_mac_utils.h"
 
 #import <FileProvider/FileProvider.h>
@@ -33,6 +34,114 @@
 
 @end
 
+@interface TestClientCommunicationService : NSObject <ClientCommunicationProtocol>
+@property (nonatomic, copy) NSString *domainIdentifier;
+@property (nonatomic, retain) NSError *error;
+@property (nonatomic, assign) BOOL *deallocatedFlag;
+@end
+
+@implementation TestClientCommunicationService
+
+- (void)getFileProviderDomainIdentifierWithCompletionHandler:(void (^)(NSString *, NSError *))completionHandler
+{
+    completionHandler(self.domainIdentifier, self.error);
+}
+
+- (void)hasDirtyUserDataWithCompletionHandler:(void (^)(BOOL))completionHandler
+{
+    completionHandler(NO);
+}
+
+- (void)configureAccountWithUser:(NSString *)user
+                          userId:(NSString *)userId
+                       serverUrl:(NSString *)serverUrl
+                        password:(NSString *)password
+                       userAgent:(NSString *)userAgent
+{
+}
+
+- (void)removeAccountConfig
+{
+}
+
+- (void)setIgnoreList:(NSArray<NSString *> *)ignoreList
+{
+}
+
+- (void)processFileIdsChanged:(NSArray<NSNumber *> *)fileIds completionHandler:(void (^)(BOOL))completionHandler
+{
+    completionHandler(NO);
+}
+
+- (void)dealloc
+{
+    if (self.deallocatedFlag != nullptr) {
+        *self.deallocatedFlag = YES;
+    }
+
+    [_domainIdentifier release];
+    [_error release];
+    [super dealloc];
+}
+
+@end
+
+@interface TestXPCConnection : NSObject
+@property (nonatomic, retain) NSObject<ClientCommunicationProtocol> *remoteService;
+@property (nonatomic, assign) BOOL *deallocatedFlag;
+@property (nonatomic, assign) BOOL invalidated;
+@property (nonatomic, assign) BOOL resumed;
+@end
+
+@implementation TestXPCConnection
+
+- (void)setExportedInterface:(NSXPCInterface *)exportedInterface
+{
+}
+
+- (void)setExportedObject:(id)exportedObject
+{
+}
+
+- (void)setRemoteObjectInterface:(NSXPCInterface *)remoteObjectInterface
+{
+}
+
+- (void)setInterruptionHandler:(dispatch_block_t)interruptionHandler
+{
+}
+
+- (void)setInvalidationHandler:(dispatch_block_t)invalidationHandler
+{
+}
+
+- (void)resume
+{
+    self.resumed = YES;
+}
+
+- (void)invalidate
+{
+    self.invalidated = YES;
+}
+
+- (id)remoteObjectProxyWithErrorHandler:(void (^)(NSError *))errorHandler
+{
+    return self.remoteService;
+}
+
+- (void)dealloc
+{
+    if (self.deallocatedFlag != nullptr) {
+        *self.deallocatedFlag = YES;
+    }
+
+    [_remoteService release];
+    [super dealloc];
+}
+
+@end
+
 class TestFileProviderXPCUtils : public QObject
 {
     Q_OBJECT
@@ -56,6 +165,7 @@ private Q_SLOTS:
 
         const auto services = OCC::Mac::FileProviderXPCUtils::getFileProviderServices(managers);
         QCOMPARE(services.count, managerCount);
+        [services release];
     }
 
     void serviceWithoutNameIsIgnored()
@@ -67,8 +177,55 @@ private Q_SLOTS:
         const auto services = OCC::Mac::FileProviderXPCUtils::getFileProviderServices(@[(NSFileProviderManager *)manager]);
         QCOMPARE(services.count, 0);
 
+        [services release];
         [service release];
         [manager release];
+    }
+
+    void clientCommunicationDomainIdentifierCanBeReleasedByCaller()
+    {
+        const auto service = [TestClientCommunicationService new];
+        service.domainIdentifier = @"domain-id";
+
+        const auto domainIdentifier = OCC::Mac::FileProviderXPCUtils::getFileProviderDomainIdentifier((NSObject<ClientCommunicationProtocol> *)service);
+
+        QCOMPARE(QString::fromNSString(domainIdentifier), QStringLiteral("domain-id"));
+        [domainIdentifier release];
+        [service release];
+    }
+
+    void clientCommunicationConnectionValuesHaveOneRetain()
+    {
+        auto serviceDeallocated = false;
+        const auto service = [TestClientCommunicationService new];
+        service.domainIdentifier = @"domain-id";
+        service.deallocatedFlag = &serviceDeallocated;
+
+        auto connectionDeallocated = false;
+        const auto connection = [TestXPCConnection new];
+        connection.remoteService = service;
+        connection.deallocatedFlag = &connectionDeallocated;
+
+        const auto connections = [[NSMutableArray alloc] initWithObjects:(NSXPCConnection *)connection, nil];
+
+        const auto clientCommConnections = OCC::Mac::FileProviderXPCUtils::processClientCommunicationConnections(connections, nullptr);
+
+        QCOMPARE(clientCommConnections.size(), 1);
+        const auto clientCommConnection = clientCommConnections.value(QStringLiteral("domain-id"));
+        const auto returnedService = (NSObject *)clientCommConnection.clientCommunicationService;
+        const auto returnedConnection = (TestXPCConnection *)clientCommConnection.xpcConnection;
+        QVERIFY(returnedService != nil);
+        QVERIFY(returnedConnection.resumed);
+
+        [returnedService release];
+        [returnedConnection invalidate];
+        [returnedConnection release];
+        [connections release];
+        [connection release];
+        [service release];
+
+        QVERIFY(serviceDeallocated);
+        QVERIFY(connectionDeallocated);
     }
 };
 
