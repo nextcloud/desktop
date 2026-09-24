8d2edced7258edb0d5082ed17128f49e412358c7
Matthieu Gallien <matthieu.gallien@nextcloud.com>
2026-09-24 18:35:03 +0200

    fix(log): be less verbose by default with permissions on Windows
    put some logs back to debug level such that they are skipped by default
    
    Signed-off-by: Matthieu Gallien <matthieu.gallien@nextcloud.com>

--- src/common/filesystembase.cpp
+++ src/common/filesystembase.cpp
@@ -654,15 +654,11 @@ bool FileSystem::remove(const QString &fileName, QString *errorString)
                     return (static_cast<bool>(currentPermissions & testedPermission) ? permissionChar : '-');
                 };
 
-                qCInfo(lcFileSystem()) << unitaryHelper(std::filesystem::perms::owner_read, 'r')
-                                       << unitaryHelper(std::filesystem::perms::owner_write, 'w')
-                                       << unitaryHelper(std::filesystem::perms::owner_exec, 'x')
-                                       << unitaryHelper(std::filesystem::perms::group_read, 'r')
-                                       << unitaryHelper(std::filesystem::perms::group_write, 'w')
-                                       << unitaryHelper(std::filesystem::perms::group_exec, 'x')
-                                       << unitaryHelper(std::filesystem::perms::others_read, 'r')
-                                       << unitaryHelper(std::filesystem::perms::others_write, 'w')
-                                       << unitaryHelper(std::filesystem::perms::others_exec, 'x');
+                qCDebug(lcFileSystem()) << unitaryHelper(std::filesystem::perms::owner_read, 'r') << unitaryHelper(std::filesystem::perms::owner_write, 'w')
+                                        << unitaryHelper(std::filesystem::perms::owner_exec, 'x') << unitaryHelper(std::filesystem::perms::group_read, 'r')
+                                        << unitaryHelper(std::filesystem::perms::group_write, 'w') << unitaryHelper(std::filesystem::perms::group_exec, 'x')
+                                        << unitaryHelper(std::filesystem::perms::others_read, 'r') << unitaryHelper(std::filesystem::perms::others_write, 'w')
+                                        << unitaryHelper(std::filesystem::perms::others_exec, 'x');
             };
 
             const auto unsafeFilePermissions = filePermissionsWin(windowsSafeFileName);
